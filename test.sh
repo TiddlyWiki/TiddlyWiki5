21e06b9833432febe1b3cfccc1608ebe061220f0
Jeremy Ruston <jeremy@osmosoft.com>
2011-11-25 15:51:02 +0000

    Introduced tmp/ directory for test artefacts

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# create a temporary directory if it doesn't already exist
+mkdir -p tmp
+
 # cook the recipe with the old cook.rb (assuming you have set it up as described in github.com/TiddlyWiki/cooker)
-cook $PWD/test/data/tiddlywiki.com/index.html.recipe -d $PWD -o oldcooked.html
+cook $PWD/test/data/tiddlywiki.com/index.html.recipe -d $PWD -o tmp/oldcooked.html || exit 1
 
 # cook it with cook.js
-node cook.js $PWD/test/data/tiddlywiki.com/index.html.recipe > newcooked.html
+node cook.js $PWD/test/data/tiddlywiki.com/index.html.recipe > tmp/newcooked.html || exit 1
 
 # compare the two
-opendiff oldcooked.html newcooked.html
+opendiff tmp/oldcooked.html tmp/newcooked.html
