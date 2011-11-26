c4ab9ec218908ebe19f90b89328bdd6d8348fa99
Jeremy Ruston <jeremy@osmosoft.com>
2011-11-26 13:23:26 +0000

    Improved test script
    Now it recooks the ginsud tiddlers

--- test.sh
+++ test.sh
@@ -15,9 +15,13 @@ node cook.js $PWD/test/data/tiddlywiki.com/index.html.recipe > tmp/newcooked.htm
 # split the newly cooked tiddlywiki into tiddlers, first with the new ginsu
 mkdir -p tmp/newtiddlers
 rm -f tmp/newtiddlers/*
-node ginsu.js tmp/newcooked.html tmp/newtiddlers
+node ginsu.js tmp/newcooked.html tmp/newtiddlers || exit 1
 
-# and now cook them with the old
+# and now ginsu them with the old ginsu.rb
 rm -r tmp/oldtiddlers
-ginsu tmp/oldcooked
+ginsu tmp/oldcooked || exit 1
 mv oldcooked.html.0 tmp/oldtiddlers/
+
+# now cook those tiddlers back again with the respective versions of cook
+#cook $PWD/test/data/recipes/oldtiddlers.recipe -d $PWD -o tmp/oldrecooked.html || exit 1
+node cook.js $PWD/test/data/recipes/newtiddlers.recipe > tmp/newrecooked.html || exit 1
