ff7398745785015fb0aa890cc011b6ed1da7f93c
Jeremy Ruston <jeremy@osmosoft.com>
2011-12-08 18:09:22 +0000

    Removed skeletons for macros that are not immediately planned for implementation

--- js/WikiTextMacros.js
+++ js/WikiTextMacros.js
@@ -38,35 +38,7 @@ wikiTextMacros.macros = {
 		handler: function(macroNode,store) {
 		}
 	},
-	annotations: {
-		handler: function(macroNode,store) {
-		}
-	},
-	backstage: {
-		handler: function(macroNode,store) {
-		}
-	},
 	br: {
-		handler: function(macroNode,store) {
-		}
-	},
-	closeAll: {
-		handler: function(macroNode,store) {
-		}
-	},
-	download: {
-		handler: function(macroNode,store) {
-		}
-	},
-	edit: {
-		handler: function(macroNode,store) {
-		}
-	},
-	gradient: {
-		handler: function(macroNode,store) {
-		}
-	},
-	importTiddlers: {
 		handler: function(macroNode,store) {
 		}
 	},
@@ -74,51 +46,7 @@ wikiTextMacros.macros = {
 		handler: function(macroNode,store) {
 		}
 	},
-	message: {
-		handler: function(macroNode,store) {
-		}
-	},
-	newJournal: {
-		handler: function(macroNode,store) {
-		}
-	},
-	newTiddler: {
-		handler: function(macroNode,store) {
-		}
-	},
-	option: {
-		handler: function(macroNode,store) {
-		}
-	},
-	options: {
-		handler: function(macroNode,store) {
-		}
-	},
-	permaview: {
-		handler: function(macroNode,store) {
-		}
-	},
-	plugins: {
-		handler: function(macroNode,store) {
-		}
-	},
-	refreshDisplay: {
-		handler: function(macroNode,store) {
-		}
-	},
-	saveChanges: {
-		handler: function(macroNode,store) {
-		}
-	},
-	search: {
-		handler: function(macroNode,store) {
-		}
-	},
 	slider: {
-		handler: function(macroNode,store) {
-		}
-	},
-	sync: {
 		handler: function(macroNode,store) {
 		}
 	},
@@ -127,10 +55,6 @@ wikiTextMacros.macros = {
 		}
 	},
 	tag: {
-		handler: function(macroNode,store) {
-		}
-	},
-	tagChooser: {
 		handler: function(macroNode,store) {
 		}
 	},
