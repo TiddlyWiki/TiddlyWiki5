a223f4300a1115d12d7b0daf95bc1ed1fc70bd9b
Jeremy Ruston <jeremy@osmosoft.com>
2011-12-12 08:59:10 +0000

    Removed extraneous bits of test script

--- test.sh
+++ test.sh
@@ -10,15 +10,6 @@ node tiddlywiki.js --recipe $PWD/test/tiddlywiki.2.6.5/source/tiddlywiki.com/ind
 # compare the two
 opendiff tmp/newcooked/index.html test/tiddlywiki.2.6.5/target/index.2.6.5.html
 
-# split the newly cooked tiddlywiki into tiddlers, first with the new ginsu
-#mkdir -p tmp/newtiddlers
-#rm -f tmp/newtiddlers/*
-#node ginsu.js tmp/newcooked.html tmp/newtiddlers || exit 1
-
-# now cook those tiddlers back again with the respective versions of cook
-#cook $PWD/test/data/recipes/oldtiddlers.recipe -d $PWD -o tmp/oldrecooked.html || exit 1
-#node cook.js $PWD/test/data/recipes/newtiddlers.recipe > tmp/newrecooked.html || exit 1
-
 # Run the wikification tests
 node wikitest.js test/wikitests/
 
