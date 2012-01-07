5bb2a1e1b7b05522bc484531a2ecfafbdb32ffa3
Jeremy Ruston <jeremy@osmosoft.com>
2012-01-07 20:09:01 +0000

    Added support for the "with" parameter to the tiddler macro

--- js/macros/tiddler.js
+++ js/macros/tiddler.js
@@ -15,10 +15,23 @@ exports.macro = {
 	cascadeParams: true, // Cascade names of named parameters to following anonymous parameters
 	params: {
 		target: {byName: "default", type: "tiddler", optional: false},
-		"with": {byName: true, type: "text", optional: true}
+		"with": {byName: true, type: "text", optional: true, dependentAll: true}
 	},
 	code: function(type,tiddler,store,params) {
-		return store.renderTiddler(type,params.target);
+		if(params["with"]) {
+			// Parameterised transclusion
+			var targetTiddler = store.getTiddler(params.target),
+				text = targetTiddler.fields.text;
+			var withTokens = [params["with"]];
+			for(var t=0; t<withTokens.length; t++) {
+				var placeholderRegExp = new RegExp("\\$"+(t+1),"mg");
+				text = text.replace(placeholderRegExp,withTokens[t]);
+			}
+			return store.renderText(targetTiddler.fields.type,text,type,tiddler.fields.title);
+		} else {
+			// There's no parameterisation, so we can just render the target tiddler directly
+			return store.renderTiddler(type,params.target,tiddler.fields.title);
+		}
 	}
 };
 
