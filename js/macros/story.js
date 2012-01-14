b671ecdeddb154a444bc9a5c7f8bd15be08e0293
Jeremy Ruston <jeremy@osmosoft.com>
2012-01-14 17:24:25 +0000

    Refactored story macro to use renderTiddler instead of renderText

--- js/macros/story.js
+++ js/macros/story.js
@@ -17,21 +17,18 @@ exports.macro = {
 		template: {byName: true, type: "text", optional: true}
 	},
 	code: function(type,tiddler,store,params) {
-		var templateType = "text/x-tiddlywiki",
-			templateText = "<<view text wikified>>",
-			template = params.template ? store.getTiddler(params.template) : null,
-			tiddlers = store.getTiddlerText(params.story).split("\n"),
+		var tiddlers = store.getTiddlerText(params.story).split("\n"),
 			t,
 			output = [];
-		if(template) {
-			templateType = template.fields.type;
-			templateText = template.fields.text;
-		}
 		for(t=0; t<tiddlers.length; t++) {
 			var title = tiddlers[t].trim();
 			if(title !== "") {
 				output.push("<article>");
-				output.push(store.renderText(templateType,templateText,"text/html",title));
+				if(params.template) {
+					output.push(store.renderTiddler(type,params.template,title));
+				} else {
+					output.push(store.renderTiddler(type,title));
+				}
 				output.push("</article>");
 			}
 		}
