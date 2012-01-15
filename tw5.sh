6ec259ca380e48bccecf0109839ac535ebabbfe9
Jeremy Ruston <jeremy@osmosoft.com>
2012-01-15 11:45:58 +0000

    Automatically build the readme file

--- tw5.sh
+++ tw5.sh
@@ -7,7 +7,8 @@ mkdir -p tmp
 mkdir -p tmp/tw5
 
 # cook TiddlyWiki5
-node tiddlywiki.js --recipe $PWD/tiddlywiki5/tiddlywiki5.recipe --savewiki tmp/tw5 || exit 1
+node tiddlywiki.js --recipe $PWD/tiddlywiki5/tiddlywiki5.recipe --savewiki tmp/tw5 --savetiddler ReadMe readme.html || exit 1
+
 
 # open the result
 #open -a /Applications/Google\ Chrome.app tmp/tw5/index.html
