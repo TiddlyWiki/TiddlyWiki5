9de3bb6f59cdcf88569d76a6f8264cbc088e323d
Jeremy Ruston <jeremy@osmosoft.com>
2012-03-03 13:52:10 +0000

    Fixed problem with SVG images identified by file extension

--- js/ImageParser.js
+++ js/ImageParser.js
@@ -21,7 +21,7 @@ var ImageParser = function(options) {
 ImageParser.prototype.parse = function(type,text) {
     var src;
     if(type === "image/svg+xml" || type === ".svg") {
-        src = "data:" + type + "," + encodeURIComponent(text);
+        src = "data:image/svg+xml," + encodeURIComponent(text);
 	} else {
         src = "data:" + type + ";base64," + text;
 	}
