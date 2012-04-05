62eb0ef5b962898a6ad06f0b2b7e916274bcab29
Jeremy Ruston <jeremy@osmosoft.com>
2012-04-05 12:19:20 +0100

    Improved TW5 http serving batch file to make a copy of the store before execution
    Makes it easier to test the saving code

--- tw5s.sh
+++ tw5s.sh
@@ -2,5 +2,10 @@
 
 # serve TiddlyWiki5 over HTTP
 
+# make a copy of the content store
+mkdir -p tmp/store
+rm tmp/store/*.*
+cp tiddlywiki5/store/*.* tmp/store
+
 # cook TiddlyWiki5
-node tiddlywiki.js --recipe $PWD/tiddlywiki5/tiddlywiki5.recipe --store tiddlywiki5/store --servewiki 8080 || exit 1
+node tiddlywiki.js --recipe $PWD/tiddlywiki5/tiddlywiki5.recipe --store tmp/store --servewiki 8080 || exit 1
