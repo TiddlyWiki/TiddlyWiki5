6120019e7589cfbfb4b9599ff601d72250ebbd98
Jeremy Ruston <jeremy@osmosoft.com>
2012-05-03 21:46:35 +0100

    Command to save a tiddler as a file

---
+++ rabbithole/core/modules/commands/savetiddler.js
@@ -0,0 +1,43 @@
+/*\
+title: $:/core/modules/commands/savetiddler.js
+type: application/javascript
+module-type: command
+
+Save tiddlers command
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+"use strict";
+
+exports.info = {
+	name: "savetiddler",
+	synchronous: false
+};
+
+var Command = function(params,commander,callback) {
+	this.params = params;
+	this.commander = commander;
+	this.callback = callback;
+};
+
+Command.prototype.execute = function() {
+	if(this.params.length < 2) {
+		return "Missing filename";
+	}
+	var self = this,
+		fs = require("fs"),
+		path = require("path"),
+		title = this.params[0],
+		filename = this.params[1],
+		type = this.params[2] || "text/html";
+	fs.writeFile(filename,this.commander.wiki.renderTiddler(type,title),"utf8",function(err) {
+		self.callback(err);
+	});
+	return null;
+};
+
+exports.Command = Command;
+
+})();
