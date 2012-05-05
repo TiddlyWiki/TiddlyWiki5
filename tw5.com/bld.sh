186ec4052dedb43e470bcdebfe825a7f016de211
Jeremy Ruston <jeremy@osmosoft.com>
2012-05-05 23:08:00 +0100

    Update docs and readme generation

--- tw5.com/bld.sh
+++ tw5.com/bld.sh
@@ -8,5 +8,6 @@ mkdir -p tmp/tw5
 
 # cook TiddlyWiki5
 node ../core/boot.js --verbose \
+	--savetiddler ReadMe ../readme.md text/html \
 	--savetiddler $:/core/tiddlywiki5.template.html tmp/tw5/index.html text/plain \
 	--savetiddler $:/core/static.template.html tmp/tw5/static.html text/plain || exit 1
