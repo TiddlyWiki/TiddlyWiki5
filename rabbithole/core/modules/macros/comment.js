2e01add7f3cf3919e3c8530c358829ef663dd045
Jeremy Ruston <jeremy@osmosoft.com>
2012-05-05 11:22:43 +0100

    Added comment macro
    Being <<!blah blah>> means that it is easy to comment out a macro call

---
+++ rabbithole/core/modules/macros/comment.js
@@ -0,0 +1,25 @@
+/*\
+title: $:/core/modules/macros/comment.js
+type: application/javascript
+module-type: macro
+
+Comment macro, a no-op.
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+exports.info = {
+	name: "!",
+	params: {}
+};
+
+exports.executeMacro = function() {
+	return [];
+};
+
+
+})();
