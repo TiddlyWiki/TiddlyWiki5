3b80d7e9c104ccc17ec49d8fcdef8bd969781d00
Jeremy Ruston <jeremy@osmosoft.com>
2012-05-05 22:38:37 +0100

    Added a template for building a static HTML version of the wiki

--- rabbithole/tw5.com/bld.sh
+++ rabbithole/tw5.com/bld.sh
@@ -7,4 +7,6 @@ mkdir -p tmp
 mkdir -p tmp/tw5
 
 # cook TiddlyWiki5
-node ../core/boot.js --verbose --savetiddler $:/core/tiddlywiki5.template.html tmp/tw5/index.html text/plain || exit 1
+node ../core/boot.js --verbose \
+	--savetiddler $:/core/tiddlywiki5.template.html tmp/tw5/index.html text/plain \
+	--savetiddler $:/core/static.template.html tmp/tw5/static.html text/plain || exit 1
