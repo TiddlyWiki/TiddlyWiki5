82f7a547fe6c75106f7eeca6d46b367fc1b4de4a
Jeremy Ruston <jeremy@osmosoft.com>
2012-05-20 15:20:21 +0100

    Moved templates in the title hierarchy

--- bld.sh
+++ bld.sh
@@ -13,8 +13,8 @@ pushd tw5.com > /dev/null
 node ../core/boot.js \
 	--verbose \
 	--savetiddler ReadMe ../readme.md text/html \
-	--savetiddler $:/core/tiddlywiki5.template.html ../tmp/tw5/index.html text/plain \
-	--savetiddler $:/core/static.template.html ../tmp/tw5/static.html text/plain \
+	--savetiddler $:/core/templates/tiddlywiki5.template.html ../tmp/tw5/index.html text/plain \
+	--savetiddler $:/core/templates/static.template.html ../tmp/tw5/static.html text/plain \
 	|| exit 1
 
 popd > /dev/null
