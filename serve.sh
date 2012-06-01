44e2b246bac2f43315a6b1c05e84561b34dbfcf9
Jeremy Ruston <jeremy@osmosoft.com>
2012-06-01 18:27:20 +0100

    Added a batch file for testing serving over HTTP

---
+++ serve.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# serve TiddlyWiki5 over HTTP
+
+pushd tw5.com > /dev/null
+
+node ../core/boot.js \
+	--verbose \
+	--server 8080 $:/core/templates/tiddlywiki5.template.html text/plain text/html \
+	|| exit 1
+
+popd > /dev/null
