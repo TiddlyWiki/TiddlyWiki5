ee7fba92971316c0c86c40ea3ee5804d343c989f
Jeremy Ruston <jeremy@osmosoft.com>
2012-06-05 17:01:17 +0100

    Added support for macros with embedded content

--- core/modules/parsers/newwikitextparser/rules/macro.js
+++ core/modules/parsers/newwikitextparser/rules/macro.js
@@ -17,16 +17,21 @@ exports.name = "macro";
 exports.runParser = true;
 exports.blockParser = true;
 
-exports.regExpString = "<<";
+exports.regExpString = "<<(?:(?:[!@£\\$%\\^\\&\\*\\(\\)`\\~'\"\\|\\\\\\/;\\:\\.\\,\\+\\=\\-\\_\\{\\}])|(?:[^>\\s]+))(?:\\s*)(?:(?:[^>]|(?:>(?!>|<)))*)>(?:>|<)";
 
 exports.parse = function(match,isBlock) {
-	var regExp = /<<(?:([!@£\$%\^\&\*\(\)`\~'"\|\\\/;\:\.\,\+\=\-\_\{\}])|([^>\s]+))(?:\s*)((?:[^>]|(?:>(?!>)))*)>>/mg;
+	var regExp = /<<(?:([!@£\$%\^\&\*\(\)`\~'"\|\\\/;\:\.\,\+\=\-\_\{\}])|([^>\s]+))(?:\s*)((?:[^>]|(?:>(?!>|<)))*)>(?:(>)|(<))/mg,
+		content = [];
 	regExp.lastIndex = this.pos;
 	match = regExp.exec(this.source);
 	if(match && match.index === this.pos) {
 		this.pos = match.index + match[0].length;
+		if(match[5]) {
+			content = this.parseRun(/(>>)/mg);
+		}
 		var macroNode = $tw.Tree.Macro(match[1] || match[2],{
 				srcParams: match[3],
+				content: content,
 				isBlock: isBlock,
 				wiki: this.wiki
 			});
