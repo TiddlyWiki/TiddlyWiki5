698f2c39cb4d5d89ecf62596ba551ccf9d109212
Jeremy Ruston <jeremy@osmosoft.com>
2012-06-07 13:09:22 +0100

    Updated wikitext `class` rule to use parseBlockTerminated() method

--- core/modules/parsers/newwikitextparser/rules/class.js
+++ core/modules/parsers/newwikitextparser/rules/class.js
@@ -39,31 +39,9 @@ exports.parse = function(match,isBlock) {
 	match = reStart.exec(this.source);
 	if(match) {
 		this.pos = match.index + match[0].length;
-		// Skip any whitespace
-		this.skipWhitespace();
-		//  Check if we've got the end marker
-		reEnd.lastIndex = this.pos;
-		endMatch = reEnd.exec(this.source);
-		// Parse the text into blocks
-		while(this.pos < this.sourceLength && !(endMatch && endMatch.index === this.pos)) {
-			var blocks = this.parseBlock();
-			for(var t=0; t<blocks.length; t++) {
-				blocks[t].addClass(match[1]);
-				tree.push(blocks[t]);
-			}
-			// Skip any whitespace
-			this.skipWhitespace();
-			//  Check if we've got the end marker
-			reEnd.lastIndex = this.pos;
-			endMatch = reEnd.exec(this.source);
-		}
-		reEnd.lastIndex = this.pos;
-		endMatch = reEnd.exec(this.source);
-		if(endMatch) {
-			this.pos = endMatch.index + endMatch[0].length;
-		}
-		return tree;
+		tree = this.parseBlockTerminated(reEnd,match[1]);
 	}
+	return tree;
 };
 
 })();
