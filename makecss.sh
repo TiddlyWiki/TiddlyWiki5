663fd2fe103e6976099577ba44a8572c2833282e
Jeremy Ruston <jeremy@osmosoft.com>
2012-06-24 17:07:38 +0100

    More styling adjustments
    And started using Bootstrap's responsive stylesheets, so we can start
    to look decent on phones

--- makecss.sh
+++ makecss.sh
@@ -4,6 +4,8 @@
 
 recess --compile ./cssbuild/tiddlywiki.less > ./core/styles/tiddlywiki.css | exit
 
+recess --compile ./cssbuild/twitter-bootstrap/less/responsive.less > ./core/styles/bootstrap-responsive.css | exit
+
 # Call bld.sh to build TiddlyWiki
 
 ./bld.sh
