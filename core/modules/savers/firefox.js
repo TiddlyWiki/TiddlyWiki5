6a0a4ae00dad44e5ebd5588b2e9904841475bc29
Jeremy Ruston <jeremy@osmosoft.com>
2012-07-11 15:51:52 +0100

    Removed extraneous console.log

--- core/modules/savers/firefox.js
+++ core/modules/savers/firefox.js
@@ -23,7 +23,6 @@ FirefoxSaver.prototype.save = function(text) {
 			ioService = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService),
 			fileHandler = ioService.getProtocolHandler("file").QueryInterface(Components.interfaces.nsIFileProtocolHandler),
 			fileSpec = fileHandler.getFileFromURLSpec(url);
-console.log("Saving to",fileSpec.path);
 		// Try to save the file
 		var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 		file.initWithPath(fileSpec.path);
