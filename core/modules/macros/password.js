b4a75b25fc71a2cb5827c05f879517f74a2b7c9a
Jeremy Ruston <jeremy@osmosoft.com>
2012-07-13 13:03:07 +0100

    New password macro for setting passwords
    They are stored unencrypted in localStorage

---
+++ core/modules/macros/password.js
@@ -0,0 +1,43 @@
+/*\
+title: $:/core/modules/macros/password.js
+type: application/javascript
+module-type: macro
+
+Allows a password to be set
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+exports.info = {
+	name: "password",
+	params: {
+		name: {byName: "default", type: "text"}
+	}
+};
+
+exports.executeMacro = function() {
+	var password = $tw.utils.getPassword(this.params.name);
+	password = password || "";
+	var attributes = {
+		type: "password",
+		value: password
+	};
+	if(this.classes) {
+		attributes["class"] = this.classes.slice(0);
+	}
+	return $tw.Tree.Element("input",attributes,[],{
+		events: ["keyup","input"],
+		eventHandler: this
+	});
+};
+
+exports.handleEvent = function(event) {
+	var password = this.child.domNode.value;
+	return $tw.utils.savePassword(this.params.name,password);
+};
+
+})();
