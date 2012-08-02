1f52b40cb65ea305dd46825ce5d7acc72aa56091
Jeremy Ruston <jeremy@osmosoft.com>
2012-08-02 22:06:52 +0100

    Added parser rule for HTML format comments
    And try it out in the PageTemplate, which is getting pretty
    unintelligable

---
+++ core/modules/parsers/newwikitextparser/rules/comment.js
@@ -0,0 +1,33 @@
+/*\
+title: $:/core/modules/parsers/newwikitextparser/rules/comment.js
+type: application/javascript
+module-type: wikitextrule
+
+Wiki text run rule for HTML comments. For example:
+
+{{{
+<!-- This is a comment -->
+}}}
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+exports.name = "comment";
+
+exports.blockParser = true;
+exports.runParser = true;
+
+// Regexp by Stephen Ostermiller, http://ostermiller.org/findhtmlcomment.html
+
+exports.regExpString = "\\<![ \\r\\n\\t]*(?:--(?:[^\\-]|[\\r\\n]|-[^\\-])*--[ \\r\\n\\t]*)\\>";
+
+exports.parse = function(match,isBlock) {
+	this.pos = match.index + match[0].length;
+	return this.parseBlock();
+};
+
+})();
