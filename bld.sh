68efce20c410e3bddee310dd45d59deef0dd7276
Jeremy Ruston <jeremy@osmosoft.com>
2012-08-25 12:07:39 +0100

    Starting building an empty.html

--- bld.sh
+++ bld.sh
@@ -9,7 +9,8 @@ echo "five.tiddlywiki.com" > ../../jermolene.github.com/CNAME
 node ../core/boot.js \
 	--verbose \
 	--savetiddler ReadMe ../readme.md text/html \
-	--savetiddler $:/core/templates/tiddlywiki5.template.html ../../jermolene.github.com/index.html text/plain \
+	--savetiddler $:/core/templates/tiddlywiki5.template.html ../../jermolene.github.com/index.html text/plain [!is[shadow]]\
+	--savetiddler $:/core/templates/tiddlywiki5.template.html ../../jermolene.github.com/empty.html text/plain [!is[shadow]is[shadow]]\
 	--savetiddler $:/core/templates/static.template.html ../../jermolene.github.com/static.html text/plain \
 	|| exit 1
 
