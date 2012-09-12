7a17e7b2163ee113525cba11957fa27beb1a3434
Jeremy Ruston <jeremy@osmosoft.com>
2012-09-12 10:46:39 +0100

    Added full screen mode plugin

---
+++ core/plugins/fullscreen/init.js
@@ -0,0 +1,30 @@
+/*\
+title: $:/plugins/tiddlywiki/fullscreen/init.js
+type: application/javascript
+module-type: browser-startup
+
+Message handler for full screen mode
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var toggleFullScreen = function() {
+	if(document[$tw.browser.isFullScreen]) {
+		document[$tw.browser.cancelFullScreen]();
+	} else {
+		document.documentElement[$tw.browser.requestFullScreen]();
+	}
+};
+
+exports.startup = function() {
+	// Install the full screen handler
+	document.addEventListener("tw-full-screen",function(event) {
+		toggleFullScreen();
+	},false);
+};
+
+})();
