5c1ab19573f51b8cb872b169e694c39a136d69b6
Jeremy Ruston <jeremy@osmosoft.com>
2012-10-10 17:19:19 +0100

    Started refactoring tw5dropbox into separate wikis
    One wiki for the hosted shared app, and one for the wiki app that is
    created in the dropbox sandbox

--- dbld.sh
+++ dbld.sh
@@ -2,13 +2,13 @@
 
 # build TiddlyWiki5 in the Sky for Dropbox
 
-pushd tw5dropbox > /dev/null
+pushd tw5dropbox/mainwiki > /dev/null
 
-node ../core/boot.js \
+node ../../core/boot.js \
 	--verbose \
-	--savetiddler $:/core/templates/tw5dropbox.template.js ../../../../Apps/Static\ Web\ Apps/TiddlyWiki5/public/tw5dropbox.js text/plain [!is[shadow]] \
-	--savetiddler $:/core/templates/index.template.html ../../../../Apps/Static\ Web\ Apps/TiddlyWiki5/public/index.html text/plain [!is[shadow]] \
-	--savetiddler $:/core/templates/styles.template.css ../../../../Apps/Static\ Web\ Apps/TiddlyWiki5/public/styles.css text/plain [!is[shadow]] \
+	--savetiddler $:/core/templates/tw5dropbox.template.js ../../../../../Apps/Static\ Web\ Apps/TiddlyWiki5/public/tw5dropbox.js text/plain [!is[shadow]] \
+	--savetiddler $:/core/templates/index.template.html ../../../../../Apps/Static\ Web\ Apps/TiddlyWiki5/public/index.html text/plain [!is[shadow]] \
+	--savetiddler $:/core/templates/styles.template.css ../../../../../Apps/Static\ Web\ Apps/TiddlyWiki5/public/styles.css text/plain [!is[shadow]] \
 	|| exit 1
 
 popd > /dev/null
