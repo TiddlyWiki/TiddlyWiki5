5732cd4239b097b20c5b579d8b9f18b037adacac
Jeremy Ruston <jeremy@osmosoft.com>
2012-10-10 16:33:07 +0100

    Updates to tw5dropbox

--- dbld.sh
+++ dbld.sh
@@ -6,7 +6,9 @@ pushd tw5dropbox > /dev/null
 
 node ../core/boot.js \
 	--verbose \
-	--savetiddler $:/core/templates/tw5dropbox.template.html ../../../../Apps/Static\ Web\ Apps/TiddlyWiki5/public/index.html text/plain [!is[shadow]] \
+	--savetiddler $:/core/templates/tw5dropbox.template.js ../../../../Apps/Static\ Web\ Apps/TiddlyWiki5/public/tw5dropbox.js text/plain [!is[shadow]] \
+	--savetiddler $:/core/templates/index.template.html ../../../../Apps/Static\ Web\ Apps/TiddlyWiki5/public/index.html text/plain [!is[shadow]] \
+	--savetiddler $:/core/templates/styles.template.css ../../../../Apps/Static\ Web\ Apps/TiddlyWiki5/public/styles.css text/plain [!is[shadow]] \
 	|| exit 1
 
 popd > /dev/null
