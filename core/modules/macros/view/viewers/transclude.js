f4f042d2be480834d71fe929b0e3be3c9cb7897c
Jeremy Ruston <jeremy@osmosoft.com>
2012-10-18 18:59:57 +0100

    Fix to transclusion viewer

--- core/modules/macros/view/viewers/transclude.js
+++ core/modules/macros/view/viewers/transclude.js
@@ -15,12 +15,12 @@ A viewer that transcludes the tiddler whose title is specified in the viewed fie
 function renderValue(tiddler,field,value,viewMacro) {
 	if(tiddler && viewMacro.params.field && (viewMacro.params.field in tiddler.fields)) {
 		var children = viewMacro.wiki.parseTiddler(tiddler.fields[viewMacro.params.field]).tree,
-			childrenClone = [];
+			childrenClone = [],t;
 		for(t=0; t<children.length; t++) {
 			childrenClone.push(children[t].clone());
 		}
 		for(t=0; t<childrenClone.length; t++) {
-			childrenClone[t].execute(parents,viewMacro.tiddlerTitle);
+			childrenClone[t].execute(viewMacro.parents,viewMacro.tiddlerTitle);
 		}
 		return $tw.Tree.Element(viewMacro.isBlock ? "div" : "span",{},childrenClone);
 	}
