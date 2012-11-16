1b23ecee0f1a471650321800b2d39366c9b1e0be
Jeremy Ruston <jeremy@osmosoft.com>
2012-11-16 21:40:59 +0000

    Introduce new "empty" edition
    The old mechanism was clunky

--- bld.sh
+++ bld.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# build TiddlyWiki5
+# build TiddlyWiki5 for five.tiddlywiki.com
+
+# First, index.html: the main file, including content
 
 pushd editions/tw5.com > /dev/null
 
@@ -10,10 +12,23 @@ node ../../core/boot.js \
 	--verbose \
 	--password password \
 	--savetiddler ReadMe ../../readme.md text/html \
-	--savetiddler $:/core/templates/tiddlywiki5.template.html ../../../jermolene.github.com/index.html text/plain [!is[shadow]]\
-	--savetiddler $:/core/templates/tiddlywiki5.template.html ../../../jermolene.github.com/empty.html text/plain [!is[shadow]is[shadow]]\
-	--savetiddler $:/core/templates/tiddlywiki5.encrypted.template.html ../../../jermolene.github.com/encrypted.html text/plain [!is[shadow]]\
+	--savetiddler $:/core/templates/tiddlywiki5.template.html ../../../jermolene.github.com/index.html text/plain [!is[shadow]] \
+	--savetiddler $:/core/templates/tiddlywiki5.template.html ../../../jermolene.github.com/empty.html text/plain [!is[shadow]is[shadow]] \
+	--savetiddler $:/core/templates/tiddlywiki5.encrypted.template.html ../../../jermolene.github.com/encrypted.html text/plain [!is[shadow]] \
 	--savetiddler $:/core/templates/static.template.html ../../../jermolene.github.com/static.html text/plain \
+	|| exit 1
+
+popd > /dev/null
+
+# Second, empty.html: empty wiki for reuse
+
+pushd editions/empty > /dev/null
+
+node ../../core/boot.js \
+	--verbose \
+	--password password \
+	--savetiddler $:/core/templates/tiddlywiki5.template.html ../../../jermolene.github.com/empty.html text/plain [!is[shadow]] \
+	--savetiddler $:/core/templates/tiddlywiki5.encrypted.template.html ../../../jermolene.github.com/empty_encrypted.html text/plain [!is[shadow]] \
 	|| exit 1
 
 popd > /dev/null
