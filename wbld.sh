c846784ab8ab08764975a9f94601b0a6bec5a752
Jeremy Ruston <jeremy@osmosoft.com>
2012-11-20 16:31:39 +0000

    Update tiddler name for app

--- wbld.sh
+++ wbld.sh
@@ -19,4 +19,4 @@ echo "type: text/html" > tmp/tmp.txt
 echo "" >> tmp/tmp.txt
 cat ../jermolene.github.com/tiddlyweb.html >> tmp/tmp.txt
 
-curl -u $1:$2 -X PUT -H "content-type: text/plain" http://tw5tiddlyweb.tiddlyspace.com/bags/tw5tiddlyweb_public/tiddlers/tw5tiddlyweb --data-binary @tmp/tmp.txt
+curl -u $1:$2 -X PUT -H "content-type: text/plain" http://tw5tiddlyweb.tiddlyspace.com/bags/tw5tiddlyweb_public/tiddlers/tw5 --data-binary @tmp/tmp.txt
