60dca0f38add3dd1b562c8799855a17a992e14d7
Jeremy Ruston <jeremy@osmosoft.com>
2012-12-15 11:38:59 +0000

    Added parse tree utility functions

---
+++ core/modules/utils/parsetree.js
@@ -0,0 +1,32 @@
+/*\
+title: $:/core/modules/utils/parsetree.js
+type: application/javascript
+module-type: utils
+
+Parse tree utility functions.
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+exports.addAttributeToParseTreeNode = function(node,name,value) {
+	if(node.type === "element") {
+		node.attributes = node.attributes || {};
+		node.attributes[name] = {type: "string", value: value};
+	}
+};
+
+exports.addClassToParseTreeNode = function(node,classString) {
+	if(node.type === "element") {
+		node.attributes = node.attributes || {};
+		node.attributes["class"] = node.attributes["class"] || {type: "string", value: ""};
+		if(node.attributes["class"].type === "string") {
+			node.attributes["class"].value += " " + classString;
+		}
+	}
+};
+
+})();
