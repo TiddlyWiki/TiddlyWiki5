f8cd65d38d878c26d7d786ba360fd7f7225ed136
Jeremy Ruston <jeremy@osmosoft.com>
2013-01-23 12:37:10 +0000

    Improve shell variable name for main build script

--- bld.sh
+++ bld.sh
@@ -2,29 +2,34 @@
 
 # build TiddlyWiki5 for five.tiddlywiki.com
 
-# First, index.html: the main file, including content
-
 pushd editions/tw5.com > /dev/null
 
-if [  -z "$JERMOLENE_HOME" ]; then
-    JERMOLENE_HOME=../../../jermolene.github.com
+# Set up the build output directory
+
+if [  -z "$TW5_BUILD_OUTPUT" ]; then
+    TW5_BUILD_OUTPUT=../../../jermolene.github.com
 fi
 
-if [  ! -d "$JERMOLENE_HOME" ]; then
-    echo 'A valid JERMOLENE_HOME directory must be set'
+if [  ! -d "$TW5_BUILD_OUTPUT" ]; then
+    echo 'A valid TW5_BUILD_OUTPUT directory must be set'
     exit 1
 fi
 
-echo "Using JERMOLENE_HOME as [$JERMOLENE_HOME]"
-echo "five.tiddlywiki.com" > $JERMOLENE_HOME/CNAME
+echo "Using TW5_BUILD_OUTPUT as [$TW5_BUILD_OUTPUT]"
+
+# Make the CNAME file that GitHub Pages requires
+
+echo "five.tiddlywiki.com" > $TW5_BUILD_OUTPUT/CNAME
+
+# First, index.html: the main file, including content
 
 node ../../tiddlywiki.js \
 	--verbose \
 	--password password \
 	--savetiddler ReadMe ../../readme.md text/html \
-	--savetiddler $:/core/templates/tiddlywiki5.template.html $JERMOLENE_HOME/index.html text/plain \
-	--savetiddler $:/core/templates/tiddlywiki5.encrypted.template.html $JERMOLENE_HOME/encrypted.html text/plain \
-	--savetiddler $:/core/templates/static.template.html $JERMOLENE_HOME/static.html text/plain \
+	--savetiddler $:/core/templates/tiddlywiki5.template.html $TW5_BUILD_OUTPUT/index.html text/plain \
+	--savetiddler $:/core/templates/tiddlywiki5.encrypted.template.html $TW5_BUILD_OUTPUT/encrypted.html text/plain \
+	--savetiddler $:/core/templates/static.template.html $TW5_BUILD_OUTPUT/static.html text/plain \
 	|| exit 1
 
 popd > /dev/null
@@ -36,8 +41,8 @@ pushd editions/empty > /dev/null
 node ../../tiddlywiki.js \
 	--verbose \
 	--password password \
-	--savetiddler $:/core/templates/tiddlywiki5.template.html $JERMOLENE_HOME/empty.html text/plain \
-	--savetiddler $:/core/templates/tiddlywiki5.encrypted.template.html $JERMOLENE_HOME/empty_encrypted.html text/plain \
+	--savetiddler $:/core/templates/tiddlywiki5.template.html $TW5_BUILD_OUTPUT/empty.html text/plain \
+	--savetiddler $:/core/templates/tiddlywiki5.encrypted.template.html $TW5_BUILD_OUTPUT/empty_encrypted.html text/plain \
 	|| exit 1
 
 popd > /dev/null
