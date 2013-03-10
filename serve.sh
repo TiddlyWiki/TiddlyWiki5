bf2fb9cdadb85c346bc0a6395d806fd8ec509e6a
Jeremy Ruston <jeremy@osmosoft.com>
2013-03-10 19:28:58 +0000

    Add new clientserver edition
    Runs an instance of TW5 on the server and another in the browser

--- serve.sh
+++ serve.sh
@@ -3,7 +3,7 @@
 # serve TiddlyWiki5 over HTTP
 
 node ./tiddlywiki.js \
-	editions/tw5.com \
+	editions/clientserver \
 	--verbose \
 	--server 8080 $:/core/templates/tiddlywiki5.template.html text/plain text/html \
 	|| exit 1
