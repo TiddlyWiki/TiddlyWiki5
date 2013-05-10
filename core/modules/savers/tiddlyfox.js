6e44a2080436dcd3b95e81832402c0737a6102ef
Jeremy Ruston <jeremy@osmosoft.com>
2013-05-10 18:14:01 +0100

    Fix problem with TiddlyFox on Windows
    Fix #97

--- core/modules/savers/tiddlyfox.js
+++ core/modules/savers/tiddlyfox.js
@@ -18,9 +18,18 @@ var TiddlyFoxSaver = function(wiki) {
 TiddlyFoxSaver.prototype.save = function(text,callback) {
 	var messageBox = document.getElementById("tiddlyfox-message-box");
 	if(messageBox) {
+		// Get the pathname of this document
+		var pathname = document.location.pathname;
+		// Test for a Windows path of the form /x:/blah/blah
+		if(/^\/[A-Z]\:\//.test(pathname)) {
+			// Remove the leading slash
+			pathname = pathname.substr(1);
+			// Convert slashes to backslashes
+			pathname = pathname.replace(/\//g,"\\");
+		}
 		// Create the message element and put it in the message box
 		var message = document.createElement("div");
-		message.setAttribute("data-tiddlyfox-path",decodeURIComponent(document.location.pathname));
+		message.setAttribute("data-tiddlyfox-path",decodeURIComponent(pathname));
 		message.setAttribute("data-tiddlyfox-content",text);
 		messageBox.appendChild(message);
 		// Add an event handler for when the file has been saved
