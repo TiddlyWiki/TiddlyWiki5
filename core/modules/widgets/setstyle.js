456d50b6ef0d643c103db9ed9fa44026e395e54c
Jeremy Ruston <jeremy@osmosoft.com>
2013-06-02 23:18:54 +0100

    Make the setstyle widget refresh itself properly
    This will allow us to dynamically change the colour of tags

--- core/modules/widgets/setstyle.js
+++ core/modules/widgets/setstyle.js
@@ -35,6 +35,24 @@ SetStyleWidget.prototype.generate = function() {
 	this.children = this.renderer.renderTree.createRenderers(this.renderer,this.renderer.parseTreeNode.children);
 };
 
+SetStyleWidget.prototype.refreshInDom = function(changedAttributes,changedTiddlers) {
+	// Check if any of our attributes have changed, or if a tiddler we're interested in has changed
+	if(changedAttributes.name || changedAttributes.value || changedAttributes["class"]) {
+		// Regenerate and rerender the widget and replace the existing DOM node
+		this.generate();
+		var oldDomNode = this.renderer.domNode,
+			newDomNode = this.renderer.renderInDom();
+		oldDomNode.parentNode.replaceChild(newDomNode,oldDomNode);
+	} else {
+		// We don't need to refresh ourselves, so just refresh any child nodes
+		$tw.utils.each(this.children,function(node) {
+			if(node.refreshInDom) {
+				node.refreshInDom(changedTiddlers);
+			}
+		});
+	}
+};
+
 exports.setstyle = SetStyleWidget;
 
 })();
