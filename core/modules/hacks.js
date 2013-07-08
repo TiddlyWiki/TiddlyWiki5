ea8c795d01d91fe47f0893917cc19eb12800dd1e
Jeremy Ruston <jeremy@osmosoft.com>
2013-07-08 15:17:29 +0100

    Add a hack to switch slowmo animation mode
    Just type `$tw.slowmo()` in the developer console to engage slowmo mode.

---
+++ core/modules/hacks.js
@@ -0,0 +1,27 @@
+/*\
+title: $:/core/modules/hacks.js
+type: application/javascript
+module-type: global
+
+These functions are designed to be invoked in the browser developer tools or the node.js REPL.
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Switch slowmotion animation mode on or off
+*/
+exports.slowmo = function(status) {
+	if(status === undefined || status) {
+		$tw.config.preferences.animationDuration = 4000;
+	} else {
+		$tw.config.preferences.animationDuration = 400;
+	}
+	$tw.config.preferences.animationDurationMs = $tw.config.preferences.animationDuration + "ms";
+};
+
+})();
