55d479c540f5e0a05e6c59ef7eb14606b89857c3
Jeremy Ruston <jeremy@osmosoft.com>
2013-10-17 16:55:23 +0100

    Extend macrocall widget to take parameters both from the parse tree node and the attributes
    This allows us to invoke macros using the ordinary widget syntax, which
    in turn allows us to use features like indirection on the parameters.

--- core/modules/new_widgets/macrocall.js
+++ core/modules/new_widgets/macrocall.js
@@ -37,8 +37,13 @@ MacroCallWidget.prototype.render = function(parent,nextSibling) {
 Compute the internal state of the widget
 */
 MacroCallWidget.prototype.execute = function() {
+	// Merge together the parameters specified in the parse tree with the specified attributes
+	var params = this.parseTreeNode.params ? this.parseTreeNode.params.slice(0) : [];
+	$tw.utils.each(this.attributes,function(attribute,name) {
+		params.push({name: name, value: attribute});
+	});
 	// Get the macro value
-	var text = this.getVariable(this.parseTreeNode.name,this.parseTreeNode.params);
+	var text = this.getVariable(this.parseTreeNode.name || this.getAttribute("$name"),params);
 	// Parse the macro
 	var parser = this.wiki.new_parseText("text/vnd.tiddlywiki",text,
 						{parseAsInline: !this.parseTreeNode.isBlock}),
@@ -51,7 +56,14 @@ MacroCallWidget.prototype.execute = function() {
 Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
 */
 MacroCallWidget.prototype.refresh = function(changedTiddlers) {
-	return this.refreshChildren(changedTiddlers);
+	var changedAttributes = this.computeAttributes();
+	if($tw.utils.count(changedAttributes) > 0) {
+		// Rerender ourselves
+		this.refreshSelf();
+		return true;
+	} else {
+		return this.refreshChildren(changedTiddlers);
+	}
 };
 
 exports.macrocall = MacroCallWidget;
