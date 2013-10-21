c9668f93b5ea86fe7ee04d28e260ca78b3cbaa91
Jeremy Ruston <jeremy@osmosoft.com>
2013-10-21 18:33:25 +0100

    Test building static css and individual tiddlers

--- nbld.sh
+++ nbld.sh
@@ -24,6 +24,8 @@ node ./tiddlywiki.js \
 	--new_rendertiddler ReadMe ./readme.md text/html \
 	--new_rendertiddler ContributingTemplate ./contributing.md text/html \
 	--new_rendertiddler $:/core/templates/static.template.html $TW5_BUILD_OUTPUT/static.html text/plain \
+	--new_rendertiddler $:/core/templates/static.template.css $TW5_BUILD_OUTPUT/static/static.css text/plain \
+	--new_rendertiddlers [!is[system]] $:/core/templates/static.tiddler.html $TW5_BUILD_OUTPUT/static text/plain \
 	|| exit 1
 
 # Run tests
