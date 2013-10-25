bdcead624a93f61f2bf2ef6c3e136f480ca542a1
Jeremy Ruston <jeremyruston@SilverSlim.local>
2013-10-25 22:29:49 +0100

    Add a changecount macro

---
+++ core/modules/macros/changecount.js
@@ -0,0 +1,30 @@
+/*\
+title: $:/core/modules/macros/changecount.js
+type: application/javascript
+module-type: macro
+
+Macro to return the changecount for the current tiddler
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Information about this macro
+*/
+
+exports.name = "changecount";
+
+exports.params = [];
+
+/*
+Run the macro
+*/
+exports.run = function() {
+	return this.wiki.getChangeCount(this.getVariable("tiddlerTitle")) + "";
+};
+
+})();
