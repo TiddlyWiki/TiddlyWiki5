e54148033bce3c3fbaf60e3fd4d3a1cc1c5558d8
Jermolene <jeremy@osmosoft.com>
2013-11-08 21:34:47 +0000

    Fix client server edition syncing

--- core/modules/syncer.js
+++ core/modules/syncer.js
@@ -181,7 +181,7 @@ Syncer.prototype.syncFromServer = function() {
 			for(var t=0; t<tiddlers.length; t++) {
 				// Get the incoming tiddler fields, and the existing tiddler
 				var tiddlerFields = tiddlers[t],
-					incomingRevision = tiddlerFields.revision,
+					incomingRevision = tiddlerFields.revision + "",
 					tiddler = self.wiki.getTiddler(tiddlerFields.title),
 					tiddlerInfo = self.tiddlerInfo[tiddlerFields.title],
 					currRevision = tiddlerInfo ? tiddlerInfo.revision : null;
