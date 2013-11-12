791033d7517b38135564a13fdaf6f427d0222b70
Jermolene <jeremy@osmosoft.com>
2013-11-12 20:49:00 +0000

    First pass at a saver for IE10 and above
    It's not great, sadly. Clicking save in the browser pulls up an
    unobtrusive bar at the bottom of the browser window where you can click
    "save". You then get a new copy of your wiki in the downloads folder.

---
+++ core/modules/savers/msdownload.js
@@ -0,0 +1,57 @@
+/*\
+title: $:/core/modules/savers/msdownload.js
+type: application/javascript
+module-type: saver
+
+Handles saving changes via window.navigator.msSaveBlob()
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Select the appropriate saver module and set it up
+*/
+var MsDownloadSaver = function(wiki) {
+};
+
+MsDownloadSaver.prototype.save = function(text) {
+	// Get the current filename
+	var filename = "tiddlywiki.html",
+		p = document.location.pathname.lastIndexOf("/");
+	if(p !== -1) {
+		filename = document.location.pathname.substr(p+1);
+	}
+	// Set up the link
+	var blob = new Blob([text], {type: "text/html"});
+	window.navigator.msSaveBlob(blob,filename);
+	return true;
+};
+
+/*
+Information about this saver
+*/
+MsDownloadSaver.prototype.info = {
+	name: "msdownload",
+	priority: 110
+};
+
+/*
+Static method that returns true if this saver is capable of working
+*/
+exports.canSave = function(wiki) {
+	alert("Boo")
+	return !!window.navigator.msSaveBlob;
+};
+
+/*
+Create an instance of this saver
+*/
+exports.create = function(wiki) {
+	return new MsDownloadSaver(wiki);
+};
+
+})();
