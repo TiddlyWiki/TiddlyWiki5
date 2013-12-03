7d0b9303354cddeb117de4c1f74bcc614e31be66
Jermolene <jeremy@osmosoft.com>
2013-12-03 10:09:37 +0000

    Update `verbump` script to include version number in commit message
    Easier to find.

--- verbump.cmd
+++ verbump.cmd
@@ -3,13 +3,13 @@
 rem Bump to a new version number
 
 if "x%1" == "x" (
-	echo Missing version (eg '5.0.0-alpha.99'^)
+	echo Missing version (eg '5.0.0-alpha'^)
 	exit 1
 )
 
 rem Set the new version number (will also commit and tag the release)
 
-npm version %1 -m "Version update" || exit 1
+npm version %1 -m "Version number update for %1" || exit 1
 
 rem Make sure our tags are pushed to the origin server
 
--- verbump.sh
+++ verbump.sh
@@ -4,13 +4,13 @@
 
 if [ -z "$1" ]
   then
-    echo "Missing version (eg '5.0.0-alpha.99')"
+    echo "Missing version (eg '5.0.0-alpha')"
     exit 1
 fi
 
 # Set the new version number (will also commit and tag the release)
 
-npm version $1 -m "Version update" || exit 1
+npm version $1 -m "Version number update for $1" || exit 1
 
 # Make sure our tags are pushed to the origin server
 
