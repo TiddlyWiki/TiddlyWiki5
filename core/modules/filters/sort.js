ce8c79ecfa3071cfe6f5b555c9b7fc4ae1a3ae72
Stephan Hradek <github.mails.skeeve@xoxy.net>
2013-12-29 00:15:11 +0100

    changed sort and sortcs to support nsort and nsortcs

--- core/modules/filters/sort.js
+++ core/modules/filters/sort.js
@@ -1,5 +1,5 @@
 /*\
-title: $:/core/modules/filters/sort.js
+title: $:/core/modules/filters/nsort.js
 type: application/javascript
 module-type: filteroperator
 
@@ -16,6 +16,30 @@ Filter operator for sorting
 Export our filter function
 */
 exports.sort = function(source,operator,options) {
+	var results = prepare_results(source);
+	options.wiki.sortTiddlers(results,operator.operand,operator.prefix === "!",false,false);
+	return results;
+};
+
+exports.nsort = function(source,operator,options) {
+	var results = prepare_results(source);
+	options.wiki.sortTiddlers(results,operator.operand,operator.prefix === "!",false,true);
+	return results;
+};
+
+exports.sortcs = function(source,operator,options) {
+	var results = prepare_results(source);
+	options.wiki.sortTiddlers(results,operator.operand,operator.prefix === "!",true,false);
+	return results;
+};
+
+exports.nsortcs = function(source,operator,options) {
+	var results = prepare_results(source);
+	options.wiki.sortTiddlers(results,operator.operand,operator.prefix === "!",true,true);
+	return results;
+};
+
+var prepare_results = function (source) {
 	var results;
 	if($tw.utils.isArray(source)) {
 		results = source;
@@ -25,8 +49,7 @@ exports.sort = function(source,operator,options) {
 			results.push(title);
 		});
 	}
-	options.wiki.sortTiddlers(results,operator.operand,operator.prefix === "!");
 	return results;
-};
+}
 
 })();
