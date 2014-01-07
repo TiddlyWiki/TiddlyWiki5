82a48cf85ce250dfe7986bfc128496f17666d3fd
João Bolila <joao@bolila.com>
2014-01-07 22:57:46 +0000

    codeblock as a widget and plugin for highlight code blocks

---
+++ core/modules/widgets/codeblock.js
@@ -0,0 +1,64 @@
+/*\
+title: $:/core/modules/widgets/codeblock.js
+type: application/javascript
+module-type: widget
+
+Code block node widget
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var Widget = require("$:/core/modules/widgets/widget.js").widget;
+
+var CodeBlockWidget = function(parseTreeNode,options) {
+	this.initialise(parseTreeNode,options);
+};
+
+/*
+Inherit from the base widget class
+*/
+CodeBlockWidget.prototype = new Widget();
+
+/*
+Render this widget into the DOM
+*/
+CodeBlockWidget.prototype.render = function(parent,nextSibling) {
+	this.parentDomNode = parent;
+	this.computeAttributes();
+	this.execute();
+	var codeNode = this.document.createElement("code");
+	if (this.getAttribute("language")) {
+		codeNode.setAttribute("class",this.getAttribute("language"));
+	}
+	var domNode = this.document.createElement("pre");
+	codeNode.appendChild(this.document.createTextNode(this.getAttribute("code")));
+	domNode.appendChild(codeNode);
+	parent.insertBefore(domNode,nextSibling);
+	this.domNodes.push(domNode);
+
+	if(this.postRender) {
+		this.postRender();
+	}
+};
+
+/*
+Compute the internal state of the widget
+*/
+CodeBlockWidget.prototype.execute = function() {
+	// Nothing to do for a text node
+};
+
+/*
+Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
+*/
+CodeBlockWidget.prototype.refresh = function(changedTiddlers) {
+	return false;
+};
+
+exports.codeblock = CodeBlockWidget;
+
+})();
--- plugins/tiddlywiki/highlight/highlightblock.js
+++ plugins/tiddlywiki/highlight/highlightblock.js
@@ -1,53 +1,28 @@
 /*\
 title: $:/plugins/tiddlywiki/highlight/highlightblock.js
 type: application/javascript
-module-type: parser
+module-type: widget
 
 Wraps up the fenced code blocks parser for highlight and use in TiddlyWiki5
 
 \*/
 (function() {
 
-    /*jslint node: true, browser: true */
-    /*global $tw: false */
-    "use strict";
+		/*jslint node: true, browser: true */
+		/*global $tw: false */
+		"use strict";
 
-    var hljs = require("$:/plugins/tiddlywiki/highlight/highlight.js").hljs,
-        WikiParser = require("$:/core/modules/parsers/wikiparser/wikiparser.js")["text/vnd.tiddlywiki"],
-        BlockParsers = $tw.modules.createClassesFromModules("wikirule", "block", $tw.WikiRuleBase);
+		var CodeBlockWidget = require("$:/core/modules/widgets/codeblock.js").codeblock;
 
-    BlockParsers.codeblock.prototype.parse = function() {
-        var reEnd = /(\r?\n```$)/mg;
-        // Move past the match
-        this.parser.pos = this.matchRegExp.lastIndex;
-        // Look for the end of the block
-        reEnd.lastIndex = this.parser.pos;
-        var match = reEnd.exec(this.parser.source),
-            text;
-        // Process the block
-        if (match) {
-            text = this.parser.source.substring(this.parser.pos, match.index);
-            this.parser.pos = match.index + match[0].length;
-        } else {
-            text = this.parser.source.substr(this.parser.pos);
-            this.parser.pos = this.parser.sourceLength;
-        }
+		CodeBlockWidget.prototype.postRender = function() {
+				var self = this,
+						lang = this.domNodes[0].getElementsByTagName('code')[0].className,
+						hljs = require("$:/plugins/tiddlywiki/highlight/highlight.js").hljs;
 
-        // Return the pre element
-        return [{
-            type: "element",
-            tag: "pre",
-            children: [{
-                type: "element",
-                tag: "code",
-                children: [{
-                    type: "raw",
-                    html: hljs.highlightAuto(text).value
-                }]
-            }]
-        }];
-    };
-
-    WikiParser.prototype.blockRuleClasses = BlockParsers;
+				if ($tw.browser && lang !== 'no-highlight') {
+						hljs.tabReplace = '    ';
+						hljs.highlightBlock(this.domNodes[0]);
+				}
+		};
 
 })();
