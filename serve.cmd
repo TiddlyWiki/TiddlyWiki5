042698b508ed79d205779c5a510838a0552f95a3
Jermolene <jeremy@osmosoft.com>
2014-02-24 13:28:55 +0000

    Some rejigging of the editions folders
    Now we’re optimising the editions for use with the —init command.
    Documentation coming up.

--- serve.cmd
+++ serve.cmd
@@ -5,7 +5,7 @@ rem serve TiddlyWiki5 over HTTP
 rem Optional parameter is the username for signing edits
 
 node .\tiddlywiki.js ^
-	editions\clientserver ^
+	editions\tw5.com-server ^
 	--verbose ^
 	--server 8080 $:/core/save/all text/plain text/html %1 %2^
 	|| exit 1
