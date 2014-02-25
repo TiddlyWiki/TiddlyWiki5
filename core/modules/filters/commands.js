2ed8df0d1f52126b63ad7aea52761db9d97149ac
Jermolene <jeremy@osmosoft.com>
2014-02-25 14:49:56 +0000

    Add a new "commands" filter

---
+++ core/modules/filters/commands.js
@@ -0,0 +1,27 @@
+/*\
+title: $:/core/modules/filters/commands.js
+type: application/javascript
+module-type: filteroperator
+
+Filter operator for returning the names of the commands available in this wiki
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Export our filter function
+*/
+exports.commands = function(source,operator,options) {
+	var results = [];
+	$tw.utils.each($tw.commands,function(commandInfo,name) {
+		results.push(name);
+	});
+	results.sort();
+	return results;
+};
+
+})();
