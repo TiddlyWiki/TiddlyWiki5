bdbbf94326f70db0f8ef196270ab9e92bfde10fb
Jermolene <jeremy@osmosoft.com>
2014-03-17 21:44:10 +0000

    Update transclusion wikitext syntax to allow a template without a target tiddler
    This allows us to transclude a tiddler without changing the current
    tiddler with `{{||MyTiddler}}`.

--- core/modules/parsers/wikiparser/rules/transcludeblock.js
+++ core/modules/parsers/wikiparser/rules/transcludeblock.js
@@ -23,19 +23,17 @@ exports.types = {block: true};
 exports.init = function(parser) {
 	this.parser = parser;
 	// Regexp to match
-	this.matchRegExp = /\{\{([^\{\}\|]+)(?:\|\|([^\|\{\}]+))?\}\}(?:\r?\n|$)/mg;
+	this.matchRegExp = /\{\{([^\{\}\|]*)(?:\|\|([^\|\{\}]+))?\}\}(?:\r?\n|$)/mg;
 };
 
 exports.parse = function() {
 	// Move past the match
 	this.parser.pos = this.matchRegExp.lastIndex;
+	// Move past the match
+	this.parser.pos = this.matchRegExp.lastIndex;
 	// Get the match details
-	var textRef = $tw.utils.trim(this.match[1]),
-		tr = $tw.utils.parseTextReference(textRef),
-		targetTitle = tr.title,
-		targetField = tr.field,
-		targetIndex = tr.index,
-		template = $tw.utils.trim(this.match[2]);
+	var template = $tw.utils.trim(this.match[2]),
+		textRef = $tw.utils.trim(this.match[1]);
 	// Prepare the transclude widget
 	var transcludeNode = {
 			type: "element",
@@ -43,27 +41,43 @@ exports.parse = function() {
 			attributes: {},
 			isBlock: true
 		};
-	var tiddlerNode = {
-		type: "element",
-		tag: "$tiddler",
-		attributes: {
-			tiddler: {type: "string", value: targetTitle}
-		},
-		isBlock: true,
-		children: [transcludeNode]
-	};
+	// Prepare the tiddler widget
+	if(textRef) {
+		var tr = $tw.utils.parseTextReference(textRef),
+			targetTitle = tr.title,
+			targetField = tr.field,
+			targetIndex = tr.index,
+			tiddlerNode = {
+				type: "element",
+				tag: "$tiddler",
+				attributes: {
+					tiddler: {type: "string", value: targetTitle}
+				},
+				isBlock: true,
+				children: [transcludeNode]
+			};
+	}
 	if(template) {
 		transcludeNode.attributes.tiddler = {type: "string", value: template};
-	} else {
-		transcludeNode.attributes.tiddler = {type: "string", value: targetTitle};
-		if(targetField) {
-			transcludeNode.attributes.field = {type: "string", value: targetField};
+		if(textRef) {
+			return [tiddlerNode];
+		} else {
+			return [transcludeNode];
 		}
-		if(targetIndex) {
-			transcludeNode.attributes.index = {type: "string", value: targetIndex};
+	} else {
+		if(textRef) {
+			transcludeNode.attributes.tiddler = {type: "string", value: targetTitle};
+			if(targetField) {
+				transcludeNode.attributes.field = {type: "string", value: targetField};
+			}
+			if(targetIndex) {
+				transcludeNode.attributes.index = {type: "string", value: targetIndex};
+			}
+			return [tiddlerNode];
+		} else {
+			return [transcludeNode];
 		}
 	}
-	return [tiddlerNode];
 };
 
 })();
