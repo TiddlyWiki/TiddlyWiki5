4c648e7cdc6a8fcead8fdd943371b7ceb4f70502
Jermolene <jeremy@osmosoft.com>
2014-04-14 11:17:28 +0100

    Exclude more illegal characters from external links

--- core/modules/parsers/wikiparser/rules/extlink.js
+++ core/modules/parsers/wikiparser/rules/extlink.js
@@ -26,7 +26,7 @@ exports.types = {inline: true};
 exports.init = function(parser) {
 	this.parser = parser;
 	// Regexp to match
-	this.matchRegExp = /~?(?:file|http|https|mailto|ftp|irc|news|data|skype):[^\s\|'"<>]+(?:\/|\b)/mg;
+	this.matchRegExp = /~?(?:file|http|https|mailto|ftp|irc|news|data|skype):[^\s<>{}\[\]`|'"\\^~]+(?:\/|\b)/mg;
 };
 
 exports.parse = function() {
