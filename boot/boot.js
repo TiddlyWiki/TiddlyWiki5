c73853288c5b4b0716da94fea2f2edec09345643
Jermolene <jeremy@osmosoft.com>
2014-07-24 19:53:51 +0100

    Allow .htm as an alias for .html files

--- boot/boot.js
+++ boot/boot.js
@@ -1727,6 +1727,7 @@ $tw.boot.startup = function(options) {
 	$tw.utils.registerFileType("text/plain","utf8",".txt");
 	$tw.utils.registerFileType("text/css","utf8",".css");
 	$tw.utils.registerFileType("text/html","utf8",".html");
+	$tw.config.fileExtensionInfo[".htm"] = {type: "text/html"};
 	$tw.utils.registerFileType("application/hta","utf16le",".hta",{deserializerType:"text/html"});
 	$tw.utils.registerFileType("application/javascript","utf8",".js");
 	$tw.utils.registerFileType("application/json","utf8",".json");
