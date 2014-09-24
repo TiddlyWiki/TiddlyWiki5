a159b5baf3ad91d8defc68cbf81c78d01b69c416
Jermolene <jeremy@osmosoft.com>
2014-09-24 13:15:16 +0200

    Add tiddlerInfo to the syncadaptor saveTiddler method

--- core/modules/syncer.js
+++ core/modules/syncer.js
@@ -467,6 +467,8 @@ Syncer.prototype.dispatchTask = function(task,callback) {
 				};
 				// Invoke the callback
 				callback(null);
+			},{
+				tiddlerInfo: self.tiddlerInfo[task.title]
 			});
 		} else {
 			this.logger.log(" Not Dispatching 'save' task:",task.title,"tiddler does not exist");
