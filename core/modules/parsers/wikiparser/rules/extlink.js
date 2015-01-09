795a9291870d2fda636f22375cd841eb3e8b3257
Tobias Beer <beertobias@gmail.com>
2015-01-09 23:51:07 +0100

    create syslink.js, revert extlink.js
    introduces new parser for system links

--- core/modules/parsers/wikiparser/rules/extlink.js
+++ core/modules/parsers/wikiparser/rules/extlink.js
@@ -3,19 +3,15 @@ title: $:/core/modules/parsers/wikiparser/rules/extlink.js
 type: application/javascript
 module-type: wikirule
 
-Wiki text inline rule for external links and system tiddler links.
-Can be suppressed preceding them with `~`.
+Wiki text inline rule for external links. For example:
 
 ```
-; system tiddler
-: $:/config
+An external link: http://www.tiddlywiki.com/
 
-; external link
-: http://www.tiddlywiki.com/
-
-; suppressed external link
-: ~http://www.tiddlyspace.com/
+A suppressed external link: ~http://www.tiddlyspace.com/
 ```
+
+External links can be suppressed by preceding them with `~`.
 
 \*/
 (function(){
@@ -30,38 +26,26 @@ exports.types = {inline: true};
 exports.init = function(parser) {
 	this.parser = parser;
 	// Regexp to match
-	this.matchRegExp = /~?(?:\$|file|http|https|mailto|ftp|irc|news|data|skype):[^\s<>{}\[\]`|'"\\^~]+(?:\/|\b)/mg;
+	this.matchRegExp = /~?(?:file|http|https|mailto|ftp|irc|news|data|skype):[^\s<>{}\[\]`|'"\\^~]+(?:\/|\b)/mg;
 };
 
 exports.parse = function() {
-	var match = this.match[0];
 	// Move past the match
 	this.parser.pos = this.matchRegExp.lastIndex;
 	// Create the link unless it is suppressed
-	if(match.substr(0,1) === "~") {
-		return [{type: "text", text: match.substr(1)}];
-	} else if(match.substr(0,1) === "$") {
-		return [{
-			type: "link",
-			attributes: {
-				to: {type: "string", value: match}
-			},
-			children: [{
-				type: "text",
-				text: match
-			}]
-		}];
+	if(this.match[0].substr(0,1) === "~") {
+		return [{type: "text", text: this.match[0].substr(1)}];
 	} else {
 		return [{
 			type: "element",
 			tag: "a",
 			attributes: {
-				href: {type: "string", value: match},
+				href: {type: "string", value: this.match[0]},
 				"class": {type: "string", value: "tc-tiddlylink-external"},
 				target: {type: "string", value: "_blank"}
 			},
 			children: [{
-				type: "text", text: match
+				type: "text", text: this.match[0]
 			}]
 		}];
 	}
