8b74706e359b0f1ab69cd40d799dee194179b682
Tobias Beer <beertobias@gmail.com>
2015-01-27 02:02:15 +0100

    fixed helper function and added tests
    tests: ok

--- bin/test.cmd
+++ bin/test.cmd
@@ -4,8 +4,8 @@ rem test TiddlyWiki5 for tiddlywiki.com
 
 rem Run the test edition to run the node.js tests and to generate test.html for tests in the browser
 
-node .\tiddlywiki.js ^
-	.\editions\test ^
+node ..\tiddlywiki.js ^
+	..\editions\test ^
 	--verbose ^
 	--rendertiddler $:/core/save/all test.html text/plain ^
 	|| exit 1
