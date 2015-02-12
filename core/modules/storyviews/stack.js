ef635b200991743209c4c2a7ae3bb38a724c22fd
Jermolene <jeremy@osmosoft.com>
2015-02-12 20:15:27 +0000

    First commit of experimental stacked view
    A new storyview that shows tiddlers as an overlapping stack of cards.
    
    Prompted by the discussion in #1290

---
+++ core/modules/storyviews/stack.js
@@ -0,0 +1,77 @@
+/*\
+title: $:/core/modules/storyviews/stack.js
+type: application/javascript
+module-type: storyview
+
+Keeps tiddlers in a stack
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var easing = "cubic-bezier(0.645, 0.045, 0.355, 1)"; // From http://easings.net/#easeInOutCubic
+
+var StackListView = function(listWidget) {
+	var self = this;
+	this.listWidget = listWidget;
+	this.placeTiddlers();
+};
+
+StackListView.prototype.placeTiddlers = function() {
+	// Initialise the stack of tiddler titles
+	this.listStack = [];
+	var numItems = this.listWidget.children.length,
+		t, itemWidget,
+		duration = $tw.utils.getAnimationDuration();
+	for(t=numItems-1; t>=0; t--) {
+		itemWidget = this.listWidget.children[t];
+		this.listStack.push(itemWidget.parseTreeNode.itemTitle);
+	}
+	// Ensure the tiddler at the top of the history stack is at the top of the array
+	var history = this.listWidget.wiki.getTiddlerData(this.listWidget.historyTitle,[]);
+	for(t=0; t<history.length; t++) {
+		var title = history[t].title;
+		if(this.listStack.indexOf(title) !== -1) {
+			$tw.utils.pushTop(this.listStack,title);
+		}
+	}
+	// Make all the tiddlers position absolute, and hide all but the top (or first) one
+	for(var t=numItems-1; t>=0; t--) {
+		// Get the DOM node for this entry
+		itemWidget = this.listWidget.children[t];
+		var domNode = itemWidget.findFirstDomNode();
+		if(domNode instanceof Element) {
+			var pos = this.listStack.indexOf(itemWidget.parseTreeNode.itemTitle);
+			if(pos !== -1) {
+				var posFactor = pos/(numItems-1);
+				$tw.utils.setStyle(domNode,[
+					{position: "absolute"},
+					{transformOrigin: "50% 0"},
+					{transition: $tw.utils.roundTripPropertyName("transform") + " " + duration + "ms " + easing},
+					{transform: "translateX(0px) translateY(" + (100 * posFactor * posFactor) + "px) scale(" + (0.1 + posFactor * 0.9) + ")"},
+					{zIndex: pos + ""}
+				]);
+			}
+		}
+	}
+};
+
+StackListView.prototype.navigateTo = function(historyInfo) {
+	this.placeTiddlers();
+};
+
+StackListView.prototype.insert = function(widget) {
+	this.placeTiddlers();
+};
+
+StackListView.prototype.remove = function(widget) {
+	widget.removeChildDomNodes();
+	this.placeTiddlers();
+};
+
+exports.stack = StackListView;
+
+})();
