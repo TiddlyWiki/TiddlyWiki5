57c27cdb13df44cdaf1cf1ea0150a790e8183e78
Jermolene <jeremy@osmosoft.com>
2015-02-20 21:43:07 +0000

    Extend list widget to invoke storyview during refresh

--- core/modules/widgets/list.js
+++ core/modules/widgets/list.js
@@ -126,20 +126,29 @@ ListWidget.prototype.makeItemTemplate = function(title) {
 Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
 */
 ListWidget.prototype.refresh = function(changedTiddlers) {
-	var changedAttributes = this.computeAttributes();
+	var changedAttributes = this.computeAttributes(),
+		result;
+	// Call the storyview
+	if(this.storyview && this.storyview.refreshStart) {
+		this.storyview.refreshStart(changedTiddlers,changedAttributes);
+	}
 	// Completely refresh if any of our attributes have changed
 	if(changedAttributes.filter || changedAttributes.template || changedAttributes.editTemplate || changedAttributes.emptyMessage || changedAttributes.storyview || changedAttributes.history) {
 		this.refreshSelf();
-		return true;
+		result = true;
 	} else {
 		// Handle any changes to the list
-		var hasChanged = this.handleListChanges(changedTiddlers);
+		result = this.handleListChanges(changedTiddlers);
 		// Handle any changes to the history stack
 		if(this.historyTitle && changedTiddlers[this.historyTitle]) {
 			this.handleHistoryChanges();
 		}
-		return hasChanged;
 	}
+	// Call the storyview
+	if(this.storyview && this.storyview.refreshEnd) {
+		this.storyview.refreshEnd(changedTiddlers,changedAttributes);
+	}
+	return result;
 };
 
 /*
