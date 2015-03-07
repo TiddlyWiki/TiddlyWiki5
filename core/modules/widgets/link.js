d3268c6d6507099dfdaddd8c2da91fa3b4c14360
jed <inmysocks@gmail.com>
2015-03-07 11:26:21 -0700

    I didn't have the newest version
    And I hadn't updated the names. I may not be very good at this.

--- core/modules/widgets/link.js
+++ core/modules/widgets/link.js
@@ -1,5 +1,5 @@
 /*\
-title: $:/plugins/inmysocsk/WidgetsInvokeActions/link-invoke-actions.js
+title: $:/core/modules/widgets/link.js
 type: application/javascript
 module-type: widget
 
@@ -14,19 +14,19 @@ Link widget
 
 var Widget = require("$:/core/modules/widgets/widget.js").widget;
 
-var LinkWidgetInvokeActions = function(parseTreeNode,options) {
+var LinkWidget = function(parseTreeNode,options) {
 	this.initialise(parseTreeNode,options);
 };
 
 /*
 Inherit from the base widget class
 */
-LinkWidgetInvokeActions.prototype = new Widget();
+LinkWidget.prototype = new Widget();
 
 /*
 Render this widget into the DOM
 */
-LinkWidgetInvokeActions.prototype.render = function(parent,nextSibling) {
+LinkWidget.prototype.render = function(parent,nextSibling) {
 	// Save the parent dom node
 	this.parentDomNode = parent;
 	// Compute our attributes
@@ -51,10 +51,15 @@ LinkWidgetInvokeActions.prototype.render = function(parent,nextSibling) {
 /*
 Render this widget into the DOM
 */
-LinkWidgetInvokeActions.prototype.renderLink = function(parent,nextSibling) {
+LinkWidget.prototype.renderLink = function(parent,nextSibling) {
 	var self = this;
+	// Sanitise the specified tag
+	var tag = this.linkTag;
+	if($tw.config.htmlUnsafeElements.indexOf(tag) !== -1) {
+		tag = "a";
+	}
 	// Create our element
-	var domNode = this.document.createElement("a");
+	var domNode = this.document.createElement(tag);
 	// Assign classes
 	var classes = [];
 	if(this.linkClasses) {
@@ -77,7 +82,11 @@ LinkWidgetInvokeActions.prototype.renderLink = function(parent,nextSibling) {
 		wikiLinkTemplate = wikiLinkTemplateMacro ? wikiLinkTemplateMacro.trim() : "#$uri_encoded$",
 		wikiLinkText = wikiLinkTemplate.replace("$uri_encoded$",encodeURIComponent(this.to));
 	wikiLinkText = wikiLinkText.replace("$uri_doubleencoded$",encodeURIComponent(encodeURIComponent(this.to)));
-	domNode.setAttribute("href",wikiLinkText);
+	wikiLinkText = this.getVariable("tv-get-export-link",{params: [{name: "to",value: this.to}],defaultValue: wikiLinkText});
+	if(tag === "a") {
+		domNode.setAttribute("href",wikiLinkText);		
+	}
+	domNode.setAttribute("tabindex",this.tabIndex);
 	// Set the tooltip
 	// HACK: Performance issues with re-parsing the tooltip prevent us defaulting the tooltip to "<$transclude field='tooltip'><$transclude field='title'/></$transclude>"
 	var tooltipWikiText = this.tooltip || this.getVariable("tv-wikilink-tooltip");
@@ -97,16 +106,20 @@ LinkWidgetInvokeActions.prototype.renderLink = function(parent,nextSibling) {
 	// Add a click event handler
 	$tw.utils.addEventListeners(domNode,[
 		{name: "click", handlerObject: this, handlerMethod: "handleClickEvent"},
-		{name: "dragstart", handlerObject: this, handlerMethod: "handleDragStartEvent"},
-		{name: "dragend", handlerObject: this, handlerMethod: "handleDragEndEvent"}
 	]);
+	if(this.draggable === "yes") {
+		$tw.utils.addEventListeners(domNode,[
+			{name: "dragstart", handlerObject: this, handlerMethod: "handleDragStartEvent"},
+			{name: "dragend", handlerObject: this, handlerMethod: "handleDragEndEvent"}
+		]);
+	}
 	// Insert the link into the DOM and render any children
 	parent.insertBefore(domNode,nextSibling);
 	this.renderChildren(domNode,null);
 	this.domNodes.push(domNode);
 };
 
-LinkWidgetInvokeActions.prototype.handleClickEvent = function(event) {
+LinkWidget.prototype.handleClickEvent = function(event) {
 	var handled;
 	// Invoke any actions
 	if(this.invokeActions(event)) {
@@ -123,14 +136,17 @@ LinkWidgetInvokeActions.prototype.handleClickEvent = function(event) {
 		},
 		navigateSuppressNavigation: event.metaKey || event.ctrlKey || (event.button === 1)
 	});
-	event.preventDefault();
-	event.stopPropagation();
-	return false;
+	if(this.domNodes[0].hasAttribute("href")) {
+		event.preventDefault();
+		event.stopPropagation();
+		return false;
+	}
 };
 
-LinkWidgetInvokeActions.prototype.handleDragStartEvent = function(event) {
+LinkWidget.prototype.handleDragStartEvent = function(event) {
 	if(event.target === this.domNodes[0]) {
 		if(this.to) {
+			$tw.dragInProgress = true;
 			// Set the dragging class on the element being dragged
 			$tw.utils.addClass(event.target,"tc-tiddlylink-dragging");
 			// Create the drag image elements
@@ -165,9 +181,9 @@ LinkWidgetInvokeActions.prototype.handleDragStartEvent = function(event) {
 			if(!$tw.browser.isIE) {
 				dataTransfer.setData("text/vnd.tiddler",jsonData);
 				dataTransfer.setData("text/plain",title);
-				dataTransfer.setData("text/x-moz-url","data:text/vnd.tiddler," + encodeURI(jsonData));
+				dataTransfer.setData("text/x-moz-url","data:text/vnd.tiddler," + encodeURIComponent(jsonData));
 			}
-			dataTransfer.setData("URL","data:text/vnd.tiddler," + encodeURI(jsonData));
+			dataTransfer.setData("URL","data:text/vnd.tiddler," + encodeURIComponent(jsonData));
 			dataTransfer.setData("Text",title);
 			event.stopPropagation();
 		} else {
@@ -176,8 +192,9 @@ LinkWidgetInvokeActions.prototype.handleDragStartEvent = function(event) {
 	}
 };
 
-LinkWidgetInvokeActions.prototype.handleDragEndEvent = function(event) {
+LinkWidget.prototype.handleDragEndEvent = function(event) {
 	if(event.target === this.domNodes[0]) {
+		$tw.dragInProgress = false;
 		// Remove the dragging class on the element being dragged
 		$tw.utils.removeClass(event.target,"tc-tiddlylink-dragging");
 		// Delete the drag image element
@@ -190,14 +207,15 @@ LinkWidgetInvokeActions.prototype.handleDragEndEvent = function(event) {
 /*
 Compute the internal state of the widget
 */
-LinkWidgetInvokeActions.prototype.execute = function() {
-	// Get the target tiddler title
+LinkWidget.prototype.execute = function() {
+	// Pick up our attributes
 	this.to = this.getAttribute("to",this.getVariable("currentTiddler"));
-	// Get the link title and aria label
 	this.tooltip = this.getAttribute("tooltip");
 	this["aria-label"] = this.getAttribute("aria-label");
-	// Get the link classes
 	this.linkClasses = this.getAttribute("class");
+	this.tabIndex = this.getAttribute("tabindex");
+	this.draggable = this.getAttribute("draggable","yes");
+	this.linkTag = this.getAttribute("tag","a");
 	// Determine the link characteristics
 	this.isMissing = !this.wiki.tiddlerExists(this.to);
 	this.isShadow = this.wiki.isShadowTiddler(this.to);
@@ -208,7 +226,7 @@ LinkWidgetInvokeActions.prototype.execute = function() {
 /*
 Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
 */
-LinkWidgetInvokeActions.prototype.refresh = function(changedTiddlers) {
+LinkWidget.prototype.refresh = function(changedTiddlers) {
 	var changedAttributes = this.computeAttributes();
 	if(changedAttributes.to || changedTiddlers[this.to] || changedAttributes["aria-label"] || changedAttributes.tooltip) {
 		this.refreshSelf();
@@ -217,6 +235,6 @@ LinkWidgetInvokeActions.prototype.refresh = function(changedTiddlers) {
 	return this.refreshChildren(changedTiddlers);
 };
 
-exports.linkInvokeActions = LinkWidgetInvokeActions;
+exports.link = LinkWidget;
 
 })();
