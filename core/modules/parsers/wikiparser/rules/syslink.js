2cb6400773096b02b71c1851fb0fac5dfefbbd6f
Jermolene <jeremy@jermolene.com>
2015-07-13 14:42:55 +0100

    Extend automatic system tiddler linking to digits and underscore
    Fixes #1877

--- core/modules/parsers/wikiparser/rules/syslink.js
+++ core/modules/parsers/wikiparser/rules/syslink.js
@@ -18,7 +18,7 @@ exports.types = {inline: true};
 exports.init = function(parser) {
 	this.parser = parser;
 	// Regexp to match
-	this.matchRegExp = /~?\$:\/[a-zA-Z/.-]+/mg;
+	this.matchRegExp = /~?\$:\/[a-zA-Z0-9/.-_]+/mg;
 };
 
 exports.parse = function() {
