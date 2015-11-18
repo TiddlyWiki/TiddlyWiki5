3465d8bc7d23e6a7cd0cccbf87e1347e4701289e
Tobias Beer <beertobias@gmail.com>
2015-11-18 15:40:14 +0100

    implements #2042 — $tw.utils.uuid() and <<uuid>> macro
    these two components should cater for all uuid generation needs of #2042

--- boot/boot.js
+++ boot/boot.js
@@ -476,6 +476,28 @@ $tw.utils.PasswordPrompt.prototype.setWrapperDisplay = function() {
 };
 
 /*
+Creates a uuid, fast (source: http://stackoverflow.com/a/21963136)
+*/
+$tw.utils.uuid = function() {
+	var lut = [];
+	for (var i=0; i<256; i++) {
+		lut[i] = (i<16?'0':'')+(i).toString(16);
+	}
+	function uuid() {
+	  var
+	  	d0 = Math.random()*0xffffffff|0,
+	  	d1 = Math.random()*0xffffffff|0,
+	  	d2 = Math.random()*0xffffffff|0,
+	  	d3 = Math.random()*0xffffffff|0;
+	  return lut[d0&0xff]+lut[d0>>8&0xff]+lut[d0>>16&0xff]+lut[d0>>24&0xff]+'-'+
+		lut[d1&0xff]+lut[d1>>8&0xff]+'-'+lut[d1>>16&0x0f|0x40]+lut[d1>>24&0xff]+'-'+
+		lut[d2&0x3f|0x80]+lut[d2>>8&0xff]+'-'+lut[d2>>16&0xff]+lut[d2>>24&0xff]+
+		lut[d3&0xff]+lut[d3>>8&0xff]+lut[d3>>16&0xff]+lut[d3>>24&0xff];
+	}
+	return uuid();
+};
+
+/*
 Adds a new password prompt. Options are:
 submitText: text to use for submit button (defaults to "Login")
 serviceName: text of the human readable service name
