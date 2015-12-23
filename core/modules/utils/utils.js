57ceffd67c72e54ea520109b58d5863db95d8b98
Jermolene <jeremy@jermolene.com>
2015-12-23 12:19:47 +0000

    Merge #1909 from @nameanyone

--- core/modules/utils/utils.js
+++ core/modules/utils/utils.js
@@ -643,4 +643,15 @@ exports.tagToCssSelector = function(tagName) {
 };
 
 
+/*
+IE does not have sign function
+*/
+exports.sign = Math.sign || function(x) {
+	x = +x; // convert to a number
+	if (x === 0 || isNaN(x)) {
+		return x;
+	}
+	return x > 0 ? 1 : -1;
+};
+
 })();
