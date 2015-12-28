659468597c61192fa518e2334d12f2b8a4f60456
William Jackson <matabele.bill@gmail.com>
2015-12-28 08:30:32 +0200

    Change attribute name and tidy the code

--- core/modules/widgets/checklist.js
+++ core/modules/widgets/checklist.js
@@ -3,29 +3,22 @@ title: $:/core/modules/widgets/checklist.js
 type: application/javascript
 module-type: widget
 
-checklist widget
+The Checklist widget toggles (or exchanges) the specified item in the specified list
 
 \*/
 (function () {
-
-    /*jslint node: true, browser: true */
-    /*global $tw: false */
     "use strict";
-
+    //Initialise the widget
     var Widget = require("$:/core/modules/widgets/widget.js").widget;
     var ChecklistWidget = function (parseTreeNode, options) {
         this.initialise(parseTreeNode, options);
     };
     ChecklistWidget.prototype = new Widget();
-
-    /*
-    Render this widget into the DOM
-    */
+    //Set the widget properties
     ChecklistWidget.prototype.render = function (parent, nextSibling) {
         this.parentDomNode = parent;
         this.computeAttributes();
         this.execute();
-        // Create our elements
         this.labelDomNode = this.document.createElement("label");
         this.labelDomNode.setAttribute("class", this.checkboxClass);
         this.inputDomNode = this.document.createElement("input");
@@ -36,50 +29,35 @@ checklist widget
         this.labelDomNode.appendChild(this.inputDomNode);
         this.spanDomNode = this.document.createElement("span");
         this.labelDomNode.appendChild(this.spanDomNode);
-        // Add a click event handler
         $tw.utils.addEventListeners(this.inputDomNode, [{
             name: "change",
             handlerObject: this,
             handlerMethod: "handleChangeEvent"
         }]);
-        // Insert the label into the DOM and render any children
         parent.insertBefore(this.labelDomNode, nextSibling);
         this.renderChildren(this.spanDomNode, null);
         this.domNodes.push(this.labelDomNode);
     };
-
-    //Replacement for hasTag() to handle any field
+    //Repalcement for hasTag, for any field
     ChecklistWidget.prototype.hasItem = function (item, itemList) {
         var list = $tw.utils.parseStringArray(itemList);
         return list && list.indexOf(item) !== -1;
     };
-
-    //Determines if the list contains the item
+    //Determine if checkbox state matches
     ChecklistWidget.prototype.getValue = function () {
         var tiddler = this.wiki.getTiddler(this.checklistTitle);
         if (tiddler && this.checklistItem) {
-            var HasItem = this.hasItem(this.checklistItem, tiddler.fields[this.checklistList]);
-            if (this.checklistInvert) {
-                return !HasItem;
-            } else {
-                return HasItem;
-            }
+            var HasItem = this.hasItem(this.checklistItem, tiddler.fields[this.checklistField]);
+            return (this.checklistInvert) ? !HasItem : HasItem;
         }
         return false;
     };
-
-    //Determines if checkbox states match
     ChecklistWidget.prototype.tagCheck = function (tiddler) {
-        var hasTag = tiddler && this.hasItem(this.checklistItem, tiddler.fields[this.checklistList]),
+        var hasTag = tiddler && this.hasItem(this.checklistItem, tiddler.fields[this.checklistField]),
             checked = this.inputDomNode.checked;
-        if (this.checklistItem && this.checklistInvert) {
-            return hasTag === checked;
-        } else if (this.checklistItem) {
-            return hasTag !== checked;
-        }
-        return false;
+        return (this.checklistItem) ? (this.checklistInvert) ? (hasTag === checked) : (hasTag !== checked) : false;
     };
-
+    //Execute on change
     ChecklistWidget.prototype.handleChangeEvent = function (event) {
         var tiddler = this.wiki.getTiddler(this.checklistTitle),
             fallbackFields = {
@@ -89,9 +67,8 @@ checklist widget
                 title: this.checklistTitle
             },
             hasChanged = false;
-        // Set the tag if specified
         if (this.checklistItem && (!tiddler || this.tagCheck(tiddler))) {
-            var array = tiddler ? $tw.utils.parseStringArray(tiddler.fields[this.checklistList] || []).slice(0) : [],
+            var array = tiddler ? $tw.utils.parseStringArray(tiddler.fields[this.checklistField] || []).slice(0) : [],
                 pos = array.indexOf(this.checklistItem),
                 altpos = array.indexOf(this.checklistAlt),
                 checked = this.inputDomNode.checked;
@@ -105,7 +82,7 @@ checklist widget
                     array.push(this.checklistItem);
                 }
             } else {
-                if (pos !== -1) {
+                   if (pos !== -1) {
                     array[pos] = this.checklistAlt;
                 } else if (altpos !== -1) {
                     array[altpos] = this.checklistItem;
@@ -113,40 +90,27 @@ checklist widget
                     array.push(this.checklistItem);
                 }
             }
-            newFields[this.checklistList] = $tw.utils.stringifyList(array);
+            newFields[this.checklistField] = $tw.utils.stringifyList(array);
             this.wiki.addTiddler(new $tw.Tiddler(tiddler, newFields));
-            //hasChanged = true;
         }
         if (hasChanged) {
             this.wiki.addTiddler(new $tw.Tiddler(this.wiki.getCreationFields(), fallbackFields, tiddler, newFields, this.wiki.getModificationFields()));
         }
     };
-
-    /*
-    Compute the internal state of the widget
-    */
+    //Fetch the widget attributes
     ChecklistWidget.prototype.execute = function () {
-        // Get the parameters from the attributes
         this.checklistTitle = this.getAttribute("tiddler", this.getVariable("currentTiddler"));
         this.checklistItem = this.getAttribute("item");
         this.checklistAlt = this.getAttribute("alt", "");
         this.checklistClass = this.getAttribute("class", "");
         this.checklistInvert = this.getAttribute("invert", "");
-        if (this.hasAttribute("list")) {
-            this.checklistList = this.getAttribute("list");
-        } else {
-            this.checklistList = this.getAttribute("field", "tags");
-        }
-        // Make the child widgets
+        this.checklistField = this.getAttribute("field", "tags");
         this.makeChildWidgets();
     };
-
-    /*
-    Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
-    */
+    //Refresh the widget whenever attributes change
     ChecklistWidget.prototype.refresh = function (changedTiddlers) {
         var changedAttributes = this.computeAttributes();
-        if (changedAttributes.tiddler || changedAttributes.item || changedAttributes.alt || changedAttributes.invert || changedAttributes.list || changedAttributes["class"]) {
+        if (changedAttributes.tiddler || changedAttributes.item || changedAttributes.alt || changedAttributes.invert || changedAttributes.field || changedAttributes["class"]) {
             this.refreshSelf();
             return true;
         } else {
@@ -158,7 +122,5 @@ checklist widget
             return this.refreshChildren(changedTiddlers) || refreshed;
         }
     };
-
     exports.checklist = ChecklistWidget;
-
 })();
