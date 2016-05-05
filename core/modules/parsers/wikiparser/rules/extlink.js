3a3754aebb73a55d0be56e14628ec6317aa6ad50
Jermolene <jeremy@jermolene.com>
2016-05-05 11:49:40 +0100

    Add rel="noopener noreferrer" to external links
    Background:
    https://medium.com/@jitbit/target-blank-the-most-underestimated-vulnerab
    ility-ever-96e328301f4c#.hduwdbjlb

--- core/modules/parsers/wikiparser/rules/extlink.js
+++ core/modules/parsers/wikiparser/rules/extlink.js
@@ -42,7 +42,8 @@ exports.parse = function() {
 			attributes: {
 				href: {type: "string", value: this.match[0]},
 				"class": {type: "string", value: "tc-tiddlylink-external"},
-				target: {type: "string", value: "_blank"}
+				target: {type: "string", value: "_blank"},
+				rel: {type: "string", value: "noopener noreferrer"}
 			},
 			children: [{
 				type: "text", text: this.match[0]
