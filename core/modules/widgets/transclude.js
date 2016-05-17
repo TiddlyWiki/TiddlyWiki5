6388f859d166c0c8932896a687918c7722efd1a8
Myeongjin <aranet100@gmail.com>
2016-05-18 05:58:47 +0900

    Add localisable strings for error (#2447)
    * Add localisable strings for error
    
    * Update tiddler Transclusion Basic Usage in fr-FR

--- core/modules/widgets/transclude.js
+++ core/modules/widgets/transclude.js
@@ -68,7 +68,7 @@ TranscludeWidget.prototype.execute = function() {
 			parseTreeNodes = [{type: "element", tag: "span", attributes: {
 				"class": {type: "string", value: "tc-error"}
 			}, children: [
-				{type: "text", text: $tw.language.getString("RecursiveTransclusion")}
+				{type: "text", text: $tw.language.getString("Error/RecursiveTransclusion")}
 			]}];
 		}
 	}
