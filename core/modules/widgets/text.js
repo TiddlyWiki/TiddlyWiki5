bab414aff1d3a352362a09067fd8beefe533e94e
Daniel Rodríguez Rivero <rdanielo@gmail.com>
2016-08-31 01:43:26 +0200

    Update text widget to allow length definition
    Text widget is very simple, and handy. It would be even more useful if you can decide the length of the text displayed.
    A positive number means the number of characters to show from the beginning
    A negative number means the number of characters to remove from the end.

--- core/modules/widgets/text.js
+++ core/modules/widgets/text.js
@@ -31,7 +31,9 @@ TextNodeWidget.prototype.render = function(parent,nextSibling) {
 	this.computeAttributes();
 	this.execute();
 	var text = this.getAttribute("text",this.parseTreeNode.text || "");
+	var length = this.getAttribute("length");
 	text = text.replace(/\r/mg,"");
+	text = text.slice(0,length);
 	var textNode = this.document.createTextNode(text);
 	parent.insertBefore(textNode,nextSibling);
 	this.domNodes.push(textNode);
