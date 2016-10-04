c1fe43a301ad0f945a1636e6b86cbb5f24f572bc
Tobias Beer <beertobias@gmail.com>
2016-10-04 22:02:09 +0200

    allows textPrimitives.anyLetter and backslash in syslink

--- core/modules/parsers/wikiparser/rules/syslink.js
+++ core/modules/parsers/wikiparser/rules/syslink.js
@@ -18,7 +18,12 @@ exports.types = {inline: true};
 exports.init = function(parser) {
 	this.parser = parser;
 	// Regexp to match
-	this.matchRegExp = /~?\$:\/[a-zA-Z0-9/.\-_]+/mg;
+	this.matchRegExp = new RegExp(
+		"~?\\$:\\/(?:" +
+			$tw.config.textPrimitives.anyLetter +
+			"|[\\\\/._-])+",
+		"mg"
+	);
 };
 
 exports.parse = function() {
