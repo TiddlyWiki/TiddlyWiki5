f97c1226aac9f3a5b563fc8d95bfeff91b8ab810
Jermolene <jeremy@jermolene.com>
2016-10-12 13:17:53 +0100

    Prevent HTML parser from mis-recognising email addresses
    Email addresses such as `<jeremy@example.com>` were being erroneously
    parsed as HTML tags.
    
    Fixes #2604

--- core/modules/parsers/wikiparser/rules/html.js
+++ core/modules/parsers/wikiparser/rules/html.js
@@ -101,6 +101,11 @@ exports.parseTag = function(source,pos,options) {
 		node.type = node.tag.substr(1);
 	}
 	pos = token.end;
+	// Check that the tag is terminated by a space, / or >
+	if(!$tw.utils.parseWhiteSpace(source,pos) && !(source.charAt(pos) === "/") && !(source.charAt(pos) === ">") ) {
+console.log("Aborting illegal element tag")
+		return null;
+	}
 	// Process attributes
 	var attribute = $tw.utils.parseAttribute(source,pos);
 	while(attribute) {
