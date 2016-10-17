TiddlyWiki/TiddlyWiki5

commit c7b31b02422265e11b68992e58ef5638cf2e568e

allows textPrimitives.anyLetter in syslink (#2596)
* allows textPrimitives.anyLetter in syslink

now only supports anyletter as per request by @jermolene

core/modules/parsers/wikiparser/rules/syslink.js
@@ -18,7 +18,12 @@ exports.types = {inline: true};
 exports.init = function(parser) {
 	this.parser = parser;
 	// Regexp to match
-	this.matchRegExp = /~?\$:\/[a-zA-Z0-9/.\-_]+/mg;
+	this.matchRegExp = new RegExp(
+		"~?\\$:\\/(?:" +
+			$tw.config.textPrimitives.anyLetter +
+			"|[\/._-])+",
+		"mg"
+	);
 };
 
 exports.parse = function() {

commit accd4a1b65e40cd307d25f3495371126869b583b

Fix regexp performance problem introduced in c7b31b02422265e11b68992e58ef5638cf2e568e
This fixes a problem introduced in
c7b31b02422265e11b68992e58ef5638cf2e568e.

The changes by @tobibeer inadvertently made the regular expression
evaluation significantly more expensive because of lookahead. The is
less elegant but reverts the performance problem.

core/modules/parsers/wikiparser/rules/syslink.js
@@ -19,9 +19,9 @@ exports.init = function(parser) {
 	this.parser = parser;
 	// Regexp to match
 	this.matchRegExp = new RegExp(
-		"~?\\$:\\/(?:" +
-			$tw.config.textPrimitives.anyLetter +
-			"|[\/._-])+",
+		"~?\\$:\\/[" +
+		$tw.config.textPrimitives.anyLetter.substr(1,$tw.config.textPrimitives.anyLetter.length - 2) +
+		"\/._-]+",
 		"mg"
 	);
 };