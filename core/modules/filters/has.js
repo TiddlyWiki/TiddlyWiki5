7781d7c817705dc314c1575e89eb81e94f2930ac
Tobias Beer <beertobias@gmail.com>
2016-10-22 11:39:35 +0200

    fixed inverted condition

--- core/modules/filters/has.js
+++ core/modules/filters/has.js
@@ -36,7 +36,7 @@ exports.has = function(source,operator,options) {
 	} else {
 		if(invert) {
 			source(function(tiddler,title) {
-				if(!tiddler || !$tw.utils.hop(tiddler.fields,operator.operand) && !(tiddler.fields[operator.operand] === "" || tiddler.fields[operator.operand].length === 0)) {
+				if(!tiddler || !$tw.utils.hop(tiddler.fields,operator.operand) || (tiddler.fields[operator.operand] !== "")) {
 					results.push(title);
 				}
 			});
