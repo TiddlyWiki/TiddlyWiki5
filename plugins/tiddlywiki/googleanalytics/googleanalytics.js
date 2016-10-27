364a7c79bcf3162dae8feb7cf49a85771a0146d7
Sylvain Comte <sylvain.comte@yahoo.fr>
2016-10-27 11:36:36 +0200

    trying to implement new googleanalytics tracker

--- plugins/tiddlywiki/googleanalytics/googleanalytics.js
+++ plugins/tiddlywiki/googleanalytics/googleanalytics.js
@@ -21,15 +21,13 @@ var GOOGLE_ANALYTICS_ACCOUNT = "$:/GoogleAnalyticsAccount",
 	GOOGLE_ANALYTICS_DOMAIN = "$:/GoogleAnalyticsDomain";
 
 exports.startup = function() {
-	window._gaq = window._gaq || [];
-	_gaq.push(["_setAccount", $tw.wiki.getTiddlerText(GOOGLE_ANALYTICS_ACCOUNT)]);
-	_gaq.push(["_setDomainName", $tw.wiki.getTiddlerText(GOOGLE_ANALYTICS_DOMAIN)]);
-	_gaq.push(["_trackPageview"]);
-	var ga = document.createElement("script");
-	ga.type = "text/javascript";
-	ga.async = true;
-	ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
-	document.body.appendChild(ga);
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+   ga('create', $tw.wiki.getTiddlerText(GOOGLE_ANALYTICS_ACCOUNT),$tw.wiki.getTiddlerText(GOOGLE_ANALYTICS_DOMAIN));
+   ga('send', 'pageview');
 };
 
 })();
