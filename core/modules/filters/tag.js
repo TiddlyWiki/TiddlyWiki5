a9b54d6fce56055afa323df43e259c531d216506
Jeremy Ruston <jeremy@jermolene.com>
2016-11-28 10:40:55 +0000

    Revert "Add provisional version of new tagger manager"

--- core/modules/filters/tag.js
+++ core/modules/filters/tag.js
@@ -17,30 +17,19 @@ Export our filter function
 */
 exports.tag = function(source,operator,options) {
 	var results = [];
-	if((operator.suffix || "").toLowerCase() === "strict" && !operator.operand) {
-		// New semantics:
-		// Always return copy of input if operator.operand is missing
+	if(operator.prefix === "!") {
 		source(function(tiddler,title) {
-			results.push(title);
+			if(tiddler && !tiddler.hasTag(operator.operand)) {
+				results.push(title);
+			}
 		});
 	} else {
-		// Old semantics:
-		if(operator.prefix === "!") {
-			// Returns a copy of the input if operator.operand is missing
-			source(function(tiddler,title) {
-				if(tiddler && !tiddler.hasTag(operator.operand)) {
-					results.push(title);
-				}
-			});
-		} else {
-			// Returns empty results if operator.operand is missing
-			source(function(tiddler,title) {
-				if(tiddler && tiddler.hasTag(operator.operand)) {
-					results.push(title);
-				}
-			});
-			results = options.wiki.sortByList(results,operator.operand);
-		}		
+		source(function(tiddler,title) {
+			if(tiddler && tiddler.hasTag(operator.operand)) {
+				results.push(title);
+			}
+		});
+		results = options.wiki.sortByList(results,operator.operand);
 	}
 	return results;
 };
