8f1114960a20ba3d84dd1633ad1976db9461a41c
Jermolene <jeremy@jermolene.com>
2017-03-23 14:23:33 +0000

    Refactor draggable stuff for easier reuse
    And in the process, make the button widget draggable.
    
    Unfortunately, Firefox has a bug that prevents buttons from being
    dragged (see https://bugzilla.mozilla.org/show_bug.cgi?id=568313 and
    https://bugzilla.mozilla.org/show_bug.cgi?id=646823). So we have to use
    the “tag” attribute to make it use a different element.

--- core/modules/widgets/draggable.js
+++ core/modules/widgets/draggable.js
@@ -47,87 +47,17 @@ DraggableWidget.prototype.render = function(parent,nextSibling) {
 		classes.push(this.draggableClasses);
 	}
 	domNode.setAttribute("class",classes.join(" "));
-	domNode.setAttribute("draggable","true");
 	// Add event handlers
-	$tw.utils.addEventListeners(domNode,[
-		{name: "dragstart", handlerObject: this, handlerMethod: "handleDragStartEvent"},
-		{name: "dragend", handlerObject: this, handlerMethod: "handleDragEndEvent"}
-	]);
+	$tw.utils.makeDraggable({
+		domNode: domNode,
+		dragTiddlerFn: function() {return self.getAttribute("tiddler");},
+		dragFilterFn: function() {return self.getAttribute("filter");},
+		widget: this
+	});
 	// Insert the link into the DOM and render any children
 	parent.insertBefore(domNode,nextSibling);
 	this.renderChildren(domNode,null);
 	this.domNodes.push(domNode);
-};
-
-DraggableWidget.prototype.handleDragStartEvent = function(event) {
-	var self = this;
-	// Collect the tiddlers
-	var titles = [];
-	if(this.draggableTiddler) {
-		titles.push(this.draggableTiddler);
-	}
-	if(this.draggableFilter) {
-		titles.push.apply(titles,this.wiki.filterTiddlers(this.draggableFilter,this));
-	}
-	var titleString = $tw.utils.stringifyList(titles);
-	if(titles.length > 0 && event.target === this.domNodes[0]) {
-		$tw.dragInProgress = this.domNodes[0];
-		// Set the dragging class on the element being dragged
-		$tw.utils.addClass(event.target,"tc-dragging");
-		// Create the drag image elements
-		this.dragImage = this.document.createElement("div");
-		this.dragImage.className = "tc-tiddler-dragger";
-		var inner = this.document.createElement("div");
-		inner.className = "tc-tiddler-dragger-inner";
-		inner.appendChild(this.document.createTextNode(
-			titles.length === 1 ? 
-				titles[0] :
-				titles.length + " tiddlers"
-		));
-		this.dragImage.appendChild(inner);
-		this.document.body.appendChild(this.dragImage);
-		// Set the data transfer properties
-		var dataTransfer = event.dataTransfer;
-		// Set up the image
-		dataTransfer.effectAllowed = "copy";
-		if(dataTransfer.setDragImage) {
-			dataTransfer.setDragImage(this.dragImage.firstChild,-16,-16);
-		}
-		// Set up the data transfer
-		dataTransfer.clearData();
-		var jsonData = [];
-		if(titles.length > 1) {
-			titles.forEach(function(title) {
-				jsonData.push(self.wiki.getTiddlerAsJson(title));
-			});
-			jsonData = "[" + jsonData.join(",") + "]";
-		} else {
-			jsonData = this.wiki.getTiddlerAsJson(titles[0]);
-		}
-		// IE doesn't like these content types
-		if(!$tw.browser.isIE) {
-			dataTransfer.setData("text/vnd.tiddler",jsonData);
-			dataTransfer.setData("text/plain",titleString);
-			dataTransfer.setData("text/x-moz-url","data:text/vnd.tiddler," + encodeURIComponent(jsonData));
-		}
-		dataTransfer.setData("URL","data:text/vnd.tiddler," + encodeURIComponent(jsonData));
-		dataTransfer.setData("Text",titleString);
-		event.stopPropagation();
-	} else {
-		event.preventDefault();
-	}
-};
-
-DraggableWidget.prototype.handleDragEndEvent = function(event) {
-	if(event.target === this.domNodes[0]) {
-		$tw.dragInProgress = null;
-		// Remove the dragging class on the element being dragged
-		$tw.utils.removeClass(event.target,"tc-dragging");
-		// Delete the drag image element
-		if(this.dragImage) {
-			this.dragImage.parentNode.removeChild(this.dragImage);
-		}
-	}
 };
 
 /*
@@ -135,8 +65,6 @@ Compute the internal state of the widget
 */
 DraggableWidget.prototype.execute = function() {
 	// Pick up our attributes
-	this.draggableTiddler = this.getAttribute("tiddler");
-	this.draggableFilter = this.getAttribute("filter");
 	this.draggableTag = this.getAttribute("tag","div");
 	this.draggableClasses = this.getAttribute("class");
 	// Make the child widgets
@@ -148,7 +76,7 @@ Selectively refreshes the widget if needed. Returns true if the widget or any of
 */
 DraggableWidget.prototype.refresh = function(changedTiddlers) {
 	var changedAttributes = this.computeAttributes();
-	if(changedAttributes.tiddler || changedTiddlers.tag || changedTiddlers["class"]) {
+	if(changedTiddlers.tag || changedTiddlers["class"]) {
 		this.refreshSelf();
 		return true;
 	}
