d89010752a8b169a4917bde9a9028ddd871e743d
saqimtiaz <saq.imtiaz@gmail.com>
2017-06-14 19:16:13 +0200

    Modified handleClickEvent to send original event data with tm-navigate event. Fixes 2889 (#2900)

--- core/modules/widgets/link.js
+++ core/modules/widgets/link.js
@@ -135,7 +135,11 @@ LinkWidget.prototype.handleClickEvent = function(event) {
 		navigateFromNode: this,
 		navigateFromClientRect: { top: bounds.top, left: bounds.left, width: bounds.width, right: bounds.right, bottom: bounds.bottom, height: bounds.height
 		},
-		navigateSuppressNavigation: event.metaKey || event.ctrlKey || (event.button === 1)
+		navigateSuppressNavigation: event.metaKey || event.ctrlKey || (event.button === 1),
+		metaKey: event.metaKey,
+		ctrlKey: event.ctrlKey,
+		altKey: event.altKey,
+		shiftKey: event.shiftKey
 	});
 	if(this.domNodes[0].hasAttribute("href")) {
 		event.preventDefault();
