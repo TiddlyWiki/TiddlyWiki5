33461a584801774ad2e19c305a94fe6ade35edf4
Mark <throaway@yahoo.com>
2017-08-30 09:03:49 -0700

    New filter 'equals' for string text comparison Documentation can be added to branch if it appears this will be approved

---
+++ core/modules/filters/equals.js
@@ -0,0 +1,63 @@
+/*\
+title: $:/core/modules/filters/equals.js
+type: application/javascript
+module-type: filteroperator
+
+Filter operator for comparing text for equality
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Export our filter function
+*/
+exports.equals = function(source,operator,options) {
+	var results = [],
+		fieldname = (operator.suffix || operator.operator || "title").toLowerCase();
+	if(operator.prefix === "!") {
+		if(operator.regexp) {
+			source(function(tiddler,title) {
+
+				var text = title ;
+					if(text !== null && !operator.regexp.exec(text)) {
+						results.push(title);
+					}
+			});
+		} else {
+			source(function(tiddler,title) {
+
+				var text = title ;
+					if(text !== null && text !== operator.operand) {
+						results.push(title);
+					}
+			});
+		}
+	} else {
+		if(operator.regexp) {
+			source(function(tiddler,title) {
+				
+				var text = title ;
+				if(text !== null && !!operator.regexp.exec(text)) {
+					results.push(title);
+				}
+				
+			});
+		} else {
+			source(function(tiddler,title) {
+				
+				var text = title ;
+				if(text !== null && text === operator.operand) {
+					results.push(title);
+				}
+				
+			});
+		}
+	}
+	return results;
+};
+
+})();
