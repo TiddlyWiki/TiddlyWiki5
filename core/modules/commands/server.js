0608544e0283b9aa8508c83b48aaaca448eca464
Arlen22 <arlenbee@gmail.com>
2017-11-30 18:40:24 +0800

    Invoke hook when server starts
    Invokes the `th-server-command-start` hook when the server is started, with the server object as the parameter. This allows adding a WebSocket listener to the server.

--- core/modules/commands/server.js
+++ core/modules/commands/server.js
@@ -306,6 +306,7 @@ Command.prototype.execute = function() {
 	if(!$tw.wiki.getTiddler("$:/plugins/tiddlywiki/tiddlyweb") || !$tw.wiki.getTiddler("$:/plugins/tiddlywiki/filesystem")) {
 		$tw.utils.warning("Warning: Plugins required for client-server operation (\"tiddlywiki/filesystem\" and \"tiddlywiki/tiddlyweb\") are missing from tiddlywiki.info file");
 	}
+	$tw.hooks.invokeHook('th-server-command-start', this.server);
 	return null;
 };
 
