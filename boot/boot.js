57e1fc6cd8401f6fef1b020076fe95fdded75510
Jermolene <jeremy@jermolene.com>
2017-12-21 22:02:04 +0000

    Add file type for true type fonts

--- boot/boot.js
+++ boot/boot.js
@@ -1984,6 +1984,7 @@ $tw.boot.startup = function(options) {
 	$tw.utils.registerFileType("image/svg+xml","utf8",".svg",{flags:["image"]});
 	$tw.utils.registerFileType("image/x-icon","base64",".ico",{flags:["image"]});
 	$tw.utils.registerFileType("application/font-woff","base64",".woff");
+	$tw.utils.registerFileType("application/x-font-ttf","base64",".woff");
 	$tw.utils.registerFileType("audio/ogg","base64",".ogg");
 	$tw.utils.registerFileType("video/mp4","base64",".mp4");
 	$tw.utils.registerFileType("audio/mp3","base64",".mp3");
