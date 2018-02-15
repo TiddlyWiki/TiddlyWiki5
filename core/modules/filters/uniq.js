025ae55c0dbecd4f0d8e0f26ccb77bff77758dcf
Stephan Hradek <Stephan.Hradek@ebuconnect.de>
2018-02-15 12:14:34 +0100

    Fixed tabs to conform to the conventions. Changed wording a bit.

--- core/modules/filters/uniq.js
+++ core/modules/filters/uniq.js
@@ -19,25 +19,25 @@ exports.uniq = function(source,operator,options) {
 	var results =[] ,
 	value,values = {},
 	field = operator.operand || "title";
-  if(field === "title") {
-    source(function(tiddler,title) {
-      if(!$tw.utils.hop(values,title)) {
-        values[value] = true;
-        results.push(title);
-      }
-    });
-  }
-  else {
-  	source(function(tiddler,title) {
-      if(tiddler) {
-  			value = tiddler.getFieldString(field);
-  			if(!$tw.utils.hop(values,value)) {
-  				values[value] = true;
-  				results.push(title);
-  			}
-  		}
-  	});
-  }
+	if(field === "title") {
+		source(function(tiddler,title) {
+			if(!$tw.utils.hop(values,title)) {
+				values[value] = true;
+				results.push(title);
+			}
+		});
+	}
+	else {
+		source(function(tiddler,title) {
+			if(tiddler) {
+				value = tiddler.getFieldString(field);
+				if(!$tw.utils.hop(values,value)) {
+					values[value] = true;
+					results.push(title);
+				}
+			}
+		});
+	}
 	return results;
 };
 
