f9f4c63bf5df8d7a29e3409692e0ab5208f6220a
Ibnishak <ibnishak@live.com>
2018-02-23 07:02:02 +0530

    Removing Case sensitive option, Removing default value for isAlphanumeric parameter, updating formatting

--- core/modules/filters/sort.js
+++ core/modules/filters/sort.js
@@ -6,57 +6,54 @@ module-type: filteroperator
 Filter operator for sorting
 
 \*/
-(function() {
+(function(){
 
-	/*jslint node: true, browser: true */
-	/*global $tw: false */
-	"use strict";
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
 
-	/*
-	Export our filter function
-	*/
-	exports.sort = function(source, operator, options) {
-		var results = prepare_results(source);
-		options.wiki.sortTiddlers(results, operator.operand || "title", operator.prefix === "!", false, false);
-		return results;
-	};
+/*
+Export our filter function
+*/
+exports.sort = function(source,operator,options) {
+	var results = prepare_results(source);
+	options.wiki.sortTiddlers(results,operator.operand || "title",operator.prefix === "!",false,false);
+	return results;
+};
 
-	exports.nsort = function(source, operator, options) {
-		var results = prepare_results(source);
-		options.wiki.sortTiddlers(results, operator.operand || "title", operator.prefix === "!", false, true);
-		return results;
-	};
+exports.nsort = function(source,operator,options) {
+	var results = prepare_results(source);
+	options.wiki.sortTiddlers(results,operator.operand || "title",operator.prefix === "!",false,true);
+	return results;
+};
 
-	exports.sortan = function(source, operator, options) {
-		var results = prepare_results(source);
-		options.wiki.sortTiddlers(results, operator.operand || "title", operator.prefix === "!", false, false, true);
-		return results;
-	};
+exports.sortan = function(source, operator, options) {
+	var results = prepare_results(source);
+	options.wiki.sortTiddlers(results, operator.operand || "title", operator.prefix === "!",false,false,true);
+	return results;
+};
 
-	exports.sortcs = function(source, operator, options) {
-		var results = prepare_results(source);
-		options.wiki.sortTiddlers(results, operator.operand || "title", operator.prefix === "!", true, false);
-		return results;
-	};
+exports.sortcs = function(source,operator,options) {
+	var results = prepare_results(source);
+	options.wiki.sortTiddlers(results,operator.operand || "title",operator.prefix === "!",true,false);
+	return results;
+};
 
-	exports.nsortcs = function(source, operator, options) {
-		var results = prepare_results(source);
-		options.wiki.sortTiddlers(results, operator.operand || "title", operator.prefix === "!", true, true);
-		return results;
-	};
+exports.nsortcs = function(source,operator,options) {
+	var results = prepare_results(source);
+	options.wiki.sortTiddlers(results,operator.operand || "title",operator.prefix === "!",true,true);
+	return results;
+};
 
-	exports.sortancs = function(source, operator, options) {
-		var results = prepare_results(source);
-		options.wiki.sortTiddlers(results, operator.operand || "title", operator.prefix === "!", true, false, true);
-		return results;
-	};
-
-	var prepare_results = function(source) {
-		var results = [];
-		source(function(tiddler, title) {
-			results.push(title);
-		});
-		return results;
-	};
+var prepare_results = function (source) {
+	var results = [];
+	source(function(tiddler,title) {
+		results.push(title);
+	});
+	return results;
+};
 
 })();
+
+
+
