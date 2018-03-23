1c6942402ee7b6d938f01bad23130ebe75b109f2
Jermolene <jeremy@jermolene.com>
2018-03-23 09:35:39 +0000

    Add support for startup actions

--- core/modules/startup/startup.js
+++ core/modules/startup/startup.js
@@ -87,18 +87,29 @@ exports.startup = function() {
 	});
 	// Kick off the keyboard manager
 	$tw.keyboardManager = new $tw.KeyboardManager();
+	// Create a root widget for attaching event handlers. By using it as the parentWidget for another widget tree, one can reuse the event handlers
+	$tw.rootWidget = new widget.widget({
+		type: "widget",
+		children: []
+	},{
+		wiki: $tw.wiki,
+		document: $tw.browser ? document : $tw.fakeDocument
+	});
+	// Execute any startup actions
+	var executeStartupTiddlers = function(tag) {
+		$tw.utils.each($tw.wiki.filterTiddlers("[all[shadows+tiddlers]tag[" + tag + "]!has[draft.of]]"),function(title) {
+			$tw.rootWidget.invokeActionString($tw.wiki.getTiddlerText(title),$tw.rootWidget);
+		});
+	};
+	executeStartupTiddlers("$:/tags/StartupAction");
+	if($tw.browser) {
+		executeStartupTiddlers("$:/tags/StartupAction/Browser");		
+	}
+	if($tw.node) {
+		executeStartupTiddlers("$:/tags/StartupAction/Node");		
+	}
 	// Clear outstanding tiddler store change events to avoid an unnecessary refresh cycle at startup
 	$tw.wiki.clearTiddlerEventQueue();
-	// Create a root widget for attaching event handlers. By using it as the parentWidget for another widget tree, one can reuse the event handlers
-	if($tw.browser) {
-		$tw.rootWidget = new widget.widget({
-			type: "widget",
-			children: []
-		},{
-			wiki: $tw.wiki,
-			document: document
-		});
-	}
 	// Find a working syncadaptor
 	$tw.syncadaptor = undefined;
 	$tw.modules.forEachModuleOfType("syncadaptor",function(title,module) {
