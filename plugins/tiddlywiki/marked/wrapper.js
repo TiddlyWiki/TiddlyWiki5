3bfd9cb6939e0b73013c98a191a62d9a22bb1940
Sebastian Silva <sebastian@fuentelibre.org>
2018-05-01 02:36:04 -0500

    New plugin with alternative Markdown parser.

---
+++ plugins/tiddlywiki/marked/wrapper.js
@@ -0,0 +1,47 @@
+/*\
+title: $:/plugins/tiddlywiki/markdown/wrapper.js
+type: application/javascript
+module-type: parser
+
+Wraps up the marked parser for use in TiddlyWiki5
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var marked = require("$:/plugins/tiddlywiki/markdown/marked.js");
+
+
+function transformNodes(nodes) {
+	var results = [];
+	for(var index=0; index<nodes.length; index++) {
+		var node = [nodes[index]];
+		node.links = nodes.links;
+		var widget = {type: "raw", html: marked.parser(node)};
+		results.push(widget);
+	}
+	return results;
+}
+
+var MarkdownParser = function(type,text,options) {
+	var markdownTree = marked.lexer(text);
+	this.tree = transformNodes(markdownTree);
+};
+
+/*
+
+[ 'html',
+  [ 'p', 'something' ],
+  [ 'h1',
+    'heading and ',
+    [ 'strong', 'other' ] ] ]
+
+*/
+
+exports["text/x-markdown"] = MarkdownParser;
+
+})();
+
