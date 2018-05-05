8add5dbaa0d2e477e05af5c1b453a04e88426c92
Matt Lauber <github@mklauber.com>
2018-05-05 07:08:41 -0400

    Update is.js
    * extracted `subops.length` to `num_of_subops`
    * renamed `subop` to `operator` for clarity/differentiation from `subops`
    * refactored to avoid using a `Set` object.

--- core/modules/filters/is.js
+++ core/modules/filters/is.js
@@ -28,37 +28,37 @@ Export our filter function
 exports.is = function(source,operator,options) {
 
 	var isFilterOperators = getIsFilterOperators(),
-		subops = operator.operand.split("+");
+		subops = operator.operand.split("+"),
+		num_of_subops = subops.length;
 
 	//Make sure all the operands are defined.
-	for (var t = 0; t < subops.length; t++){
+	for (var t = 0; t < num_of_subops; t++){
 		if( !isFilterOperators[subops[t]] ) {
 			return [$tw.language.getString("Error/IsFilterOperator")];
 		}
 	}
 
-
-	if(subops.length === 0) { 	// Return all tiddlers if the operand is missing
+	if(num_of_subops === 0) { 	// Return all tiddlers if the operand is missing
 		var results = [];
 		source(function(tiddler,title) {
 			results.push(title);
 		});
 		return results;
-	} else if(subops.length === 1) {	// Shortcut the Single Operator
-		var subop = isFilterOperators[subops[0]];
-		return subop(source,operator.prefix,options);
+	} else if(num_of_subops === 1) {	// Shortcut the Single Operator
+		var operator = isFilterOperators[subops[0]];
+		return operator(source,operator.prefix,options);
 
 	} else {	// Handle multiple operators
-		var filtered_results = new Set(),
+		var filtered_results = {},
 			results = [];
-		for(var t=0; t < subops.length; t++){
-			var subop = isFilterOperators[subops[t]];
-			subop(source,operator.prefix,options).forEach(function(element) { filtered_results.add(element)});
+		for(var t=0; t < num_of_subops; t++){
+			var operator = isFilterOperators[subops[t]];
+			operator(source,operator.prefix,options).forEach(function(element) { filtered_results[element] = True});
 		}
 
 		// Sort the output by the input (There may be a better way to do this)
 		source(function(tiddler,title) {
-			if(filtered_results.has(title)) {
+			if(filtered_results[title] === True) {
 				results.push(title);
 			}
 		});
