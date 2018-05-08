TiddlyWiki/TiddlyWiki5

commit f9be417204695d26d5f458fbc53870b2adb9a372

Minor tweak for RetrieveETag (#3257)
Need to check if the server responses a null value for ETag.
(In the case testing with build-in webdav server of CarotDAV on Windows 10)

core/modules/savers/put.js
@@ -25,8 +25,9 @@ var RetrieveETag = function(self) {
 		type: "HEAD",
 		headers: headers,
 		callback: function(err, data, xhr) {
-			if(!err) {
-				self.etag = xhr.getResponseHeader("ETag").replace(/^W\//,"");
+			var etag = xhr.getResponseHeader("ETag");
+			if(!err && etag) {
+				self.etag = etag.replace(/^W\//,"");
 			}
 		}
 	});

commit 5cf600726bed3786f3e64d2e1fa9899773a8ee0e

Fix undefined xhr bug
Closes #3261

Bug introduced in commit f9be41720 ("Minor tweak for RetrieveETag
(#3257)", 2018-05-05)

core/modules/savers/put.js
@@ -25,10 +25,10 @@ var RetrieveETag = function(self) {
 		type: "HEAD",
 		headers: headers,
 		callback: function(err, data, xhr) {
+			if(err) return;
 			var etag = xhr.getResponseHeader("ETag");
-			if(!err && etag) {
-				self.etag = etag.replace(/^W\//,"");
-			}
+			if(!etag) return;
+			self.etag = etag.replace(/^W\//,"");
 		}
 	});
 };