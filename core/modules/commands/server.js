dd6575dd8a39f23868f7778c61ba22dea1af33a3
Arlen22 <arlenbee@gmail.com>
2018-05-12 18:29:13 -0400

    Add name field to server start hook
    Server command identifies itself as `tiddlywiki`.

--- core/modules/commands/server.js
+++ core/modules/commands/server.js
@@ -309,7 +309,7 @@ Command.prototype.execute = function() {
 	if(!$tw.wiki.getTiddler("$:/plugins/tiddlywiki/tiddlyweb") || !$tw.wiki.getTiddler("$:/plugins/tiddlywiki/filesystem")) {
 		$tw.utils.warning("Warning: Plugins required for client-server operation (\"tiddlywiki/filesystem\" and \"tiddlywiki/tiddlyweb\") are missing from tiddlywiki.info file");
 	}
-	$tw.hooks.invokeHook('th-server-command-post-start', this.server, nodeServer);
+	$tw.hooks.invokeHook('th-server-command-post-start', this.server, nodeServer, "tiddlywiki");
 	return null;
 };
 
