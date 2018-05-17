92ba986d16db86b9ef6478f065efba17477ad7b8
BurningTreeC <hypnotizedkangaroo@gmail.com>
2018-05-17 16:56:45 +0200

    refactor code

--- core/modules/startup/keyboard.js
+++ core/modules/startup/keyboard.js
@@ -24,17 +24,23 @@ exports.startup = function() {
 			shortcutKeysList = [],
 			shortcutActionList = [],
 			shortcutParsedList = [];
-		for (var i = 0; i < shortcutTiddlers.length; i++) {
-			var title = shortcutTiddlers[i];
-			var tiddlerFields = $tw.wiki.getTiddler(title).fields;
-			shortcutKeysList[i] = tiddlerFields["key"] !== undefined ? tiddlerFields["key"] : undefined;
-			shortcutActionList[i] = tiddlerFields["text"];
-			shortcutParsedList[i] = shortcutKeysList !== undefined ? $tw.keyboardManager.parseKeyDescriptors(shortcutKeysList[i]) : undefined;
-		}
+
+		function updateShortcutLists(tiddlerList) {
+			shortcutTiddlers = tiddlerList;
+			for (var i=0; i < tiddlerList.length; i++) {
+				var title = tiddlerList[i],
+					tiddlerFields = $tw.wiki.getTiddler(title).fields;
+				shortcutKeysList[i] = tiddlerFields["key"] !== undefined ? tiddlerFields["key"] : undefined;
+				shortcutActionList[i] = tiddlerFields["text"];
+				shortcutParsedList[i] = shortcutKeysList !== undefined ? $tw.keyboardManager.parseKeyDescriptors(shortcutKeysList[i]) : undefined;
+			}
+		};
+
+		updateShortcutLists(shortcutTiddlers);
 
 		document.addEventListener("keydown",function(event) {
 			var key, action;
-			for (i = 0; i < shortcutTiddlers.length; i++) {
+			for (var i = 0; i < shortcutTiddlers.length; i++) {
 				if (shortcutParsedList[i] !== undefined && $tw.keyboardManager.checkKeyDescriptors(event,shortcutParsedList[i])) {
 					key = shortcutParsedList[i];
 					action = shortcutActionList[i];
@@ -50,41 +56,27 @@ exports.startup = function() {
 		$tw.wiki.addEventListener("change",function(changes) {
 			var newList = $tw.wiki.filterTiddlers("[all[shadows+tiddlers]tag[$:/tags/KeyboardShortcut]!has[draft.of]]");
 			var hasChanged = false;
-			for (i = 0; i < shortcutTiddlers.length; i++) {
-				if($tw.utils.hop(changes,shortcutTiddlers[i])) {
-					hasChanged = true;
-					break;
-				}
-			}
-			if(!hasChanged) {
-				for (i = 0; i < newList.length; i++) {
-					if($tw.utils.hop(changes,newList[i])) {
-						hasChanged = true;
-						break;
+			
+			function changedTiddlers(tiddlerList) {
+				for (var i = 0; i < tiddlerList.length; i++) {
+					if($tw.utils.hop(changes,tiddlerList[i])) {
+						return true;
 					}
 				}
+				return false;
+			};
+
+			hasChanged = changedTiddlers(shortcutTiddlers);
+
+			if(!hasChanged) {
+				hasChanged = changedTiddlers(newList);
 			}
 			if(!hasChanged) {
 				var shortcutConfigTiddlers = $tw.wiki.filterTiddlers("[all[shadows+tiddlers]prefix[$:/config/shortcuts]]");
-				for(i = 0; i < shortcutConfigTiddlers.length; i++) {
-					if($tw.utils.hop(changes,shortcutConfigTiddlers[i])) {
-						hasChanged = true;
-						break;
-					}
-				}
+				hasChanged = changedTiddlers(shortcutConfigTiddlers);
 			}
 			if(hasChanged) {
-				shortcutTiddlers = newList;
-				shortcutKeysList = [];
-				shortcutActionList = [];
-				shortcutParsedList = [];
-				for (i = 0; i < shortcutTiddlers.length; i++) {
-					var title = shortcutTiddlers[i];
-					var tiddlerFields = $tw.wiki.getTiddler(title).fields;
-					shortcutKeysList[i] = tiddlerFields["key"] !== undefined ? tiddlerFields["key"] : undefined;
-					shortcutActionList[i] = tiddlerFields["text"];
-					shortcutParsedList[i] = $tw.keyboardManager.parseKeyDescriptors(shortcutKeysList[i]);
-				}
+				updateShortcutLists(newList);
 			}
 		});
 	}
