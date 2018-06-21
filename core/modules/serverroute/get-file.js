db000efc9e656dd779cbaa1a9c0e41b25068c522
Jermolene <jeremy@jermolene.com>
2018-06-21 12:34:01 +0100

    Move support for attachments from a plugin into the core

---
+++ core/modules/serverroute/get-file.js
@@ -0,0 +1,50 @@
+/*\
+title: $:/core/modules/serverroute/get-file.js
+type: application/javascript
+module-type: serverroute
+
+GET /files/:filepath
+
+\*/
+(function() {
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+exports.method = "GET";
+
+exports.path = /^\/files\/(.+)$/;
+
+exports.handler = function(request,response,state) {
+	var path = require("path"),
+		fs = require("fs"),
+		util = require("util");
+	var filename = path.join($tw.boot.wikiPath,"files",decodeURIComponent(state.params[0])),
+		extension = path.extname(filename);
+	fs.readFile(filename,function(err,content) {
+		var status,content,type = "text/plain";
+		if(err) {
+			if(err.code === "ENOENT") {
+				status = 404;
+				content = "File '" + filename + "' not found";
+			} if(err.code === "EACCES") {
+				status = 403;
+				content = "You do not have permission to access the file '" + filename + "'";
+			} else {
+				status = 500;
+				content = err.toString();
+			}
+		} else {
+			status = 200;
+			content = content;
+			type = $tw.config.fileExtensionInfo[extension] || "application/octet-stream";
+		}
+		response.writeHead(status,{
+			"Content-Type": type
+		});
+		response.end(content);
+	});
+};
+
+}());
