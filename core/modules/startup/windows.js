5fba11b947dcbc63779095ddee841d9b14dc041e
BurningTreeC <hypnotizedkangaroo@gmail.com>
2018-06-24 19:11:24 +0200

    Update windows.js

--- core/modules/startup/windows.js
+++ core/modules/startup/windows.js
@@ -68,6 +68,8 @@ exports.startup = function() {
 				}
 			}
 			if(key !== undefined) {
+				event.preventDefault();
+				event.stopPropagation();
 				$tw.rootWidget.invokeActionString(action,$tw.rootWidget);
 				return true;
 			}
