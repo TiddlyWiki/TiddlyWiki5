cc2f5e0d115a22ec400a557ca7a1c751debfece7
Jermolene <jeremy@jermolene.com>
2018-07-01 12:05:51 +0100

    Tweaks to the single tiddler static view
    Adding a simple sidebar

--- core/modules/server/routes/get-tiddler-html.js
+++ core/modules/server/routes/get-tiddler-html.js
@@ -20,16 +20,18 @@ exports.handler = function(request,response,state) {
 	var title = decodeURIComponent(state.params[0]),
 		tiddler = state.wiki.getTiddler(title);
 	if(tiddler) {
-		var serveType,template;
+		var outputType,serveType,template;
 		// Render ordinary tiddlers as HTML, and system tiddlers in plain text
 		if(state.wiki.isSystemTiddler(title)) {
+			outputType = "text/plain";
 			serveType = "text/plain";
 			template = "$:/core/templates/wikified-tiddler";
 		} else {
+			outputType = "text/html";
 			serveType = "text/html";
 			template = "$:/core/templates/server/static.tiddler.html";
 		}
-		var text = state.wiki.renderTiddler("text/plain",template,{variables: {currentTiddler: title}});
+		var text = state.wiki.renderTiddler(outputType,template,{variables: {currentTiddler: title}});
 		// Naughty not to set a content-type, but it's the easiest way to ensure the browser will see HTML pages as HTML, and accept plain text tiddlers as CSS or JS
 		response.writeHead(200);
 		response.end(text,"utf8");
