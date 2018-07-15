98758a52d586676f0ceee1dc0cc03c40a3e4b356
Daniel Rodríguez Rivero <rdanielo@gmail.com>
2018-07-15 13:20:51 +0200

    feat(utils): add set tags
    added a small helper function to override the tags of a tiddler

--- core/modules/utils/utils.js
+++ core/modules/utils/utils.js
@@ -799,5 +799,22 @@ exports.strEndsWith = function(str,ending,position) {
 		return lastIndex !== -1 && lastIndex === position;
 	}
 };
+	
+/**
+ * @function setTags
+ * @description Helper function to override the tags of a tiddler
+ * If the tiddler does not exist it will be created
+ * @param  {String} title Tiddler you want to override its' tags
+ * @param  {[String]} tags  List of tags that will be the new tiddler tags
+ * @return {Void} This function does not return anything
+ */
+exports.setTags = (title, tags) => {
+    $tw.wiki.addTiddler(
+        new $tw.Tiddler(
+            $tw.wiki.getTiddler(title),
+            {title: title, tags: tags}
+        )
+    )
+};	
 
 })();
