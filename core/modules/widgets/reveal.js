TiddlyWiki/TiddlyWiki5

commit a3a4c28143138ca2bde1b8426c910b5e3d6a7110

Fix bug with reveal widget not refreshing when state tiddler changes

core/modules/widgets/reveal.js
@@ -174,18 +174,16 @@ RevealWidget.prototype.refresh = function(changedTiddlers) {
 		this.refreshSelf();
 		return true;
 	} else {
-		var refreshed = false,
-			currentlyOpen = this.isOpen;
-		this.readState();
-		if(this.isOpen !== currentlyOpen) {
+		var currentlyOpen = this.isOpen;
+		if(this.isOpen !== currentlyOpen || (this.stateTitle && changedTiddlers[this.stateTitle])) {
 			if(this.retain === "yes") {
 				this.updateState();
 			} else {
 				this.refreshSelf();
-				refreshed = true;
+				return true;
 			}
 		}
-		return this.refreshChildren(changedTiddlers) || refreshed;
+		return this.refreshChildren(changedTiddlers);
 	}
 };
 

commit 2d231a2e07b40e270a969df79723c3b155513d1d

Fix typo in reveal widget
Introduced in a3a4c28143138ca2bde1b8426c910b5e3d6a7110

core/modules/widgets/reveal.js
@@ -175,6 +175,7 @@ RevealWidget.prototype.refresh = function(changedTiddlers) {
 		return true;
 	} else {
 		var currentlyOpen = this.isOpen;
+		this.readState();
 		if(this.isOpen !== currentlyOpen || (this.stateTitle && changedTiddlers[this.stateTitle])) {
 			if(this.retain === "yes") {
 				this.updateState();