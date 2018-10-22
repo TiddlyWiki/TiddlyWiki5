6bcc0d1fef9c28af56c899d626390fc8fab0756f
BurningTreeC <hypnotizedkangaroo@gmail.com>
2018-10-22 08:34:50 +0200

    move initializations to the constructor

--- core/modules/startup/keyboard.js
+++ core/modules/startup/keyboard.js
@@ -22,18 +22,8 @@ exports.startup = function() {
 	if($tw.browser) {
 		var man = $tw.keyboardManager;
 		// Get existing shortcut-tiddlers
-		man.shortcutTiddlers = man.getShortcutTiddlerList(),
-		man.shortcutKeysList = [], // Stores the shortcut-key descriptors
-		man.shortcutActionList = [], // Stores the corresponding action strings
-		man.shortcutParsedList = []; // Stores the parsed key descriptors
-		
-		man.lookupNames = [];
-		man.lookupNames.push("shortcuts");
-		$tw.platform.isMac ? man.lookupNames.push("shortcuts-mac") : man.lookupNames.push("shortcuts-not-mac");
-		$tw.platform.isWindows ? man.lookupNames.push("shortcuts-windows") : man.lookupNames.push("shortcuts-not-windows");
-		$tw.platform.isLinux ? man.lookupNames.push("shortcuts-linux") : man.lookupNames.push("shortcuts-not-linux");
-
-		// Cache shortcuts and corresponding actions at startup
+		man.shortcutTiddlers = man.getShortcutTiddlerList();
+		// Cache shortcuts and corresponding actions
 		man.updateShortcutLists(man.shortcutTiddlers);
 
 		// Keydown Listener for shortcuts
