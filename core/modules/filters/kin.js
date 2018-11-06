ccc4d8705a158365f0367bce6a26e870225fbb83
bimlas <bimbalaszlo@gmail.com>
2018-11-06 22:47:35 +0100

    Change indentation, make it more readable

--- core/modules/filters/kin.js
+++ core/modules/filters/kin.js
@@ -3,105 +3,105 @@ title: $:/core/modules/filters/kin.js
 type: application/javascript
 module-type: filteroperator
 
-Filter operator that recursively finds kindred between tiddlers
+Finds out where a tiddler originates from and what other tiddlers originate from it
 
 \*/
-(function () {
+(function() {
 
-  /*jslint node: true, browser: true */
-  /*global $tw: false */
-  "use strict";
+	/*jslint node: true, browser: true */
+	/*global $tw: false */
+	"use strict";
 
-  // TODO: Should I set global tw to true?
+	// TODO: Should I set global tw to true?
 
-  function collectFamilyMembers(tiddler, title, fieldname, direction) {
-    var family_members_from = [],
-      family_members_to = [];
+	function collectTitlesRecursively(baseTiddler,baseTitle,fieldName,direction) {
+		var titlesPointingFromBase = [],
+			titlesPointingToBase = [];
 
-    function addToResultsIfNotFoundAlready(list, title) {
-      if (list.includes(title)) {
-        return false;
-      }
-      list.push(title);
-      return true
-    }
+		function addToResultsIfNotFoundAlready(list,title) {
+			if(list.includes(title)) {
+				return false;
+			}
+			list.push(title);
+			return true
+		}
 
-    function findKindredFrom(tiddler, title) {
-      if (addToResultsIfNotFoundAlready(family_members_from, title)) {
-        if (tiddler) {
-          tiddler.getFieldList(fieldname).forEach(function (target_title) {
-            findKindredFrom($tw.wiki.getTiddler(target_title), target_title);
-          });
-        }
-      }
-    }
+		function collectTitlesPointingFrom(tiddler,title) {
+			if(addToResultsIfNotFoundAlready(titlesPointingFromBase,title)) {
+				if(tiddler) {
+					tiddler.getFieldList(fieldName).forEach(function(targetTitle) {
+						collectTitlesPointingFrom($tw.wiki.getTiddler(targetTitle),targetTitle);
+					});
+				}
+			}
+		}
 
-    function findKindredTo(title) {
-      if (addToResultsIfNotFoundAlready(family_members_to, title)) {
-        $tw.wiki.findListingsOfTiddler(title, fieldname).forEach(function (target_title) {
-          findKindredTo(target_title);
-        });
-      }
-    }
+		function collectTitlesPointingTo(title) {
+			if(addToResultsIfNotFoundAlready(titlesPointingToBase,title)) {
+				$tw.wiki.findListingsOfTiddler(title,fieldName).forEach(function(targetTitle) {
+					collectTitlesPointingTo(targetTitle);
+				});
+			}
+		}
 
-    if ((direction === 'from') || (direction === 'with')) {
-      findKindredFrom(tiddler, title);
-    }
-    if ((direction === 'to') || (direction === 'with')) {
-      findKindredTo(title);
-    }
-    return uniqueArray(family_members_from.concat(family_members_to));
-  }
+		if((direction === "from") || (direction === "with")) {
+			collectTitlesPointingFrom(baseTiddler,baseTitle);
+		}
+		if((direction === "to") || (direction === "with")) {
+			collectTitlesPointingTo(baseTitle);
+		}
+		return uniqueArray(titlesPointingFromBase.concat(titlesPointingToBase));
+	}
 
-  // TODO: Is there a better way for unique?
-  function uniqueArray(input) {
-    var seen = {},
-      output = [],
-      len = input.length,
-      j = 0;
-    for (var i = 0; i < len; i++) {
-      var item = input[i];
-      if (seen[item] !== 1) {
-        seen[item] = 1;
-        output[j++] = item;
-      }
-    }
-    return output;
-  }
+	// TODO: Is there a better way for unique?
+	function uniqueArray(input) {
+		var seen = {},
+			output = [],
+			len = input.length,
+			j = 0;
+		for(var i = 0; i < len; i++) {
+			var item = input[i];
+			if(seen[item] !== 1) {
+				seen[item] = 1;
+				output[j++] = item;
+			}
+		}
+		return output;
+	}
 
-  /*
-    Export our filter function
+	/*
+	  Export our filter function
 
-    TODO: May I add tests? (editions/test/tiddlers/tests)
-    */
-  exports.kin = function (source, operator, options) {
-    var results = [],
-      needs_exclusion = operator.prefix === '!',
-      suffix_list = (operator.suffix || '').split(':'),
-      fieldname = (suffix_list[0] || 'tags').toLowerCase(),
-      direction = (suffix_list[1] || 'with').toLowerCase();
+	  TODO: May I add tests? (editions/test/tiddlers/tests)
+	  */
+	exports.kin = function(source,operator,options) {
+		var results = [],
+			needsExclusion = operator.prefix === "!",
+			suffixList = (operator.suffix || "").split(":"),
+			fieldName = (suffixList[0] || "tags").toLowerCase(),
+			direction = (suffixList[1] || "with").toLowerCase();
 
-    if ((operator.operand === '') && (needs_exclusion)) {
-      return [];
-    }
+		if((operator.operand === "") && (needsExclusion)) {
+			return [];
+		}
 
-    if (operator.operand !== '') {
-      var title_from_family = operator.operand,
-        tiddler_from_family = $tw.wiki.getTiddler(title_from_family),
-        family_members = collectFamilyMembers(tiddler_from_family, title_from_family, fieldname, direction);
+		if(operator.operand !== "") {
+			var baseTitle = operator.operand,
+				baseTiddler = $tw.wiki.getTiddler(baseTitle),
+				foundTitles = collectTitlesRecursively(baseTiddler,baseTitle,fieldName,direction);
 
-      source(function (tiddler, title) {
-        if (needs_exclusion !== family_members.includes(title)) {
-          results.push(title);
-        }
-      });
-    } else {
-      source(function (tiddler, title) {
-        results = results.concat(collectFamilyMembers(tiddler, title, fieldname, direction));
-      });
-      results = uniqueArray(results);
-    }
+			source(function(tiddler,title) {
+				if(needsExclusion !== foundTitles.includes(title)) {
+					results.push(title);
+				}
+			});
+		} else {
+			source(function(tiddler,title) {
+				results = results.concat(collectTitlesRecursively(tiddler,title,fieldName,direction));
+			});
+			results = uniqueArray(results);
+		}
 
-    return results;
-  }
+		return results;
+	}
 })();
