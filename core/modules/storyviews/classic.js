5d51cd1f0c7f64355a7a24461d4b61dd3c46ab25
BurningTreeC <hypnotizedkangaroo@gmail.com>
2018-11-08 09:18:43 +0100

    update animation to be conform
    also adds `zIndex: "-1"` which appears a bit cleaner but it's a matter of taste

--- core/modules/storyviews/classic.js
+++ core/modules/storyviews/classic.js
@@ -46,36 +46,36 @@ ClassicStoryView.prototype.insert = function(widget,options) {
 		currMarginTop = parseInt(computedStyle.marginTop,10),
 		currHeight = targetElement.offsetHeight + currMarginTop,
 	    	isBottom = options.index === options.listLength - 1;
-	var initialStyle = isBottom ? [
-		{transition: "none"},
-		{opacity: "0.0"}
-	] : [
-		{transition: "none"},
-		{marginBottom: (-currHeight) + "px"},
-		{opacity: "0.0"}
-	];
-	var finalStyle = isBottom ? [
-		{transition: "opacity " + duration + "ms " + easing + ", " +
-					"margin-bottom " + duration + "ms " + easing},
-		{opacity: "1.0"}
-	] : [
-		{transition: "opacity " + duration + "ms " + easing + ", " +
-					"margin-bottom " + duration + "ms " + easing},
-		{marginBottom: currMarginBottom + "px"},
-		{opacity: "1.0"}
-	];
+	var initialStyle = isBottom ? [{marginTop: (-currHeight) + "px"}] : [{marginBottom: (-currHeight) + "px"}];
+	var finalStyle = isBottom ? [{transition: "opacity " + duration + "ms " + easing + ", " +
+						"margin-top " + duration + "ms " + easing},
+					{marginTop: currMarginTop + "px"}] : 
+					[{transition: "opacity " + duration + "ms " + easing + ", " +
+						"margin-bottom " + duration + "ms " + easing},
+					{marginBottom: currMarginBottom + "px"}];
 	// Reset the margin once the transition is over
 	setTimeout(function() {
 		$tw.utils.setStyle(targetElement,[
 			{transition: "none"},
-			{marginBottom: ""}
+			{marginBottom: ""},
+			{marginTop: ""},
+			{zIndex: ""}
 		]);
 	},duration);
 	// Set up the initial position of the element
 	$tw.utils.setStyle(targetElement,initialStyle);
+	$tw.utils.setStyle(targetElement,[
+		{transition: "none"},
+		{zIndex: "-1"},
+		{opacity: "0.0"}
+	]);
 	$tw.utils.forceLayout(targetElement);
 	// Transition to the final position
 	$tw.utils.setStyle(targetElement,finalStyle);
+	$tw.utils.setStyle(targetElement,[
+		{opacity: "1.0"},
+		{zIndex: "-1"}
+	]);
 };
 
 ClassicStoryView.prototype.remove = function(widget,options) {
@@ -96,36 +96,33 @@ ClassicStoryView.prototype.remove = function(widget,options) {
 		currMarginTop = parseInt(computedStyle.marginTop,10),
 		currHeight = targetElement.offsetHeight + currMarginTop,
 	    	isBottom = options.index === options.listLength;
-	var initialStyle = isBottom ? [
-		{transition: "none"},
-		{transform: "translateX(0px)"},
-		{opacity: "1.0"}
-	] : [
-		{transition: "none"},
-		{transform: "translateX(0px)"},
-		{marginBottom:  currMarginBottom + "px"},
-		{opacity: "1.0"}
-	];
-	var finalStyle = isBottom ? [
-		{transition: $tw.utils.roundTripPropertyName("transform") + " " + duration + "ms " + easing + ", " +
-					"opacity " + duration + "ms " + easing + ", " +
-					"margin-bottom " + duration + "ms " + easing},
-		{transform: "translateX(-" + currWidth + "px)"},
-		{opacity: "0.0"}
-	] : [
-		{transition: $tw.utils.roundTripPropertyName("transform") + " " + duration + "ms " + easing + ", " +
-					"opacity " + duration + "ms " + easing + ", " +
-					"margin-bottom " + duration + "ms " + easing},
-		{transform: "translateX(-" + currWidth + "px)"},
-		{marginBottom: (-currHeight) + "px"},
-		{opacity: "0.0"}
-	];
+	var initialStyle = isBottom ? [{marginTop:  currMarginTop + "px"}]: [{marginBottom:  currMarginBottom + "px"}];
+	var finalStyle = isBottom ? [{transition: $tw.utils.roundTripPropertyName("transform") + " " + duration + "ms " + easing + ", " +
+						"opacity " + duration + "ms " + easing + ", " +
+						"margin-top " + duration + "ms " + easing},
+					{marginTop: (-currHeight) + "px"}] :
+					[{transition: $tw.utils.roundTripPropertyName("transform") + " " + duration + "ms " + easing + ", " +
+						"opacity " + duration + "ms " + easing + ", " +
+						"margin-bottom " + duration + "ms " + easing},
+					{marginBottom: (-currHeight) + "px"}];
 	// Remove the dom nodes of the widget at the end of the transition
 	setTimeout(removeElement,duration);
 	// Animate the closure
 	$tw.utils.setStyle(targetElement,initialStyle);
+	$tw.utils.setStyle(targetElement,[
+		{transition: "none"},
+		{transform: "translateX(0px)"},
+		{opacity: "1.0"},
+		{zIndex: "-1"}
+	]);
 	$tw.utils.forceLayout(targetElement);
 	$tw.utils.setStyle(targetElement,finalStyle);
+	$tw.utils.setStyle(targetElement,[
+		{transform: "translateX(-" + currWidth + "px)"},
+		{opacity: "0.0"},
+		{zIndex: "-1"}
+	]);
+
 };
 
 exports.classic = ClassicStoryView;
