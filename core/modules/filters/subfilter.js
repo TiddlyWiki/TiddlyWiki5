33dd367a6548824567995bb48e87eece755681e8
Jeremy Ruston <jeremy@jermolene.com>
2018-11-16 11:27:19 +0000

    Introduce new subfilter operator (#3508)

---
+++ core/modules/filters/subfilter.js
@@ -0,0 +1,33 @@
+/*\
+title: $:/core/modules/filters/subfilter.js
+type: application/javascript
+module-type: filteroperator
+
+Filter operator returning its operand evaluated as a filter
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Export our filter function
+*/
+exports.subfilter = function(source,operator,options) {
+	var list = options.wiki.filterTiddlers(operator.operand,options.widget,source);
+	if(operator.prefix === "!") {
+		var results = [];
+		source(function(tiddler,title) {
+			if(list.indexOf(title) === -1) {
+				results.push(title);
+			}
+		});
+		return results;
+	} else {
+		return list;
+	}
+};
+
+})();
