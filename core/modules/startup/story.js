f98e3aa8fb9397fa2e8ae69cc64ebbb7818c4194
BurningTreeC <hypnotizedkangaroo@gmail.com>
2018-11-28 14:50:28 +0100

    Update story.js

--- core/modules/startup/story.js
+++ core/modules/startup/story.js
@@ -152,8 +152,10 @@ function openStartupTiddlers(options) {
 		if(target.indexOf("[[") === 0 && target.substr(-2) === "]]") {
 			target = target.substr(2,target.length - 4);
 		}
+		$tw.wiki.setText(DEFAULT_HISTORY_TITLE,"focus-tiddler",undefined,target);
 		$tw.wiki.addToHistory(target);
 	} else if(storyList.length > 0) {
+		$tw.wiki.setText(DEFAULT_HISTORY_TITLE,"focus-tiddler",undefined,storyList[0]);
 		$tw.wiki.addToHistory(storyList[0]);
 	}
 }
