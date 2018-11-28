3dbc967c16693b8dad90c66274dc8848246b2e59
BurningTreeC <hypnotizedkangaroo@gmail.com>
2018-11-28 14:05:59 +0100

    Update factory.js

--- core/modules/editor/factory.js
+++ core/modules/editor/factory.js
@@ -208,6 +208,7 @@ function editTextWidgetFactory(toolbarEngine,nonToolbarEngine) {
 		this.editShowToolbar = this.wiki.getTiddlerText(ENABLE_TOOLBAR_TITLE,"yes");
 		this.editShowToolbar = (this.editShowToolbar === "yes") && !!(this.children && this.children.length > 0) && (!this.document.isTiddlyWikiFakeDom);
 		this.editSaveTiddler = this.getAttribute("saveTiddler");
+		this.editRefreshTiddler = this.getAttribute("refreshTiddler");
 		this.editRefreshCondition = this.getAttribute("refreshCondition");
 	};
 
@@ -220,7 +221,7 @@ function editTextWidgetFactory(toolbarEngine,nonToolbarEngine) {
 		if(changedAttributes.tiddler || changedAttributes.field || changedAttributes.index || changedAttributes["default"] || changedAttributes["class"] || changedAttributes.placeholder || changedAttributes.size || changedAttributes.autoHeight || changedAttributes.minHeight || changedAttributes.focusPopup ||  changedAttributes.rows || changedTiddlers[HEIGHT_MODE_TITLE] || changedTiddlers[ENABLE_TOOLBAR_TITLE]) {
 			this.refreshSelf();
 			return true;
-		} else if(this.editRefreshCondition && changedAttributes[this.editRefreshCondition] && (this.editRefreshCondition === "true" || this.editRefreshCondition === "yes")) {
+		} else if(this.editRefreshTiddler && this.editRefreshCondition && changedTiddlers[this.editRefreshTiddler] && (this.editRefreshCondition === "true" || this.editRefreshCondition === "yes")) {
 			editInfo = this.getEditInfo();
 			this.engine.domNode.value = editInfo.value;
 			this.engine.focus();
