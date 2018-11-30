f39925cb8a3e1129dd72c32f9931d2bebdbf3d47
BurningTreeC <hypnotizedkangaroo@gmail.com>
2018-11-30 11:16:08 +0100

    Update css-escape-polyfill.js

--- core/modules/startup/css-escape-polyfill.js
+++ core/modules/startup/css-escape-polyfill.js
@@ -19,16 +19,7 @@ exports.synchronous = true;
 
 /*! https://mths.be/cssescape v1.5.1 by @mathias | MIT license */
 // https://github.com/umdjs/umd/blob/master/returnExports.js
-if (typeof exports == 'object') {
-	// For Node.js.
-	module.exports = factory(root);
-} else if (typeof define == 'function' && define.amd) {
-	// For AMD. Register as an anonymous module.
-	define([], factory.bind(root, root));
-} else {
-	// For browser globals (not exposing the function separately).
-	factory(root);
-}
+exports.startup = factory(root);
 }(typeof global != 'undefined' ? global : this, function(root) {
 
 	if (root.CSS && root.CSS.escape) {
@@ -119,6 +110,5 @@ if (typeof exports == 'object') {
 	}
 
 	root.CSS.escape = cssEscape;
-	return cssEscape;
 
 }));
