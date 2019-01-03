2738585c5b5b6c94cd270651d8298ae55e27f3c3
Moritz Ulrich <moritz@tarn-vedra.de>
2019-01-03 11:58:36 +0100

    get-index: Enable deflate and gzip compression

--- core/modules/server/routes/get-index.js
+++ core/modules/server/routes/get-index.js
@@ -12,14 +12,32 @@ GET /
 /*global $tw: false */
 "use strict";
 
+var zlib = require('zlib');
+
 exports.method = "GET";
 
 exports.path = /^\/$/;
 
 exports.handler = function(request,response,state) {
-	response.writeHead(200, {"Content-Type": state.server.get("root-serve-type")});
-	var text = state.wiki.renderTiddler(state.server.get("root-render-type"),state.server.get("root-tiddler"));
-	response.end(text,"utf8");
+  var acceptEncoding = request.headers['accept-encoding'];
+  if (!acceptEncoding) { acceptEncoding = ''; }
+
+  var text = state.wiki.renderTiddler(state.server.get("root-render-type"),state.server.get("root-tiddler"));
+
+  var responseHeaders = {
+    "Content-Type": state.server.get("root-serve-type")
+  };
+
+  if (/\bdeflate\b/.test(acceptEncoding)) {
+    responseHeaders['Content-Encoding'] = 'deflate';
+    text = zlib.deflateSync(text);
+  } else if (/\bgzip\b/.test(acceptEncoding)) {
+    responseHeaders['Content-Encoding'] = 'gzip';
+    text = zlib.gzipSync(text);
+  }
+
+	response.writeHead(200, responseHeaders);
+	response.end(text);
 };
 
 }());
