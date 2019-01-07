decbe33f1f218bb5eddc455cd907551c62672cd1
Jermolene <jeremy@jermolene.com>
2019-01-07 17:08:25 +0000

    CI: More tweaking git commands...

--- bin/travis-push.sh
+++ bin/travis-push.sh
@@ -9,7 +9,7 @@ git config --global user.email "travis@travis-ci.org" || exit 1
 
 git config --global user.name "Travis CI" || exit 1
 
-git add -all || exit 1
+git add -A . || exit 1
 
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER" || exit 1
 
