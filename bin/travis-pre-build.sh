d5a25c9a964e678171a557889b62661abfb6f8ef
Jermolene <jeremy@jermolene.com>
2019-01-07 18:40:22 +0000

    CI: Start building properly

--- bin/travis-pre-build.sh
+++ bin/travis-pre-build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Pull existing GitHub pages content
+# Install latest current release from npm
+# (we need to force because otherwise npm will refuse to install a module of the same name)
 
 npm --force install tiddlywiki || exit 1
+
+# Pull existing GitHub pages content
 
 git clone --depth=1 --branch=master "https://github.com/Jermolene/Testing2019.git" output
