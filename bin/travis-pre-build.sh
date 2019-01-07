8205893af99dd07c12472fb471653b36ea097a20
Jermolene <jeremy@jermolene.com>
2019-01-07 17:01:12 +0000

    CI: Further attempts to fix up the git integration

--- bin/travis-pre-build.sh
+++ bin/travis-pre-build.sh
@@ -4,18 +4,4 @@
 
 npm --force install tiddlywiki || exit 1
 
-mkdir -p output || exit 1
-
-cd output || exit 1
-
-git init || exit 1
-
-git config --global user.email "travis@travis-ci.org" || exit 1
-
-git config --global user.name "Travis CI" || exit 1
-
-git remote add upstream "https://$GH_TOKEN@github.com/Jermolene/Testing2019.git" || exit 1
-
-git fetch upstream || exit 1
-
-cd .. || exit 1
+git clone --depth=1 --branch=master "https://github.com/Jermolene/Testing2019.git" output
--- bin/travis-push.sh
+++ bin/travis-push.sh
@@ -2,12 +2,19 @@
 
 # Push output back to GitHub
 
+
 cd output || exit 1
 
-git add -A . || exit 1
+git config --global user.email "travis@travis-ci.org" || exit 1
+
+git config --global user.name "Travis CI" || exit 1
+
+git add -all || exit 1
 
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER" || exit 1
 
-git push -q upstream HEAD:master >/dev/null 2>&1 || exit 1
+git remote add deploy "https://$GH_TOKEN@github.com/Jermolene/Testing2019.git" &>/dev/null || exit 1
+
+git push deploy master &>/dev/null || exit 1
 
 cd .. || exit 1
