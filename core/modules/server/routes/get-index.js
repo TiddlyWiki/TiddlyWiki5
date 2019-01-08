0b327a4a339ba743d36f7f449203c98ef3666ffa
Moritz Ulrich <moritz@tarn-vedra.de>
2019-01-08 13:49:39 +0100

    Spaces -> Tabs

--- core/modules/server/routes/get-index.js
+++ core/modules/server/routes/get-index.js
@@ -19,22 +19,22 @@ exports.method = "GET";
 exports.path = /^\/$/;
 
 exports.handler = function(request,response,state) {
-  var acceptEncoding = request.headers['accept-encoding'];
-  if (!acceptEncoding) { acceptEncoding = ''; }
+	var acceptEncoding = request.headers['accept-encoding'];
+	if (!acceptEncoding) { acceptEncoding = ''; }
 
-  var text = state.wiki.renderTiddler(state.server.get("root-render-type"),state.server.get("root-tiddler"));
+	var text = state.wiki.renderTiddler(state.server.get("root-render-type"),state.server.get("root-tiddler"));
 
-  var responseHeaders = {
-    "Content-Type": state.server.get("root-serve-type")
-  };
+	var responseHeaders = {
+		"Content-Type": state.server.get("root-serve-type")
+	};
 
-  if (/\bdeflate\b/.test(acceptEncoding)) {
-    responseHeaders['Content-Encoding'] = 'deflate';
-    text = zlib.deflateSync(text);
-  } else if (/\bgzip\b/.test(acceptEncoding)) {
-    responseHeaders['Content-Encoding'] = 'gzip';
-    text = zlib.gzipSync(text);
-  }
+	if (/\bdeflate\b/.test(acceptEncoding)) {
+		responseHeaders['Content-Encoding'] = 'deflate';
+		text = zlib.deflateSync(text);
+	} else if (/\bgzip\b/.test(acceptEncoding)) {
+		responseHeaders['Content-Encoding'] = 'gzip';
+		text = zlib.gzipSync(text);
+	}
 
 	response.writeHead(200, responseHeaders);
 	response.end(text);
