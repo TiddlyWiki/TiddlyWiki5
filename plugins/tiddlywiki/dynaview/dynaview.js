e14e69bedc9b63da207469d1a50dfadcbd9fcd8a
Jermolene <jeremy@jermolene.com>
2019-01-11 17:50:52 +0000

    DynaView plugin: add optional scroll position preservation

--- plugins/tiddlywiki/dynaview/dynaview.js
+++ plugins/tiddlywiki/dynaview/dynaview.js
@@ -24,11 +24,21 @@ var isWaitingForAnimationFrame = 0, // Bitmask:
 	ANIM_FRAME_CAUSED_BY_RESIZE = 4; // Animation frame was requested because of window resize
 
 exports.startup = function() {
+	var topmost = null, lastScrollY;
 	window.addEventListener("load",onLoad,false);
 	window.addEventListener("scroll",onScroll,false);
 	window.addEventListener("resize",onResize,false);
+	$tw.hooks.addHook("th-page-refreshing",function() {
+		if(shouldPreserveScrollPosition()) {
+			topmost = findTopmostTiddler();
+		}
+		lastScrollY = window.scrollY;
+	});
 	$tw.hooks.addHook("th-page-refreshed",function() {
-		checkTopmost();
+		if(lastScrollY === window.scrollY) { // Don't do scroll anchoring if the scroll position got changed
+			scrollToTiddler(topmost);			
+		}
+		updateAddressBar();
 		checkVisibility();
 		saveViewportDimensions();
 	});
@@ -60,7 +70,7 @@ function worker() {
 		saveViewportDimensions();
 	}
 	setZoomClasses();
-	checkTopmost();
+	updateAddressBar();
 	checkVisibility();
 	isWaitingForAnimationFrame = 0;
 }
@@ -134,28 +144,62 @@ function checkVisibility() {
 	});
 }
 
-function checkTopmost() {
+function updateAddressBar() {
 	if($tw.wiki.getTiddlerText("$:/config/DynaView/UpdateAddressBar") === "yes") {
-		var elements = document.querySelectorAll(".tc-tiddler-frame[data-tiddler-title]"),
-			topmostElement = null,
-			topmostElementTop = 1 * 1000 * 1000;
-		$tw.utils.each(elements,function(element) {
-			// Check if the element is visible
-			var elementRect = element.getBoundingClientRect();
-			if((elementRect.top < topmostElementTop) && (elementRect.bottom > 0)) {
-				topmostElement = element;
-				topmostElementTop = elementRect.top;
-			}
-		});
-		if(topmostElement) {
-			var title = topmostElement.getAttribute("data-tiddler-title"),
-				hash = "#" + encodeURIComponent(title) + ":" + encodeURIComponent("[list[$:/StoryList]]");
+		var top = findTopmostTiddler();
+		if(top.element) {
+			var hash = "#" + encodeURIComponent(top.title) + ":" + encodeURIComponent("[list[$:/StoryList]]");
 			if(title && $tw.locationHash !== hash) {
 				$tw.locationHash = hash;
 				window.location.hash = hash;			
 			}
 		}
 	}
+}
+
+/*
+tiddlerDetails: {title: <title of tiddler to scroll to>, offset: <offset in pixels from the top of the tiddler>}
+*/
+function scrollToTiddler(tiddlerDetails) {
+	if(shouldPreserveScrollPosition() && !$tw.pageScroller.isScrolling() && tiddlerDetails) {
+		var elements = document.querySelectorAll(".tc-tiddler-frame[data-tiddler-title]"),
+			topmostTiddlerElement = null;
+		$tw.utils.each(elements,function(element) {
+			if(element.getAttribute("data-tiddler-title") === tiddlerDetails.title) {
+				topmostTiddlerElement = element;
+			}
+		});
+		if(topmostTiddlerElement) {
+			var rect = topmostTiddlerElement.getBoundingClientRect(),
+				scrollY = Math.round(window.scrollY + rect.top + tiddlerDetails.offset);
+			if(scrollY !== window.scrollY) {
+				window.scrollTo(window.scrollX,scrollY);					
+			}
+		}
+	}
+}
+
+function shouldPreserveScrollPosition() {
+	return $tw.wiki.getTiddlerText("$:/config/DynaView/PreserveScrollPosition") === "yes";
+}
+
+function findTopmostTiddler() {
+	var elements = document.querySelectorAll(".tc-tiddler-frame[data-tiddler-title]"),
+		topmostElement = null,
+		topmostElementTop = 1 * 1000 * 1000;
+	$tw.utils.each(elements,function(element) {
+		// Check if the element is visible
+		var elementRect = element.getBoundingClientRect();
+		if((elementRect.top < topmostElementTop) && (elementRect.bottom > 0)) {
+			topmostElement = element;
+			topmostElementTop = elementRect.top;
+		}
+	});
+	return {
+		element: topmostElement,
+		offset: -topmostElementTop,
+		title: topmostElement.getAttribute("data-tiddler-title")
+	};
 }
 
 var previousViewportWidth, previousViewportHeight;
