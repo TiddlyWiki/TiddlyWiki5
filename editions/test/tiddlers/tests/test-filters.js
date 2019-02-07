9b2d52716a24197ba9fd67b708cbe2c56fa1b5d0
Jermolene <jeremy@jermolene.com>
2019-02-07 11:18:53 +0000

    Add new string operators: length, join, split, concat etc.

--- editions/test/tiddlers/tests/test-filters.js
+++ editions/test/tiddlers/tests/test-filters.js
@@ -361,7 +361,17 @@ describe("Filter tests", function() {
 		expect(wiki.filterTiddlers("[list[TiddlerSeventh]before[MissingTiddler]]").join(",")).toBe("a fourth tiddler");
 	});
 
-	it("should handle the math operators", function() {
+	it("should handle the string operators", function() {
+		expect(wiki.filterTiddlers("John Paul George Ringo +[length[]]").join(",")).toBe("4,4,6,5");
+		expect(wiki.filterTiddlers("John Paul George Ringo +[uppercase[]]").join(",")).toBe("JOHN,PAUL,GEORGE,RINGO");
+		expect(wiki.filterTiddlers("John Paul George Ringo +[lowercase[]]").join(",")).toBe("john,paul,george,ringo");
+		expect(wiki.filterTiddlers("John Paul George Ringo +[concat[y]]").join(",")).toBe("Johny,Pauly,Georgey,Ringoy");
+		expect(wiki.filterTiddlers("John Paul George Ringo +[split[]]").join(",")).toBe("J,o,h,n,P,a,u,l,G,e,o,r,g,e,R,i,n,g,o");
+		expect(wiki.filterTiddlers("John Paul George Ringo +[join[ ]split[e]join[ee]split[ ]]").join(",")).toBe("John,Paul,G,org,,Ringo");
+		expect(wiki.filterTiddlers("[[ John ]] [[Paul ]] [[ George]] Ringo +[trim[]join[-]]").join(",")).toBe("John-Paul-George-Ringo");
+	});
+
+	it("should handle the mathematics operators", function() {
 		expect(wiki.filterTiddlers("[[2]add[2]]").join(",")).toBe("4");
 		expect(wiki.filterTiddlers("[[4]subtract[2]]").join(",")).toBe("2");
 		expect(wiki.filterTiddlers("[[24]divide[8]]").join(",")).toBe("3");
