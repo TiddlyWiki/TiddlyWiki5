71f1cc0f11e68789b8b1fed57e90ca64ab7003c4
Brooks Boyd <bro@oks.me>
2019-03-21 21:47:46 -0500

    Replace Markdown parsing library with Remarkable

--- plugins/tiddlywiki/markdown/wrapper.js
+++ plugins/tiddlywiki/markdown/wrapper.js
@@ -3,7 +3,7 @@ title: $:/plugins/tiddlywiki/markdown/wrapper.js
 type: application/javascript
 module-type: parser
 
-Wraps up the markdown-js parser for use in TiddlyWiki5
+Wraps up the remarkable parser for use as a Parser in TiddlyWiki
 
 \*/
 (function(){
@@ -12,78 +12,142 @@ Wraps up the markdown-js parser for use in TiddlyWiki5
 /*global $tw: false */
 "use strict";
 
-var markdown = require("$:/plugins/tiddlywiki/markdown/markdown.js");
+var Remarkable = require('$:/plugins/midnight/remarkable/remarkable.js');
 
-var CONFIG_DIALECT_TIDDLER = "$:/config/markdown/dialect",
-	DEFAULT_DIALECT = "Gruber";
+var md = new Remarkable();
 
-function transformNodes(nodes) {
-	var results = [];
-	for(var index=0; index<nodes.length; index++) {
-		results.push(transformNode(nodes[index]));
+function findTagWithType(nodes, startPoint, type) {
+	for (var i = startPoint; i < nodes.length; i++) {
+		if (nodes[i].type == type) {
+			return i;
+		}
 	}
-	return results;
+	return false;
 }
 
-function transformNode(node) {
-	if($tw.utils.isArray(node)) {
-		var p = 0,
-			widget = {type: "element", tag: node[p++]};
-		if(!$tw.utils.isArray(node[p]) && typeof(node[p]) === "object") {
-			widget.attributes = {};
-			$tw.utils.each(node[p++],function(value,name) {
-				widget.attributes[name] = {type: "string", value: value};
+
+/**
+ * Remarkable creates nodes that look like:
+ * [
+ *   { type: 'paragraph_open'},
+ *   { type: 'inline', content: 'Hello World', children:[{type: 'text', content: 'Hello World'}]},
+ *   { type: 'paragraph_close'}
+ * ]
+ *
+ * But TiddlyWiki wants the Parser (https://tiddlywiki.com/dev/static/Parser.html) to emit nodes like:
+ *
+ * [
+ *   { type: 'element', tag: 'p', children: [{type: 'text', text: 'Hello World'}]}
+ * ]
+ */
+function convertNodes(remarkableTree) {
+	let out = [];
+
+	function wrappedElement(elementTag, currentIndex, closingType, nodes) {
+		var j = findTagWithType(nodes, currentIndex + 1, closingType);
+		out.push({
+			type: 'element',
+			tag: elementTag,
+			children: convertNodes(nodes.slice(currentIndex + 1, j))
+		});
+		return j;
+	}
+
+	for (var i = 0; i < remarkableTree.length; i++) {
+		var currentNode = remarkableTree[i];
+		if (currentNode.type == 'paragraph_open') {
+			i = wrappedElement('p', i, 'paragraph_close', remarkableTree);
+		} else if (currentNode.type == 'heading_open') {
+			i = wrappedElement('h' + currentNode.hLevel, i, 'heading_close', remarkableTree);
+		} else if (currentNode.type == 'bullet_list_open') {
+			i = wrappedElement('ul', i, 'bullet_list_close', remarkableTree);
+		} else if (currentNode.type == 'list_item_open') {
+			i = wrappedElement('li', i, 'list_item_close', remarkableTree);
+		} else if (currentNode.type == 'link_open') {
+			var j = findTagWithType(remarkableTree, i + 1, 'link_close');
+
+			if (currentNode.href.substr(0, 4) == 'http') {
+				// External link
+				out.push({
+					type: 'element',
+					tag: 'a',
+					attributes: {
+						href: { type: 'string', value: currentNode.href }
+					},
+					children: convertNodes(remarkableTree.slice(i + 1, j))
+				});
+			} else {
+				// Internal link
+				out.push({
+				type: 'link',
+				attributes: {
+					to: {type: 'string', value: currentNode.href}
+				},
+				children: convertNodes(remarkableTree.slice(i + 1, j))
+				});
+			}
+			i = j;
+		} else if (currentNode.type.substr(currentNode.type.length - 5) == '_open') {
+			var tagName = currentNode.type.substr(0, currentNode.type.length - 5);
+			i = wrappedElement(tagName, i, tagName + '_close', remarkableTree);
+			} else if (currentNode.type == 'code') {
+			out.push({
+				type: 'element',
+				tag: currentNode.block ? 'pre' : 'code',
+				children: [{type: 'text', text: currentNode.content}]
+			});
+		} else if (currentNode.type == 'fence') {
+			out.push({
+				type: 'codeblock',
+				attributes: {
+				language: { type: 'string', value: currentNode.params },
+				code: { type: 'string', value: currentNode.content }
+				}
+			});
+		} else if (currentNode.type == 'image') {
+			out.push({
+				type: 'image',
+				attributes: {
+				tooltip: { type: 'string', value: currentNode.alt },
+				source: { type: 'string', value: currentNode.src }
+				}
+			});
+		} else if (currentNode.type == 'inline') {
+			out = out.concat(convertNodes(currentNode.children));
+		} else if (currentNode.type == 'text') {
+			let wikiParser = $tw.wiki.parseText('text/vnd.tiddlywiki', currentNode.content, {
+				parseAsInline: false
+			});
+
+			if (wikiParser.tree.length == 1
+				&& wikiParser.tree[0].type == 'element'
+				&& wikiParser.tree[0].tag == 'p'
+			) {
+				out = out.concat(wikiParser.tree[0].children);
+			} else {
+				out = out.concat(wikiParser.tree);
+			}
+			} else {
+			console.error('Unknown node type: ' + currentNode.type, currentNode);
+			out.push({
+				type: 'text',
+				text: currentNode.content
 			});
 		}
-		widget.children = transformNodes(node.slice(p++));
-		// Massage images into the image widget
-		if(widget.tag === "img") {
-			widget.type = "image";
-			if(widget.attributes.alt) {
-				widget.attributes.tooltip = widget.attributes.alt;
-				delete widget.attributes.alt;
-			}
-			if(widget.attributes.src) {
-				widget.attributes.source = widget.attributes.src;
-				delete widget.attributes.src;
-			}
-		}
-		// Convert internal links to proper wikilinks
-		if (widget.tag === "a" && widget.attributes.href.value[0] === "#") {
-			widget.type = "link";
-			widget.attributes.to = widget.attributes.href;
-			if (widget.attributes.to.type === "string") {
-				//Remove '#' before conversion to wikilink
-				widget.attributes.to.value = widget.attributes.to.value.substr(1);
-			}
-			//Children is fine
-			delete widget.tag;
-			delete widget.attributes.href;
-		}
-		return widget;
-	} else {
-		return {type: "text", text: node};
 	}
+	return out;
 }
 
-var MarkdownParser = function(type,text,options) {
-	var dialect = options.wiki.getTiddlerText(CONFIG_DIALECT_TIDDLER,DEFAULT_DIALECT),
-		markdownTree = markdown.toHTMLTree(text,dialect),
-		node = $tw.utils.isArray(markdownTree[1]) ? markdownTree.slice(1) : markdownTree.slice(2);
-	this.tree = transformNodes(node);
-};
+	var MarkdownParser = function(type, text, options) {
+		var tree = md.parse(text, {});
+		//console.debug(tree);
+		tree = convertNodes(tree);
+		//console.debug(tree);
 
-/*
+		this.tree = tree;
+	};
 
-[ 'html',
-  [ 'p', 'something' ],
-  [ 'h1',
-    'heading and ',
-    [ 'strong', 'other' ] ] ]
-
-*/
-
-exports["text/x-markdown"] = MarkdownParser;
+	exports['text/x-markdown'] = MarkdownParser;
 
 })();
 
