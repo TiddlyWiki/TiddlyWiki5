cc2af4fe1d9707569e915e9a22d89d333afd89ef
Brooks Boyd <bro@oks.me>
2019-03-22 22:02:58 -0500

    Add config options for Parser actions

--- plugins/tiddlywiki/markdown/wrapper.js
+++ plugins/tiddlywiki/markdown/wrapper.js
@@ -14,6 +14,16 @@ Wraps up the remarkable parser for use as a Parser in TiddlyWiki
 
 var Remarkable = require('$:/plugins/tiddlywiki/markdown/remarkable.js');
 
+///// Set up configuration options /////
+function parseAsBoolean(tiddlerName) {
+	return $tw.wiki.getTiddlerText(tiddlerName).toLowerCase() === "true";
+}
+var pluginOpts = {
+	linkNewWindow: parseAsBoolean("$:/config/markdown/linkNewWindow"),
+	renderWikiText: parseAsBoolean("$:/config/markdown/renderWikiText"),
+	renderWikiTextPragma: $tw.wiki.getTiddlerText("$:/config/markdown/renderWikiTextPragma").trim()
+};
+
 var md = new Remarkable();
 
 function findTagWithType(nodes, startPoint, type, level) {
@@ -74,13 +84,16 @@ function convertNodes(remarkableTree, isStartOfInline) {
 
 			if (currentNode.href.substr(0, 4) == 'http') {
 				// External link
+				var attributes = {
+					href: { type: 'string', value: currentNode.href }
+				};
+				if (pluginOpts.linkNewWindow) {
+					attributes.target = { type: 'string', value: '_blank' };
+				}
 				out.push({
 					type: 'element',
 					tag: 'a',
-					attributes: {
-						href: { type: 'string', value: currentNode.href },
-						target: { type: 'string', value: '_blank' }
-					},
+					attributes: attributes,
 					children: convertNodes(remarkableTree.slice(i + 1, j))
 				});
 			} else {
@@ -127,33 +140,44 @@ function convertNodes(remarkableTree, isStartOfInline) {
 		} else if (currentNode.type == 'inline') {
 			out = out.concat(convertNodes(currentNode.children, true));
 		} else if (currentNode.type == 'text') {
-			// The Markdown compiler thinks this is just text.
-			// Hand off to the WikiText parser to see if there's more to render
+			if (!pluginOpts.renderWikiText) {
+				out.push({
+					type: 'text',
+					text: currentNode.content
+				});
+			} else {
+				// The Markdown compiler thinks this is just text.
+				// Hand off to the WikiText parser to see if there's more to render
 
-			// If we're inside a block element (div, p, td, h1), and this is the first child in the tree,
-			// handle as a block-level parse. Otherwise not.
-			var parseAsInline = !(isStartOfInline && i === 0);
-			var wikiParser = $tw.wiki.parseText('text/vnd.tiddlywiki', currentNode.content, {
-				parseAsInline: parseAsInline
-			});
-			var rs = wikiParser.tree;
+				// If we're inside a block element (div, p, td, h1), and this is the first child in the tree,
+				// handle as a block-level parse. Otherwise not.
+				var parseAsInline = !(isStartOfInline && i === 0);
+				var textToParse = currentNode.content;
+				if (pluginOpts.renderWikiTextPragma !== "") {
+					textToParse = pluginOpts.renderWikiTextPragma + "\n" + textToParse;
+				}
+				var wikiParser = $tw.wiki.parseText('text/vnd.tiddlywiki', textToParse, {
+					parseAsInline: parseAsInline
+				});
+				var rs = wikiParser.tree;
 
-			// If we parsed as a block, but the root element the WikiText parser gave is a paragraph,
-			// we should discard the paragraph, since the way Remarkable nests its nodes, this "inline"
-			// node is always inside something else that's a block-level element
-			if (!parseAsInline
-				&& rs.length == 1
-				&& rs[0].type == 'element'
-				&& rs[0].tag == 'p'
-			) {
-				rs = rs[0].children;
+				// If we parsed as a block, but the root element the WikiText parser gave is a paragraph,
+				// we should discard the paragraph, since the way Remarkable nests its nodes, this "inline"
+				// node is always inside something else that's a block-level element
+				if (!parseAsInline
+					&& rs.length == 1
+					&& rs[0].type == 'element'
+					&& rs[0].tag == 'p'
+				) {
+					rs = rs[0].children;
+				}
+
+				// If the original text element started with a space, add it back in
+				if (rs[0].type == 'text' && currentNode.content[0] == ' ') {
+					rs[0].text = ' ' + rs[0].text;
+				}
+				out = out.concat(rs);
 			}
-
-			// If the original text element started with a space, add it back in
-			if (rs[0].type == 'text' && currentNode.content[0] == ' ') {
-				rs[0].text = ' ' + rs[0].text;
-			}
-			out = out.concat(rs);
 		} else {
 			console.error('Unknown node type: ' + currentNode.type, currentNode);
 			out.push({
@@ -165,15 +189,15 @@ function convertNodes(remarkableTree, isStartOfInline) {
 	return out;
 }
 
-	var MarkdownParser = function(type, text, options) {
-		var tree = md.parse(text, {});
-		//console.debug(tree);
-		tree = convertNodes(tree);
-		//console.debug(tree);
+var MarkdownParser = function(type, text, options) {
+	var tree = md.parse(text, {});
+	//console.debug(tree);
+	tree = convertNodes(tree);
+	//console.debug(tree);
 
-		this.tree = tree;
-	};
+	this.tree = tree;
+};
 
-	exports['text/x-markdown'] = MarkdownParser;
+exports['text/x-markdown'] = MarkdownParser;
+
 })();
-
