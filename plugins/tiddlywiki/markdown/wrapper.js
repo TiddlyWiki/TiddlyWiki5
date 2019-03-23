e86efc04f33f1c55ea0fc01460c8813803a1c853
Brooks Boyd <bro@oks.me>
2019-03-22 22:15:43 -0500

    Match code style

--- plugins/tiddlywiki/markdown/wrapper.js
+++ plugins/tiddlywiki/markdown/wrapper.js
@@ -12,7 +12,7 @@ Wraps up the remarkable parser for use as a Parser in TiddlyWiki
 /*global $tw: false */
 "use strict";
 
-var Remarkable = require('$:/plugins/tiddlywiki/markdown/remarkable.js');
+var Remarkable = require("$:/plugins/tiddlywiki/markdown/remarkable.js");
 
 ///// Set up configuration options /////
 function parseAsBoolean(tiddlerName) {
@@ -33,7 +33,7 @@ var md = new Remarkable(remarkableOpts);
 
 function findTagWithType(nodes, startPoint, type, level) {
 	for (var i = startPoint; i < nodes.length; i++) {
-		if (nodes[i].type == type && nodes[i].level == level) {
+		if (nodes[i].type === type && nodes[i].level === level) {
 			return i;
 		}
 	}
@@ -60,14 +60,14 @@ function convertNodes(remarkableTree, isStartOfInline) {
 	function wrappedElement(elementTag, currentIndex, currentLevel, closingType, nodes) {
 		var j = findTagWithType(nodes, currentIndex + 1, closingType, currentLevel);
 		if (j === false) {
-			console.error(`Failed to find a ${closingType} node after position ${currentIndex}`);
+			console.error("Failed to find a " + closingType + " node after position " + currentIndex);
 			console.log(nodes);
 			return currentIndex + 1;
 		}
 		let children = convertNodes(nodes.slice(currentIndex + 1, j));
 
 		out.push({
-			type: 'element',
+			type: "element",
 			tag: elementTag,
 			children: children
 		});
@@ -76,78 +76,78 @@ function convertNodes(remarkableTree, isStartOfInline) {
 
 	for (var i = 0; i < remarkableTree.length; i++) {
 		var currentNode = remarkableTree[i];
-		if (currentNode.type == 'paragraph_open') {
-			i = wrappedElement('p', i, currentNode.level, 'paragraph_close', remarkableTree);
-		} else if (currentNode.type == 'heading_open') {
-			i = wrappedElement('h' + currentNode.hLevel, i, currentNode.level, 'heading_close', remarkableTree);
-		} else if (currentNode.type == 'bullet_list_open') {
-			i = wrappedElement('ul', i, currentNode.level, 'bullet_list_close', remarkableTree);
-		} else if (currentNode.type == 'list_item_open') {
-			i = wrappedElement('li', i, currentNode.level, 'list_item_close', remarkableTree);
-		} else if (currentNode.type == 'link_open') {
-			var j = findTagWithType(remarkableTree, i + 1, 'link_close', currentNode.level);
+		if (currentNode.type === "paragraph_open") {
+			i = wrappedElement("p", i, currentNode.level, "paragraph_close", remarkableTree);
+		} else if (currentNode.type === "heading_open") {
+			i = wrappedElement("h" + currentNode.hLevel, i, currentNode.level, "heading_close", remarkableTree);
+		} else if (currentNode.type === "bullet_list_open") {
+			i = wrappedElement("ul", i, currentNode.level, "bullet_list_close", remarkableTree);
+		} else if (currentNode.type === "list_item_open") {
+			i = wrappedElement("li", i, currentNode.level, "list_item_close", remarkableTree);
+		} else if (currentNode.type === "link_open") {
+			var j = findTagWithType(remarkableTree, i + 1, "link_close", currentNode.level);
 
-			if (currentNode.href.substr(0, 4) == 'http') {
+			if (currentNode.href[0] !== "#") {
 				// External link
 				var attributes = {
-					href: { type: 'string', value: currentNode.href }
+					href: { type: "string", value: currentNode.href }
 				};
 				if (pluginOpts.linkNewWindow) {
-					attributes.target = { type: 'string', value: '_blank' };
+					attributes.target = { type: "string", value: "_blank" };
 				}
 				out.push({
-					type: 'element',
-					tag: 'a',
+					type: "element",
+					tag: "a",
 					attributes: attributes,
 					children: convertNodes(remarkableTree.slice(i + 1, j))
 				});
 			} else {
 				// Internal link
 				out.push({
-					type: 'link',
+					type: "link",
 					attributes: {
-						to: {type: 'string', value: decodeURI(currentNode.href)}
+						to: { type: "string", value: decodeURI(currentNode.href.substr(1)) }
 					},
 					children: convertNodes(remarkableTree.slice(i + 1, j))
 				});
 			}
 			i = j;
-		} else if (currentNode.type.substr(currentNode.type.length - 5) == '_open') {
+		} else if (currentNode.type.substr(currentNode.type.length - 5) === "_open") {
 			var tagName = currentNode.type.substr(0, currentNode.type.length - 5);
-			i = wrappedElement(tagName, i, currentNode.level, tagName + '_close', remarkableTree);
-		} else if (currentNode.type == 'code') {
+			i = wrappedElement(tagName, i, currentNode.level, tagName + "_close", remarkableTree);
+		} else if (currentNode.type === "code") {
 			out.push({
-				type: 'element',
-				tag: currentNode.block ? 'pre' : 'code',
-				children: [{type: 'text', text: currentNode.content}]
+				type: "element",
+				tag: currentNode.block ? "pre" : "code",
+				children: [{ type: "text", text: currentNode.content }]
 			});
-		} else if (currentNode.type == 'fence') {
+		} else if (currentNode.type === "fence") {
 			out.push({
-				type: 'codeblock',
+				type: "codeblock",
 				attributes: {
-					language: { type: 'string', value: currentNode.params },
-					code: { type: 'string', value: currentNode.content }
+					language: { type: "string", value: currentNode.params },
+					code: { type: "string", value: currentNode.content }
 				}
 			});
-		} else if (currentNode.type == 'image') {
+		} else if (currentNode.type === "image") {
 			out.push({
-				type: 'image',
+				type: "image",
 				attributes: {
-					tooltip: {type: 'string', value: currentNode.alt},
-					source: {type: 'string', value: currentNode.src}
+					tooltip: { type: "string", value: currentNode.alt },
+					source: { type: "string", value: currentNode.src }
 				}
 			});
-		} else if (currentNode.type == 'softbreak') {
+		} else if (currentNode.type === "softbreak") {
 			out.push({
-				type: 'element',
-				tag: 'br',
+				type: "element",
+				tag: "br",
 			});
-		} else if (currentNode.type == 'inline') {
+		} else if (currentNode.type === "inline") {
 			out = out.concat(convertNodes(currentNode.children, true));
-		} else if (currentNode.type == 'text') {
+		} else if (currentNode.type === "text") {
 			if (!pluginOpts.renderWikiText) {
 				out.push({
-					type: 'text',
+					type: "text",
 					text: currentNode.content
 				});
 			} else {
@@ -161,7 +161,7 @@ function convertNodes(remarkableTree, isStartOfInline) {
 				if (pluginOpts.renderWikiTextPragma !== "") {
 					textToParse = pluginOpts.renderWikiTextPragma + "\n" + textToParse;
 				}
-				var wikiParser = $tw.wiki.parseText('text/vnd.tiddlywiki', textToParse, {
+				var wikiParser = $tw.wiki.parseText("text/vnd.tiddlywiki", textToParse, {
 					parseAsInline: parseAsInline
 				});
 				var rs = wikiParser.tree;
@@ -170,23 +170,23 @@ function convertNodes(remarkableTree, isStartOfInline) {
 				// we should discard the paragraph, since the way Remarkable nests its nodes, this "inline"
 				// node is always inside something else that's a block-level element
 				if (!parseAsInline
-					&& rs.length == 1
-					&& rs[0].type == 'element'
-					&& rs[0].tag == 'p'
+					&& rs.length === 1
+					&& rs[0].type === "element"
+					&& rs[0].tag === "p"
 				) {
 					rs = rs[0].children;
 				}
 
 				// If the original text element started with a space, add it back in
-				if (rs[0].type == 'text' && currentNode.content[0] == ' ') {
-					rs[0].text = ' ' + rs[0].text;
+				if (rs[0].type === "text" && currentNode.content[0] === " ") {
+					rs[0].text = " " + rs[0].text;
 				}
 				out = out.concat(rs);
 			}
 		} else {
-			console.error('Unknown node type: ' + currentNode.type, currentNode);
+			console.error("Unknown node type: " + currentNode.type, currentNode);
 			out.push({
-				type: 'text',
+				type: "text",
 				text: currentNode.content
 			});
 		}
@@ -203,6 +203,6 @@ var MarkdownParser = function(type, text, options) {
 	this.tree = tree;
 };
 
-exports['text/x-markdown'] = MarkdownParser;
+exports["text/x-markdown"] = MarkdownParser;
 
 })();
