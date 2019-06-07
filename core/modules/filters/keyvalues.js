6b3743d1b5f2035b86669fdd97615b66fc6385de
pmario <pmariojo@gmail.com>
2019-06-07 21:08:54 +0200

    keyvalues operator including regexp filter. + documentation

--- core/modules/filters/keyvalues.js
+++ core/modules/filters/keyvalues.js
@@ -21,30 +21,67 @@ exports.keyvalues = function(source,operator,options) {
 		sep = ":",
 		p1 = "key",
 		p2 = "value",
-		v1, v2;
+		useKey = true,
+		useValue = false,
+		v1, v2, flags, pass, match, data,
+		regexp, regexpstring;
 
+	// Process key, value parameters
 	if(operator.suffixes && operator.suffixes[0].length > 0) {
 		fieldList = operator.suffixes[0];
 		p1 = fieldList[0] || "";
 		p2 = fieldList[1] || "";
-
 	}
+	// Process <separator>
 	if(operator.suffixes && operator.suffixes[1]) {
 		sep = operator.suffixes[1][0] || ":";
 	}
+	// Process usekey or usevalue flags
+	if(operator.suffixes && operator.suffixes[2] && operator.suffixes[2][0]) {
+		useKey = (operator.suffixes[2][0].toLowerCase() === "usekey");
+		useValue = (operator.suffixes[2][0].toLowerCase() === "usevalue");
+	}
+	// Process regexp operand if available
+	regexpString = operator.operand;
+	if (regexpString) {
+		match = /^\(\?([gim]+)\)/.exec(regexpString);
+		if(match) {
+			flags = match[1];
+			regexpString = regexpString.substr(match[0].length);
+		} else {
+			match = /\(\?([gim]+)\)$/.exec(regexpString);
+			if(match) {
+				flags = match[1];
+				regexpString = regexpString.substr(0,regexpString.length - match[0].length);
+			}
+		}
+		try {
+			regexp = new RegExp(regexpString,flags);
+		} catch(e) {
+			return ["" + e];
+		}
+	}
+	// Process incoming values
 	source(function(tiddler,title) {
-		var data = options.wiki.getTiddlerDataCached(title);
+		data = options.wiki.getTiddlerDataCached(title);
 		if(data) {
+			// remove separator if there is no v2 element
+			sep = (p2) ? sep : "";
 			for (var [key, value] of Object.entries(data)) {
 				v1 = (p1 === "key") ? key : (p1 === "value") ? value : "";
 				v2 = (p2 === "key") ? key : (p2 === "value") ? value : "";
-				// remove separator if there is no v2 element
-				sep = (v2) ? sep : "";
-				results.push(v1+sep+v2);
-			}
-		}
+
+				if (regexpString) {
+					pass = (useKey) ? regexp.exec(key) : (useValue) ? regexp.exec(value) : false;
+					if (pass) {
+						results.push(v1+sep+v2);
+					}
+				} else {
+					results.push(v1+sep+v2);
+				} // if regexpstring
+			} // for
+		} // if data
 	});
-//	results.sort();
 	return results;
 };
 })();
