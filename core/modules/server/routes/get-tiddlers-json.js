d26750218e8daca7f93c32ef356404698be6bcd5
bimlas <bimbalaszlo@gmail.com>
2019-07-03 23:29:43 +0200

    Merge filtered and unfiltered API routes

--- core/modules/server/routes/get-tiddlers-json.js
+++ core/modules/server/routes/get-tiddlers-json.js
@@ -12,24 +12,57 @@ GET /recipes/default/tiddlers/tiddlers.json
 /*global $tw: false */
 "use strict";
 
+function getTiddlerFields(tiddler, state, title) {
+	var tiddlerFields = {};
+	$tw.utils.each(tiddler.fields, function (field, name) {
+		if (name !== "text") {
+			tiddlerFields[name] = tiddler.getFieldString(name);
+		}
+	});
+	tiddlerFields.revision = state.wiki.getChangeCount(title);
+	tiddlerFields.type = tiddlerFields.type || "text/vnd.tiddlywiki";
+	return tiddlerFields;
+}
+
+function getAllTiddlers(state) {
+	var tiddlers = [];
+	state.wiki.forEachTiddler({sortField: "title"},function(title,tiddler) {
+		var tiddlerFields = getTiddlerFields(tiddler, state, title);
+		tiddlers.push(tiddlerFields);
+	});
+	return tiddlers;
+}
+
+function getFilteredTiddlers(state, filter) {
+	var tiddlers = [];
+	$tw.utils.each(
+		state.wiki.filterTiddlers(filter),
+		function(title) {
+			var tiddler = state.wiki.getTiddler(title);
+			if (!tiddler) {
+				return;
+			}
+			var tiddlerFields = getTiddlerFields(tiddler, state, title);
+			tiddlers.push(tiddlerFields);
+		}
+	);
+	return tiddlers;
+}
+
 exports.method = "GET";
 
-exports.path = /^\/recipes\/default\/tiddlers.json$/;
+exports.path = /^\/recipes\/default\/tiddlers.json(:.*)?$/;
 
 exports.handler = function(request,response,state) {
 	response.writeHead(200, {"Content-Type": "application/json"});
-	var tiddlers = [];
-	state.wiki.forEachTiddler({sortField: "title"},function(title,tiddler) {
-		var tiddlerFields = {};
-		$tw.utils.each(tiddler.fields,function(field,name) {
-			if(name !== "text") {
-				tiddlerFields[name] = tiddler.getFieldString(name);
-			}
-		});
-		tiddlerFields.revision = state.wiki.getChangeCount(title);
-		tiddlerFields.type = tiddlerFields.type || "text/vnd.tiddlywiki";
-		tiddlers.push(tiddlerFields);
-	});
+	var filter = state.params[0];
+	var tiddlers;
+	if(filter) {
+		filter = decodeURIComponent(state.params[0]).substring(1);
+		tiddlers = getFilteredTiddlers(state, filter);
+	} else {
+		tiddlers = getAllTiddlers(state);
+	}
 	var text = JSON.stringify(tiddlers);
 	response.end(text,"utf8");
 };
