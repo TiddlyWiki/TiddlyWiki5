38c1bca5626e1c169763278603325b973984baf5
Simon Huber <huber.simon@protonmail.com>
2019-07-16 09:36:36 +0200

    add methods to generate unique "transclusion-footprints" ...
    ... by finding the first widget that has a different transclusion variable than the current one and counting its children, concatenating the resulting children-counts
    
    this is used in factory.js to generate unique strings by concatenating this footprint with a qualified state

--- core/modules/widgets/widget.js
+++ core/modules/widgets/widget.js
@@ -574,6 +574,30 @@ Widget.prototype.allowActionPropagation = function() {
 	return true;
 };
 
+Widget.prototype.findParentTransclusionWidget = function() {
+	var node = this;
+	var transclusionVariable = this.getVariable("transclusion");
+	while(node.getVariable("transclusion") === transclusionVariable) {
+		node = node.parentWidget;
+	}
+	return node !== this ? node : null;
+};
+
+Widget.prototype.generateTransclusionFootprint = function() {
+	var parentTransclusionWidget = this.findParentTransclusionWidget(),
+	    node = this,
+	    footprint = node.parentWidget.children.indexOf(node);
+	while(node) {
+		node = node.parentWidget;
+		if(node === parentTransclusionWidget) {
+			break;
+		} else if(node.parentWidget && node.parentWidget.children) {
+			footprint = footprint + "-" + node.parentWidget.children.indexOf(node);
+		}
+	}
+	return footprint;
+};
+
 exports.widget = Widget;
 
 })();
