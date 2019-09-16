8c894612914e21cf941a1daa953538c28ce91d8e
Jeremy Ruston <jeremy@jermolene.com>
2019-09-16 16:16:03 +0100

    Adding [is[binary]] filter -- Missed off 1150c87ed

---
+++ core/modules/filters/is/binary.js
@@ -0,0 +1,36 @@
+/*\
+title: $:/core/modules/filters/is/binary.js
+type: application/javascript
+module-type: isfilteroperator
+
+Filter function for [is[binary]]
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Export our filter function
+*/
+exports.binary = function(source,prefix,options) {
+	var results = [];
+	if(prefix === "!") {
+		source(function(tiddler,title) {
+			if(!options.wiki.isBinaryTiddler(title)) {
+				results.push(title);
+			}
+		});
+	} else {
+		source(function(tiddler,title) {
+			if(options.wiki.isBinaryTiddler(title)) {
+				results.push(title);
+			}
+		});
+	}
+	return results;
+};
+
+})();
