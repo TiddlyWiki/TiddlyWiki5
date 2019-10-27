a429f3f672970d23bf2e73c5836444a762313bd9
heronils <nilsherolindemann@gmail.com>
2019-10-27 05:47:35 +0100

    Wait, we actually dot need these here

--- editions/tw5.com/Create Single Tiddler - Master.bat
+++
@@ -1,3 +0,0 @@
-echo off
-node ../../tiddlywiki.js ../tw5.com --rendertiddler $:/core/save/all single-file-master.html text/plain
-echo DONE
--- editions/tw5.com/Create Static HTML - Master.bat
+++
@@ -1,3 +0,0 @@
-echo off
-node ../../tiddlywiki.js ../tw5.com --rendertiddler $:/core/templates/alltiddlers.template.html static-output-master.html text/plain
-echo DONE
