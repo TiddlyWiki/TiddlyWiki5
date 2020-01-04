090e8128164d35a3b4dfd0179367e5f1c0cea822
pmario <pmariojo@gmail.com>
2020-01-04 11:57:10 +0100

    add a new-line before the log text to increase readability of the test output

--- core/modules/filters.js
+++ core/modules/filters.js
@@ -82,7 +82,7 @@ function parseFilterOperation(operators,filterString,p) {
 				if(rexMatch) {
 					operator.regexp = new RegExp(rexMatch[1], rexMatch[2]);
 // DEPRECATION WARNING
-console.log("WARNING: Filter",operator.operator,"has a deprecated regexp operand",operator.regexp);
+console.log("\nWARNING: Filter",operator.operator,"has a deprecated regexp operand",operator.regexp);
 					nextBracketPos = p + rex.lastIndex - 1;
 				}
 				else {
