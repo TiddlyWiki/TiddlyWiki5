cd2948c2eca7dbf0f5d658e61d6821402ee5359d
pmario <pmariojo@gmail.com>
2020-01-31 23:47:00 +0100

    add $template and $overwrite parameter

--- core/modules/widgets/action-createtiddler.js
+++ core/modules/widgets/action-createtiddler.js
@@ -9,7 +9,7 @@ Action widget to create a new tiddler with a unique name and specified fields.
 (function(){
 
 /*jslint node: true, browser: true */
-/*global $tw: false */
+/*global $tw:false, require:false, exports:false */
 "use strict";
 
 var Widget = require("$:/core/modules/widgets/widget.js").widget;
@@ -36,9 +36,15 @@ Compute the internal state of the widget
 */
 CreateTiddlerWidget.prototype.execute = function() {
 	this.actionBaseTitle = this.getAttribute("$basetitle");
+	this.hasBase = !!this.actionBaseTitle;
 	this.actionSaveTitle = this.getAttribute("$savetitle");
 	this.actionSaveDraftTitle = this.getAttribute("$savedrafttitle");
 	this.actionTimestamp = this.getAttribute("$timestamp","yes") === "yes";
+	//Following params are new since 5.1.22
+	this.actionTemplate = this.getAttribute("$template");
+	this.useTemplate = !!this.actionTemplate;
+	this.actionOverwrite = this.getAttribute("$overwrite","no");
+
 };
 
 /*
@@ -57,7 +63,7 @@ CreateTiddlerWidget.prototype.refresh = function(changedTiddlers) {
 Invoke the action associated with this widget
 */
 CreateTiddlerWidget.prototype.invokeAction = function(triggeringWidget,event) {
-	var title = this.wiki.generateNewTitle(this.actionBaseTitle),
+	var title = this.wiki.getTiddlerText("$:/language/DefaultNewTiddlerTitle"), // Get the initial new-tiddler title
 		fields = {},
 		creationFields,
 		modificationFields;
@@ -70,7 +76,22 @@ CreateTiddlerWidget.prototype.invokeAction = function(triggeringWidget,event) {
 		creationFields = this.wiki.getCreationFields();
 		modificationFields = this.wiki.getModificationFields();
 	}
-	var tiddler = this.wiki.addTiddler(new $tw.Tiddler(creationFields,fields,modificationFields,{title: title}));
+	if(this.hasBase && this.actionOverwrite === "no") {
+		title = this.wiki.generateNewTitle(this.actionBaseTitle);
+	} else if (this.hasBase && this.actionOverwrite === "yes") {
+		title = this.actionBaseTitle
+	}
+	// NO $basetitle BUT $template parameter is available
+	// the title MUST be unique, otherwise the template would be overwritten
+	if (!this.hasBase && this.useTemplate) {
+		title = this.wiki.generateNewTitle(this.actionTemplate);
+	} else if (!this.hasBase && !this.useTemplate) {
+		// If NO $basetitle AND NO $template use initial title
+		// DON'T overwrite any stuff
+		title = this.wiki.generateNewTitle(title);
+	}
+	var templateTiddler = this.wiki.getTiddler(this.actionTemplate) || {};
+	var tiddler = this.wiki.addTiddler(new $tw.Tiddler(templateTiddler.fields,creationFields,fields,modificationFields,{title: title}));
 	if(this.actionSaveTitle) {
 		this.wiki.setTextReference(this.actionSaveTitle,title,this.getVariable("currentTiddler"));
 	}
