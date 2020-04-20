TiddlyWiki/TiddlyWiki5

commit 4ea79cb0c34fa488dfa3e6782f1f3c3fa57237f5

fix(syncer): restore tiddlerInfo param to saveTiddler (#4584)
This was added on a159b5baf3ad91d8defc68cbf81c78d01b69c416 and lost in #4373.
Will be a good idea to introduce tests against this kind of changes

core/modules/syncer.js
@@ -581,6 +581,8 @@ SaveTiddlerTask.prototype.run = function(callback) {
 			};
 			// Invoke the callback
 			callback(null);
+		},{
+			tiddlerInfo: self.tiddlerInfo[task.title]
 		});
 	} else {
 		this.syncer.logger.log(" Not Dispatching 'save' task:",this.title,"tiddler does not exist");

commit 2649d2f7dc36f9a7d4e473e4238b6beee6be3b3b

Syncer: Fix typos
Fixes a bug introduced in #4584

core/modules/syncer.js
@@ -584,7 +584,7 @@ SaveTiddlerTask.prototype.run = function(callback) {
 			// Invoke the callback
 			callback(null);
 		},{
-			tiddlerInfo: self.tiddlerInfo[task.title]
+			tiddlerInfo: self.syncer.tiddlerInfo[self.title]
 		});
 	} else {
 		this.syncer.logger.log(" Not Dispatching 'save' task:",this.title,"tiddler does not exist");