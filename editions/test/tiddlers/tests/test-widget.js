2be4020e3897d7fa102b82ff86b20e64f2a64161
Cameron Fischer <fischer.cameron@gmail.com>
2020-04-22 23:46:15 -0400

    Fixed issue: dead variables in import might linger

--- editions/test/tiddlers/tests/test-widget.js
+++ editions/test/tiddlers/tests/test-widget.js
@@ -518,6 +518,41 @@ describe("Widget module", function() {
 		expect(wrapper.innerHTML).toBe("<p>A B C D</p>");
 	});
 
+	it("import doesn't hold onto dead variables", function() {
+		var wiki = new  $tw.Wiki();
+		wiki.addTiddlers([
+			{title: "ABC", text: "\\define A() A\n\\define B() B\n<$set name=C value=C>\n\n</$set>"},
+			{title: "DE", text: "\\define D() D\n\\define E() E"}]);
+		var text = "\\import ABC DE\ncontent";
+		var widgetNode = createWidgetNode(parseText(text,wiki),wiki);
+		// Render the widget node to the DOM
+		renderWidgetNode(widgetNode);
+		var childNode = widgetNode;
+		while (childNode.children.length > 0) {
+			childNode = childNode.children[0];
+		}
+		// First make sure A and B imported
+		expect(childNode.getVariable("A")).toBe("A");
+		expect(childNode.getVariable("B")).toBe("B");
+		expect(childNode.getVariable("C")).toBe("C");
+		expect(childNode.getVariable("D")).toBe("D");
+		expect(childNode.getVariable("E")).toBe("E");
+		// Then change A and remove B
+		wiki.addTiddler({title: "ABC", text: "\\define A() A2\n<$set name=C value=C2>\n\n</$set>"});
+		wiki.addTiddler({title: "DE", text: "\\define D() D2"});
+		widgetNode.refresh({"ABC": {modified: true}, "DE": {modified: true}});
+		var childNode = widgetNode;
+		while (childNode.children.length > 0) {
+			childNode = childNode.children[0];
+		}
+		// Make sure \import recognized changes and deletions
+		expect(childNode.getVariable("A")).toBe("A2");
+		expect(childNode.getVariable("B")).toBe(undefined);
+		expect(childNode.getVariable("C")).toBe("C2");
+		expect(childNode.getVariable("D")).toBe("D2");
+		expect(childNode.getVariable("E")).toBe(undefined);
+	});
+
 	/** Special case. <$importvariables> has different handling if
 	 *  it doesn't end up importing any variables. Make sure it
 	 *  doesn't forget its childrenNodes.
