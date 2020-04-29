85c55883274b53cf99bf037f398309f9d13dca40
Arlen22 <arlenbee@gmail.com>
2020-04-29 13:24:30 -0400

    Use the host string from tiddlyweb

--- plugins/tiddlywiki/tiddlyweb/sse-client.js
+++ plugins/tiddlywiki/tiddlyweb/sse-client.js
@@ -17,7 +17,14 @@ exports.after = ["startup"];
 exports.synchronous = true;
 exports.platforms = ["browser"];
 exports.startup = function () {
-  var events = new EventSource("/events/plugins/tiddlywiki/tiddlyweb");
+  //make sure we're actually being used
+  if($tw.syncadaptor.name !== "tiddlyweb") return;
+  //get the mount point in case a path prefix is used
+  var host = $tw.syncadaptor.getHost();
+  //make sure it ends with a slash (it usually does)
+  if(!host.endsWith("/")) host += "/";
+  //setup the event listener 
+  var events = new EventSource(host + "events/plugins/tiddlywiki/tiddlyweb");
   events.addEventListener("change", function () {
     $tw.syncer.syncFromServer();
   });
