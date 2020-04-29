8da6630aee05d5f6fbfc04d58c73d84a86fb6f0c
Arlen22 <arlenbee@gmail.com>
2020-04-29 12:49:59 -0400

    Implement server sent events

---
+++ plugins/tiddlywiki/tiddlyweb/sse-server.js
@@ -0,0 +1,29 @@
+/*\
+title: $:/plugins/tiddlywiki/tiddlyweb/sse-server.js
+type: application/javascript
+module-type: route
+
+GET /events/plugins/tiddlywiki/tiddlyweb
+
+\*/
+var ServerSentEvents = require("$:/core/modules/server/server-sent-events.js").ServerSentEvents;
+/** @type {Record<any, {
+ *   request: import("http").IncomingMessage, 
+ *   state, 
+ *   emit: (event: string, data: string) => void, 
+ *   end: () => void
+ * }[]>} */
+var wikis = {};
+function setupWiki(wiki) {
+  wikis[wiki] = [];
+  wiki.addEventListener("change", function (changes) {
+    wikis[wiki].forEach(function (item) {
+      item.emit("change", JSON.stringify(changes));
+    });
+  });
+}
+var events = new ServerSentEvents("plugins/tiddlywiki/tiddlyweb", (request, state, emit, end) => {
+  if (!wikis[state.wiki]) setupWiki(state.wiki);
+  wikis[state.wiki].push({ request: request, state: state, emit: emit, end: end });
+});
+module.exports = events.getExports();
