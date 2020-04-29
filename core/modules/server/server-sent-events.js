eca1147ddc1a5e761bba3218d6945855f581d17b
Arlen22 <arlenbee@gmail.com>
2020-04-29 13:02:09 -0400

    Convert to ES5 and wrap in function

--- core/modules/server/server-sent-events.js
+++ core/modules/server/server-sent-events.js
@@ -3,65 +3,73 @@ title: $:/core/modules/server/server-sent-events.js
 type: application/javascript
 module-type: library
 \*/
-class ServerSentEvents {
-  /**
-   * @param {string} prefix 
-   * Usually the plugin path, such as `plugins/tiddlywiki/tiddlyweb`. 
-   * The route will match `/events/${prefix}` exactly.
-   * @param {(
-   *   request: import("http").IncomingMessage, 
-   *   state, 
-   *   emit: (event: string, data: string) => void, 
-   *   end: () => void
-   * ) => void} handler 
-   * A function that will be called each time a request 
-   * comes in with the request and state from the 
-   * route and an emit function to call.
-   */
-  constructor(prefix, handler) {
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/**
+ * @param {string} prefix
+ * Usually the plugin path, such as `plugins/tiddlywiki/tiddlyweb`.
+ * The route will match `/events/${prefix}` exactly.
+ * @param {(
+ *   request: import("http").IncomingMessage,
+ *   state,
+ *   emit: (event: string, data: string) => void,
+ *   end: () => void
+ * ) => void} handler
+ * A function that will be called each time a request
+ * comes in with the request and state from the
+ * route and an emit function to call.
+ */
+var ServerSentEvents = /** @class */ (function () {
+
+  function ServerSentEvents(prefix, handler) {
     this.handler = handler;
     this.prefix = prefix;
   }
-  getExports() {
+  ServerSentEvents.prototype.getExports = function () {
     return {
       bodyFormat: "stream",
       method: "GET",
       path: new RegExp("^/events/" + this.prefix + "$"),
       handler: this.handleEventRequest.bind(this)
-    }
-  }
+    };
+  };
   /**
-   * 
-   * @param {import("http").IncomingMessage} request 
-   * @param {import("http").ServerResponse} response 
-   * @param {*} state 
+   *
+   * @param {import("http").IncomingMessage} request
+   * @param {import("http").ServerResponse} response
+   * @param {*} state
    */
-  handleEventRequest(request, response, state) {
+  ServerSentEvents.prototype.handleEventRequest = function (request, response, state) {
     if (request.headers.accept && request.headers.accept.startsWith('text/event-stream')) {
       response.writeHead(200, {
         'Content-Type': 'text/event-stream',
         'Cache-Control': 'no-cache',
         'Connection': 'keep-alive'
       });
-
       this.handler(request, state, this.emit.bind(this, response), this.end.bind(this, response));
-    } else {
+    }
+    else {
       response.writeHead(406, "Not Acceptable", {});
       response.end();
     }
-  }
-  emit(response, event, data) {
-
+  };
+  ServerSentEvents.prototype.emit = function (response, event, data) {
     if (typeof event !== "string" || event.indexOf("\n") !== -1)
       throw new Error("type must be a single-line string");
-
     if (typeof data !== "string" || data.indexOf("\n") !== -1)
       throw new Error("data must be a single-line string");
-
-    response.write(`event: ${event}\ndata: ${data}\n\n`, "utf8");
-  }
-  end(response) {
+    response.write("event: " + event + "\ndata: " + data + "\n\n", "utf8");
+  };
+  ServerSentEvents.prototype.end = function (response) {
     response.end();
-  }
-}
+  };
+  return ServerSentEvents;
+}());
+
 exports.ServerSentEvents = ServerSentEvents;
+
+})();
--- plugins/tiddlywiki/tiddlyweb/sse-server.js
+++ plugins/tiddlywiki/tiddlyweb/sse-server.js
@@ -6,11 +6,17 @@ module-type: route
 GET /events/plugins/tiddlywiki/tiddlyweb
 
 \*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
 var ServerSentEvents = require("$:/core/modules/server/server-sent-events.js").ServerSentEvents;
 /** @type {Record<any, {
- *   request: import("http").IncomingMessage, 
- *   state, 
- *   emit: (event: string, data: string) => void, 
+ *   request: import("http").IncomingMessage,
+ *   state,
+ *   emit: (event: string, data: string) => void,
  *   end: () => void
  * }[]>} */
 var wikis = {};
@@ -22,8 +28,11 @@ function setupWiki(wiki) {
     });
   });
 }
-var events = new ServerSentEvents("plugins/tiddlywiki/tiddlyweb", (request, state, emit, end) => {
-  if (!wikis[state.wiki]) setupWiki(state.wiki);
+var events = new ServerSentEvents("plugins/tiddlywiki/tiddlyweb", function (request, state, emit, end) {
+  if (!wikis[state.wiki])
+    setupWiki(state.wiki);
   wikis[state.wiki].push({ request: request, state: state, emit: emit, end: end });
 });
 module.exports = events.getExports();
+
+})();
