e9bae9df480d9a597503a17e7fdd6e9437f40fa6
tobidot <16611056+idotobi@users.noreply.github.com>
2020-05-02 11:44:10 +0200

    Move non-tiddler functionality to existing route
    Move the functionality to include non-tiddlers (useful for obtaining all
    tags) from the previously new created route `/recipes/default/tags.json`
    to the already existing route `/recipes/default/tiddlers.json`.

--- core/modules/server/routes/get-tiddlers-json.js
+++ core/modules/server/routes/get-tiddlers-json.js
@@ -13,6 +13,7 @@ GET /recipes/default/tiddlers/tiddlers.json?filter=<filter>
 "use strict";
 
 var DEFAULT_FILTER = "[all[tiddlers]!is[system]sort[title]]";
+var DEFAULT_INCLUDE_NON_TIDDLERS = false;
 
 exports.method = "GET";
 
@@ -20,6 +21,7 @@ exports.path = /^\/recipes\/default\/tiddlers.json$/;
 
 exports.handler = function(request,response,state) {
 	var filter = state.queryParameters.filter || DEFAULT_FILTER;
+	var include_non_tiddlers = state.queryParameters.include_non_tiddlers || DEFAULT_INCLUDE_NON_TIDDLERS;
 	if($tw.wiki.getTiddlerText("$:/config/Server/AllowAllExternalFilters") !== "yes") {
 		if($tw.wiki.getTiddlerText("$:/config/Server/ExternalFilters/" + filter) !== "yes") {
 			console.log("Blocked attempt to GET /recipes/default/tiddlers/tiddlers.json with filter: " + filter);
@@ -31,17 +33,24 @@ exports.handler = function(request,response,state) {
 	var excludeFields = (state.queryParameters.exclude || "text").split(","),
 		titles = state.wiki.filterTiddlers(filter);
 	response.writeHead(200, {"Content-Type": "application/json"});
-	var tiddlers = [];
+	var response_body = [];
 	$tw.utils.each(titles,function(title) {
 		var tiddler = state.wiki.getTiddler(title);
 		if(tiddler) {
 			var tiddlerFields = tiddler.getFieldStrings({exclude: excludeFields});
 			tiddlerFields.revision = state.wiki.getChangeCount(title);
 			tiddlerFields.type = tiddlerFields.type || "text/vnd.tiddlywiki";
-			tiddlers.push(tiddlerFields);
+			response_body.push(tiddlerFields);
+		} else {
+			if(include_non_tiddlers) {
+				let entry = {};
+				entry.title = title
+				entry.non_tiddler = true;
+				response_body.push(entry);
+			}
 		}
 	});
-	var text = JSON.stringify(tiddlers);
+	var text = JSON.stringify(response_body);
 	response.end(text,"utf8");
 };
 
