d0a9ade65414a4de586436631244057988c1f378
Joshua Fontany <joshua.fontany@gmail.com>
2020-05-14 19:46:44 -0700

    cleanup

--- plugins/tiddlywiki/filesystem/filesystemadaptor.js
+++ plugins/tiddlywiki/filesystem/filesystemadaptor.js
@@ -100,34 +100,28 @@ Save a tiddler and invoke the callback with (err,adaptorInfo,revision)
 FileSystemAdaptor.prototype.saveTiddler = function(tiddler,callback) {
 	var self = this;
 	this.getTiddlerFileInfo(tiddler,function(err,fileInfo,options) {
+		options = options || {};
 		if (err) {
 			return callback(err);
 		}
-		if (options && options.fileInfo !== null && typeof options.fileInfo !== "undefined") {
-			// The file's info (location, extension, hasMetaFile) has changed,
-			// call deleteTiddler via options
+		if (options.fileInfo !== null && typeof options.fileInfo !== "undefined") {
+			// New fileInfo (location, extension, hasMetaFile), call deleteTiddler via options
 			$tw.utils.saveTiddlerToFile(tiddler,fileInfo,function(err) {
 				if(err) {
 					return callback(err);
 				}
 				// Check if only the hasMetaFile value has changed
-				if( (fileInfo.filepath === options.fileInfo.filepath)
-					&& (fileInfo.hasMetaFile !== options.fileInfo.hasMetaFile)
-				) {
-					try {
-						if (fileInfo.hasMetaFile === false && options.fileInfo.hasMetaFile === true) {
-							fs.unlink(options.fileInfo.filepath + ".meta",function(err) {
-								if(err) {
-									return callback(err);
-								}
-								return $tw.utils.deleteEmptyDirs(path.dirname(options.fileInfo.filepath),callback);
-							});
-						} else {
-							return callback(null)
-						}						
-					} catch (error) {
-						return callback(error);
-					}
+				if(fileInfo.filepath === options.fileInfo.filepath) {
+					if (fileInfo.hasMetaFile === false && options.fileInfo.hasMetaFile === true) {
+						fs.unlink(options.fileInfo.filepath + ".meta",function(err) {
+							if(err) {
+								return callback(err);
+							}
+							return $tw.utils.deleteEmptyDirs(path.dirname(options.fileInfo.filepath),callback);
+						});
+					} else {
+						return callback(null)
+					}						
 				} else {
 					self.deleteTiddler(null,callback,options);
 				}	
