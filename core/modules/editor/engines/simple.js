960d6b3ae603a9aabf86aeed19d3c92c59ef8670
Simon Huber <huber.simon@protonmail.com>
2020-05-16 16:29:02 +0200

    Update simple.js

--- core/modules/editor/engines/simple.js
+++ core/modules/editor/engines/simple.js
@@ -67,7 +67,7 @@ Set the text of the engine if it doesn't currently have focus
 */
 SimpleEngine.prototype.setText = function(text,type) {
 	if(!this.domNode.isTiddlyWikiFakeDom) {
-		if(this.domNode.value !== text) {
+		if(this.domNode.ownerDocument.activeElement !== this.domNode || text === "") {
 			this.updateDomNodeText(text);
 		}
 		// Fix the height if needed
