91c73e1627cc4f5e3030673d491383c44dd8a27a
ento <ento+github@i.pearlwaffles.xyz>
2020-08-28 09:51:39 -0800

    Rename TRAVIS_BRANCH to TW5_BUILD_BRANCH
    Since we don't use Travis anymore

--- bin/ci-push.sh
+++ bin/ci-push.sh
@@ -11,7 +11,7 @@ git config --global user.name "GitHub Actions" || exit 1
 
 git add -A . || exit 1
 
-git commit --message "GitHub build: $GITHUB_RUN_NUMBER of $TRAVIS_BRANCH ($(date +'%F %T %Z'))" || exit 1
+git commit --message "GitHub build: $GITHUB_RUN_NUMBER of $TW5_BUILD_BRANCH ($(date +'%F %T %Z'))" || exit 1
 
 git remote add deploy "https://$GH_TOKEN@github.com/Jermolene/jermolene.github.io.git" &>/dev/null || exit 1
 
