87dc67d0cd55454ff65a90ffa8839b4caef6e3b5
jeremy@jermolene.com <jeremy@jermolene.com>
2020-09-08 12:59:37 +0100

    Update CI scripts

---
+++ bin/ci-push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Push output back to GitHub
+
+
+cd output || exit 1
+
+git config --global user.email "actions@github.com" || exit 1
+
+git config --global user.name "GitHub Actions" || exit 1
+
+git add -A . || exit 1
+
+git commit --message "GitHub build: $GITHUB_RUN_NUMBER of $TW5_BUILD_BRANCH ($(date +'%F %T %Z'))" || exit 1
+
+git remote add deploy "https://$GH_TOKEN@github.com/Jermolene/jermolene.github.io.git" &>/dev/null || exit 1
+
+git push deploy master &>/dev/null || exit 1
+
+cd .. || exit 1
