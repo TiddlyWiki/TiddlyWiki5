7dd90bf0effa6e98f3dbf1b422695d7144ea9829
Robin Munn <rmunn@pobox.com>
2020-10-09 15:36:36 +0700

    Simplify ci-push.sh: end script on any error
    Use the bash -e option to exit on any error, so || exit 1 is not needed.
    
    As a bonus, this lets us get the actual return code from any failures.

--- bin/ci-push.sh
+++ bin/ci-push.sh
@@ -2,19 +2,14 @@
 
 # Push output back to GitHub
 
+# Exit script immediately if any command fails
+set -e
 
-cd output || exit 1
-
-git config --global user.email "actions@github.com" || exit 1
-
-git config --global user.name "GitHub Actions" || exit 1
-
-git add -A . || exit 1
-
-git commit --message "GitHub build: $GITHUB_RUN_NUMBER of $TW5_BUILD_BRANCH ($(date +'%F %T %Z'))" || exit 1
-
-git remote add deploy "https://$GH_TOKEN@github.com/Jermolene/jermolene.github.io.git" &>/dev/null || exit 1
-
-git push deploy master &>/dev/null || exit 1
-
-cd .. || exit 1
+cd output
+git config --global user.email "actions@github.com"
+git config --global user.name "GitHub Actions"
+git add -A .
+git commit --message "GitHub build: $GITHUB_RUN_NUMBER of $TW5_BUILD_BRANCH ($(date +'%F %T %Z'))"
+git remote add deploy "https://$GH_TOKEN@github.com/Jermolene/jermolene.github.io.git" &>/dev/null
+git push deploy master &>/dev/null
+cd ..
