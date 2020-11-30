9423ec4e7b44b08b3f61f8d348982e06bbfcf42e
Joshua Fontany <joshua.fontany@gmail.com>
2020-11-29 18:03:40 -0800

    test for wikiPath, not wikiPath/tiddlers

--- core/modules/utils/filesystem.js
+++ core/modules/utils/filesystem.js
@@ -368,7 +368,7 @@ exports.generateTiddlerFilepath = function(title,options) {
 		count++;
 	} while(fs.existsSync(fullPath));
 	// Only return full paths that start with the Wiki's directory
-	fullPath = (fullPath.indexOf(directory) == 0) ? fullPath: path.resolve(directory, path.normalize(fullPath).encodeURIComponent());
+	fullPath = (fullPath.indexOf($tw.boot.wikiPath) == 0) ? fullPath: path.resolve(directory, path.normalize(fullPath).encodeURIComponent());
 	// Return the full path to the file
 	return fullPath;
 };
