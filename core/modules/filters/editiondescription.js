df969218d4f0773218d5bfb744190ddeb2c4cf93
Joshua Fontany <joshua.fontany@gmail.com>
2020-12-06 16:10:45 -0800

    only run if $tw.node, else return empty set

--- core/modules/filters/editiondescription.js
+++ core/modules/filters/editiondescription.js
@@ -16,14 +16,16 @@ Filter operator for returning the descriptions of the specified edition names
 Export our filter function
 */
 exports.editiondescription = function(source,operator,options) {
-	var results = [],
-		editionInfo = $tw.utils.getEditionInfo();
-	if(editionInfo) {
-		source(function(tiddler,title) {
-			if($tw.utils.hop(editionInfo,title)) {
-				results.push(editionInfo[title].description || "");				
-			}
-		});
+	var results = [];
+	if($tw.node) {
+		var editionInfo = $tw.utils.getEditionInfo();
+		if(editionInfo) {
+			source(function(tiddler,title) {
+				if($tw.utils.hop(editionInfo,title)) {
+					results.push(editionInfo[title].description || "");				
+				}
+			});
+		}
 	}
 	return results;
 };
