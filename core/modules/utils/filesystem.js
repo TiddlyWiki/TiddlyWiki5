07c1416e86f7a63a66e044c0d72bb51cb0c53851
jed <inmysocks@fastmail.com>
2020-12-12 19:14:06 +0100

    Add a hook to allow modifying the the behaviour creating tiddler paths
    This is needed for Bob to use the core to generate tiddler fileInfo 
    
    I don't know if this is the best way to make the hook, but it works for what I need

--- core/modules/utils/filesystem.js
+++ core/modules/utils/filesystem.js
@@ -382,13 +382,15 @@ exports.generateTiddlerFilepath = function(title,options) {
 		count++;
 	} while(fs.existsSync(fullPath));
 	//If the path does not start with the wikiPath directory or the wikiTiddlersPath directory, or if the last write failed
-	var encode = !(fullPath.indexOf(path.resolve($tw.boot.wikiPath)) == 0 || fullPath.indexOf($tw.boot.wikiTiddlersPath) == 0) || ((options.fileInfo || {writeError: false}).writeError == true);
+	var newPath;
+	var encode = !(fullPath.indexOf($tw.boot.wikiPath) == 0 || fullPath.indexOf($tw.boot.wikiTiddlersPath) == 0) || ((options.fileInfo || {writeError: false}).writeError == true);
 	if(encode){
 		//encodeURIComponent() and then resolve to tiddler directory
-		fullPath = path.resolve(directory, encodeURIComponent(fullPath));
+		newPath = path.resolve(directory, encodeURIComponent(fullPath));
 	}
+	var alternatePath = $tw.hooks.invokeHook("th-make-tiddler-path", null, fullPath);
 	// Return the full path to the file
-	return fullPath;
+	return alternatePath || newPath || fullPath;
 };
 
 /*
