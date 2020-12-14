2c1337bab959fe4f61594d3c1bdd6bee663d01c0
Nicolas Petton <nicolas@petton.fr>
2020-12-14 13:54:39 +0100

    Fix formatting of server-sent-events.js
    Fix formatting and comments following TW core guidelines.

--- core/modules/server/server-sent-events.js
+++ core/modules/server/server-sent-events.js
@@ -9,24 +9,19 @@ module-type: library
 /*global $tw: false */
 "use strict";
 
-/**
- * @param {string} prefix
- * Usually the plugin path, such as `plugins/tiddlywiki/tiddlyweb`.
- * The route will match `/events/${prefix}` exactly.
- * @param {(
- *   request: import("http").IncomingMessage,
- *   state,
- *   emit: (event: string, data: string) => void,
- *   end: () => void
- * ) => void} handler
- * A function that will be called each time a request
- * comes in with the request and state from the
- * route and an emit function to call.
- */
+/*
+parameters:
+		prefix - usually the plugin path, such as `plugins/tiddlywiki/tiddlyweb`. The
+			route will match `/events/${prefix}` exactly.
+
+		handler - a function that will be called each time a request comes in with the
+			request and state from the route and an emit function to call.
+*/
+
 var ServerSentEvents = function ServerSentEvents(prefix, handler) {
   this.handler = handler;
   this.prefix = prefix;
-}
+};
 
 ServerSentEvents.prototype.getExports = function() {
   return {
@@ -37,27 +32,26 @@ ServerSentEvents.prototype.getExports = function() {
   };
 };
 
-/**
- *
- * @param {import("http").IncomingMessage} request
- * @param {import("http").ServerResponse} response
- * @param {*} state
- */
-ServerSentEvents.prototype.handleEventRequest = function(request, response, state) {
+ServerSentEvents.prototype.handleEventRequest = function(request,response,state) {
   if(request.headers.accept && request.headers.accept.startsWith("text/event-stream")) {
     response.writeHead(200, {
       "Content-Type": "text/event-stream",
       "Cache-Control": "no-cache",
       "Connection": "keep-alive"
     });
-    this.handler(request, state, this.emit.bind(this, response), this.end.bind(this, response));
+    this.handler(request,state,this.emit.bind(this,response),this.end.bind(this,response));
   } else {
-    response.writeHead(406, "Not Acceptable", {});
+    response.writeHead(406,"Not Acceptable",{});
     response.end();
   }
 };
 
-ServerSentEvents.prototype.emit = function(response, event, data) {
+ServerSentEvents.prototype.isEventStreamRequest = function(request) {
+	return request.headers.accept &&
+		request.headers.accept.match(/^text\/event-stream/);
+};
+
+ServerSentEvents.prototype.emit = function(response,event,data) {
   if(typeof event !== "string" || event.indexOf("\n") !== -1) {
     throw new Error("Type must be a single-line string");
   }
@@ -70,7 +64,7 @@ ServerSentEvents.prototype.emit = function(response, event, data) {
 ServerSentEvents.prototype.end = function(response) {
   response.end();
 };
-  
+
 exports.ServerSentEvents = ServerSentEvents;
 
 })();
