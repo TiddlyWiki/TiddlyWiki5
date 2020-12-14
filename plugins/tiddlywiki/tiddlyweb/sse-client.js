aee5cb404f285f9bf2cbd0c1fa813cc7fca469f7
Nicolas Petton <nicolas@petton.fr>
2020-12-14 13:27:41 +0100

    Fix formatting of tiddlyweb/sse-client.js
    Fix formatting according to TW core.

--- plugins/tiddlywiki/tiddlyweb/sse-client.js
+++ plugins/tiddlywiki/tiddlyweb/sse-client.js
@@ -17,30 +17,34 @@ exports.after = ["startup"];
 exports.synchronous = true;
 exports.platforms = ["browser"];
 exports.startup = function() {
-  // Make sure we're actually being used
-  if($tw.syncadaptor.name !== "tiddlyweb") return;
-  // Get the mount point in case a path prefix is used
-  var host = $tw.syncadaptor.getHost();
-  // Make sure it ends with a slash (it usually does)
-  if(host[host.length - 1] !== "/") host += "/";
-  // Setup the event listener 
-  setupEvents(host);
-}
+	// Make sure we're actually being used
+	if($tw.syncadaptor.name !== "tiddlyweb") {
+		return;
+	}
+	// Get the mount point in case a path prefix is used
+	var host = $tw.syncadaptor.getHost();
+	// Make sure it ends with a slash (it usually does)
+	if(host[host.length - 1] !== "/") {
+		host += "/";
+	}
+	// Setup the event listener
+	setupEvents(host);
+};
 
-function setupEvents(host){
-  var events = new EventSource(host + "events/plugins/tiddlywiki/tiddlyweb");
-  var timeout = null;
-  events.addEventListener("change", function() {
-    if(timeout) clearTimeout(timeout);
-    timeout = setTimeout(function(){
-      $tw.syncer.syncFromServer();
-    }, $tw.syncer.throttleInterval);
-  });
-  events.onerror = function() {
-    events.close();
-    setTimeout(function() { 
-      setupEvents(host); 
-    }, $tw.syncer.errorRetryInterval)
-  };
+function setupEvents(host) {
+	var events = new EventSource(host + "events/plugins/tiddlywiki/tiddlyweb");
+	var timeout = null;
+	events.addEventListener("change",function() {
+		if(timeout) clearTimeout(timeout);
+		timeout = setTimeout(function(){
+			$tw.syncer.syncFromServer();
+		},$tw.syncer.throttleInterval);
+	});
+	events.onerror = function() {
+		events.close();
+		setTimeout(function() {
+			setupEvents(host);
+		},$tw.syncer.errorRetryInterval);
+	};
 }
 })();
