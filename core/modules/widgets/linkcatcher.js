f85f85d75dd149939e900c1bd7f886f54eff2857
Simon Huber <huber.simon@protonmail.com>
2021-01-09 13:01:43 +0100

    linkcatcher: add EventListeners in render() instead of constructor

--- core/modules/widgets/linkcatcher.js
+++ core/modules/widgets/linkcatcher.js
@@ -16,9 +16,6 @@ var Widget = require("$:/core/modules/widgets/widget.js").widget;
 
 var LinkCatcherWidget = function(parseTreeNode,options) {
 	this.initialise(parseTreeNode,options);
-	this.addEventListeners([
-		{type: "tm-navigate", handler: "handleNavigateEvent"}
-	]);
 };
 
 /*
@@ -33,6 +30,9 @@ LinkCatcherWidget.prototype.render = function(parent,nextSibling) {
 	this.parentDomNode = parent;
 	this.computeAttributes();
 	this.execute();
+	this.addEventListeners([
+		{type: "tm-navigate", handler: "handleNavigateEvent"}
+	]);
 	this.renderChildren(parent,nextSibling);
 };
 
