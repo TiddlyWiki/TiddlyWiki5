38a8ce132a2fb4083f80b5b653680425e19cc3e2
Simon Huber <huber.simon@protonmail.com>
2021-01-23 15:55:37 +0100

    Update focus.js

--- core/modules/focus.js
+++ core/modules/focus.js
@@ -17,12 +17,14 @@ function FocusManager(options) {
 	this.interceptFocusPreservation = false;
 }
 
-FocusManager.prototype.findWidgetOwningDomNode = function(widget,domNode) {
+FocusManager.prototype.findWidgetOwningDomNode = function(widget,domNode,index) {
 	for(var domNodeIndex=0; domNodeIndex<widget.domNodes.length; domNodeIndex++) {
 		if(widget.domNodes[domNodeIndex] === domNode) {
 			var widgetInfo = {
 				widget: widget,
-				index: domNodeIndex
+				index: domNodeIndex,
+				qualifier: widget.getStateQualifier() + "_" + widget.generateRenderTreeFootprint(),
+				widgetIndex: index
 			};
 			if(widget.engine && widget.engine.getSelectionRange) {
 				var selections = widget.engine.getSelectionRange();
@@ -33,7 +35,7 @@ FocusManager.prototype.findWidgetOwningDomNode = function(widget,domNode) {
 		}
 	}
 	for(var childIndex=0; childIndex<widget.children.length; childIndex++) {
-		var result = this.findWidgetOwningDomNode(widget.children[childIndex],domNode);
+		var result = this.findWidgetOwningDomNode(widget.children[childIndex],domNode,childIndex);
 		if(result) {
 			return result;
 		}
@@ -52,8 +54,8 @@ FocusManager.prototype.generateRenderTreeFootprint = function(widget) {
 	}
 	return footprint.reverse();
 };
-	
-	FocusManager.prototype.findWidgetByQualifier = function(qualifier,widget) {
+
+FocusManager.prototype.findWidgetByQualifier = function(qualifier,widget) {
 	for(var widgetIndex=0; widgetIndex<widget.children.length; widgetIndex++) {
 		var childWidget = widget.children[widgetIndex];
 		var childWidgetQualifier = childWidget.getStateQualifier() + "_" + childWidget.generateRenderTreeFootprint();
@@ -70,7 +72,8 @@ FocusManager.prototype.generateRenderTreeFootprint = function(widget) {
 	return null;
 };
 
-FocusManager.prototype.findParentWidgetWithDomNodes = function(widget,index) {
+FocusManager.prototype.findParentWidgetWithDomNodes = function(startingWidget,index) {
+	var widget = startingWidget;
 	while(widget) {
 		widget = widget.parentWidget;
 		if(widget) {
@@ -79,6 +82,20 @@ FocusManager.prototype.findParentWidgetWithDomNodes = function(widget,index) {
 					widget.children[i].domNodes[index].nodeType !== Node.TEXT_NODE &&
 					widget.children[i].domNodes[index].getAttribute("hidden") !== "true" && 
 					widget.children[i].domNodes[index].focus) {
+					return widget.children[i];
+				}
+			}
+		}
+	}
+	widget = startingWidget;
+	while(widget) {
+		widget = widget.parentWidget;
+		if(widget) {
+			for(var i=0; i<widget.children.length; i++) {
+				if(widget && widget.children[i].domNodes.length > 0 && widget.children[i].domNodes[0] &&
+					widget.children[i].domNodes[0].nodeType !== Node.TEXT_NODE &&
+					widget.children[i].domNodes[0].getAttribute("hidden") !== "true" && 
+					widget.children[i].domNodes[0].focus) {
 					return widget.children[i];
 				}
 			}
@@ -100,7 +117,7 @@ FocusManager.prototype.findChildWidgetWithDomNodes = function(widget,index) {
 	return null;
 };
 
-FocusManager.prototype.findWidgetByFootprint = function(footprint,startingWidget) {
+FocusManager.prototype.findWidgetByFootprint = function(footprint,startingWidget,widgetInfo) {
 	var index,
 		count = 0,
 		widget = startingWidget,
@@ -122,19 +139,29 @@ FocusManager.prototype.findWidgetByFootprint = function(footprint,startingWidget
 	if(!widget) {
 		widget = lastFoundWidget;
 	}
-	if(widget.domNodes.length === 0 || widget.domNodes[widgetInfo.index].nodeType === Node.TEXT_NODE ||
-		widget.domNodes[widgetInfo.index].getAttribute("hidden") === "true") {
+	if(widget && (widget.domNodes.length === 0 || (widget.domNodes[widgetInfo.index] && widget.domNodes[widgetInfo.index].nodeType === Node.TEXT_NODE) ||
+		(widget.domNodes[widgetInfo.index] && widget.domNodes[widgetInfo.index].getAttribute("hidden") === "true"))) {
 		widget = this.findParentWidgetWithDomNodes(widget,widgetInfo.index);
 	}
-	while(widget.domNodes[widgetInfo.index].classList.contains("tc-reveal") || widget.domNodes[widgetInfo.index].classList.contains("tc-keyboard")) {
-		widget = this.findChildWidgetWithDomNodes(widget,widgetInfo.widgetIndex,widgetInfo.index);
+	while(widget && widget.domNodes[widgetInfo.index] && (widget.domNodes[widgetInfo.index].classList.contains("tc-reveal") || widget.domNodes[widgetInfo.index].classList.contains("tc-keyboard"))) {
+		widget = this.findChildWidgetWithDomNodes(widget,widgetInfo.widgetIndex);
+	}
+	while(widget && !widget.domNodes[widgetInfo.index] && widget.domNodes[0] && (widget.domNodes[0].classList.contains("tc-reveal") || widget.domNodes[0].classList.contains("tc-keyboard"))) {
+		widget = this.findChildWidgetWithDomNodes(widget,widgetInfo.widgetIndex);
 	}
 	return widget;
 };
 
 FocusManager.prototype.restoreFocus = function(widget,widgetInfo) {
 	if(!this.interceptFocusPreservation && widget && widget.domNodes[widgetInfo.index] && widget.domNodes[widgetInfo.index].focus) {
-		widget.domNodes[widgetInfo.index].focus();
+		widget.domNodes[widgetInfo.index].focus({preventScroll: true});
+		if(widgetInfo.selectionStart && widgetInfo.selectionEnd) {
+			if(widget.engine && widget.engine.setSelectionRange) {
+				widget.engine.setSelectionRange(widgetInfo.selectionStart,widgetInfo.selectionEnd);
+			}
+		}
+	} else if(!this.interceptFocusPreservation && widget && !widget.domNodes[widgetInfo.index] && widget.domNodes[0] && widget.domNodes[0].focus) {
+		widget.domNodes[0].focus({preventScroll: true});
 		if(widgetInfo.selectionStart && widgetInfo.selectionEnd) {
 			if(widget.engine && widget.engine.setSelectionRange) {
 				widget.engine.setSelectionRange(widgetInfo.selectionStart,widgetInfo.selectionEnd);
