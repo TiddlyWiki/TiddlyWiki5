7a8bbf324a36278c15bf15a1ea94d39d3ddd0b94
Simon Huber <huber.simon@protonmail.com>
2021-01-23 14:12:16 +0100

    Update focus.js

--- core/modules/focus.js
+++ core/modules/focus.js
@@ -52,17 +52,82 @@ FocusManager.prototype.generateRenderTreeFootprint = function(widget) {
 	}
 	return footprint.reverse();
 };
+	
+	FocusManager.prototype.findWidgetByQualifier = function(qualifier,widget) {
+	for(var widgetIndex=0; widgetIndex<widget.children.length; widgetIndex++) {
+		var childWidget = widget.children[widgetIndex];
+		var childWidgetQualifier = childWidget.getStateQualifier() + "_" + childWidget.generateRenderTreeFootprint();
+		if(childWidgetQualifier === qualifier) {
+			return widget.children[widgetIndex];
+		}
+	}
+	for(var childIndex=0; childIndex<widget.children.length; childIndex++) {
+		var result = this.findWidgetByQualifier(qualifier,widget.children[childIndex]);
+		if(result) {
+			return result;
+		}
+	}	
+	return null;
+};
+
+FocusManager.prototype.findParentWidgetWithDomNodes = function(widget,index) {
+	while(widget) {
+		widget = widget.parentWidget;
+		if(widget) {
+			for(var i=0; i<widget.children.length; i++) {
+				if(widget && widget.children[i].domNodes.length > 0 && widget.children[i].domNodes[index] &&
+					widget.children[i].domNodes[index].nodeType !== Node.TEXT_NODE &&
+					widget.children[i].domNodes[index].getAttribute("hidden") !== "true" && 
+					widget.children[i].domNodes[index].focus) {
+					return widget.children[i];
+				}
+			}
+		}
+	}
+	return null;
+};
+
+FocusManager.prototype.findChildWidgetWithDomNodes = function(widget,index) {
+	if(widget.children[index].domNodes.length > 0) {
+		return widget.children[index];
+	}
+	for(var childIndex=0; childIndex<widget.children.length; childIndex++) {
+		var result = this.findChildWidgetWithDomNodes(widget.children[childIndex],index);
+		if(result) {
+			return result;
+		}
+	}
+	return null;
+};
 
 FocusManager.prototype.findWidgetByFootprint = function(footprint,startingWidget) {
 	var index,
 		count = 0,
-		widget = startingWidget;
+		widget = startingWidget,
+		lastFoundWidget;
 	while(count < footprint.length) {
-		index = footprint[count];
-		if(widget && widget.children && widget.children[index]) {
+		index = footprint[count],
+		lastFoundWidget = widget;
+		if(widget && widget.children) {
 			widget = widget.children[index];
 		}
+		if(widget === undefined) {
+			break;
+		}
 		count++;
+	}
+	if(widget === undefined) {
+		widget = this.findWidgetByQualifier(widgetInfo.qualifier,startingWidget);
+	}
+	if(!widget) {
+		widget = lastFoundWidget;
+	}
+	if(widget.domNodes.length === 0 || widget.domNodes[widgetInfo.index].nodeType === Node.TEXT_NODE ||
+		widget.domNodes[widgetInfo.index].getAttribute("hidden") === "true") {
+		widget = this.findParentWidgetWithDomNodes(widget,widgetInfo.index);
+	}
+	while(widget.domNodes[widgetInfo.index].classList.contains("tc-reveal") || widget.domNodes[widgetInfo.index].classList.contains("tc-keyboard")) {
+		widget = this.findChildWidgetWithDomNodes(widget,widgetInfo.widgetIndex,widgetInfo.index);
 	}
 	return widget;
 };
