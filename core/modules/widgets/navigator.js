ee091cbbbcedd7b66653e4ead39c2e480f906979
pmario <pmariojo@gmail.com>
2021-02-03 20:23:12 +0100

    add docs for th-before-importing into "dev" edition

--- core/modules/widgets/navigator.js
+++ core/modules/widgets/navigator.js
@@ -561,7 +561,7 @@ NavigatorWidget.prototype.handlePerformImportEvent = function(event) {
 		importReport = [];
 	// Add the tiddlers to the store
 	importReport.push($tw.language.getString("Import/Imported/Hint") + "\n");
-	// Let the loggers do their work. 
+	// Let the loggers do their work
 	importTiddler = $tw.hooks.invokeHook("th-before-importing",importTiddler);
 	$tw.utils.each(importData.tiddlers,function(tiddlerFields) {
 		var title = tiddlerFields.title;
