bfa062f23d2727c6f38e8d0ba48648ab906144b3
Joshua Fontany <joshua.fontany@gmail.com>
2021-02-04 08:11:07 -0800

    Fix filesystem (#5465)

--- core/modules/utils/filesystem.js
+++ core/modules/utils/filesystem.js
@@ -252,7 +252,7 @@ exports.generateTiddlerFileInfo = function(tiddler,options) {
 				extFilters: options.extFilters,
 				wiki: options.wiki
 			});
-			if(metaExt){
+			if(metaExt) {
 				if(metaExt === ".tid") {
 					// Overriding to the .tid extension needs special handling
 					fileInfo.type = "application/x-tiddler";
@@ -388,20 +388,18 @@ exports.generateTiddlerFilepath = function(title,options) {
 	// If the last write failed with an error, or if path does not start with:
 	//	the resolved options.directory, the resolved wikiPath directory, or the wikiTiddlersPath directory, 
 	//	then encodeURIComponent() and resolve to tiddler directory
-	var newPath = fullPath,
+	var writePath = $tw.hooks.invokeHook("th-make-tiddler-path",fullPath),
 		encode = (options.fileInfo || {writeError: false}).writeError == true;
-	if(!encode){
+	if(!encode) {
 		encode = !(fullPath.indexOf(path.resolve(directory)) == 0 ||
 			fullPath.indexOf(path.resolve($tw.boot.wikiPath)) == 0 ||
 			fullPath.indexOf($tw.boot.wikiTiddlersPath) == 0);
 		}
-	if(encode){
-		fullPath = path.resolve(directory, encodeURIComponent(fullPath));
+	if(encode) {
+		writePath = path.resolve(directory,encodeURIComponent(fullPath));
 	}
-	// Call hook to allow plugins to modify the final path
-	fullPath = $tw.hooks.invokeHook("th-make-tiddler-path", newPath, fullPath);
 	// Return the full path to the file
-	return fullPath;
+	return writePath;
 };
 
 /*
@@ -419,14 +417,29 @@ exports.saveTiddlerToFile = function(tiddler,fileInfo,callback) {
 			if(err) {
 				return callback(err);
 			}
-			fs.writeFile(fileInfo.filepath + ".meta",tiddler.getFieldStringBlock({exclude: ["text","bag"]}),"utf8",callback);
+			fs.writeFile(fileInfo.filepath + ".meta",tiddler.getFieldStringBlock({exclude: ["text","bag"]}),"utf8",function(err) {
+				if(err) {
+					return callback(err);
+				}
+				return callback(null,fileInfo);
+			});
 		});
 	} else {
 		// Save the tiddler as a self contained templated file
 		if(fileInfo.type === "application/x-tiddler") {
-			fs.writeFile(fileInfo.filepath,tiddler.getFieldStringBlock({exclude: ["text","bag"]}) + (!!tiddler.fields.text ? "\n\n" + tiddler.fields.text : ""),"utf8",callback);
+			fs.writeFile(fileInfo.filepath,tiddler.getFieldStringBlock({exclude: ["text","bag"]}) + (!!tiddler.fields.text ? "\n\n" + tiddler.fields.text : ""),"utf8",function(err) {
+				if(err) {
+					return callback(err);
+				}
+				return callback(null,fileInfo);
+			});
 		} else {
-			fs.writeFile(fileInfo.filepath,JSON.stringify([tiddler.getFieldStrings({exclude: ["bag"]})],null,$tw.config.preferences.jsonSpaces),"utf8",callback);
+			fs.writeFile(fileInfo.filepath,JSON.stringify([tiddler.getFieldStrings({exclude: ["bag"]})],null,$tw.config.preferences.jsonSpaces),"utf8",function(err) {
+				if(err) {
+					return callback(err);
+				}
+				return callback(null,fileInfo);
+			});
 		}
 	}
 };
@@ -457,10 +470,12 @@ exports.saveTiddlerToFileSync = function(tiddler,fileInfo) {
 /*
 Delete a file described by the fileInfo if it exits
 */
-exports.deleteTiddlerFile = function(fileInfo, callback) {
+exports.deleteTiddlerFile = function(fileInfo,callback) {
 	//Only attempt to delete files that exist on disk
 	if(!fileInfo.filepath || !fs.existsSync(fileInfo.filepath)) {
-		return callback(null);
+		//For some reason, the tiddler is only in memory or we can't modify the file at this path
+		$tw.syncer.displayError("Server deleteTiddlerFile task failed for filepath: "+fileInfo.filepath);
+		return callback(null,fileInfo);
 	}
 	// Delete the file
 	fs.unlink(fileInfo.filepath,function(err) {
@@ -473,10 +488,20 @@ exports.deleteTiddlerFile = function(fileInfo, callback) {
 				if(err) {
 					return callback(err);
 				}
-				return $tw.utils.deleteEmptyDirs(path.dirname(fileInfo.filepath),callback);
+				return $tw.utils.deleteEmptyDirs(path.dirname(fileInfo.filepath),function(err) {
+					if(err) {
+						return callback(err);
+					}
+					return callback(null,fileInfo);
+				});
 			});
 		} else {
-			return $tw.utils.deleteEmptyDirs(path.dirname(fileInfo.filepath),callback);
+			return $tw.utils.deleteEmptyDirs(path.dirname(fileInfo.filepath),function(err) {
+				if(err) {
+					return callback(err);
+				}
+				return callback(null,fileInfo);
+			});
 		}
 	});
 };
@@ -486,25 +511,25 @@ Cleanup old files on disk, by comparing the options values:
 	adaptorInfo from $tw.syncer.tiddlerInfo
 	bootInfo from $tw.boot.files
 */
-exports.cleanupTiddlerFiles = function(options, callback) {
+exports.cleanupTiddlerFiles = function(options,callback) {
 	var adaptorInfo = options.adaptorInfo || {},
 	bootInfo = options.bootInfo || {},
 	title = options.title || "undefined";
 	if(adaptorInfo.filepath && bootInfo.filepath && adaptorInfo.filepath !== bootInfo.filepath) {
-		return $tw.utils.deleteTiddlerFile(adaptorInfo, function(err){
+		$tw.utils.deleteTiddlerFile(adaptorInfo,function(err) {
 			if(err) {
 				if ((err.code == "EPERM" || err.code == "EACCES") && err.syscall == "unlink") {
 					// Error deleting the previous file on disk, should fail gracefully
-					$tw.syncer.displayError("Server desynchronized. Error cleaning up previous file for tiddler: "+title, err);
-					return callback(null);
+					$tw.syncer.displayError("Server desynchronized. Error cleaning up previous file for tiddler: \""+title+"\"",err);
+					return callback(null,bootInfo);
 				} else {
 					return callback(err);
 				}
 			}
-			return callback(null);
+			return callback(null,bootInfo);
 		});
 	} else {
-		return callback(null);
+		return callback(null,bootInfo);
 	}
 };
 
