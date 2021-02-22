58d291c116a2843cf0a5bf45366c66c1c9fefab6
jeremy@jermolene.com <jeremy@jermolene.com>
2021-02-22 19:20:58 +0000

    First commit

--- core/modules/startup/render.js
+++ core/modules/startup/render.js
@@ -32,10 +32,15 @@ exports.startup = function() {
 	$tw.titleWidgetNode = $tw.wiki.makeTranscludeWidget(PAGE_TITLE_TITLE,{document: $tw.fakeDocument, parseAsInline: true});
 	$tw.titleContainer = $tw.fakeDocument.createElement("div");
 	$tw.titleWidgetNode.render($tw.titleContainer,null);
-	document.title = $tw.titleContainer.textContent;
+	$tw.titlePublisher = new $tw.utils.BrowserMessagingPublisher({type: "PAGETITLE", onsubscribe: publishTitle});
+	var publishTitle = function() {
+		$tw.titlePublisher.send({verb: "PAGETITLE",body: document.title});
+		document.title = $tw.titleContainer.textContent;
+	};
+	publishTitle();
 	$tw.wiki.addEventListener("change",function(changes) {
 		if($tw.titleWidgetNode.refresh(changes,$tw.titleContainer,null)) {
-			document.title = $tw.titleContainer.textContent;
+			publishTitle();
 		}
 	});
 	// Set up the styles
