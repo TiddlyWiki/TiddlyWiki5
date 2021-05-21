270ead47012525d757947b9ee171184af28d2567
Saq Imtiaz <saq.imtiaz@gmail.com>
2021-05-21 10:43:20 +0200

    Eventcatcher: Fixed FF and IE bugs, added stopPropagation attribute (#5711)

--- core/modules/widgets/eventcatcher.js
+++ core/modules/widgets/eventcatcher.js
@@ -47,38 +47,46 @@ EventWidget.prototype.render = function(parent,nextSibling) {
 		domNode.addEventListener(type,function(event) {
 			var selector = self.getAttribute("selector"),
 				actions = self.getAttribute("actions-"+type),
+				stopPropagation = self.getAttribute("stopPropagation","onaction"),
 				selectedNode = event.target,
 				selectedNodeRect,
 				catcherNodeRect,
 				variables = {};
+			// Firefox can fire dragover and dragenter events on text nodes instead of their parents
+			if(selectedNode.nodeType === 3) {
+				selectedNode = selectedNode.parentNode;
+			}
 			if(selector) {
 				// Search ancestors for a node that matches the selector
-				while(!selectedNode.matches(selector) && selectedNode !== domNode) {
+				while(!$tw.utils.domMatchesSelector(selectedNode,selector) && selectedNode !== domNode) {
 					selectedNode = selectedNode.parentNode;
 				}
 				// If we found one, copy the attributes as variables, otherwise exit
-				if(selectedNode.matches(selector)) {
-					$tw.utils.each(selectedNode.attributes,function(attribute) {
-						variables["dom-" + attribute.name] = attribute.value.toString();
-					});
-					//Add a variable with a popup coordinate string for the selected node
-					variables["tv-popup-coords"] = "(" + selectedNode.offsetLeft + "," + selectedNode.offsetTop +"," + selectedNode.offsetWidth + "," + selectedNode.offsetHeight + ")";
-					
-					//Add variables for offset of selected node
-					variables["tv-selectednode-posx"] = selectedNode.offsetLeft.toString();
-					variables["tv-selectednode-posy"] = selectedNode.offsetTop.toString();
-					variables["tv-selectednode-width"] = selectedNode.offsetWidth.toString();
-					variables["tv-selectednode-height"] = selectedNode.offsetHeight.toString();
+				if($tw.utils.domMatchesSelector(selectedNode,selector)) {
+					// Only set up variables if we have actions to invoke
+					if(actions) {
+						$tw.utils.each(selectedNode.attributes,function(attribute) {
+							variables["dom-" + attribute.name] = attribute.value.toString();
+						});
+						//Add a variable with a popup coordinate string for the selected node
+						variables["tv-popup-coords"] = "(" + selectedNode.offsetLeft + "," + selectedNode.offsetTop +"," + selectedNode.offsetWidth + "," + selectedNode.offsetHeight + ")";
 
-					//Add variables for event X and Y position relative to selected node
-					selectedNodeRect = selectedNode.getBoundingClientRect();				
-					variables["event-fromselected-posx"] = (event.clientX - selectedNodeRect.left).toString();
-					variables["event-fromselected-posy"] = (event.clientY - selectedNodeRect.top).toString();
+						//Add variables for offset of selected node
+						variables["tv-selectednode-posx"] = selectedNode.offsetLeft.toString();
+						variables["tv-selectednode-posy"] = selectedNode.offsetTop.toString();
+						variables["tv-selectednode-width"] = selectedNode.offsetWidth.toString();
+						variables["tv-selectednode-height"] = selectedNode.offsetHeight.toString();
 
-					//Add variables for event X and Y position relative to event catcher node
-					catcherNodeRect = self.domNode.getBoundingClientRect();
-					variables["event-fromcatcher-posx"] = (event.clientX - catcherNodeRect.left).toString();
-					variables["event-fromcatcher-posy"] = (event.clientY - catcherNodeRect.top).toString();
+						//Add variables for event X and Y position relative to selected node
+						selectedNodeRect = selectedNode.getBoundingClientRect();
+						variables["event-fromselected-posx"] = (event.clientX - selectedNodeRect.left).toString();
+						variables["event-fromselected-posy"] = (event.clientY - selectedNodeRect.top).toString();
+
+						//Add variables for event X and Y position relative to event catcher node
+						catcherNodeRect = self.domNode.getBoundingClientRect();
+						variables["event-fromcatcher-posx"] = (event.clientX - catcherNodeRect.left).toString();
+						variables["event-fromcatcher-posy"] = (event.clientY - catcherNodeRect.top).toString();
+					}
 				} else {
 					return false;
 				}
@@ -106,6 +114,8 @@ EventWidget.prototype.render = function(parent,nextSibling) {
 					variables["event-detail"] = event.detail.toString();
 				}
 				self.invokeActionString(actions,self,event,variables);
+			}
+			if((actions && stopPropagation === "onaction") || stopPropagation === "always") {
 				event.preventDefault();
 				event.stopPropagation();
 				return true;
