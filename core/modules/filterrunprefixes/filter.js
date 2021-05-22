3fc7895af296b84865901ec41fbb628ffe94756d
Saq Imtiaz <saq.imtiaz@gmail.com>
2021-05-22 20:43:37 +0200

    CurrentTiddler variable consistency in subfilters and prefixes (#5691)
    * Make currentTiddler variable consistent in subfilters and filter run prefixes
    
    * Updated filterun prefix and subfilter operators to use ..currentTiddler instead of outerCurrentTiddler

--- core/modules/filterrunprefixes/filter.js
+++ core/modules/filterrunprefixes/filter.js
@@ -17,10 +17,21 @@ exports.filter = function(operationSubFunction,options) {
 	return function(results,source,widget) {
 		if(results.length > 0) {
 			var resultsToRemove = [];
-			results.each(function(result) {
-				var filtered = operationSubFunction(options.wiki.makeTiddlerIterator([result]),widget);
+			results.each(function(title) {
+				var filtered = operationSubFunction(options.wiki.makeTiddlerIterator([title]),{
+					getVariable: function(name) {
+						switch(name) {
+							case "currentTiddler":
+								return "" + title;
+							case "..currentTiddler":
+								return widget.getVariable("currentTiddler");
+							default:
+								return widget.getVariable(name);
+						}
+					}
+				});
 				if(filtered.length === 0) {
-					resultsToRemove.push(result);
+					resultsToRemove.push(title);
 				}
 			});
 			results.remove(resultsToRemove);
