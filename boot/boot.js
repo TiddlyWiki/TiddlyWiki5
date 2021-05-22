16ca0e9c6d567a6a435cdce17ad5ae12f1e755d0
jeremy@jermolene.com <jeremy@jermolene.com>
2021-05-22 22:22:19 +0100

    Move JSON tiddlers into their own store area, and fix support for encrypted tiddlers
    Also add a dummy old-style store area for backwards compatibility
    
    The current arrangement is that JSON tiddlers will always override old-style tiddlers.

--- boot/boot.js
+++ boot/boot.js
@@ -1724,7 +1724,7 @@ $tw.modules.define("$:/boot/tiddlerdeserializer/dom","tiddlerdeserializer",{
 			},
 			t,result = [];
 		if(node) {
-			var format = (node.getAttribute && node.getAttribute("data-tiddlywiki-store-format")) || "div";
+			var format = (node.getAttribute && node.getAttribute("data-tiddlywiki-tiddler-store-format")) || "div";
 			switch(format) {
 				case "div":
 					for(t = 0; t < node.childNodes.length; t++) {
@@ -1747,17 +1747,23 @@ $tw.modules.define("$:/boot/tiddlerdeserializer/dom","tiddlerdeserializer",{
 
 $tw.loadTiddlersBrowser = function() {
 	// In the browser, we load tiddlers from certain elements
-	var containerIds = [
-		"libraryModules",
-		"modules",
-		"bootKernelPrefix",
-		"bootKernel",
-		"styleArea",
-		"storeArea",
-		"systemArea"
+	var containerSelectors = [
+		// IDs for old-style v5.1.x tiddler stores
+		"#libraryModules",
+		"#modules",
+		"#bootKernelPrefix",
+		"#bootKernel",
+		"#styleArea",
+		"#storeArea",
+		"#systemArea",
+		// Classes for new-style v5.2.x JSON tiddler stores
+		".tiddlywiki-tiddler-store"
 	];
-	for(var t=0; t<containerIds.length; t++) {
-		$tw.wiki.addTiddlers($tw.wiki.deserializeTiddlers("(DOM)",document.getElementById(containerIds[t])));
+	for(var t=0; t<containerSelectors.length; t++) {
+		var nodes = document.querySelectorAll(containerSelectors[t]);
+		for(var n=0; n<nodes.length; n++) {
+			$tw.wiki.addTiddlers($tw.wiki.deserializeTiddlers("(DOM)",nodes[n]));
+		}
 	}
 };
 
