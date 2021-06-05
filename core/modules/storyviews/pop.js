97e26252d7e66c05f6ff545f95958e184e2d26d1
Simon Huber <huber.simon@protonmail.com>
2021-06-05 10:54:56 +0200

    Make navigation in new windows work for pop storyview

--- core/modules/storyviews/pop.js
+++ core/modules/storyviews/pop.js
@@ -24,7 +24,7 @@ PopStoryView.prototype.navigateTo = function(historyInfo) {
 	var listItemWidget = this.listWidget.children[listElementIndex],
 		targetElement = listItemWidget.findFirstDomNode();
 	// Abandon if the list entry isn't a DOM element (it might be a text node)
-	if(!(targetElement instanceof Element)) {
+	if(targetElement.nodeType === Node.TEXT_NODE) {
 		return;
 	}
 	// Scroll the node into view
