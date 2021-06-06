80c6dd51b776b651feaf9d762680e4dae85a5a37
Simon Huber <huber.simon@protonmail.com>
2021-06-06 09:38:17 +0200

    Create tiddler-change.js

---
+++ core/modules/tiddler-change.js
@@ -0,0 +1,87 @@
+/*\
+title: $:/core/modules/tiddler-change.js
+type: application/javascript
+module-type: global
+
+Tiddler change handling utilities
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+function TiddlerChangeManager(options) {
+	var self = this;
+	options = options || "";
+	this.tiddlerChangeList = [],
+	this.tiddlerActionList = [];
+	this.updateTiddlerChangeLists(this.getTiddlerList());
+	$tw.wiki.addEventListener("change",function(changes) {
+		self.handleShortcutChanges(changes);
+	});
+}
+
+TiddlerChangeManager.prototype.getTiddlerList = function() {
+	return $tw.wiki.getTiddlersWithTag("$:/tags/TiddlerChangeAction");
+};
+
+TiddlerChangeManager.prototype.updateTiddlerChangeLists = function(tiddlerList) {
+	this.actionTiddlers = tiddlerList;
+	for(var i=0; i<tiddlerList.length; i++) {
+		var title = tiddlerList[i],
+			tiddlerFields = $tw.wiki.getTiddler(title).fields;
+		this.tiddlerChangeList[i] = tiddlerFields["change-tiddler"] !== undefined ? tiddlerFields["change-tiddler"] : undefined;
+		this.tiddlerActionList[i] = tiddlerFields.text;
+	}
+};
+
+TiddlerChangeManager.prototype.handleChangedTiddlerEvent = function(event) {
+	var action;
+	for(var i=0; i<this.actionTiddlers.length; i++) {
+		if(this.tiddlerChangeList[i] !== undefined && $tw.utils.hop(event,this.tiddlerChangeList[i])) {
+			action = this.tiddlerActionList[i];
+			break;
+		}
+	}
+	if(action !== undefined) {
+		$tw.rootWidget.invokeActionString(action,$tw.rootWidget);
+		return true;
+	}
+	return false;
+};
+
+TiddlerChangeManager.prototype.detectNewShortcuts = function(changedTiddlers) {
+	var shortcutConfigTiddlers = [],
+		handled = false;
+	$tw.utils.each(this.lookupNames,function(platformDescriptor) {
+		var descriptorString = "$:/config/" + platformDescriptor + "/";
+		Object.keys(changedTiddlers).forEach(function(configTiddler) {
+			var configString = configTiddler.substr(0, configTiddler.lastIndexOf("/") + 1);
+			if(configString === descriptorString) {
+				shortcutConfigTiddlers.push(configTiddler);
+				handled = true;
+			}
+		});
+	});
+	if(handled) {
+		return $tw.utils.hopArray(changedTiddlers,shortcutConfigTiddlers);
+	} else {
+		return false;
+	}
+};
+
+TiddlerChangeManager.prototype.handleShortcutChanges = function(changedTiddlers) {
+	var newList = this.getTiddlerList();
+	var hasChanged = $tw.utils.hopArray(changedTiddlers,this.actionTiddlers) ? true :
+		($tw.utils.hopArray(changedTiddlers,newList) ? true : false);
+	// Re-cache tiddlers if something changed
+	if(hasChanged) {
+		this.updateTiddlerChangeLists(newList);
+	}
+};
+
+exports.TiddlerChangeManager = TiddlerChangeManager;
+
+})();
