aef736f65207fbf4b01ff0868760fcbb55779782
Simon Huber <huber.simon@protonmail.com>
2021-06-06 15:11:34 +0200

    Update tiddler-change.js

--- core/modules/tiddler-change.js
+++ core/modules/tiddler-change.js
@@ -2,9 +2,7 @@
 title: $:/core/modules/tiddler-change.js
 type: application/javascript
 module-type: global
-
 Tiddler change handling utilities
-
 \*/
 (function(){
 
@@ -13,39 +11,64 @@ Tiddler change handling utilities
 "use strict";
 
 function TiddlerChangeManager(options) {
-	var self = this;
+	var self = this,
 	options = options || "";
-	this.tiddlerChangeList = [],
-	this.tiddlerActionList = [];
-	this.updateTiddlerChangeLists(this.getTiddlerList());
+	this.tiddlerFilters = [],
+	this.tiddlerActions = [],
+	this.tiddlerFilterLists = [];
+	this.updateTiddlerFilterLists(this.getTiddlerList());
 	$tw.wiki.addEventListener("change",function(changes) {
 		self.handleTiddlerChanges(changes);
-	});
+	})
 }
 
 TiddlerChangeManager.prototype.getTiddlerList = function() {
 	return $tw.wiki.getTiddlersWithTag("$:/tags/TiddlerChangeAction");
 };
 
-TiddlerChangeManager.prototype.updateTiddlerChangeLists = function(tiddlerList) {
+TiddlerChangeManager.prototype.updateTiddlerFilterLists = function(tiddlerList) {
 	this.actionTiddlers = tiddlerList;
 	for(var i=0; i<tiddlerList.length; i++) {
 		var title = tiddlerList[i],
 			tiddlerFields = $tw.wiki.getTiddler(title).fields;
-		this.tiddlerChangeList[i] = tiddlerFields["change-tiddler"] !== undefined ? tiddlerFields["change-tiddler"] : undefined;
-		this.tiddlerActionList[i] = tiddlerFields.text;
+		this.tiddlerFilters[i] = tiddlerFields["tiddler-filter"] !== undefined ? tiddlerFields["tiddler-filter"] : undefined;
+		this.tiddlerActions[i] = tiddlerFields.text;
+		this.tiddlerFilterLists[i] = $tw.wiki.filterTiddlers(this.tiddlerChangeList[i]);
 	}
+};
+
+TiddlerChangeManager.prototype.isEqual = function(a,b) {
+	if(a === b) {
+		return true;
+	}
+	if(a == null || b == null) {
+		return false;
+	}
+	if(a.length !== b.length) {
+		return false;
+	}
+
+	for(var i=0; i<a.length; ++i) {
+		if(a[i] !== b[i]) {
+			return false;
+		}
+	}
+	return true;
 };
 
 TiddlerChangeManager.prototype.handleChangedTiddlerEvent = function(event) {
 	var actions = [];
 	for(var i=0; i<this.actionTiddlers.length; i++) {
-		if(this.tiddlerChangeList[i] !== undefined && $tw.utils.hop(event,this.tiddlerChangeList[i])) {
-			actions.push = this.tiddlerActionList[i];
+		var tiddlerListBefore = this.tiddlerFilterLists[i];
+		var tiddlerListNow = $tw.wiki.filterTiddlers(this.tiddlerFilters[i]);
+		if(!this.isEqual(tiddlerListNow,tiddlerListBefore)) {
+			actions.push(this.tiddlerActions[i]);
+			this.tiddlerFilterLists[i] = tiddlerListNow;
 		}
 	}
 	for(i=0; i<actions.length; i++) {
 		$tw.rootWidget.invokeActionString(actions[i],$tw.rootWidget);
+		console.log("ACTION INVOKED");
 	}
 	return true;
 };
@@ -56,7 +79,7 @@ TiddlerChangeManager.prototype.handleTiddlerChanges = function(changedTiddlers) 
 		($tw.utils.hopArray(changedTiddlers,newList) ? true : false);
 	// Re-cache tiddlers if something changed
 	if(hasChanged) {
-		this.updateTiddlerChangeLists(newList);
+		this.updateTiddlerFilterLists(newList);
 	}
 };
 
