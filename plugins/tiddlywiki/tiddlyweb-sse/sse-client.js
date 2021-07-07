d373a187b865b9cdc241de728530f36085709706
Arlen22 <arlenbee@gmail.com>
2021-07-07 00:05:48 -0400

    a couple fixes

--- plugins/tiddlywiki/tiddlyweb-sse/sse-client.js
+++ plugins/tiddlywiki/tiddlyweb-sse/sse-client.js
@@ -19,7 +19,7 @@ exports.platforms = ["browser"];
 exports.synchronous = true;
 exports.startup = function() {
 	var source = null;
-	$tw.hooks.addHook("th-syncer-status-response",function(syncer, success) {
+	$tw.hooks.addHook("th-syncer-status-response",function(syncer,success) {
 		// technically any syncer instance can be used, but we only use the global one for this
 		if(!success || syncer.syncadaptor.name !== "tiddlyweb" || $tw.syncer !== syncer) { return; }
 		// check if we have a previous one and close it if we do
@@ -28,23 +28,24 @@ exports.startup = function() {
 		var host = syncer.syncadaptor.getHost();
 		// Make sure it ends with a slash (it usually does)
 		if(host[host.length - 1] !== "/") { host += "/"; }
+		var endpoint = host + "events/plugins/tiddlywiki/tiddlyweb-sse/wiki-change";
 		// Setup the event listener
-		source = setupSSE(host, syncer);
+		source = exports.setupSSE(endpoint,syncer);
 	});
 }
 
-function debounce(callback) {
+function debounce(interval,callback) {
 	var timeout = null;
 	return function() {
 		clearTimeout(timeout);
-		timeout = setTimeout(callback,$tw.syncer.throttleInterval);
+		timeout = setTimeout(callback,interval);
 	};
 }
 
-exports.setupSSE = function setupSSE(host,syncer,refresh) {
+exports.setupSSE = function setupSSE(endpoint,syncer,refresh) {
 	if(window.EventSource) {
-		var source = new EventSource(host + "events/plugins/tiddlywiki/tiddlyweb-sse/wiki-change",{ withCredentials: true });
-		var debouncedSync = debounce(syncer.syncFromServer.bind(syncer));
+		var source = new EventSource(endpoint,{ withCredentials: true });
+		var debouncedSync = debounce(syncer.throttleInterval,syncer.syncFromServer.bind(syncer));
 		source.addEventListener("change",debouncedSync);
 		source.onerror = function() {
 			// return if we're reconnecting because that's handled automatically
@@ -52,12 +53,12 @@ exports.setupSSE = function setupSSE(host,syncer,refresh) {
 			// wait for the errorRetryInterval
 			setTimeout(function() {
 				//call this function to set everything up again
-				setupSSE(host,true);
+				exports.setupSSE(endpoint,syncer,true);
 			},syncer.errorRetryInterval);
 		};
 		source.onopen = function() {
 			// only run this on first open, not on auto reconnect
-			source.onopen = function() { };
+			source.onopen = function() {};
 			// once we've properly opened, disable polling
 			syncer.wiki.addTiddler({ title: syncer.titleSyncDisablePolling,text: "yes" });
 			//sync from server manually here to make sure we stay up to date
