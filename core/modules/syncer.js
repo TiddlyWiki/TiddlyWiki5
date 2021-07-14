be026aa308e366ed3e1e67d14e1de8f6ef3e0000
Arlen22 <arlenbee@gmail.com>
2021-07-14 12:16:57 -0400

    Revert "Add server sent events (#5279)" (#5880)
    This reverts commit 17b4f53ba27fbf09b531f8fcb2e8dd2bfbf507c4 according to Github Desktop.
    
    git checkout that commit
    revert commit in GitHub Desktop
    git switch -c revert-sse
    uncommit in Github Desktop
    switch to master, bringing changes
    resolve deletions with command line

--- core/modules/syncer.js
+++ core/modules/syncer.js
@@ -20,7 +20,6 @@ Syncer.prototype.titleIsAnonymous = "$:/status/IsAnonymous";
 Syncer.prototype.titleIsReadOnly = "$:/status/IsReadOnly";
 Syncer.prototype.titleUserName = "$:/status/UserName";
 Syncer.prototype.titleSyncFilter = "$:/config/SyncFilter";
-Syncer.prototype.titleSyncDisablePolling = "$:/config/SyncDisablePolling";
 Syncer.prototype.titleSyncPollingInterval = "$:/config/SyncPollingInterval";
 Syncer.prototype.titleSyncDisableLazyLoading = "$:/config/SyncDisableLazyLoading";
 Syncer.prototype.titleSavedNotification = "$:/language/Notifications/Save/Done";
@@ -90,7 +89,7 @@ function Syncer(options) {
 		if(filteredChanges.length > 0) {
 			self.processTaskQueue();
 		} else {
-			// Look for deletions of tiddlers we're already syncing
+			// Look for deletions of tiddlers we're already syncing	
 			var outstandingDeletion = false
 			$tw.utils.each(changes,function(change,title,object) {
 				if(change.deleted && $tw.utils.hop(self.tiddlerInfo,title)) {
@@ -122,7 +121,7 @@ function Syncer(options) {
 				self.login(username,password,function() {});
 			} else {
 				// No username and password, so we display a prompt
-				self.handleLoginEvent();
+				self.handleLoginEvent();				
 			}
 		});
 		$tw.rootWidget.addEventListener("tm-logout",function() {
@@ -139,7 +138,7 @@ function Syncer(options) {
 	if(!this.disableUI && this.wiki.getTiddlerText(this.titleSyncDisableLazyLoading) !== "yes") {
 		this.wiki.addEventListener("lazyLoad",function(title) {
 			self.handleLazyLoadEvent(title);
-		});
+		});		
 	}
 	// Get the login status
 	this.getStatus(function(err,isLoggedIn) {
@@ -174,8 +173,8 @@ Syncer.prototype.getTiddlerRevision = function(title) {
 	if(this.syncadaptor && this.syncadaptor.getTiddlerRevision) {
 		return this.syncadaptor.getTiddlerRevision(title);
 	} else {
-		return this.wiki.getTiddler(title).fields.revision;
-	}
+		return this.wiki.getTiddler(title).fields.revision;	
+	} 
 };
 
 /*
@@ -268,7 +267,7 @@ Syncer.prototype.getStatus = function(callback) {
 		// Mark us as not logged in
 		this.wiki.addTiddler({title: this.titleIsLoggedIn,text: "no"});
 		// Get login status
-		this.syncadaptor.getStatus(function(err,isLoggedIn,username,isReadOnly,isAnonymous,isPollingDisabled) {
+		this.syncadaptor.getStatus(function(err,isLoggedIn,username,isReadOnly,isAnonymous) {
 			if(err) {
 				self.logger.alert(err);
 			} else {
@@ -278,9 +277,6 @@ Syncer.prototype.getStatus = function(callback) {
 				self.wiki.addTiddler({title: self.titleIsLoggedIn,text: isLoggedIn ? "yes" : "no"});
 				if(isLoggedIn) {
 					self.wiki.addTiddler({title: self.titleUserName,text: username || ""});
-				}
-				if(isPollingDisabled) {
-					self.wiki.addTiddler({title: self.titleSyncDisablePolling, text: "yes"});
 				}
 			}
 			// Invoke the callback
@@ -305,15 +301,12 @@ Syncer.prototype.syncFromServer = function() {
 			}
 		},
 		triggerNextSync = function() {
-			if(pollingEnabled) {
-				self.pollTimerId = setTimeout(function() {
-					self.pollTimerId = null;
-					self.syncFromServer.call(self);
-				},self.pollTimerInterval);
-			}
+			self.pollTimerId = setTimeout(function() {
+				self.pollTimerId = null;
+				self.syncFromServer.call(self);
+			},self.pollTimerInterval);
 		},
-		syncSystemFromServer = (self.wiki.getTiddlerText("$:/config/SyncSystemTiddlersFromServer") === "yes"),
-		pollingEnabled = (self.wiki.getTiddlerText(self.titleSyncDisablePolling) !== "yes");
+		syncSystemFromServer = (self.wiki.getTiddlerText("$:/config/SyncSystemTiddlersFromServer") === "yes" ? true : false);
 	if(this.syncadaptor && this.syncadaptor.getUpdatedTiddlers) {
 		this.logger.log("Retrieving updated tiddler list");
 		cancelNextSync();
@@ -336,7 +329,7 @@ Syncer.prototype.syncFromServer = function() {
 				});
 				if(updates.modifications.length > 0 || updates.deletions.length > 0) {
 					self.processTaskQueue();
-				}
+				}				
 			}
 		});
 	} else if(this.syncadaptor && this.syncadaptor.getSkinnyTiddlers) {
@@ -516,7 +509,7 @@ Syncer.prototype.processTaskQueue = function() {
 				} else {
 					self.updateDirtyStatus();
 					// Process the next task
-					self.processTaskQueue.call(self);
+					self.processTaskQueue.call(self);					
 				}
 			});
 		} else {
@@ -524,11 +517,11 @@ Syncer.prototype.processTaskQueue = function() {
 			this.updateDirtyStatus();
 			// And trigger a timeout if there is a pending task
 			if(task === true) {
-				this.triggerTimeout();
+				this.triggerTimeout();				
 			}
 		}
 	} else {
-		this.updateDirtyStatus();
+		this.updateDirtyStatus();		
 	}
 };
 
@@ -562,7 +555,7 @@ Syncer.prototype.chooseNextTask = function() {
 				isReadyToSave = !tiddlerInfo || !tiddlerInfo.timestampLastSaved || tiddlerInfo.timestampLastSaved < thresholdLastSaved;
 			if(hasChanged) {
 				if(isReadyToSave) {
-					return new SaveTiddlerTask(this,title);
+					return new SaveTiddlerTask(this,title); 					
 				} else {
 					havePending = true;
 				}
