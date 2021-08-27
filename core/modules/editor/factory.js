0a7c8dd90bea03372b6956c98c6b1ecd45657880
BurningTreeC <huber.simon@protonmail.com>
2021-08-27 07:31:30 +0200

    fix th-external-text-operation in factory.js

--- core/modules/editor/factory.js
+++ core/modules/editor/factory.js
@@ -81,7 +81,7 @@ function editTextWidgetFactory(toolbarEngine,nonToolbarEngine) {
 		]);
 		$tw.hooks.addHook("th-external-text-operation",function(event) {
 			event = event || {};
-			if(event.paramObject.tiddler && (event.paramObject["current-widget-id"] === self.editWidgetId) && (self.getVariable("currentTiddler") === event.paramObject.tiddler)) {
+			if(event.paramObject.tiddler && (event.paramObject["current-widget-id"] !== undefined) && (event.paramObject["current-widget-id"] === self.editWidgetId) && (self.getVariable("currentTiddler") === event.paramObject.tiddler)) {
 				self.handleEditTextOperationMessage(event);
 			}
 			return event;
