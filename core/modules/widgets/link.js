79e70e335a3eadb795f7457103aa6b2db45bd8d5
BurningTreeC <huber.simon@protonmail.com>
2021-09-01 08:37:51 +0200

    update link-widget refresh method

--- core/modules/widgets/link.js
+++ core/modules/widgets/link.js
@@ -207,7 +207,10 @@ Selectively refreshes the widget if needed. Returns true if the widget or any of
 */
 LinkWidget.prototype.refresh = function(changedTiddlers) {
 	var changedAttributes = this.computeAttributes();
-	if(changedAttributes.to || changedTiddlers[this.to] || changedAttributes["aria-label"] || changedAttributes.tooltip) {
+	var hideMissingLinks = (this.getVariable("tv-show-missing-links") || "yes") === "no";
+	if(changedAttributes.to || changedTiddlers[this.to] || changedAttributes["aria-label"] || changedAttributes.tooltip ||
+		changedAttributes["class"] || changedAttributes.tabindex || changedAttributes.draggable || changedAttributes.tag ||
+		hideMissingLinks !== this.hideMissingLinks) {
 		this.refreshSelf();
 		return true;
 	}
