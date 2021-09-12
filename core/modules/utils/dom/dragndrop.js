e9e5d37ff01cc1aaa3419798904dac8b0e9775af
jeremy@jermolene.com <jeremy@jermolene.com>
2021-09-12 14:20:03 +0100

    Draggable widget: Add option to hide drag image
    Thanks @ericshulman
    
    Fixes #6027

--- core/modules/utils/dom/dragndrop.js
+++ core/modules/utils/dom/dragndrop.js
@@ -16,7 +16,7 @@ Browser data transfer utilities, used with the clipboard and drag and drop
 Options:
 
 domNode: dom node to make draggable
-dragImageType: "pill" or "dom"
+dragImageType: "pill", "blank" or "dom" (the default)
 dragTiddlerFn: optional function to retrieve the title of tiddler to drag
 dragFilterFn: optional function to retreive the filter defining a list of tiddlers to drag
 widget: widget to use as the contect for the filter
@@ -73,6 +73,9 @@ exports.makeDraggable = function(options) {
 				if(dataTransfer.setDragImage) {
 					if(dragImageType === "pill") {
 						dataTransfer.setDragImage(dragImage.firstChild,-16,-16);
+					} else if (dragImageType === "blank") {
+						dragImage.removeChild(dragImage.firstChild);
+						dataTransfer.setDragImage(dragImage,0,0);
 					} else {
 						var r = domNode.getBoundingClientRect();
 						dataTransfer.setDragImage(domNode,event.clientX-r.left,event.clientY-r.top);
