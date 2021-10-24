63b78b41a450bca84b42f418605ef8cc285a61c3
jeremy@jermolene.com <jeremy@jermolene.com>
2021-10-24 20:24:20 +0100

    Adjust whitespace

--- core/modules/widgets/action-setmultiplefields.js
+++ core/modules/widgets/action-setmultiplefields.js
@@ -15,7 +15,7 @@ Action widget to set multiple fields or indexes on a tiddler
 var Widget = require("$:/core/modules/widgets/widget.js").widget;
 
 var SetMultipleFieldsWidget = function(parseTreeNode,options) {
-    this.initialise(parseTreeNode,options);
+	this.initialise(parseTreeNode,options);
 };
 
 /*
@@ -27,55 +27,55 @@ SetMultipleFieldsWidget.prototype = new Widget();
 Render this widget into the DOM
 */
 SetMultipleFieldsWidget.prototype.render = function(parent,nextSibling) {
-    this.computeAttributes();
-    this.execute();
+	this.computeAttributes();
+	this.execute();
 };
 
 /*
 Compute the internal state of the widget
 */
 SetMultipleFieldsWidget.prototype.execute = function() {
-    this.actionTiddler = this.getAttribute("$tiddler",this.getVariable("currentTiddler"));
-    this.actionFields = this.getAttribute("$fields");
-    this.actionIndexes = this.getAttribute("$indexes");
-    this.actionValues = this.getAttribute("$values");
-    this.actionTimestamp = this.getAttribute("$timestamp","yes") === "yes";
+	this.actionTiddler = this.getAttribute("$tiddler",this.getVariable("currentTiddler"));
+	this.actionFields = this.getAttribute("$fields");
+	this.actionIndexes = this.getAttribute("$indexes");
+	this.actionValues = this.getAttribute("$values");
+	this.actionTimestamp = this.getAttribute("$timestamp","yes") === "yes";
 };
 
 /*
 Refresh the widget by ensuring our attributes are up to date
 */
 SetMultipleFieldsWidget.prototype.refresh = function(changedTiddlers) {
-    var changedAttributes = this.computeAttributes();
-    if(changedAttributes["$tiddler"] || changedAttributes["$fields"] || changedAttributes["$indexes"] || changedAttributes["$values"] || changedAttributes["$timestamp"]) {
-        this.refreshSelf();
-        return true;
-    }
-    return this.refreshChildren(changedTiddlers);
+	var changedAttributes = this.computeAttributes();
+	if(changedAttributes["$tiddler"] || changedAttributes["$fields"] || changedAttributes["$indexes"] || changedAttributes["$values"] || changedAttributes["$timestamp"]) {
+		this.refreshSelf();
+		return true;
+	}
+	return this.refreshChildren(changedTiddlers);
 };
 
 /*
 Invoke the action associated with this widget
 */
 SetMultipleFieldsWidget.prototype.invokeAction = function(triggeringWidget,event) {
-    var tiddler = this.wiki.getTiddler(this.actionTiddler),
-        additions = {},
-        values = this.wiki.filterTiddlers(this.actionValues,this),
-        names;
-    if(this.actionFields) {
-        names = this.wiki.filterTiddlers(this.actionFields,this);
-        $tw.utils.each(names,function(fieldname,index) {
-            additions[fieldname] = values[index] || "";
-        });
-    } else if(this.actionIndexes) {
-        // TODO: Set indexes
-    }
-    // TODO: Respect this.actionTimestamp
-    this.wiki.addTiddler(new $tw.Tiddler(this.wiki.getCreationFields(),tiddler,{title: this.actionTiddler},this.wiki.getModificationFields(),additions));
-    return true; // Action was invoked
+	var tiddler = this.wiki.getTiddler(this.actionTiddler),
+		additions = {},
+		values = this.wiki.filterTiddlers(this.actionValues,this),
+		names;
+	if(this.actionFields) {
+		names = this.wiki.filterTiddlers(this.actionFields,this);
+		$tw.utils.each(names,function(fieldname,index) {
+			additions[fieldname] = values[index] || "";
+		});
+	} else if(this.actionIndexes) {
+		// TODO: Set indexes
+	}
+	// TODO: Respect this.actionTimestamp
+	this.wiki.addTiddler(new $tw.Tiddler(this.wiki.getCreationFields(),tiddler,{title: this.actionTiddler},this.wiki.getModificationFields(),additions));
+	return true; // Action was invoked
 };
 
 exports["action-setmultiplefields"] = SetMultipleFieldsWidget;
 
 })();
-    
+	
