TiddlyWiki/TiddlyWiki5

commit 88812092fd8fa7da18e6da77f9c410338469cc4e

Fix crash with "wrap-lines" text editor operation if prefix or suffix is missing
Fixes #6376

core/modules/editor/operations/text/wrap-lines.js
@@ -13,15 +13,17 @@ Text editor operation to wrap the selected lines with a prefix and suffix
 "use strict";
 
 exports["wrap-lines"] = function(event,operation) {
+	var prefix = operation.paramObject.prefix || "",
+		suffix = operation.paramObject.suffix || "";
 	// Cut just past the preceding line break, or the start of the text
 	operation.cutStart = $tw.utils.findPrecedingLineBreak(operation.text,operation.selStart);
 	// Cut to just past the following line break, or to the end of the text
 	operation.cutEnd = $tw.utils.findFollowingLineBreak(operation.text,operation.selEnd);
 	// Add the prefix and suffix
-	operation.replacement = event.paramObject.prefix + "\n" +
+	operation.replacement = prefix + "\n" +
 				operation.text.substring(operation.cutStart,operation.cutEnd) + "\n" +
-				event.paramObject.suffix + "\n";
-	operation.newSelStart = operation.cutStart + event.paramObject.prefix.length + 1;
+				suffix + "\n";
+	operation.newSelStart = operation.cutStart + prefix.length + 1;
 	operation.newSelEnd = operation.newSelStart + (operation.cutEnd - operation.cutStart);
 };
 

commit 0892994fa297ec74ffa4ef18c470fba372a181e3

fix: bug introduced with https://github.com/Jermolene/TiddlyWiki5/commit/88812092fd8fa7da18e6da77f9c410338469cc4e fixes #6419

core/modules/editor/operations/text/wrap-lines.js
@@ -13,8 +13,8 @@ Text editor operation to wrap the selected lines with a prefix and suffix
 "use strict";
 
 exports["wrap-lines"] = function(event,operation) {
-	var prefix = operation.paramObject.prefix || "",
-		suffix = operation.paramObject.suffix || "";
+	var prefix = event.paramObject.prefix || "",
+		suffix = event.paramObject.suffix || "";
 	// Cut just past the preceding line break, or the start of the text
 	operation.cutStart = $tw.utils.findPrecedingLineBreak(operation.text,operation.selStart);
 	// Cut to just past the following line break, or to the end of the text