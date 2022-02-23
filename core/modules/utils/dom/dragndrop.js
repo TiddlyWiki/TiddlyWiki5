603dfd8cf936053f2c4de0737029a648ed20e141
saqimtiaz <saq.imtiaz@gmail.com>
2022-02-23 16:09:05 +0100

    fix: remove redundant variable declaration

--- core/modules/utils/dom/dragndrop.js
+++ core/modules/utils/dom/dragndrop.js
@@ -26,9 +26,8 @@ exports.makeDraggable = function(options) {
 	var dragImageType = options.dragImageType || "dom",
 		dragImage,
 		domNode = options.domNode,
-		dragHandle,
 		selector = options.selector,
-	dragHandle = options.selector && domNode.querySelector(options.selector) || domNode;
+		dragHandle = options.selector && domNode.querySelector(options.selector) || domNode;
 	// Make the dom node draggable (not necessary for anchor tags)
 	if((domNode.tagName || "").toLowerCase() !== "a") {
 		dragHandle.setAttribute("draggable","true");
