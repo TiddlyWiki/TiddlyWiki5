TiddlyWiki/TiddlyWiki5

commit adcef4f803ba84f6bfb47f4117eb7ccb50d8b9fb

Fix possible bug (#4122)
Check for outstanding tasks to execute before the task in question seems to have invalid checks.

boot/boot.js
@@ -2596,7 +2596,8 @@ $tw.boot.isStartupTaskEligible = function(taskModule) {
 		for(t=0; t<remaining.length; t++) {
 			var task = remaining[t];
 			if(task.before && task.before.indexOf(name) !== -1) {
-				if($tw.boot.doesTaskMatchPlatform(task) || (task.name && $tw.boot.disabledStartupModules.indexOf(name) !== -1)) {
+				//check if the other task matches the platform and is not disabled
+				if($tw.boot.doesTaskMatchPlatform(task) || (task.name && $tw.boot.disabledStartupModules.indexOf(task.name) === -1)) {
 					return false;
 				}
 			}

commit bc242cf83ad1220fb7e10bf7eb2b6d84602e2caf

Fix bug introduced in #4122
Fixes #6579

boot/boot.js
@@ -2596,8 +2596,7 @@ $tw.boot.isStartupTaskEligible = function(taskModule) {
 		for(t=0; t<remaining.length; t++) {
 			var task = remaining[t];
 			if(task.before && task.before.indexOf(name) !== -1) {
-				//check if the other task matches the platform and is not disabled
-				if($tw.boot.doesTaskMatchPlatform(task) || (task.name && $tw.boot.disabledStartupModules.indexOf(task.name) === -1)) {
+				if($tw.boot.doesTaskMatchPlatform(task) && (!task.name || $tw.boot.disabledStartupModules.indexOf(task.name) === -1)) {
 					return false;
 				}
 			}