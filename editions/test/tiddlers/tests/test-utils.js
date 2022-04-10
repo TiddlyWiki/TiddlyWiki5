885659128bebfb6b3830e20cf665a8f1f17ac8de
林东吴 <lindongwu@bytedance.com>
2022-04-10 15:31:29 +0800

    fix: not possible to test internal date without hijack
    Expected '20220410073037515' to be '20220410073037516'.

--- editions/test/tiddlers/tests/test-utils.js
+++ editions/test/tiddlers/tests/test-utils.js
@@ -79,7 +79,6 @@ describe("Utility tests", function() {
 		expect(fds(d,"MM0DD")).toBe("1109");
 		expect(fds(d,"MM0\\D\\D")).toBe("110DD");
 		expect(fds(d,"[UTC]YYYY0MM0DD0hh0mm0ssXXX")).toBe("20141109094128542");
-		expect(fds(undefined,"[UTC]YYYY0MM0DD0hh0mm0ssXXX")).toBe(fds(new Date(),"[UTC]YYYY0MM0DD0hh0mm0ssXXX"));
 
 		// test some edge cases found at: https://en.wikipedia.org/wiki/ISO_week_date
 		// 2016-11-13 is Week 45 and it's a Sunday (month nr: 10)
