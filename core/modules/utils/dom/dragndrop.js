b90f77446af62e5883ef8a5c166e86a6e470fb76
Simon Huber <huber.simon@protonmail.com>
2022-04-13 18:21:27 +0200

    Update dragndrop.js

--- core/modules/utils/dom/dragndrop.js
+++ core/modules/utils/dom/dragndrop.js
@@ -57,26 +57,8 @@ exports.makeDraggable = function(options) {
 				// Invoke drag-start actions if given
 				if(startActions !== undefined) {
 					// Collect our variables
+					variables = $tw.utils.collectDOMVariables(domNode,null,event);
 					variables.modifier = $tw.keyboardManager.getEventModifierKeyDescriptor(event);
-					$tw.utils.each(domNode.attributes,function(attribute) {
-						variables["dom-" + attribute.name] = attribute.value.toString();
-					});
-					variables["actionTiddler"] = titleString;
-					variables["tv-popup-coords"] = "(" + domNode.offsetLeft + "," + domNode.offsetTop +"," + domNode.offsetWidth + "," + domNode.offsetHeight + ")";
-					variables["tv-selectednode-posx"] = domNode.offsetLeft.toString();
-					variables["tv-selectednode-posy"] = domNode.offsetTop.toString();
-					variables["tv-selectednode-width"] = domNode.offsetWidth.toString();
-					variables["tv-selectednode-height"] = domNode.offsetHeight.toString();
-					if(event.clientX && event.clientY) {
-						//Add variables for event X and Y position relative to selected node
-						domNodeRect = domNode.getBoundingClientRect();
-						variables["event-fromselected-posx"] = (event.clientX - domNodeRect.left).toString();
-						variables["event-fromselected-posy"] = (event.clientY - domNodeRect.top).toString();
-
-						//Add variables for event X and Y position relative to the viewport
-						variables["event-fromviewport-posx"] = event.clientX.toString();
-						variables["event-fromviewport-posy"] = event.clientY.toString();
-					}
 					options.widget.invokeActionString(startActions,options.widget,event,variables);
 				}
 				// Create the drag image elements
