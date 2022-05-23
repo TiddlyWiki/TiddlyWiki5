9e8d05f69905cbd3b79c19395f37542def4103b3
jeremy@jermolene.com <jeremy@jermolene.com>
2022-05-23 16:40:21 +0100

    Require period prefix for custom filter operator functions
    To ensure that custom filter operators cannot clash with future core operators.

--- core/modules/filters/unknown.js
+++ core/modules/filters/unknown.js
@@ -20,48 +20,48 @@ var fieldFilterOperatorFn = require("$:/core/modules/filters/field.js").field;
 Export our filter function
 */
 exports["[unknown]"] = function(source,operator,options) {
-	var customDefinitionTitle = "[" + operator.operator + "[]]",
-		customDefinition = options.widget && options.widget.getVariableInfo && options.widget.getVariableInfo(customDefinitionTitle);
-	if(customDefinition && customDefinition.srcVariable && customDefinition.srcVariable.isFunctionDefinition) {
-		var variables = Object.create(null);
-		$tw.utils.each(customDefinition.srcVariable.params,function(param,index) {
-			var value = operator.operands[index];
-			if(value === undefined) {
-				value = param["default"] || "";
-			}
-			variables[param.name] = value;
-		});
-		var getVariable = function(name,opts) {
-			if(name in variables) {
-				return variables[name];
-			} else {
-				return options.widget.getVariable(name,opts);
-			};
-		};
-		var getVariableInfo = function(name,opts) {
-			if(name in variables) {
-				return {
-					text: variables[name]
-				};
-			} else {
-				return options.widget.getVariableInfo(name,opts);
-			};
-		}
-		var list = options.wiki.filterTiddlers(customDefinition.srcVariable.value,{getVariable: getVariable,getVariableInfo: getVariableInfo},source);
-		if(operator.prefix === "!") {
-			var results = [];
-			source(function(tiddler,title) {
-				if(list.indexOf(title) === -1) {
-					results.push(title);
+	if(operator.operator.charAt(0) === ".") {
+		var customDefinition = options.widget && options.widget.getVariableInfo && options.widget.getVariableInfo(operator.operator);
+		if(customDefinition && customDefinition.srcVariable && customDefinition.srcVariable.isFunctionDefinition) {
+			var variables = Object.create(null);
+			$tw.utils.each(customDefinition.srcVariable.params,function(param,index) {
+				var value = operator.operands[index];
+				if(value === undefined) {
+					value = param["default"] || "";
 				}
+				variables[param.name] = value;
 			});
-			return results;
-		} else {
-			return list;
+			var getVariable = function(name,opts) {
+				if(name in variables) {
+					return variables[name];
+				} else {
+					return options.widget.getVariable(name,opts);
+				};
+			};
+			var getVariableInfo = function(name,opts) {
+				if(name in variables) {
+					return {
+						text: variables[name]
+					};
+				} else {
+					return options.widget.getVariableInfo(name,opts);
+				};
+			}
+			var list = options.wiki.filterTiddlers(customDefinition.srcVariable.value,{getVariable: getVariable,getVariableInfo: getVariableInfo},source);
+			if(operator.prefix === "!") {
+				var results = [];
+				source(function(tiddler,title) {
+					if(list.indexOf(title) === -1) {
+						results.push(title);
+					}
+				});
+				return results;
+			} else {
+				return list;
+			}
 		}
-	} else {
-		return fieldFilterOperatorFn(source,operator,options);
 	}
+	return fieldFilterOperatorFn(source,operator,options);
 };
 
 })();
