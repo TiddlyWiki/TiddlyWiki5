02ab43c639075af3e4d0ca8ed76bf6c864e921ea
btheado <brian.theado@gmail.com>
2022-10-29 14:20:27 -0400

    Initial widget tutorials extracted from https://btheado.github.io/tw-widget-tutorial/

---
+++ editions/dev/tiddlers/javascript-widget-tutorial/hello-attribute.js
@@ -0,0 +1,47 @@
+/*\
+
+Hello, World widget
+
+\*/
+(function() {
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var Widget = require("$:/core/modules/widgets/widget.js").widget;
+
+var MyWidget = function(parseTreeNode, options) {
+	this.initialise(parseTreeNode, options);
+};
+
+/*
+Inherit from the base widget class
+*/
+MyWidget.prototype = new Widget();
+
+/*
+Render this widget into the DOM
+*/
+MyWidget.prototype.render = function(parent, nextSibling) {
+	this.parentDomNode = parent;
+	this.computeAttributes();
+	var message = this.getAttribute("message", "World");
+	var textNode = this.document.createTextNode("Hello, " + message + "!");
+	parent.insertBefore(textNode, nextSibling);
+	this.domNodes.push(textNode);
+};
+
+/*
+A widget with optimized performance will selectively refresh, but here we refresh always
+*/
+MyWidget.prototype.refresh = function(changedTiddlers) {
+	// Regenerate and rerender the widget and
+	// replace the existing DOM node
+	this.refreshSelf();
+	return true;
+};
+
+exports.hello = MyWidget;
+
+})();
---
+++ editions/dev/tiddlers/javascript-widget-tutorial/refreshcount.js
@@ -0,0 +1,43 @@
+/*\
+
+widget to count the number of times this widget refreshes
+
+\*/
+(function() {
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var Widget = require("$:/core/modules/widgets/widget.js").widget;
+
+var MyWidget = function(parseTreeNode, options) {
+	this.refreshCount = 0;
+	this.initialise(parseTreeNode, options);
+};
+
+/*
+Inherit from the base widget class
+*/
+MyWidget.prototype = new Widget();
+
+/*
+Render this widget into the DOM
+*/
+MyWidget.prototype.render = function(parent, nextSibling) {
+	this.parentDomNode = parent;
+	var textNode = this.document.createTextNode(this.refreshCount + " refreshes");
+	parent.insertBefore(textNode, nextSibling);
+	this.domNodes.push(textNode);
+};
+
+MyWidget.prototype.refresh = function(changedTiddlers) {
+	// Regenerate and rerender the widget and replace the existing DOM node
+	this.refreshCount++;
+	this.refreshSelf();
+	return true;
+};
+
+exports.refreshcount = MyWidget;
+
+})();
