20a306adbdb869e28fb4284e837532abc7b8942c
Cindy Ruan <80615570+cdruan@users.noreply.github.com>
2023-01-11 15:38:32 -0800

    Improve markdown/tiddlywiki integration
    - widget block should not interrupt paragraph
    - ignore tw-syntax links inside markdown-syntax links
    - remove repeated renderWikiTextPragma parsing
    - more efficient findNextMatch when examining tw rules

--- plugins/tiddlywiki/markdown/wrapper.js
+++ plugins/tiddlywiki/markdown/wrapper.js
@@ -16,13 +16,11 @@ var require = function(m) {
 	return realRequire("$:/plugins/tiddlywiki/markdown/" + m + ".js");
 };
 
-/// Helper functions ///
+var MarkdownIt = require("markdown-it");
 
 function parseAsBoolean(tiddlerName) {
-	return $tw.wiki.getTiddlerText(tiddlerName).trim().toLowerCase() === "true";
+	return $tw.wiki.getTiddlerText(tiddlerName,"false").trim().toLowerCase() === "true";
 }
-
-/// Set up configuration options ///
 
 var pluginOpts = {
 	renderWikiText: parseAsBoolean("$:/config/markdown/renderWikiText"),
@@ -35,39 +33,108 @@ var markdownOpts = {
 	breaks: parseAsBoolean("$:/config/markdown/breaks"),
 	quotes: $tw.wiki.getTiddlerText("$:/config/markdown/quotes").trim(),
 	typographer: parseAsBoolean("$:/config/markdown/typographer"),
-	linkify: false
+	linkify: parseAsBoolean("$:/config/markdown/linkify")
 };
 
-var MarkdownIt = require("markdown-it");
-var md = new MarkdownIt(markdownOpts);
+// Retrieve needed TW rule classes and instantiated rules
+function setupWikiRules(pluginOptions) {
+	var results = {};
 
-md = md.use(require("markdown-it-sub"))
-	   .use(require("markdown-it-sup"))
-	   .use(require("markdown-it-ins"))
-	   .use(require("markdown-it-mark"))
-	   .use(require("markdown-it-footnote"))
-	   .use(require("markdown-it-deflist"))
-	   .use(require("markdown-it-tiddlywiki"),pluginOpts);
+	function collectAllRules(classes,type) {
+		var rulesInfo = [], key,
+			self = wikiParser;
+		for(key in classes) {
+			// instantiate the rule
+			var RuleClass = classes[key];
+			var rule = new RuleClass(self);
+			rule.name = key;
+			rule.class = RuleClass;
+			rule.is = {};
+			rule.is[type] = true;
+			rule.init(self);
 
-if($tw.modules.titles["$:/plugins/tiddlywiki/katex/katex.min.js"]) {
-	md = md.use(require("markdown-it-katex"));
-}
-
-// Overwrite default: render attribute strings in e"..." format instead,
-// so HTML entities can be decoded. See parseStringLiteralExt() below.
-md.renderer.renderAttrs = function renderAttrs(token) {
-	var i, l, result;
-
-	if(!token.attrs) { return ''; }
-
-	result = '';
-
-	for(i=0, l=token.attrs.length; i<l; i++) {
-		result += ' ' + md.utils.escapeHtml(token.attrs[i][0]) + '=e"' + md.utils.escapeHtml(token.attrs[i][1]) + '"';
+			rulesInfo.push({
+				rule: rule,
+				matchIndex: -1
+			});
+		};
+		return rulesInfo;
 	}
 
-	return result;
-};
+	// first pass: get all rule classes
+	var wikiParser = new $tw.Wiki.parsers["text/vnd.tiddlywiki"](null, '', {parseAsInline: true, wiki: $tw.wiki});
+
+	// restore all possible rules from each rule class
+	wikiParser.pragmaRules = collectAllRules(wikiParser.pragmaRuleClasses,'pragma');
+	wikiParser.blockRules = collectAllRules(wikiParser.blockRuleClasses,'block');
+	wikiParser.inlineRules = collectAllRules(wikiParser.inlineRuleClasses,'inline');
+
+	var pragma = pluginOptions.renderWikiText
+			? "\\rules except latex-parser extlink\n" + pluginOptions.renderWikiTextPragma
+			: "\\rules only html entity commentinline commentblock";
+
+	wikiParser.pos = 0;
+	wikiParser.source = pragma;
+	wikiParser.sourceLength = pragma.length;
+
+	// second pass: remove uninterested rules based on \rules pragma
+	wikiParser.parsePragmas();
+
+	results.blockRules = {};
+	results.inlineRules = {}
+	results.blockRuleClasses = {};
+	results.inlineRuleClasses = {};
+
+	// save the rule sets for future markdown parsing
+	wikiParser.blockRules.forEach(function(ruleinfo) {
+		results.blockRules[ruleinfo.rule.name] = ruleinfo;
+		results.blockRuleClasses[ruleinfo.rule.name] = ruleinfo.rule.class;
+	});
+	wikiParser.inlineRules.forEach(function(ruleinfo) {
+		results.inlineRules[ruleinfo.rule.name] = ruleinfo;
+		results.inlineRuleClasses[ruleinfo.rule.name] = ruleinfo.rule.class;
+	});
+	return results;
+}
+
+// Creates markdown-it parser
+function createMarkdownEngine(markdownItOptions, pluginOptions) {
+	var md = new MarkdownIt(markdownItOptions)
+				.use(require("markdown-it-sub"))
+				.use(require("markdown-it-sup"))
+				.use(require("markdown-it-ins"))
+				.use(require("markdown-it-mark"))
+				.use(require("markdown-it-footnote"))
+				.use(require("markdown-it-deflist"));
+
+	var results = setupWikiRules(pluginOptions);
+
+	MarkdownParser.prototype.blockRuleClasses = results.blockRuleClasses;
+	MarkdownParser.prototype.blockRules = results.blockRules;
+
+	MarkdownParser.prototype.inlineRuleClasses = results.inlineRuleClasses;
+	MarkdownParser.prototype.inlineRules = results.inlineRules;
+
+	if(pluginOptions.renderWikiText && $tw.modules.titles["$:/plugins/tiddlywiki/katex/katex.min.js"]) {
+		md.use(require("markdown-it-katex"));
+	}
+
+	md.use(require("markdown-it-tiddlywiki"),{
+			renderWikiText: pluginOptions.renderWikiText,
+			blockRules: results.blockRules,
+			inlineRules: results.inlineRules
+		});
+
+	$tw.utils.each(['image','prettylink','prettyextlink'], function(rule) {
+		if(MarkdownParser.prototype.inlineRules[rule]) {
+			// delegate to md; ignore the rule class in WikiParser
+			delete MarkdownParser.prototype.inlineRuleClasses[rule];
+		}
+	});
+	return md;
+}
+
+/// Parse tree post processing ///
 
 function deactivateLinks(tree) {
 	$tw.utils.each(tree,function(node) {
@@ -85,7 +152,10 @@ function deactivateLinks(tree) {
 
 // true if the node contains "_codified_" class attribute
 function isCodified(node) {
-	return node.attributes && node.attributes.class && node.attributes.class.value.split(' ').includes("_codified_");
+	return node.attributes
+		&& node.attributes.class
+		&& node.attributes.class.type === "string"
+		&& (node.attributes.class.value.split(" ").indexOf("_codified_") !== -1);
 }
 
 function decodeEntities(s) {
@@ -94,7 +164,7 @@ function decodeEntities(s) {
 
 // Add e"..." and e'....' syntax to enable decoding of HTML entities
 // in string literals.
-function parseStringLiteralExt(source,pos) {
+function parseStringLiteralExtended(source,pos) {
 	var node = {
 		type: "string",
 		start: pos
@@ -113,7 +183,7 @@ function parseStringLiteralExt(source,pos) {
 	} else {
 		return null;
 	}
-};
+}
 
 function processWikiTree(tree,hasWikiLinkRule) {
 	var stack = [].concat(tree);
@@ -139,22 +209,32 @@ function processWikiTree(tree,hasWikiLinkRule) {
 	}
 }
 
-var MarkdownParser = function(type,text,options) {
-	var pragma = pluginOpts.renderWikiText ? "\\rules except latex-parser\n" + pluginOpts.renderWikiTextPragma : "\\rules only html entity commentinline commentblock";
+// to extend MarkdownIt outside of this module, do:
+//
+// md = $tw.Wiki.parsers["text/markdown"].prototype.md;
+// md.use(plugin[, options]);
+MarkdownParser.prototype.md = createMarkdownEngine(markdownOpts,pluginOpts);
+
+function MarkdownParser(type,text,options) {
 	var env = {}
+	var md = this.md;
 	var mdTree = md.parse(text,env);
-	var textToParse = pragma + '\n<div class="markdown">\n' + md.renderer.render(mdTree,md.options,env) + '</div>';
+	var textToParse = '<div class="markdown">\n' + md.renderer.render(mdTree,md.options,env) + '</div>';
 
 	//console.log(JSON.stringify(mdTree,null,2));
-	//console.log((pluginOpts.renderWikiText ? "RenderWiki ON:" : "RenderWki OFF:") + "\n----------------\n" + textToParse);
+	//console.log("\n----------------\n" + textToParse);
 
 	var wikiParser;
 
 	var origParseStringLiteral = $tw.utils.parseStringLiteral;
-	$tw.utils.parseStringLiteral = parseStringLiteralExt;
+	$tw.utils.parseStringLiteral = parseStringLiteralExtended;
 
 	try {
-		wikiParser = $tw.wiki.parseText("text/vnd.tiddlywiki",textToParse,{parseAsInline: true, wiki: options.wiki});
+		wikiParser = new $tw.Wiki.parsers["text/vnd.tiddlywiki"](null,textToParse,{
+			parseAsInline: true,
+			wiki: options.wiki,
+			rules: { pragma: {}, block: this.blockRuleClasses, inline: this.inlineRuleClasses }
+		});
 	}
 	catch (err) {
 		wikiParser = $tw.wiki.parseText("text/vnd.tiddlywiki",
@@ -179,13 +259,7 @@ var MarkdownParser = function(type,text,options) {
 	this.source = text;
 	this.type = type || "text/markdown";
 	this.wiki = options.wiki;
-};
-
-// to extend MarkdownIt outside of this module, do:
-//
-// md = $tw.Wiki.parsers["text/markdown"].prototype.md;
-// md.use(plugin[, options]);
-MarkdownParser.prototype.md = md;
+}
 
 exports["text/markdown"] = MarkdownParser;
 exports["text/x-markdown"] = MarkdownParser;
