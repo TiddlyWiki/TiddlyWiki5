c8225875cd432cd8170b70a1632b2e55afdcee4f
yaisog <m@rcuswinter.de>
2023-01-12 07:50:30 +0100

    Create debug-log.js
    Original file was mistakenly named debug-log-js (with a hyphen).

---
+++ core/modules/filters/debug-log.js
@@ -0,0 +1,36 @@
+/*\
+title: $:/core/modules/filters/debug-log.js
+type: application/javascript
+module-type: filteroperator
+
+Filter operator for logging the current list and an optional variable value to the console
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Export our filter function
+*/
+exports["debug-log"] = function(source,operator,options) {
+	var results = [], data = {};
+
+	if (operator.operand)
+		data[operator.operand] = options.widget.getVariable(operator.operand,{defaultValue:""});
+
+	source(function(tiddler,title) {
+		results.push(title);
+	});
+
+	console.group("filter-debug");
+	$tw.utils.logTable(results);
+	if (operator.operand) $tw.utils.logTable(data);
+	console.groupEnd();
+
+	return results;
+};
+
+})();
