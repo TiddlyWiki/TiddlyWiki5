9288420c674d5a3f0fd6ce980b5d6d440b08181c
jeremy@jermolene.com <jeremy@jermolene.com>
2023-01-13 11:34:39 +0000

    Add setquerystring operator

--- core/modules/filters/url-ops.js
+++ core/modules/filters/url-ops.js
@@ -13,25 +13,11 @@ Filter operators for URL operations
 "use strict";
 
 exports["setquerystring"] = function(source,operator,options) {
-	var URL = $tw.browser ? window.URL : require("url").URL,
-		URLSearchParams = $tw.browser ? window.URLSearchParams : require("url").URLSearchParams,
-		name = operator.operands.length >= 1 ? operator.operands[0] : null,
+	var name = operator.operands.length >= 1 ? operator.operands[0] : null,
 		value = operator.operands.length >= 2 ? operator.operands[1] : "",
 		results = [];
 	source(function(tiddler,title) {
-		var url;
-		try {
-			url = new URL(title);
-		} catch(e) {
-		}
-		if(url) {
-			var params = new URLSearchParams(url.search);
-			if(name) {
-				params.set(name,value);
-			}
-			url.search = params.toString();
-			results.push(url.toString());
-		}
+		results.push($tw.utils.setQueryStringParameter(title,name,value));
 	});
 	return results;
 };
