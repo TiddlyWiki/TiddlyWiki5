88aa2bec3fdb2354266f750dcb9c85230b85c328
Cameron Fischer <fischer.cameron@gmail.com>
2023-03-23 18:11:09 -0400

    Fixed issue with trailing newlines in macrodefs

--- core/modules/parsers/wikiparser/rules/macrodef.js
+++ core/modules/parsers/wikiparser/rules/macrodef.js
@@ -58,7 +58,7 @@ exports.parse = function() {
 	var reEnd;
 	if(this.match[3]) {
 		// If so, the end of the body is marked with \end
-		reEnd = new RegExp("(\\r?\\n\\s*\\\\end[^\\S\\n\\r]*(?:" + $tw.utils.escapeRegExp(this.match[1]) + ")?(?:$|\\r?\\n))","mg");
+		reEnd = new RegExp("(\\r?\\n[^\\S\\n\\r]*\\\\end[^\\S\\n\\r]*(?:" + $tw.utils.escapeRegExp(this.match[1]) + ")?(?:$|\\r?\\n))","mg");
 	} else {
 		// Otherwise, the end of the definition is marked by the end of the line
 		reEnd = /($|\r?\n)/mg;
