83f2eef515cb0e30cd38423e48af154ffe42fd06
yaisog <m@rcuswinter.de>
2023-05-01 09:02:41 +0200

    Replace previous result only when non-empty

--- core/modules/filterrunprefixes/then.js
+++ core/modules/filterrunprefixes/then.js
@@ -17,12 +17,16 @@ Export our filter prefix function
 */
 exports.then = function(operationSubFunction) {
 	return function(results,source,widget) {
-		if(results.length !== 0) {
-			// Main result so far is not empty
-            results.clear();
-			results.pushTop(operationSubFunction(source,widget));
-		}
-	};
+        if(results.length !== 0) {
+            // Only run if previous run(s) produced results
+            var thisRunResult = operationSubFunction(source,widget);
+            if(thisRunResult.length !== 0) {
+                // Replace results only if this run actually produces a result
+                results.clear();
+                results.pushTop(thisRunResult);
+            }
+        }
+    };
 };
 
 })();
