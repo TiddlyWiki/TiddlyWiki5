09e626e8ab7924f0490de6eb874840cbe2b5fa90
jeremy@jermolene.com <jeremy@jermolene.com>
2023-05-03 08:51:55 +0100

    Further fixes to cancelling outstanding HTTP requests

--- core/modules/utils/dom/http.js
+++ core/modules/utils/dom/http.js
@@ -65,9 +65,11 @@ HttpClient.prototype.initiateHttpRequest = function(options) {
 
 HttpClient.prototype.cancelAllHttpRequests = function() {
 	var self = this;
-	for(var t=this.requests.length - 1; t--; t>=0) {
-		var requestInfo = this.requests[t];
-		requestInfo.request.cancel();
+	if(this.requests.length > 0) {
+		for(var t=this.requests.length - 1; t--; t>=0) {
+			var requestInfo = this.requests[t];
+			requestInfo.request.cancel();
+		}	
 	}
 	this.requests = [];
 	this.updateRequestTracker();
