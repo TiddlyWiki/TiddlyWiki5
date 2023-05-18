63150b996d68ef7a44b1bf748c22773dee7a80de
yaisog <m@rcuswinter.de>
2023-05-18 19:53:39 +0200

    Replace leading spaces with tabs

--- core/modules/filterrunprefixes/then.js
+++ core/modules/filterrunprefixes/then.js
@@ -17,16 +17,16 @@ Export our filter prefix function
 */
 exports.then = function(operationSubFunction) {
 	return function(results,source,widget) {
-        if(results.length !== 0) {
-            // Only run if previous run(s) produced results
-            var thisRunResult = operationSubFunction(source,widget);
-            if(thisRunResult.length !== 0) {
-                // Replace results only if this run actually produces a result
-                results.clear();
-                results.pushTop(thisRunResult);
-            }
-        }
-    };
+		if(results.length !== 0) {
+			// Only run if previous run(s) produced results
+			var thisRunResult = operationSubFunction(source,widget);
+			if(thisRunResult.length !== 0) {
+				// Replace results only if this run actually produces a result
+				results.clear();
+				results.pushTop(thisRunResult);
+			}
+		}
+	};
 };
 
 })();
