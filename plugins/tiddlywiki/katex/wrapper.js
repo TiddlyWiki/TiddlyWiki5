a9d1ce1674f10b27a6ad98bebbd3d5482fb396fc
Tavin Cole <tavin.cole@gmail.com>
2023-07-11 13:14:10 +0200

    just don't use getElementById

--- plugins/tiddlywiki/katex/wrapper.js
+++ plugins/tiddlywiki/katex/wrapper.js
@@ -50,7 +50,6 @@ KaTeXWidget.prototype = new Widget();
 Render this widget into the DOM
 */
 KaTeXWidget.prototype.render = function(parent,nextSibling) {
-	var document = this.document;
 	// Housekeeping
 	this.parentDomNode = parent;
 	this.computeAttributes();
@@ -60,13 +59,13 @@ KaTeXWidget.prototype.render = function(parent,nextSibling) {
 	var displayMode = this.getAttribute("displayMode",this.parseTreeNode.displayMode || "false") === "true";
 	katex.updateMacros();
 	// Render it into a span
-	var span = document.createElement("span"),
+	var span = this.document.createElement("span"),
 		options = {throwOnError: false, displayMode: displayMode, macros: katex.macros};
 	options.trust = function (ctx) {
 		return ctx.command == '\\href' || ctx.command == '\\htmlClass' || ctx.command == '\\htmlData' || ctx.command == '\\htmlId' && ctx.id[0] == '#';
 	};
 	try {
-		if(!document.isTiddlyWikiFakeDom) {
+		if(!this.document.isTiddlyWikiFakeDom) {
 			katex.render(text,span,options);
 		} else {
 			span.innerHTML = katex.renderToString(text,options);
@@ -79,6 +78,7 @@ KaTeXWidget.prototype.render = function(parent,nextSibling) {
 	var tiddlerFrame = parent.closest('.tc-tiddler-frame');
 	var tiddlerTitle = tiddlerFrame.dataset.tiddlerTitle;
 	$tw.utils.each(span.querySelectorAll('.katex-label [id^="#"]'), function (element) {
+		element.classList.add('katex-label' + element.id);
 		element.id = '#' + tiddlerTitle + element.id;
 	});
 	$tw.utils.each(span.querySelectorAll('.katex-eqref [href^="##"]'), function (element) {
@@ -87,12 +87,13 @@ KaTeXWidget.prototype.render = function(parent,nextSibling) {
 	// Insert it into the DOM
 	parent.insertBefore(span,nextSibling);
 	this.domNodes.push(span);
-	// compute data-katex-eqnum attributes
+	// compute data-katex-eqnum attributes on elements made by \eqref
 	$tw.utils.each(span.querySelectorAll('.katex-eqref [data-katex-label]'), function (element) {
-		var katexLabel = document.getElementById('#' + tiddlerTitle + '#' + element.dataset.katexLabel);
-		if (!katexLabel) return;
+		// find a unique matching element made by \label (or quit)
+		var katexLabels = tiddlerFrame.getElementsByClassName('katex-label#' + element.dataset.katexLabel);
+		if (katexLabels.length != 1) return;
 		// everything is a <span> and the span holding a row doesn't even have a class
-		var katexRow = katexLabel.closest('.vlist > *');
+		var katexRow = katexLabels[0].closest('.vlist > *');
 		var katexPos = Array.prototype.indexOf.call(katexRow.parentElement.children, katexRow);		// which row has the label
 		var katexEqn = katexRow.closest('.katex-html').querySelectorAll('.tag .eqn-num')[katexPos];	// which eqn num corresponds
 		if (!tiddlerFrame._katex_eqn_num_elements) {
