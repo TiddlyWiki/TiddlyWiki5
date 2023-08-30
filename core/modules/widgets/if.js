90d344bb61b76b5514ca917103e36ce3065b4ff7
Robin Munn <rmunn@pobox.com>
2023-08-30 07:05:37 +0700

    In if widget, use real widgets, not pseudo-widgets
    This moves most of the logic out of the `if` widget and into the
    condition widgets (then/elseif/else). The only thing the `if` widget
    is responsible for is setting up the singly-linked list of condition
    widgets; they are then responsible for carrying out the short-circuit
    logic and refreshing themselves as needed.

--- core/modules/widgets/if.js
+++ core/modules/widgets/if.js
@@ -16,7 +16,6 @@ var Widget = require("$:/core/modules/widgets/widget.js").widget;
 
 var IfWidget = function(parseTreeNode,options) {
 	this.initialise(parseTreeNode,options);
-	this.conditions = [];
 };
 
 /*
@@ -25,91 +24,128 @@ Inherit from the base widget class
 IfWidget.prototype = new Widget();
 
 /*
-Render this widget into the DOM
+Prepare child then and/or else widgets before they are created
 */
-IfWidget.prototype.render = function(parent,nextSibling) {
-	this.parentDomNode = parent;
-	this.computeAttributes();
-	this.execute();
-	this.renderChildren(parent,nextSibling);
-};
-
-IfWidget.prototype.safelyComputeAttribute = function(attribute,defaultValue) {
-	return attribute ? this.computeAttribute(attribute) : defaultValue;
-}
-
-IfWidget.prototype.computeConditions = function() {
-	var self = this;
-	this.filterString = this.safelyComputeAttribute(this.parseTreeNode.attributes.filter,"");
-	this.conditions = [{filter: this.filterString, parseTree: undefined}];
+IfWidget.prototype.preprocessChildNodes = function() {
+	var filter = this.parseTreeNode.attributes.filter;
 	$tw.utils.each(this.parseTreeNode.children,function(childNode) {
 		if(childNode.type === "then") {
-			self.conditions[0].parseTree = childNode;
-		} else if(childNode.type === "elseif") {
-			var childFilter = self.safelyComputeAttribute(childNode.attributes && childNode.attributes.filter,"")
-			self.conditions.push({filter: childFilter, parseTree: childNode});
+			if(filter) {
+				$tw.utils.addAttributeToParseTreeNode(childNode,filter);
+			}
 		} else if(childNode.type === "else") {
-			self.conditions.push({filter: true, parseTree: childNode});
+			$tw.utils.addAttributeToParseTreeNode(childNode,"filter","yes");
 		}
+		// elseif widgets have their own filter and don't need to be pre-processed
 	});
-}
-
-IfWidget.prototype.findFirstCondition = function() {
-	var self = this;
-	var resultIdx = -1;
-	$tw.utils.each(this.conditions,function(condition,idx) {
-		var filter = condition && condition.filter || "";
-		if(filter === true) {
-			resultIdx = idx;
-			return false; // Short-circuit
-		}
-		var filterResult = self.wiki.filterTiddlers(filter,self) || [];
-		if(filterResult && filterResult.length) {
-			resultIdx = idx;
-			return false; // Short-circuit
-		}
-	})
-	return resultIdx;
 }
 
 /*
 Compute the internal state of the widget
 */
 IfWidget.prototype.execute = function() {
-	// Get our parameters
-	this.computeConditions();
-	// Choose the appropriate child widget to construct
-	this.activeConditionIdx = this.findFirstCondition();
-	if(this.activeConditionIdx < 0) {
-		this.activeChildNodes = [];
-	} else {
-		var condition = this.conditions[this.activeConditionIdx];
-		this.activeChildNodes = condition && condition.parseTree && condition.parseTree.children;
-	}
+	// Set up then and/or else filters in parse tree
+	this.preprocessChildNodes();
 	// Make the child widgets
-	this.makeChildWidgets(this.activeChildNodes || []);
+	this.makeChildWidgets();
+	// Ensure each condition widget knows its next sibling
+	this.linkConditions();
 };
 
-/*
-Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
-*/
-IfWidget.prototype.refresh = function(changedTiddlers) {
-	var previousConditions = this.conditions;
-	this.computeConditions();
-	var changed = previousConditions.length !== this.conditions.length;
-	var previousActiveIdx = this.activeConditionIdx;
-	this.activeConditionIdx = this.findFirstCondition();
-	changed = this.activeConditionIdx !== previousActiveIdx || changed;
-	// Refresh if an attribute has changed, or the type associated with the target tiddler has changed
-	if(changed) {
-		this.refreshSelf();
-		return true;
-	} else {
-		return this.refreshChildren(changedTiddlers);
-	}
+IfWidget.prototype.linkConditions = function() {
+	var self = this,
+		conditions = [];
+	$tw.utils.each(this.children,function(childWidget) {
+		if(self.isConditionChild(childWidget)) {
+			conditions.push(childWidget);
+		}
+	});
+	$tw.utils.each(conditions,function(childWidget,idx) {
+		if(idx > 0) {
+			conditions[idx-1].nextCondition = childWidget;
+		}
+	});
+	// Last one will have nextCondition still undefined, which is what we want
+}
+
+IfWidget.prototype.isConditionChild = function(childWidget) {
+	var type = childWidget.parseTreeNode.type;
+	return (type === "then" || type === "else" || type === "elseif");
+}
+
+var ConditionWidget = function(parseTreeNode,options) {
+	this.oldShouldRender = false;
+	this.shouldRender = false;
+	this.shortCircuited = false;
+	this.nextCondition = undefined;
+	this.initialise(parseTreeNode,options);
 };
+ConditionWidget.prototype = new Widget();
+
+ConditionWidget.prototype.shortCircuit = function() {
+	this.oldShouldRender = this.shouldRender;
+	this.shouldRender = false;
+	this.shortCircuited = true;
+	if(this.nextCondition) {
+		this.nextCondition.shortCircuit();
+	}
+}
+
+ConditionWidget.prototype.calculateFilter = function() {
+	if(this.shortCircuited) {
+		return;
+	}
+	this.oldShouldRender = this.shouldRender;
+	var result = this.wiki.filterTiddlers(this.filter,this);
+	this.shouldRender = !!(result && result.length);
+	if(this.shouldRender && this.nextCondition) {
+		this.nextCondition.shortCircuit();
+	}
+}
+
+ConditionWidget.prototype.render = function(parent,nextSibling) {
+	this.parentDomNode = parent;
+	this.computeAttributes();
+	this.execute();
+	this.renderChildren(parent,nextSibling);
+};
+
+ConditionWidget.prototype.execute = function() {
+	this.filter = this.getAttribute("filter","");
+	this.calculateFilter();
+	this.shortCircuited = false;
+	var childNodes = this.shouldRender ? this.parseTreeNode.children : [];
+	this.makeChildWidgets(childNodes);
+};
+
+ConditionWidget.prototype.refresh = function(changedTiddlers) {
+	var changedAttributes = this.computeAttributes();
+	if(changedAttributes.filter) {
+		this.filter = this.getAttribute("filter","");
+	}
+	this.calculateFilter();
+	this.shortCircuited = false;
+	if(this.oldShouldRender === this.shouldRender) {
+		return this.refreshChildren(changedTiddlers);
+	} else {
+		if(this.shouldRender) {
+			// Went from false -> true, so we didn't have child widgets before
+			this.makeChildWidgets();
+			// Re-render self, but don't re-compute filter as it might be expensive and we already know the result
+			this.shortCircuited = true;
+			this.refreshSelf();
+			return true;
+		} else {
+			// Went from true -> false, so skip refreshSelf as there's no need for the this.findNextSiblingDomNode() step
+			this.removeChildDomNodes();
+			return true;
+		}
+	}
+}
 
 exports.if = IfWidget;
-// exports['if'] = IfWidget; // Uncomment if the above is a syntax error
+exports.then = ConditionWidget;
+exports.else = ConditionWidget;
+exports.elseif = ConditionWidget;
 
 })();
