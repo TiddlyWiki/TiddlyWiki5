066e553f8422aa22af2307d2db365f685f87c9d1
Jeremy Ruston <jeremy@jermolene.com>
2024-02-23 12:51:29 +0000

    Introduce command to load tiddler folders into a bag

---
+++ plugins/tiddlywiki/multiwikiserver/modules/command-mws-load-tiddlers.js
@@ -0,0 +1,40 @@
+/*\
+title: $:/plugins/tiddlywiki/multiwikiserver/mws-load-tiddlers.js
+type: application/javascript
+module-type: command
+
+Command to recursively load a directory of tiddler files into a bag
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+exports.info = {
+	name: "mws-load-tiddlers",
+	synchronous: true
+};
+
+var Command = function(params,commander,callback) {
+	this.params = params;
+	this.commander = commander;
+	this.callback = callback;
+};
+
+Command.prototype.execute = function() {
+	var self = this;
+	// Check parameters
+	if(this.params.length < 2) {
+		return "Missing pathname and/or bagname";
+	}
+	var tiddlersPath = this.params[0],
+		bagName = this.params[1];
+	$tw.mws.store.saveTiddlersFromPath(tiddlersPath,bagName);
+	return null;
+};
+
+exports.Command = Command;
+
+})();
