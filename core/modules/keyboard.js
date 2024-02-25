aa23235bb99e2fb7665ebee834394520cd41b087
Simon Huber <huber.simon@protonmail.com>
2024-02-25 07:22:55 +0100

    Update keyboard.js to make it possible to override a keyboard shortcut
    This PR makes it possible to override a keyboard shortcut like `((new-tiddler))` for example with another action in another Keyboard Shortcut tiddler with the same key descriptor if that Keyboard Shortcut tiddler has the field `override` set to `yes`

--- core/modules/keyboard.js
+++ core/modules/keyboard.js
@@ -321,6 +321,21 @@ KeyboardManager.prototype.updateShortcutLists = function(tiddlerList) {
 		this.shortcutParsedList[i] = this.shortcutKeysList[i] !== undefined ? this.parseKeyDescriptors(this.shortcutKeysList[i]) : undefined;
 		this.shortcutPriorityList[i] = tiddlerFields.priority === "yes" ? true : false;
 	}
+	for(var j=0; j<tiddlerList.length; j++) {
+		var title = tiddlerList[j],
+			tiddlerFields = $tw.wiki.getTiddler(title).fields;
+		if(tiddlerFields.override === "yes") {
+			for(var k=0; k<tiddlerList.length; k++) {
+				var tiddlerTitle = tiddlerList[k];
+				if((k !== j) && (this.parseKeyDescriptors(this.shortcutKeysList[k]) === this.parseKeyDescriptors(this.shortcutKeysList[j]))) {
+					this.shortcutKeysList.splice(k,1);
+					this.shortcutActionList.splice(k,1);
+					this.shortcutParsedList.splice(k,1);
+					this.shortcutPriorityList.splice(k,1);
+				}
+			}
+		}
+	}
 };
 
 /*
