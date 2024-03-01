03912b75cb4b3477c4d6f77b050684b17fa1ae29
Rob Hoelz <rob@hoelz.ro>
2024-03-01 09:10:11 -0600

    Add some tests for createBag

--- plugins/tiddlywiki/multiwikiserver/modules/tests-sql-tiddler-store.js
+++ plugins/tiddlywiki/multiwikiserver/modules/tests-sql-tiddler-store.js
@@ -43,6 +43,32 @@ function runSqlStoreTests(engine) {
 		expect(store.listBags()).toEqual([]);
 		expect(store.listRecipes()).toEqual([]);
 	});
+
+	it("should return a single bag after creating a bag", function() {
+		expect(store.createBag("bag-alpha", "Bag alpha")).toEqual(null);
+		expect(store.listBags()).toEqual([{
+			bag_name: "bag-alpha",
+			accesscontrol: "",
+			description: "Bag alpha"
+		}]);
+	});
+
+	it("should return empty results after failing to create a bag with an invalid name", function() {
+		expect(store.createBag("bag alpha", "Bag alpha")).toEqual({
+			message: "Invalid character(s)"
+		});
+		expect(store.listBags()).toEqual([]);
+	});
+
+	it("should return a bag with new description after re-creating", function() {
+		expect(store.createBag("bag-alpha", "Bag alpha")).toEqual(null);
+		expect(store.createBag("bag-alpha", "Different description")).toEqual(null);
+		expect(store.listBags()).toEqual([{
+			bag_name: "bag-alpha",
+			accesscontrol: "",
+			description: "Different description"
+		}]);
+	});
 }
 
 }
