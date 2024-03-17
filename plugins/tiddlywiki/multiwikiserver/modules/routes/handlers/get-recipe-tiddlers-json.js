09de91940e86c17c00f58195508f268e2a6c327d
Jeremy Ruston <jeremy@jermolene.com>
2024-03-17 19:30:22 +0000

    Typo

--- plugins/tiddlywiki/multiwikiserver/modules/routes/handlers/get-recipe-tiddlers-json.js
+++ plugins/tiddlywiki/multiwikiserver/modules/routes/handlers/get-recipe-tiddlers-json.js
@@ -3,7 +3,7 @@ title: $:/plugins/tiddlywiki/multiwikiserver/routes/handlers/get-recipe-tiddlers
 type: application/javascript
 module-type: route
 
-PUT /wiki/:recipe_name/recipes/:recipe_name/tiddlers.json?filter=:filter
+GET /wiki/:recipe_name/recipes/:recipe_name/tiddlers.json?filter=:filter
 
 NOTE: Urls currently include the recipe name twice. This is temporary to minimise the changes to the TiddlyWeb plugin
 
