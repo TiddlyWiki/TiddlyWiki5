200b592716810718bbf40fb65b17b9782d2cb93a
BurningTreeC <huber.simon@protonmail.com>
2024-04-07 13:35:37 +0200

    update view widget

--- core/modules/widgets/view.js
+++ core/modules/widgets/view.js
@@ -189,7 +189,6 @@ ViewWidget.prototype.initialiseView = function() {
 Initialise HTML wikified view methods
 */
 ViewWidget.prototype.initialiseHTMLWikifiedView = function(View) {
-	var self = this;
 
 	View.prototype.render = function(parent,nextSibling) {
 		this.renderWikified(parent,nextSibling);
@@ -209,7 +208,6 @@ ViewWidget.prototype.initialiseHTMLWikifiedView = function(View) {
 Initialise plain wikified view methods
 */
 ViewWidget.prototype.initialisePlainWikifiedView = function(View) {
-	var self = this;
 
 	View.prototype.render = function(parent,nextSibling) {
 		this.renderWikified(parent,nextSibling);
@@ -229,7 +227,6 @@ ViewWidget.prototype.initialisePlainWikifiedView = function(View) {
 Initialise HTML encoded plain wikified methods
 */
 ViewWidget.prototype.initialiseHTMLEncodedPlainWikifiedView = function(View) {
-	var self = this;
 	
 	View.prototype.render = function(parent,nextSibling) {
 		this.renderWikified(parent,nextSibling);
