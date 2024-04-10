1ae9a71539a351f305baff8bf42e16292efa652d
Joshua Fontany <joshua.fontany@gmail.com>
2024-04-09 21:56:13 -0700

    clarify comment spec reference

--- core/modules/server/server.js
+++ core/modules/server/server.js
@@ -143,7 +143,8 @@ function sendResponse(request,response,statusCode,headers,data,encoding) {
 	}
 	else
 	{
-		// broswers will try to cache 200, 203, 204, 206, 300, 301, 
+		// RFC 7231, 6.1. Overview of Status Codes:
+		// Broswer clients may cache 200, 203, 204, 206, 300, 301, 
 		// 404, 405, 410, 414, and 501 unless given explicit cache controls
 		headers["Cache-Control"] = "no-store";
 	}
