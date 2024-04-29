b981232f743c87cad03e6f34bc47a42ab2b7dfc0
pmario <pmariojo@gmail.com>
2024-04-29 14:52:22 +0200

    add "all" parameter to variables operator

--- core/modules/filters/variables.js
+++ core/modules/filters/variables.js
@@ -27,6 +27,8 @@ exports.variables = function(source,operator,options) {
 		sort,
 		widget = options.widget,
 		included = (operands[0].value) ? operands[0].value : "var fn proc macro widget";
+		// all will overwrite 
+		included = ((included.indexOf("all") !== -1)) ? "var fn proc macro widget" : included;
 
 		// variableTemplate = (operands.length > 1 && operands[1]) ? operands[1].value : "$type$ $name$($params$) $firstLine$";
 
@@ -49,19 +51,19 @@ exports.variables = function(source,operator,options) {
 			// varInfo.name = variable;
 			// variable = $tw.utils.formatVariableString(variableTemplate, varInfo);
 
-			if ( ((included.indexOf("fn") !== -1) && varInfo.srcVariable.isFunctionDefinition ) ||
+			if( ((included.indexOf("fn") !== -1) && varInfo.srcVariable.isFunctionDefinition ) ||
 				((included.indexOf("proc") !== -1) && varInfo.srcVariable.isProcedureDefinition ) ||
 				((included.indexOf("macro") !== -1) && varInfo.srcVariable.isMacroDefinition ) ||
 				((included.indexOf("widget") !== -1) && varInfo.srcVariable.isWidgetDefinition ) )
 			{
 				names.push(variable);
-			} else if ((included.indexOf("var") !== -1) && !varInfo.srcVariable.isFunctionDefinition && !varInfo.srcVariable.isProcedureDefinition && !varInfo.srcVariable.isMacroDefinition && !varInfo.srcVariable.isWidgetDefinition )
+			} else if((included.indexOf("var") !== -1) && !varInfo.srcVariable.isFunctionDefinition && !varInfo.srcVariable.isProcedureDefinition && !varInfo.srcVariable.isMacroDefinition && !varInfo.srcVariable.isWidgetDefinition )
 			{
 				names.push(variable);
 			}
 		}
 	}
-	if (sort) {
+	if(sort) {
 		return names.sort();
 	} else {
 		return names;
