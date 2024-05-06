a8957878511e3e691da441bf27f4cbfd712ed264
pmario <pmariojo@gmail.com>
2024-05-06 17:13:08 +0200

    renamed getvariablejson to jsonvariable and updated the docs

---
+++ core/modules/filters/jsonvariable.js
@@ -0,0 +1,33 @@
+/*\
+title: $:/core/modules/filters/jsonvariable.js
+type: application/javascript
+module-type: filteroperator
+
+Filter operator to get widget variable info and
+Display as JSON with basic formatting
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Export our filter function
+*/
+exports.jsonvariable = function(source,operator,options) {
+	var results = [],
+		widget = options.widget;
+	// "replacer" must be defined, otherwise JSON.stringify will throw a circular reference error RSOD
+	// "replacer" does not contain: isCacheable
+	var replacer= "params name value default resultList srcVariable text isFunctionDefinition isProcedureDefinition isWidgetDefinition isMacroDefinition".split(" ");
+	source(function(tiddler,title) {
+		var variable = widget.getVariableInfo(title, {}),
+		text = JSON.stringify(variable,replacer);
+		results.push(text || "");
+	});
+	return results;
+};
+
+})();
