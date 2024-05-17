fd5a3b5586d64a453eb75096c6c48050b3a89395
pmario <pmariojo@gmail.com>
2024-05-17 13:13:01 +0200

    revert variables and getvariables - use jsonvariable instead

--- core/modules/filters/getvariable.js
+++ core/modules/filters/getvariable.js
@@ -16,18 +16,9 @@ Filter operator for replacing input values by the value of the variable with the
 Export our filter function
 */
 exports.getvariable = function(source,operator,options) {
-	var results = [],
-		operand = operator.operand || "text",
-		widget = options.widget;
+	var results = [];
 	source(function(tiddler,title) {
-		var variable = widget.getVariableInfo(title, {}),
-			text = "";
-		if(operand === "text") {
-			text = variable.text;
-		} else if((operand === "value") && variable.srcVariable) {
-			text = variable.srcVariable.value;
-		}
-		results.push(text || "");
+		results.push(options.widget.getVariable(title) || "");
 	});
 	return results;
 };
