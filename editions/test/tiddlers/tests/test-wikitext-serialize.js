23489ce83c1a80444fd8e67f0c6792d60aafb06b
linonetwo <linonetwo012@gmail.com>
2024-09-30 18:15:30 +0800

    refactor: no need for new $tw.Wiki()

--- editions/test/tiddlers/tests/test-wikitext-serialize.js
+++ editions/test/tiddlers/tests/test-wikitext-serialize.js
@@ -8,17 +8,11 @@ Tests the wikitext inverse-rendering from Wiki AST.
 \*/
 
 describe("WikiAST serialization unit tests", function () {
-  var wiki = new $tw.Wiki();
-
   var cases = $tw.wiki.filterTiddlers("[all[shadows+tiddlers]tag[$:/tags/wikitext-serialize-test-spec]]");
   $tw.utils.each(cases, function (title) {
-    wiki.addTiddler({
-      title: title,
-      text: wiki.getTiddlerText(title),
-    });
-    it("should serialize correctly for" + title, function () {
-      var serialized = $tw.utils.serializeParseTree(wiki.parseTiddler(title).tree).trimEnd();
-      expect(serialized).toBe(wiki.getTiddlerText(title).trimEnd());
+    it("should serialize correctly for " + title, function () {
+      var serialized = $tw.utils.serializeParseTree($tw.wiki.parseTiddler(title).tree).trimEnd();
+      expect(serialized).toBe($tw.wiki.getTiddlerText(title).trimEnd());
     });
   });
 });
