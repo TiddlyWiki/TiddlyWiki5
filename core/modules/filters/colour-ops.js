a8fb07137d4aed7969feef38f75619decf5d9461
Jeremy Ruston <jeremy@jermolene.com>
2024-11-03 16:13:09 +0000

    Add expertimental colour-oklch operator

--- core/modules/filters/colour-ops.js
+++ core/modules/filters/colour-ops.js
@@ -14,12 +14,21 @@ Filter operators for colour operations
 
 var Color = require("$:/core/modules/utils/dom/color.js").Color;
 
-exports["colour-lighten"] = makeColourOperator(function (c, operator, operands) {
+exports["colour-lighten"] = makeColourOperator(function (c, operator, options) {
 	return c.lighten($tw.utils.parseNumber(operator.operand));
 });
 
-exports["colour-darken"] = makeColourOperator(function (c, operator, operands) {
+exports["colour-darken"] = makeColourOperator(function (c, operator, options) {
 	return c.darken($tw.utils.parseNumber(operator.operand));
+});
+
+exports["colour-oklch"] = makeColourOperator(function (c, operator, options) {
+	var prop = ((operator.suffixes || [])[0] || ["l"])[0];
+console.log("Prop",prop,$tw.utils.parseNumber(operator.operand))
+	if(["l","c","h"].indexOf(prop) !== -1) {
+		c.oklch[prop] = $tw.utils.parseNumber(operator.operand);
+	}
+	return c;
 });
 
 function makeColourOperator(fn) {
