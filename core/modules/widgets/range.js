3ee1b0bd26205f6b335f58b98cc0da740c5bacc0
Leilei332 <LeiYiXia29@outlook.com>
2024-12-15 15:54:55 +0800

    Tabindex support (#8819)
    * Tabindex support
    
    * Add docs
    
    * Fix refresh

--- core/modules/widgets/range.js
+++ core/modules/widgets/range.js
@@ -46,6 +46,9 @@ RangeWidget.prototype.render = function(parent,nextSibling) {
 	if(this.increment){
 		this.inputDomNode.setAttribute("step", this.increment);
 	}
+	if(this.tabIndex) {
+		this.inputDomNode.setAttribute("tabindex", this.tabIndex);
+	}
 	if(this.isDisabled === "yes") {
 		this.inputDomNode.setAttribute("disabled",true);
 	}
@@ -164,6 +167,7 @@ RangeWidget.prototype.execute = function() {
 	this.defaultValue = this.getAttribute("default","");
 	this.elementClass = this.getAttribute("class","");
 	this.isDisabled = this.getAttribute("disabled","no");
+	this.tabIndex = this.getAttribute("tabindex");
 	// Actions since 5.1.23
 	// Next 2 only fire once!
 	this.actionsMouseDown = this.getAttribute("actionsStart","");
