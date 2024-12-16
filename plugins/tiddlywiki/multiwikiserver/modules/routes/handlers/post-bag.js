1758a9ce129497b3290df295c5489ad0f664ed5e
Thomas E Tuoti <tetuoti@gmail.com>
2024-12-16 16:29:06 -0700

    MWS - added DELETE functionality to both Recipes and Bags

--- plugins/tiddlywiki/multiwikiserver/modules/routes/handlers/post-bag.js
+++ plugins/tiddlywiki/multiwikiserver/modules/routes/handlers/post-bag.js
@@ -13,42 +13,70 @@ description
 \*/
 (function() {
 
-/*jslint node: true, browser: true */
-/*global $tw: false */
-"use strict";
-
-exports.method = "POST";
-
-exports.path = /^\/bags$/;
-
-exports.bodyFormat = "www-form-urlencoded";
-
-exports.csrfDisable = true;
-
-exports.useACL = true;
-
-exports.entityName = "bag"
-
-exports.handler = function(request,response,state) {
-	if(state.data.bag_name) {
-		const result = $tw.mws.store.createBag(state.data.bag_name,state.data.description);
-		if(!result) {
-			state.sendResponse(302,{
-				"Content-Type": "text/plain",
-				"Location": "/"
-			});
+	/*jslint node: true, browser: true */
+	/*global $tw: false */
+	"use strict";
+	
+	exports.method = "POST";
+	
+	exports.path = /^\/bags$/;
+	
+	exports.bodyFormat = "www-form-urlencoded";
+	
+	exports.csrfDisable = true;
+	
+	exports.useACL = true;
+	
+	exports.entityName = "bag"
+	
+	exports.handler = function(request,response,state) {
+		var server = state.server,
+			sqlTiddlerDatabase = server.sqlTiddlerDatabase;
+		
+		// Handle DELETE request
+		if(state.data._method === "DELETE") {
+			if(state.data.bag_name) {
+				const result = $tw.mws.store.deleteBag(state.data.bag_name);
+				if(!result) {
+					state.sendResponse(302,{
+						"Content-Type": "text/plain",
+						"Location": "/"
+					});
+				} else {
+					state.sendResponse(400,{
+						"Content-Type": "text/plain"
+					},
+					result.message,
+					"utf8");
+				}
+			} else {
+				state.sendResponse(400,{
+					"Content-Type": "text/plain"
+				});
+			}
+			return;
+		}
+		
+		if(state.data.bag_name) {
+			const result = $tw.mws.store.createBag(state.data.bag_name,state.data.description);
+			if(!result) {
+				state.sendResponse(302,{
+					"Content-Type": "text/plain",
+					"Location": "/"
+				});
+			} else {
+				state.sendResponse(400,{
+					"Content-Type": "text/plain"
+				},
+				result.message,
+				"utf8");
+			}
 		} else {
 			state.sendResponse(400,{
 				"Content-Type": "text/plain"
-			},
-			result.message,
-			"utf8");
+			});
 		}
-	} else {
-		state.sendResponse(400,{
-			"Content-Type": "text/plain"
-		});
-	}
-};
-
-}());
+	};
+	
+	}());
+	
