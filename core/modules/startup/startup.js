49a33ae467b8c4297517b1539d246d13105b3e9a
Leilei332 <LeiYiXia29@outlook.com>
2024-12-20 22:25:16 +0800

    Fix switching language should not make the lang attribute empty (#8831)
    * Fix switching language should not make the lang attribute empty
    
    * Use name field

--- core/modules/startup/startup.js
+++ core/modules/startup/startup.js
@@ -88,6 +88,7 @@ exports.startup = function() {
 			if($tw.browser) {
 				var pluginTiddler = $tw.wiki.getTiddler(plugins[0]);
 				if(pluginTiddler) {
+					document.documentElement.setAttribute("lang",pluginTiddler.getFieldString("name"));
 					document.documentElement.setAttribute("dir",pluginTiddler.getFieldString("text-direction") || "auto");
 				} else {
 					document.documentElement.removeAttribute("dir");
