09689abcddc2a71e404296f0d8fd80f22bd31f6e
arlen22 <arlen22github@gmail.com>
2025-01-13 18:19:44 -0500

    fix auth bug (high priority, separate pr)

--- plugins/tiddlywiki/multiwikiserver/modules/routes/handlers/change-user-password.js
+++ plugins/tiddlywiki/multiwikiserver/modules/routes/handlers/change-user-password.js
@@ -23,11 +23,6 @@ exports.csrfDisable = true;
 
 /** @type {ServerRouteHandler<0, "www-form-urlencoded">} */
 exports.handler = async function (request, response, state) {
-  var userId = state.data.userId;
-  // Clean up any existing error/success messages
-  state.store.adminWiki.deleteTiddler("$:/temp/mws/change-password/" + userId + "/error");
-  state.store.adminWiki.deleteTiddler("$:/temp/mws/change-password/" + userId + "/success");
-  state.store.adminWiki.deleteTiddler("$:/temp/mws/login/error");
 
   if(!state.authenticatedUser) {
     state.store.adminWiki.addTiddler(new $tw.Tiddler({
@@ -38,10 +33,21 @@ exports.handler = async function (request, response, state) {
     response.end();
     return;
   }
+  var userId = state.data.get("userId");
+  var newPassword = state.data.get("newPassword");
+  var confirmPassword = state.data.get("confirmPassword");
+  if(!userId || !newPassword || !confirmPassword) {
+    response.writeHead(400);
+    response.end();
+    return;
+  }
+  // Clean up any existing error/success messages
+  state.store.adminWiki.deleteTiddler("$:/temp/mws/change-password/" + userId + "/error");
+  state.store.adminWiki.deleteTiddler("$:/temp/mws/change-password/" + userId + "/success");
+  state.store.adminWiki.deleteTiddler("$:/temp/mws/login/error");
 
-  var auth = authenticator(state.store.sqlTiddlerDatabase);
-  var newPassword = state.data.newPassword;
-  var confirmPassword = state.data.confirmPassword;
+  var auth = authenticator(state.store.sql);
+
   var currentUserId = state.authenticatedUser.user_id;
 
   var hasPermission = ($tw.utils.parseInt(userId) === currentUserId) || state.authenticatedUser.isAdmin;
@@ -66,7 +72,7 @@ exports.handler = async function (request, response, state) {
     return;
   }
 
-  var userData = await state.store.sqlTiddlerDatabase.getUser(userId);
+  var userData = await state.store.sql.getUser(userId);
 
   if(!userData) {
     state.store.adminWiki.addTiddler(new $tw.Tiddler({
@@ -79,7 +85,7 @@ exports.handler = async function (request, response, state) {
   }
 
   var newHash = auth.hashPassword(newPassword);
-  var result = await state.store.sqlTiddlerDatabase.updateUserPassword(userId, newHash);
+  var result = await state.store.sql.updateUserPassword(userId, newHash);
 
   state.store.adminWiki.addTiddler(new $tw.Tiddler({
     title: "$:/temp/mws/change-password/" + userId + "/success",
