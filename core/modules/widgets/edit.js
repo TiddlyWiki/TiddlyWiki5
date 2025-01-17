423fd2c7d5f02c0172c838d47b0b43860c2afef1
saqimtiaz <saq.imtiaz@gmail.com>
2025-01-17 17:57:14 +0100

    chore: cleanup cruft from edit widget

--- core/modules/widgets/edit.js
+++ core/modules/widgets/edit.js
@@ -56,7 +56,7 @@ EditWidget.prototype.execute = function() {
 EditWidget.prototype.getEditorType = function() {
 	// Get the content type of the thing we're editing
 	var type;
-	if(this.getAttribute("field","text") === "text") {
+	if(editField === "text") {
 		var tiddler = this.wiki.getTiddler(this.editTitle);
 		if(tiddler) {
 			type = tiddler.fields.type;
