79c326b1c1682fca5e64bf7d968ca300ce919e61
Leilei332 <LeiYiXia29@outlook.com>
2025-01-27 19:08:21 +0800

    Use plainText name

--- core/modules/startup/rootwidget.js
+++ core/modules/startup/rootwidget.js
@@ -78,7 +78,7 @@ exports.startup = function() {
 		$tw.utils.copyToClipboard(event.param,{
 			successNotification: event.paramObject && event.paramObject.successNotification,
 			failureNotification: event.paramObject && event.paramObject.failureNotification,
-			plainText: event.paramObject && event.paramObject.plain
+			plainText: event.paramObject && event.paramObject.plainText
 		},event.paramObject.type);
 	});
 	// Install the tm-focus-selector message
