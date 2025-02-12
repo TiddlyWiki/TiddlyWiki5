611adadaed9e09adfc5e00dd0e51e851135227d0
Jeremy Ruston <jeremy@jermolene.com>
2025-02-12 10:04:46 +0000

    Add apply filter run prefix
    The map filter run prefix is often used as a way to move a computed value in the input list into a variable so that it can be used as a parameter of a filter operator. The apply filter run prefix extends this idea to make the input list available as variables $1, $2 etc. Unlike the map prefix, the apply filter run is only evaluated once.

---
+++ core/modules/filterrunprefixes/apply.js
@@ -0,0 +1,29 @@
+/*\
+title: $:/core/modules/filterrunprefixes/apply.js
+type: application/javascript
+module-type: filterrunprefix
+
+
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+exports.apply = function(operationSubFunction) {
+	return function(results,source,widget) {
+		source = widget.wiki.makeTiddlerIterator([]);
+		var variables = {},
+			counter = 1;
+		results.each(function(title) {
+			variables["$" + counter] = title;
+			counter++;
+		});
+		results.clear();
+		results.pushTop(operationSubFunction(source,widget.makeFakeWidgetWithVariables(variables)));
+	};
+};
+
+})();
