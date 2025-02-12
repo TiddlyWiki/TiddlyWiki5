3d13c61c70d19fefafee1b4defe6368fe6659388
Théophile Desmedt <34781168+DesignThinkerer@users.noreply.github.com>
2025-02-12 23:15:47 +0100

    Fix: Ensure now macro format is always a string to prevent Uncaught TypeError

--- core/modules/macros/now.js
+++ core/modules/macros/now.js
@@ -26,7 +26,7 @@ exports.params = [
 Run the macro
 */
 exports.run = function(format) {
-	return $tw.utils.formatDateString(new Date(),format || "0hh:0mm, DDth MMM YYYY");
+	return String($tw.utils.formatDateString(new Date(),format || "0hh:0mm, DDth MMM YYYY"));
 };
 
 })();
