2765e995dd0d1da605159cda3eed896cb3687b4d
linonetwo <linonetwo012@gmail.com>
2025-02-19 18:58:33 +0800

    fix: link may not have attribute and children

--- core/modules/parsers/wikiparser/rules/wikilink.js
+++ core/modules/parsers/wikiparser/rules/wikilink.js
@@ -66,10 +66,10 @@ exports.parse = function() {
 };
 
 exports.serialize = function(tree,serialize) {
-	var isSuppressed = tree.children[0].text.substr(0,1) === $tw.config.textPrimitives.unWikiLink;
+	var isSuppressed = tree.children && tree.children[0].text.substr(0,1) === $tw.config.textPrimitives.unWikiLink;
 
 	var serialized = isSuppressed ? $tw.config.textPrimitives.unWikiLink : "";
-	serialized += tree.attributes.to.value;
+	serialized += tree.attributes ? tree.attributes.to.value : tree.text;
 	return serialized;
 };
 
