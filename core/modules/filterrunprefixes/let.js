8e9b30fec888a8f0c6fb35c147242328fad7ff1d
Jeremy Ruston <jeremy@jermolene.com>
2025-03-08 09:05:59 +0000

    Fix tests

--- core/modules/filterrunprefixes/let.js
+++ core/modules/filterrunprefixes/let.js
@@ -16,22 +16,29 @@ Assign a value to a variable
 Export our filter prefix function
 */
 exports.let = function(operationSubFunction,options) {
-	// Save the variable name
-	var name = options.suffixes[0][0];
-	// Return the filter run prefix function
-	return function(results,source,widget) {
-		// Set the input source to the incoming results
-		var inputSource = widget.wiki.makeTiddlerIterator(results.toArray());
-		// Assign the result of the subfunction to the variable
-		var variables = {};
-		variables[name] = operationSubFunction(inputSource,widget)[0] || "";
-		// Clear the results
-		results.clear();
-		// Return the variables
-		return {
-			variables: variables
+	if(options.suffixes[0] && options.suffixes[0][0]) {
+		// Save the variable name
+		var name = options.suffixes[0][0];
+		// Return the filter run prefix function
+		return function(results,source,widget) {
+			// Set the input source to the incoming results
+			var inputSource = widget.wiki.makeTiddlerIterator(results.toArray());
+			// Assign the result of the subfunction to the variable
+			var variables = {};
+			variables[name] = operationSubFunction(inputSource,widget)[0] || "";
+			// Clear the results
+			results.clear();
+			// Return the variables
+			return {
+				variables: variables
+			};
 		};
-	};
+	} else {
+		// Return nothing if there is no variable name
+		return function(results,source,widget) {
+		};
+
+	}
 };
 
 })();
