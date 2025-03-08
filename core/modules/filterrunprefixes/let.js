db1ed0e66b019b4fbfc04268aa899718ed1312f0
Jeremy Ruston <jeremy@jermolene.com>
2025-03-08 09:00:32 +0000

    Fix tests

--- core/modules/filterrunprefixes/let.js
+++ core/modules/filterrunprefixes/let.js
@@ -20,9 +20,11 @@ exports.let = function(operationSubFunction,options) {
 	var name = options.suffixes[0][0];
 	// Return the filter run prefix function
 	return function(results,source,widget) {
+		// Set the input source to the incoming results
+		var inputSource = widget.wiki.makeTiddlerIterator(results.toArray());
 		// Assign the result of the subfunction to the variable
 		var variables = {};
-		variables[name] = operationSubFunction(source,widget)[0] || "";
+		variables[name] = operationSubFunction(inputSource,widget)[0] || "";
 		// Clear the results
 		results.clear();
 		// Return the variables
