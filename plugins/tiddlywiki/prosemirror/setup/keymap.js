401ac3b333a711629efd32bf9a8852317875fbc3
lin onetwo <linonetwo012@gmail.com>
2025-03-25 22:53:14 +0800

    lint: indent with tab

--- plugins/tiddlywiki/prosemirror/setup/keymap.js
+++ plugins/tiddlywiki/prosemirror/setup/keymap.js
@@ -17,68 +17,68 @@ var prosemirrorModel = require("prosemirror-model");
 var mac = typeof navigator != "undefined" ? /Mac|iP(hone|[oa]d)/.test(navigator.platform) : false;
 
 function buildKeymap(schema, mapKeys) {
-  var keys = {}, type;
-  function bind(key, cmd) {
-    if (mapKeys) {
-      var mapped = mapKeys[key];
-      if (mapped === false) return;
-      if (mapped) key = mapped;
-    }
-    keys[key] = cmd;
-  }
+	var keys = {}, type;
+	function bind(key, cmd) {
+		if (mapKeys) {
+			var mapped = mapKeys[key];
+			if (mapped === false) return;
+			if (mapped) key = mapped;
+		}
+		keys[key] = cmd;
+	}
 
-  bind("Mod-z", prosemirrorHistory.undo);
-  bind("Shift-Mod-z", prosemirrorHistory.redo);
-  bind("Backspace", prosemirrorInputrules.undoInputRule);
-  if (!mac) bind("Mod-y", prosemirrorHistory.redo);
+	bind("Mod-z", prosemirrorHistory.undo);
+	bind("Shift-Mod-z", prosemirrorHistory.redo);
+	bind("Backspace", prosemirrorInputrules.undoInputRule);
+	if (!mac) bind("Mod-y", prosemirrorHistory.redo);
 
-  bind("Alt-ArrowUp", prosemirrorCommands.joinUp);
-  bind("Alt-ArrowDown", prosemirrorCommands.joinDown);
-  bind("Mod-BracketLeft", prosemirrorCommands.lift);
-  bind("Escape", prosemirrorCommands.selectParentNode);
+	bind("Alt-ArrowUp", prosemirrorCommands.joinUp);
+	bind("Alt-ArrowDown", prosemirrorCommands.joinDown);
+	bind("Mod-BracketLeft", prosemirrorCommands.lift);
+	bind("Escape", prosemirrorCommands.selectParentNode);
 
-  if (type = schema.marks.strong) {
-    bind("Mod-b", prosemirrorCommands.toggleMark(type));
-    bind("Mod-B", prosemirrorCommands.toggleMark(type));
-  }
-  if (type = schema.marks.em) {
-    bind("Mod-i", prosemirrorCommands.toggleMark(type));
-    bind("Mod-I", prosemirrorCommands.toggleMark(type));
-  }
-  if (type = schema.marks.code)
-    bind("Mod-`", prosemirrorCommands.toggleMark(type));
-  if (type = schema.nodes.blockquote)
-    bind("Ctrl->", prosemirrorCommands.wrapIn(type));
-  if (type = schema.nodes.hard_break) {
-    var br = type, cmd = prosemirrorCommands.chainCommands(prosemirrorCommands.exitCode, function(state, dispatch) {
-      if (dispatch) dispatch(state.tr.replaceSelectionWith(br.create()).scrollIntoView());
-      return true;
-    });
-    bind("Mod-Enter", cmd);
-    bind("Shift-Enter", cmd);
-    if (mac) bind("Ctrl-Enter", cmd);
-  }
-  if (type = schema.nodes.list) {
-    bind("Shift-Tab", prosemirrorFlatList.createDedentListCommand(type));
-    bind("Tab", prosemirrorFlatList.createIndentListCommand(type));
-  }
-  if (type = schema.nodes.paragraph)
-    bind("Shift-Ctrl-0", prosemirrorCommands.setBlockType(type));
-  if (type = schema.nodes.code_block)
-    bind("Shift-Ctrl-\\", prosemirrorCommands.setBlockType(type));
-  if (type = schema.nodes.heading)
-    for (var i = 1; i <= 6; i++) bind("Shift-Ctrl-" + i, prosemirrorCommands.setBlockType(type, {level: i}));
-  if (type = schema.nodes.horizontal_rule) {
-    var hr = type;
-    bind("Mod-_", function(state, dispatch) {
-      if (dispatch) dispatch(state.tr.replaceSelectionWith(hr.create()).scrollIntoView());
-      return true;
-    });
-  }
+	if (type = schema.marks.strong) {
+		bind("Mod-b", prosemirrorCommands.toggleMark(type));
+		bind("Mod-B", prosemirrorCommands.toggleMark(type));
+	}
+	if (type = schema.marks.em) {
+		bind("Mod-i", prosemirrorCommands.toggleMark(type));
+		bind("Mod-I", prosemirrorCommands.toggleMark(type));
+	}
+	if (type = schema.marks.code)
+		bind("Mod-`", prosemirrorCommands.toggleMark(type));
+	if (type = schema.nodes.blockquote)
+		bind("Ctrl->", prosemirrorCommands.wrapIn(type));
+	if (type = schema.nodes.hard_break) {
+		var br = type, cmd = prosemirrorCommands.chainCommands(prosemirrorCommands.exitCode, function(state, dispatch) {
+			if (dispatch) dispatch(state.tr.replaceSelectionWith(br.create()).scrollIntoView());
+			return true;
+		});
+		bind("Mod-Enter", cmd);
+		bind("Shift-Enter", cmd);
+		if (mac) bind("Ctrl-Enter", cmd);
+	}
+	if (type = schema.nodes.list) {
+		bind("Shift-Tab", prosemirrorFlatList.createDedentListCommand(type));
+		bind("Tab", prosemirrorFlatList.createIndentListCommand(type));
+	}
+	if (type = schema.nodes.paragraph)
+		bind("Shift-Ctrl-0", prosemirrorCommands.setBlockType(type));
+	if (type = schema.nodes.code_block)
+		bind("Shift-Ctrl-\\", prosemirrorCommands.setBlockType(type));
+	if (type = schema.nodes.heading)
+		for (var i = 1; i <= 6; i++) bind("Shift-Ctrl-" + i, prosemirrorCommands.setBlockType(type, {level: i}));
+	if (type = schema.nodes.horizontal_rule) {
+		var hr = type;
+		bind("Mod-_", function(state, dispatch) {
+			if (dispatch) dispatch(state.tr.replaceSelectionWith(hr.create()).scrollIntoView());
+			return true;
+		});
+	}
 
-  return keys;
+	return keys;
 }
 
 module.exports = {
-  buildKeymap: buildKeymap
+	buildKeymap: buildKeymap
 };
