a76baed5996861fe8d6b581c2d4cf460661a6ce5
lin onetwo <linonetwo012@gmail.com>
2025-03-26 00:52:15 +0800

    feat: better list input rule

--- plugins/tiddlywiki/prosemirror/setup/inputrules.js
+++ plugins/tiddlywiki/prosemirror/setup/inputrules.js
@@ -8,23 +8,11 @@ module-type: library
 "use strict";
 
 var { inputRules, wrappingInputRule, textblockTypeInputRule, smartQuotes, emDash, ellipsis } = require("prosemirror-inputrules");
+var { wrappingListInputRule } = require("prosemirror-flat-list");
 var { NodeType, Schema } = require("prosemirror-model");
 
 function blockQuoteRule(nodeType) {
 	return wrappingInputRule(/^\s*>\s$/, nodeType);
-}
-
-function orderedListRule(nodeType) {
-	return wrappingInputRule(
-		/^(\d+)\.\s$/,
-		nodeType,
-		function(match) { return { order: +match[1] }; },
-		function(match, node) { return node.childCount + node.attrs.order == +match[1]; }
-	);
-}
-
-function bulletListRule(nodeType) {
-	return wrappingInputRule(/^\s*([-+*])\s$/, nodeType);
 }
 
 function codeBlockRule(nodeType) {
@@ -32,17 +20,28 @@ function codeBlockRule(nodeType) {
 }
 
 function headingRule(nodeType, maxLevel) {
-	return textblockTypeInputRule(new RegExp("^(#{1," + maxLevel + "})\\s$"), nodeType, function(match) { return { level: match[1].length }; });
+	return textblockTypeInputRule(new RegExp("^(\\!{1," + maxLevel + "}|\！{1," + maxLevel + "})\\s$"), nodeType, function(match) { return { level: match[1].length }; });
 }
 
 function buildInputRules(schema) {
 	var rules = smartQuotes.concat(ellipsis, emDash), type;
 	if (type = schema.nodes.blockquote) rules.push(blockQuoteRule(type));
-	if (type = schema.nodes.ordered_list) rules.push(orderedListRule(type));
-	if (type = schema.nodes.bullet_list) rules.push(bulletListRule(type));
+	if (type = schema.nodes.list) {
+		rules.push(wrappingListInputRule(/^\s?([*-])\s$/, {
+			kind: 'bullet',
+			collapsed: false,
+		}));
+		rules.push(wrappingListInputRule(/^\s?(#)\s$|^\s?(\d+)\.\s$/, ({ match }) => {
+			const order = match[1] === "#" ? 1 : parseInteger(match[1]);
+			return {
+				kind: 'ordered',
+				collapsed: false,
+				order: order != null && order >= 2 ? order : null,
+			};
+		}));
+	}
 	if (type = schema.nodes.code_block) rules.push(codeBlockRule(type));
 	if (type = schema.nodes.heading) rules.push(headingRule(type, 6));
 	return inputRules({ rules: rules });
 }
-
 exports.buildInputRules = buildInputRules;
