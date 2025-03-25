19405b1bc7ce4903c3ee60ca34297cafff6336b6
lin onetwo <linonetwo012@gmail.com>
2025-03-25 22:53:14 +0800

    lint: indent with tab

--- plugins/tiddlywiki/prosemirror/ast/from-prosemirror.js
+++ plugins/tiddlywiki/prosemirror/ast/from-prosemirror.js
@@ -8,95 +8,95 @@ Get the Wiki AST from a Prosemirror AST
 \*/
 
 function doc(context, node) {
-  return convertNodes(context, node.content);
+	return convertNodes(context, node.content);
 }
 
 function paragraph(context, node) {
-  return {
-    type: "element",
-    tag: "p",
-    rule: "parseblock",
-    children: convertNodes(context, node.content)
-  };
+	return {
+		type: "element",
+		tag: "p",
+		rule: "parseblock",
+		children: convertNodes(context, node.content)
+	};
 }
 
 function text(context, node) {
-  return {
-    type: "text",
-    text: node.text
-  }
+	return {
+		type: "text",
+		text: node.text
+	}
 }
 
 function heading(context, node) {
-  return {
-    type: "element",
-    tag: "h" + node.attrs.level,
-    rule: "heading",
-    attributes: {
-      // TODO: restore class if any
-    },
-    children: convertNodes(context, node.content)
-  };
+	return {
+		type: "element",
+		tag: "h" + node.attrs.level,
+		rule: "heading",
+		attributes: {
+			// TODO: restore class if any
+		},
+		children: convertNodes(context, node.content)
+	};
 }
 
 function list(context, node) {
-  const listType = node.attrs && node.attrs.kind === "ordered" ? "ol" : "ul";
-  
-  const listItems = node.content.map(item => {
-    return {
-      type: "element",
-      tag: "li",
-      children: convertANode(context, item)
-    };
-  });
-  
-  return {
-    type: "element",
-    tag: listType,
-    rule: "list",
-    children: listItems
-  };
+	const listType = node.attrs && node.attrs.kind === "ordered" ? "ol" : "ul";
+	
+	const listItems = node.content.map(item => {
+		return {
+			type: "element",
+			tag: "li",
+			children: convertANode(context, item)
+		};
+	});
+	
+	return {
+		type: "element",
+		tag: listType,
+		rule: "list",
+		children: listItems
+	};
 }
 
 /**
  * Key is `node.type`, value is node converter function.
  */
 const builders = {
-  doc,
-  paragraph,
-  text,
-  heading,
-  list,
+	doc,
+	paragraph,
+	text,
+	heading,
+	list,
 };
 
 function wikiAstFromProseMirrorAst(input) {
-  return convertNodes(builders, Array.isArray(input) ? input : [input]);
+	return convertNodes(builders, Array.isArray(input) ? input : [input]);
 }
 
 exports.from = wikiAstFromProseMirrorAst;
 
 function convertNodes(builders, nodes) {
-  if (nodes === undefined || nodes.length === 0) {
-    return [];
-  }
+	if (nodes === undefined || nodes.length === 0) {
+		return [];
+	}
 
-  return nodes.reduce((accumulator, node) => {
-    return [...accumulator, ...convertANode(builders, node)];
-  }, []);
+	return nodes.reduce((accumulator, node) => {
+		return [...accumulator, ...convertANode(builders, node)];
+	}, []);
 }
 
 function restoreMetadata(node) {
-  // TODO: restore attributes, orderedAttributes, isBlock
-  return {};
+	// TODO: restore attributes, orderedAttributes, isBlock
+	return {};
 }
 function convertANode(builders, node) {
-  var builder = builders[node.type];
-  if (typeof builder === 'function') {
-    var convertedNode = builder(builders, node);
-    var arrayOfNodes = (Array.isArray(convertedNode)
-    ? convertedNode : [convertedNode]);
-    return arrayOfNodes.map((child) => ({ ...restoreMetadata(node), ...child }));
-  }
-  console.warn(`WikiAst get Unknown node type: ${JSON.stringify(node)}`);
-  return [];
+	var builder = builders[node.type];
+	if (typeof builder === 'function') {
+		var convertedNode = builder(builders, node);
+		var arrayOfNodes = (Array.isArray(convertedNode)
+		? convertedNode : [convertedNode]);
+		return arrayOfNodes.map((child) => ({ ...restoreMetadata(node), ...child }));
+	}
+	console.warn(`WikiAst get Unknown node type: ${JSON.stringify(node)}`);
+	return [];
 }
--- plugins/tiddlywiki/prosemirror/setup/inputrules.js
+++ plugins/tiddlywiki/prosemirror/setup/inputrules.js
@@ -11,34 +11,38 @@ var { inputRules, wrappingInputRule, textblockTypeInputRule, smartQuotes, emDash
 var { NodeType, Schema } = require("prosemirror-model");
 
 function blockQuoteRule(nodeType) {
-  return wrappingInputRule(/^\s*>\s$/, nodeType);
+	return wrappingInputRule(/^\s*>\s$/, nodeType);
 }
 
 function orderedListRule(nodeType) {
-  return wrappingInputRule(/^(\d+)\.\s$/, nodeType, function(match) { return { order: +match[1] }; },
-                           function(match, node) { return node.childCount + node.attrs.order == +match[1]; });
+	return wrappingInputRule(
+		/^(\d+)\.\s$/,
+		nodeType,
+		function(match) { return { order: +match[1] }; },
+		function(match, node) { return node.childCount + node.attrs.order == +match[1]; }
+	);
 }
 
 function bulletListRule(nodeType) {
-  return wrappingInputRule(/^\s*([-+*])\s$/, nodeType);
+	return wrappingInputRule(/^\s*([-+*])\s$/, nodeType);
 }
 
 function codeBlockRule(nodeType) {
-  return textblockTypeInputRule(/^```$/, nodeType);
+	return textblockTypeInputRule(/^```$/, nodeType);
 }
 
 function headingRule(nodeType, maxLevel) {
-  return textblockTypeInputRule(new RegExp("^(#{1," + maxLevel + "})\\s$"), nodeType, function(match) { return { level: match[1].length }; });
+	return textblockTypeInputRule(new RegExp("^(#{1," + maxLevel + "})\\s$"), nodeType, function(match) { return { level: match[1].length }; });
 }
 
 function buildInputRules(schema) {
-  var rules = smartQuotes.concat(ellipsis, emDash), type;
-  if (type = schema.nodes.blockquote) rules.push(blockQuoteRule(type));
-  if (type = schema.nodes.ordered_list) rules.push(orderedListRule(type));
-  if (type = schema.nodes.bullet_list) rules.push(bulletListRule(type));
-  if (type = schema.nodes.code_block) rules.push(codeBlockRule(type));
-  if (type = schema.nodes.heading) rules.push(headingRule(type, 6));
-  return inputRules({ rules: rules });
+	var rules = smartQuotes.concat(ellipsis, emDash), type;
+	if (type = schema.nodes.blockquote) rules.push(blockQuoteRule(type));
+	if (type = schema.nodes.ordered_list) rules.push(orderedListRule(type));
+	if (type = schema.nodes.bullet_list) rules.push(bulletListRule(type));
+	if (type = schema.nodes.code_block) rules.push(codeBlockRule(type));
+	if (type = schema.nodes.heading) rules.push(headingRule(type, 6));
+	return inputRules({ rules: rules });
 }
 
 exports.buildInputRules = buildInputRules;
