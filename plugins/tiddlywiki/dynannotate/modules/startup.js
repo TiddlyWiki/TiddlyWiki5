TiddlyWiki/TiddlyWiki5

commit a9f9ffd40948589b2f2942810a06fa7d5ed107d5

Introduce Tour Plugin and Confetti Plugin, improve Dynannotate Plugin (#7734)
* First commit

* Typo

* Add support for delay parameter

* Add confetti widget

* Add tour plugin

* Add element spotlight to dynannotate plugin

Useful for highlighting on screen elements for the user

* More and bigger confetti by default

* Use new element spotlight to provide hints

* Adjust hint selectors for create tiddler tour step

* Include confetti plugin in prerelease

* Clarify wording of confetti demo

* Don't link TiddlyWiki in the tour panel

* Tweaks to tour buttons

* Mark dependents of the tour plugin

* Add full screen section of tour and tour edition

* Remove Anna Freud references from welcome tiddler

* Build the tour edition in the preview

* Fix typo in build script

* Populate tour edition with solar system data

From Simple English Wikipedia

* Missing tag

* Add page control button to start tour

Also make the tour controls visible in full screen mode

* Refactor to use global procedures to control the tour

* Change "startup-actions" field to "enter-actions" to avoid confusion

* Add a tour logo

* Refactor to allow multiple tours to be loaded at once

* Remove wikification from welcome tour step

* Update docs

* Simplify styles for top bar

* Tours should have a $:/tags/Tour tag

* Tour should autostart in the tour edition, but not in the main wiki

* Better labelling for the main preview

* Fix build process

We build a separate tour.html wiki, but can include the tour in other wikis too

* Remove obsolete text

* Add "using tags" as a separate tour

* Remove old debugging code

* Add tour chooser

* Ensure that the current tour isn't listed as an option in the final step

* Use whitespace trim

Note that the setting is inherited by procedure and widget definitions

* Simplify tour step format

* Remove obsolete state tiddler

plugins/tiddlywiki/dynannotate/modules/startup.js
@@ -1,3 +1,5 @@
+const { ElementSpotlight } = require("./element-spotlight");
+
 /*\
 title: $:/plugins/tiddlywiki/dynannotate/startup.js
 type: application/javascript
@@ -22,18 +24,39 @@ var CONFIG_SELECTION_TRACKER_TITLE = "$:/config/Dynannotate/SelectionTracker/Ena
 	CONFIG_LEGACY_SELECTION_TRACKER_TITLE = "$:/config/Dynannotate/LegacySelectionTracker/Enable";
 
 var SelectionTracker = require("$:/plugins/tiddlywiki/dynannotate/selection-tracker.js").SelectionTracker,
-	LegacySelectionTracker = require("$:/plugins/tiddlywiki/dynannotate/legacy-selection-tracker.js").LegacySelectionTracker;
+	LegacySelectionTracker = require("$:/plugins/tiddlywiki/dynannotate/legacy-selection-tracker.js").LegacySelectionTracker,
+	ElementSpotlight = require("$:/plugins/tiddlywiki/dynannotate/element-spotlight.js").ElementSpotlight;
 
 exports.startup = function() {
 	$tw.dynannotate = {};
+	// Setup selection tracker
 	if($tw.wiki.getTiddlerText(CONFIG_SELECTION_TRACKER_TITLE,"yes") === "yes") {
 		$tw.dynannotate.selectionTracker = new SelectionTracker($tw.wiki);
 	}
+	// Setup legacy selection tracker
 	if($tw.wiki.getTiddlerText(CONFIG_LEGACY_SELECTION_TRACKER_TITLE,"yes") === "yes") {
 		$tw.dynannotate.legacySelectionTracker = new LegacySelectionTracker($tw.wiki,{
 			allowBlankSelectionPopup: true
 		});
 	}
+	// Set up the element spotlight
+	$tw.dynannotate.elementSpotlight = new ElementSpotlight();
+	$tw.rootWidget.addEventListener("tm-spotlight-element",function(event) {
+		var selectors = [];
+		if(event.paramObject.selector) {
+			selectors.push(event.paramObject.selector);
+		}
+		$tw.utils.each(Object.keys(event.paramObject).sort(),function(name) {
+			var SELECTOR_PROPERTY_PREFIX = "selector-";
+			if($tw.utils.startsWith(name,SELECTOR_PROPERTY_PREFIX)) {
+				selectors.push(event.paramObject[name]);
+			}
+		});
+		if(event.paramObject["selector-fallback"]) {
+			selectors.push(event.paramObject["selector-fallback"]);
+		}
+		$tw.dynannotate.elementSpotlight.shineSpotlight(selectors);
+	});
 };
 
 })();

plugins/tiddlywiki/confetti/startup.js
@@ -1,0 +1,55 @@
+/*\
+title: $:/plugins/tiddlywiki/confetti/startup.js
+type: application/javascript
+module-type: startup
+
+Setup the root widget event handlers
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+// Export name and synchronous status
+exports.name = "confetti";
+exports.platforms = ["browser"];
+exports.after = ["startup"];
+exports.synchronous = true;
+
+// Install the root widget event handlers
+exports.startup = function() {
+	$tw.confettiManager = new $tw.ConfettiManager();
+	$tw.rootWidget.addEventListener("tm-confetti-launch",function(event) {
+		var paramObject = event.paramObject || {},
+			options = {},
+			extractNumericParameter = function(name) {
+				options[name] = paramObject[name] && $tw.utils.parseNumber(paramObject[name]);
+			},
+			extractListParameter = function(name) {
+				options[name] = paramObject[name] && $tw.utils.parseStringArray(paramObject[name]);
+			},
+			extractBooleanParameter = function(name) {
+				options[name] = paramObject[name] && paramObject[name] === "yes";
+			};
+		$tw.utils.each("particleCount angle spread startVelocity decay gravity drift ticks scalar zIndex".split(" "),function(name) {
+			extractNumericParameter(name);
+		});
+		$tw.utils.each("colors shapes".split(" "),function(name) {
+			extractListParameter(name);
+		});
+		options.origin = {
+			x: paramObject.originX && $tw.utils.parseNumber(paramObject.originX),
+			y: paramObject.originY && $tw.utils.parseNumber(paramObject.originY)
+		};
+		extractBooleanParameter("disableForReducedMotion");
+		var delay = paramObject.delay ? $tw.utils.parseNumber(paramObject.delay) : 0;
+		$tw.confettiManager.launch(delay,options);
+	});
+	$tw.rootWidget.addEventListener("tm-confetti-reset",function(event) {
+		$tw.confettiManager.reset();
+	});
+};
+
+})();

commit c409d8414e99a8e78d387ba53c5a36898fb6939f

Fix bug exposed by #9030
I think the bug was originally introduced in a9f9ffd40948589b2f2942810a06fa7d5ed107d5

plugins/tiddlywiki/dynannotate/modules/startup.js
@@ -1,5 +1,3 @@
-const { ElementSpotlight } = require("./element-spotlight");
-
 /*\
 title: $:/plugins/tiddlywiki/dynannotate/startup.js
 type: application/javascript