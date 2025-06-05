34105a9b1b1af170bcea23456ceceb015364a117
lin onetwo <linonetwo012@gmail.com>
2025-06-05 20:48:10 +0800

    refactor: put stream to support HUGE static file

--- core/modules/server/routes/put-file.js
+++ core/modules/server/routes/put-file.js
@@ -12,7 +12,7 @@ exports.method = "PUT";
 
 exports.path = /^\/files\/(.+)$/;
 
-exports.bodyFormat = "buffer";
+exports.bodyFormat = "stream";
 
 exports.handler = function(request,response,state) {
 	var path = require("path"),fs = require("fs"),
@@ -21,12 +21,26 @@ exports.handler = function(request,response,state) {
 		fullPath = path.resolve(basePath,filename);
 	if(path.relative(basePath,fullPath).indexOf("..") === 0) {
 		state.sendResponse(403,{"Content-Type": "text/plain"},"Access denied");
-	} else {
-		fs.mkdir(path.dirname(fullPath), {recursive:true}, function(err) {
-			if(err && err.code !== 'EEXIST') return state.sendResponse(500,{"Content-Type": "text/plain"},"Directory error");
-			fs.writeFile(fullPath, state.data, function(err) {
-				state.sendResponse(err ? 500 : 204,{"Content-Type": "text/plain"}, err ? "Write error" : "");
-			});
-		});
+		return;
 	}
+	fs.mkdir(path.dirname(fullPath), {recursive:true}, function(err) {
+		if(err && err.code !== 'EEXIST') return state.sendResponse(500,{"Content-Type": "text/plain"},"Directory error");
+		var stream = fs.createWriteStream(fullPath);
+		stream.on("error", function(err) {
+			console.log("Error writing file " + fullPath + ": " + err.toString());
+			if(!response.headersSent) {
+				state.sendResponse(500,{"Content-Type": "text/plain"},"Write error");
+			}
+		});
+		stream.on("finish", function() {
+			if(!response.headersSent) {
+				state.sendResponse(204,{"Content-Type": "text/plain"},"");
+			}
+		});
+		request.on("error", function(err) {
+			console.log("Error reading request: " + err.toString());
+			stream.destroy();
+		});
+		request.pipe(stream);
+	});
 };
