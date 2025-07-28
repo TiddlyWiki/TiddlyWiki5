2c50d1b71aedabab97743b09831ce84cd00d17d4
Simon Huber <huber.simon@protonmail.com>
2025-07-28 20:10:07 +0200

    Create secrets.js
    secrets-state macro: returns the state of the secrets store

---
+++ core/modules/macros/secrets.js
@@ -0,0 +1,23 @@
+/*\
+title: $:/core/modules/macros/secrets.js
+type: application/javascript
+module-type: macro
+
+Macro to get secrets store state
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+exports.name = "secrets-state";
+
+exports.params = [];
+
+exports.run = function() {
+	return $tw.utils.getSecretsStoreState();
+};
+
+})();
