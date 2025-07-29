596835db268ee46179179160e2ef30ef80d3f1b5
Saq Imtiaz <saq.imtiaz@gmail.com>
2025-07-29 19:46:03 +0200

    Extend server routes to support multiple methods

--- core/modules/server/server.js
+++ core/modules/server/server.js
@@ -217,7 +217,7 @@ Server.prototype.findMatchingRoute = function(request,state) {
 		} else {
 			match = potentialRoute.path.exec(pathname);
 		}
-		if(match && request.method === potentialRoute.method) {
+		if(match && (potentialRoute.method === request.method || (Array.isArray(potentialRoute.method) && potentialRoute.method.includes(request.method)))) {
 			state.params = [];
 			for(var p=1; p<match.length; p++) {
 				state.params.push(match[p]);
