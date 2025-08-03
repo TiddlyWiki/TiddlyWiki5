cc6a01082b6aa4a169493ab6e88e2b0be925e82d
Simon Huber <huber.simon@protonmail.com>
2025-08-03 21:28:38 +0200

    Update mediaquery.js - no try - catch
    As @pmario mentioned and after some tests, this should not throw errors

--- core/modules/filters/mediaquery.js
+++ core/modules/filters/mediaquery.js
@@ -25,71 +25,66 @@ exports.mediaquery = function(source,operator,options) {
 	
 	// Only evaluate in browser environment
 	if($tw.browser && mediaQuery && widget) {
-		try {
-			// Use window.matchMedia to evaluate the media query
-			var mql = window.matchMedia(mediaQuery);
+		// Use window.matchMedia to evaluate the media query
+		var mql = window.matchMedia(mediaQuery);
+		
+		// Process each input title
+		source(function(tiddler,title) {
+			if(mql.matches) {
+				results.push(title);
+			}
+		});
+		
+		// Set up a listener for changes if we have a widget context
+		if(widget && widget.wiki) {
+			// Create a unique key for this listener
+			var listenerKey = "mql_" + (++listenerCounter);
 			
-			// Process each input title
-			source(function(tiddler,title) {
-				if(mql.matches) {
-					results.push(title);
+			// Define the change handler
+			var changeHandler = function(e) {
+				// Force a refresh of the widget when media query changes
+				if(widget.refreshSelf) {
+					widget.refreshSelf();
+				} else if(widget.parentWidget && widget.parentWidget.refreshChildren) {
+					widget.parentWidget.refreshChildren();
 				}
-			});
+			};
 			
-			// Set up a listener for changes if we have a widget context
-			if(widget && widget.wiki) {
-				// Create a unique key for this listener
-				var listenerKey = "mql_" + (++listenerCounter);
-				
-				// Define the change handler
-				var changeHandler = function(e) {
-					// Force a refresh of the widget when media query changes
-					if(widget.refreshSelf) {
-						widget.refreshSelf();
-					} else if(widget.parentWidget && widget.parentWidget.refreshChildren) {
-						widget.parentWidget.refreshChildren();
+			// Add the listener (use modern addEventListener if available)
+			if(mql.addEventListener) {
+				mql.addEventListener("change", changeHandler);
+			} else if(mql.addListener) {
+				// Fallback for older browsers
+				mql.addListener(changeHandler);
+			}
+			
+			// Store reference for cleanup
+			mediaQueryListeners[listenerKey] = {
+				mql: mql,
+				handler: changeHandler,
+				widget: widget
+			};
+			
+			// Clean up listener when widget is destroyed
+			if(widget) {
+				// Hook into widget destruction
+				var originalRemoveChildDomNodes = widget.removeChildDomNodes;
+				widget.removeChildDomNodes = function() {
+					if(mediaQueryListeners[listenerKey]) {
+						var listener = mediaQueryListeners[listenerKey];
+						if(listener.mql.removeEventListener) {
+							listener.mql.removeEventListener("change", listener.handler);
+						} else if(listener.mql.removeListener) {
+							// Fallback for older browsers
+							listener.mql.removeListener(listener.handler);
+						}
+						delete mediaQueryListeners[listenerKey];
+					}
+					if(originalRemoveChildDomNodes) {
+						originalRemoveChildDomNodes.call(this);
 					}
 				};
-				
-				// Add the listener (use modern addEventListener if available)
-				if(mql.addEventListener) {
-					mql.addEventListener("change", changeHandler);
-				} else if(mql.addListener) {
-					// Fallback for older browsers
-					mql.addListener(changeHandler);
-				}
-				
-				// Store reference for cleanup
-				mediaQueryListeners[listenerKey] = {
-					mql: mql,
-					handler: changeHandler,
-					widget: widget
-				};
-				
-				// Clean up listener when widget is destroyed
-				if(widget) {
-					// Hook into widget destruction
-					var originalRemoveChildDomNodes = widget.removeChildDomNodes;
-					widget.removeChildDomNodes = function() {
-						if(mediaQueryListeners[listenerKey]) {
-							var listener = mediaQueryListeners[listenerKey];
-							if(listener.mql.removeEventListener) {
-								listener.mql.removeEventListener("change", listener.handler);
-							} else if(listener.mql.removeListener) {
-								// Fallback for older browsers
-								listener.mql.removeListener(listener.handler);
-							}
-							delete mediaQueryListeners[listenerKey];
-						}
-						if(originalRemoveChildDomNodes) {
-							originalRemoveChildDomNodes.call(this);
-						}
-					};
-				}
 			}
-		} catch(e) {
-			// Invalid media query, return empty results
-			return results;
 		}
 	}
 	
