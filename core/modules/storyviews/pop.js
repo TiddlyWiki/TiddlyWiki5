e48bb10055fb2f53cab92c26c1397a4d30af02f0
Simon Huber <huber.simon@protonmail.com>
2025-08-05 14:18:26 +0200

    Update pop.js

--- core/modules/storyviews/pop.js
+++ core/modules/storyviews/pop.js
@@ -14,6 +14,12 @@ var PopStoryView = function(listWidget) {
 };
 
 PopStoryView.prototype.navigateTo = function(historyInfo) {
+	// Check if storyview scrolling is enabled
+	var enableScroll = this.listWidget.getVariable("tv-enable-storyview-scroll");
+	if(enableScroll !== "yes") {
+		return;
+	}
+	
 	var listElementIndex = this.listWidget.findListItem(0,historyInfo.title);
 	if(listElementIndex === undefined) {
 		return;
@@ -24,8 +30,22 @@ PopStoryView.prototype.navigateTo = function(historyInfo) {
 	if(!targetElement || targetElement.nodeType === Node.TEXT_NODE) {
 		return;
 	}
-	// Scroll the node into view
-	this.listWidget.dispatchEvent({type: "tm-scroll", target: targetElement});
+	// Highlight the target element briefly
+	var duration = $tw.utils.getAnimationDuration(),
+		easing = $tw.wiki.getTiddlerText("$:/config/StoryView/Pop/Easing","cubic-bezier(0.4, 0.0, 0.2, 1)");
+	$tw.utils.setStyle(targetElement,[
+		{transition: "none"},
+		{filter: "brightness(1.2)"}
+	]);
+	$tw.utils.forceLayout(targetElement);
+	$tw.utils.setStyle(targetElement,[
+		{transition: "filter " + duration + "ms " + easing},
+		{filter: "brightness(1)"}
+	]);
+	// Scroll the node into view with a small delay to let the highlight start
+	setTimeout(function() {
+		this.listWidget.dispatchEvent({type: "tm-scroll", target: targetElement});
+	}.bind(this), 50);
 };
 
 PopStoryView.prototype.insert = function(widget) {
@@ -35,34 +55,80 @@ PopStoryView.prototype.insert = function(widget) {
 	if(!targetElement || targetElement.nodeType === Node.TEXT_NODE) {
 		return;
 	}
+	
+	// Check if element is currently animating
+	if(targetElement.dataset.popAnimating === "true") {
+		return;
+	}
+	targetElement.dataset.popAnimating = "true";
+	
+	// Re-read configuration for live updates
+	var insertScale = parseFloat($tw.wiki.getTiddlerText("$:/config/StoryView/Pop/InsertScale","2"));
+	var insertRotation = parseFloat($tw.wiki.getTiddlerText("$:/config/StoryView/Pop/InsertRotation","0"));
+	var enableBlur = $tw.wiki.getTiddlerText("$:/config/StoryView/Pop/EnableBlur","no") === "yes";
+	var easing = $tw.wiki.getTiddlerText("$:/config/StoryView/Pop/Easing","cubic-bezier(0.4, 0.0, 0.2, 1)");
+	
+	// Build transform string
+	var transformStart = "scale(" + insertScale + ")";
+	if(insertRotation !== 0) {
+		transformStart += " rotate(" + insertRotation + "deg)";
+	}
+	
+	// Build filter string
+	var filterStart = enableBlur ? "blur(5px)" : "none";
+	
 	// Reset once the transition is over
-	setTimeout(function() {
+	var transitionEndHandler = function() {
+		targetElement.removeEventListener("transitionend", transitionEndHandler);
+		delete targetElement.dataset.popAnimating;
 		$tw.utils.setStyle(targetElement,[
-			{transition: "none"},
-			{transform: "none"}
+			{transition: ""},
+			{transform: ""},
+			{filter: ""},
+			{opacity: ""},
+			{willChange: ""}
 		]);
 		$tw.utils.setStyle(widget.document.body,[
 			{"overflow-x": ""}
 		]);
-	},duration);
+	};
+	
 	// Prevent the page from overscrolling due to the zoom factor
 	$tw.utils.setStyle(widget.document.body,[
 		{"overflow-x": "hidden"}
 	]);
+	
 	// Set up the initial position of the element
 	$tw.utils.setStyle(targetElement,[
 		{transition: "none"},
-		{transform: "scale(2)"},
-		{opacity: "0.0"}
+		{transform: transformStart},
+		{filter: filterStart},
+		{opacity: "0"},
+		{transformOrigin: "center center"},
+		{willChange: "transform, opacity, filter"}
 	]);
 	$tw.utils.forceLayout(targetElement);
-	// Transition to the final position
-	$tw.utils.setStyle(targetElement,[
-		{transition: $tw.utils.roundTripPropertyName("transform") + " " + duration + "ms ease-in-out, " +
-					"opacity " + duration + "ms ease-in-out"},
-		{transform: "scale(1)"},
-		{opacity: "1.0"}
-	]);
+	
+	// Add transition end listener
+	targetElement.addEventListener("transitionend", transitionEndHandler);
+	
+	// Transition to the final position - use setTimeout to ensure initial styles are applied
+	setTimeout(function() {
+		var transitions = [
+			$tw.utils.roundTripPropertyName("transform") + " " + duration + "ms " + easing,
+			"opacity " + duration + "ms " + easing
+		];
+		if(enableBlur) {
+			transitions.push("filter " + duration + "ms " + easing);
+		}
+		
+		$tw.utils.setStyle(targetElement,[
+			{transition: transitions.join(", ")},
+			{transform: "scale(1) rotate(0deg)"},
+			{filter: "none"},
+			{opacity: "1"}
+		]);
+	}, 10);
 };
 
 PopStoryView.prototype.remove = function(widget) {
@@ -78,21 +144,65 @@ PopStoryView.prototype.remove = function(widget) {
 		removeElement();
 		return;
 	}
-	// Remove the element at the end of the transition
-	setTimeout(removeElement,duration);
-	// Animate the closure
+	
+	// Check if element is currently animating
+	if(targetElement.dataset.popAnimating === "true") {
+		// If already animating, just remove it
+		removeElement();
+		return;
+	}
+	targetElement.dataset.popAnimating = "true";
+	
+	// Re-read configuration for live updates
+	var removeScale = parseFloat($tw.wiki.getTiddlerText("$:/config/StoryView/Pop/RemoveScale","0.1"));
+	var removeRotation = parseFloat($tw.wiki.getTiddlerText("$:/config/StoryView/Pop/RemoveRotation","0"));
+	var enableBlur = $tw.wiki.getTiddlerText("$:/config/StoryView/Pop/EnableBlur","no") === "yes";
+	var easing = $tw.wiki.getTiddlerText("$:/config/StoryView/Pop/Easing","cubic-bezier(0.4, 0.0, 0.2, 1)");
+	
+	// Build transform string
+	var transformEnd = "scale(" + removeScale + ")";
+	if(removeRotation !== 0) {
+		transformEnd += " rotate(" + removeRotation + "deg)";
+	}
+	
+	// Build filter string
+	var filterEnd = enableBlur ? "blur(5px)" : "none";
+	
+	// Set up initial state
 	$tw.utils.setStyle(targetElement,[
 		{transition: "none"},
-		{transform: "scale(1)"},
-		{opacity: "1.0"}
+		{transform: "scale(1) rotate(0deg)"},
+		{filter: "none"},
+		{opacity: "1"},
+		{transformOrigin: "center center"},
+		{willChange: "transform, opacity, filter"}
 	]);
 	$tw.utils.forceLayout(targetElement);
-	$tw.utils.setStyle(targetElement,[
-		{transition: $tw.utils.roundTripPropertyName("transform") + " " + duration + "ms ease-in-out, " +
-					"opacity " + duration + "ms ease-in-out"},
-		{transform: "scale(0.1)"},
-		{opacity: "0.0"}
-	]);
+	
+	// Remove the element at the end of the transition
+	var transitionEndHandler = function() {
+		targetElement.removeEventListener("transitionend", transitionEndHandler);
+		removeElement();
+	};
+	targetElement.addEventListener("transitionend", transitionEndHandler);
+	
+	// Animate the removal - use setTimeout to ensure initial styles are applied
+	setTimeout(function() {
+		var transitions = [
+			$tw.utils.roundTripPropertyName("transform") + " " + duration + "ms " + easing,
+			"opacity " + duration + "ms " + easing
+		];
+		if(enableBlur) {
+			transitions.push("filter " + duration + "ms " + easing);
+		}
+		
+		$tw.utils.setStyle(targetElement,[
+			{transition: transitions.join(", ")},
+			{transform: transformEnd},
+			{filter: filterEnd},
+			{opacity: "0"}
+		]);
+	}, 10);
 };
 
 exports.pop = PopStoryView;
