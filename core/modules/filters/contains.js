6994d879c683c890fbde5dd3a6b8ea33f87aa9ea
yaisog <m@rcuswinter.de>
2025-10-19 19:12:40 +0200

    Initial commit

--- core/modules/filters/contains.js
+++ core/modules/filters/contains.js
@@ -13,28 +13,15 @@ Filter operator for finding values in array fields
 Export our filter function
 */
 exports.contains = function(source,operator,options) {
-	var results = [],
-		fieldname = operator.suffix || "list";
-	if(operator.prefix === "!") {
-		source(function(tiddler,title) {
-			if(tiddler) {
-				var list = tiddler.getFieldList(fieldname);
-				if(list.indexOf(operator.operand) === -1) {
-					results.push(title);
-				}
-			} else {
-				results.push(title);
-			}
-		});
-	} else {
-		source(function(tiddler,title) {
-			if(tiddler) {
-				var list = tiddler.getFieldList(fieldname);
-				if(list.indexOf(operator.operand) !== -1) {
-					results.push(title);
-				}
-			}
-		});
-	}
+	const results = [];
+	const fieldname = operator.suffix || "list";
+	const tiddlersWithOperand = options.wiki.findListingsOfTiddler(operator.operand, fieldname);
+	const intermediateSet = new Set(tiddlersWithOperand);
+	const invert = operator.prefix === "!";
+	source((tiddler,title) => {
+		if(intermediateSet.has(title) !== invert) {
+			results.push(title);
+		}
+	});
 	return results;
 };
