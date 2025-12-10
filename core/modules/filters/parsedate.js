b7fd25946d5033b2676acb32f05ee0f32b13913f
linonetwo <linonetwo012@gmail.com>
2025-12-10 16:47:45 +0800

    Update parsedate.js

--- core/modules/filters/parsedate.js
+++ core/modules/filters/parsedate.js
@@ -17,9 +17,9 @@ exports.parsedate = function(source,operator) {
 	if(format === "JS") {
 		parser = $tw.utils.parseECMAScriptDate;
 	}
-    if(!(parser instanceof Function)) {
+	if(!(parser instanceof Function)) {
 		return [$tw.language.getString("Error/ParseDateFilterOperator")];
-    }
+	}
 
 	var results = [];
 	source(function(tiddler,title) {
