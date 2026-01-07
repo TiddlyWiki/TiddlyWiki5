6cb97ad464841f9454aceb934f88d63960710113
BurningTreeC <huber.simon@protonmail.com>
2026-01-07 06:35:06 +0100

    move all .js files into files/ subdirectory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -294,7 +294,7 @@ js.configs.recommended,
         yoda: "off",
         // temporary rules
         "no-useless-escape": "off",
-        "no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_", "caughtErrorsIgnorePattern": "^_" }],
+        "no-unused-vars": "warn",
         "no-empty": "off",
         "@stylistic/no-extra-semi": "off",
         "no-redeclare": "off",
