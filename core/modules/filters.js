15736bceb965028185625503370e25c0ced05d39
Jeremy Ruston <jeremy@jermolene.com>
2026-01-21 21:49:16 +0000

    Add test for invalid pragma

--- core/modules/filters.js
+++ core/modules/filters.js
@@ -369,7 +369,7 @@ exports.compileFilter = function(filterString,options) {
 					default:
 						return function(results,source,widget) {
 							results.clear();
-							results.push($tw.language.getString("Error/FilterRunPrefix"));
+							results.push($tw.language.getString("Error/FilterPragma"));
 						};
 				}
 				return function(results,source,widget) {
