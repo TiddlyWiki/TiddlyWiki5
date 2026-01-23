af87d7bffd7bba1093d040f1be5ef72e4c3459bb
saqimtiaz <saq.imtiaz@gmail.com>
2026-01-23 09:53:09 +0100

    feat: eventcatcher with pointer capture support

--- core/modules/widgets/eventcatcher.js
+++ core/modules/widgets/eventcatcher.js
@@ -39,75 +39,13 @@ EventWidget.prototype.render = function(parent,nextSibling) {
 	this.domNode = domNode;
 	// Assign classes
 	this.assignDomNodeClasses();
-	// Add our event handler
-	$tw.utils.each(this.types,function(type) {
-		domNode.addEventListener(type,function(event) {
-			var selector = self.getAttribute("selector"),
-				matchSelector = self.getAttribute("matchSelector"),
-				actions = self.getAttribute("$"+type),
-				stopPropagation = self.getAttribute("stopPropagation","onaction"),
-				selectedNode = event.target,
-				selectedNodeRect,
-				catcherNodeRect,
-				variables = {};
-			// Firefox can fire dragover and dragenter events on text nodes instead of their parents
-			if(selectedNode.nodeType === 3) {
-				selectedNode = selectedNode.parentNode;
-			}
-			// Check that the selected node matches any matchSelector
-			if(matchSelector && !$tw.utils.domMatchesSelector(selectedNode,matchSelector)) {
-				return false;
-			}
-			if(selector) {
-				// Search ancestors for a node that matches the selector
-				while(!$tw.utils.domMatchesSelector(selectedNode,selector) && selectedNode !== domNode) {
-					selectedNode = selectedNode.parentNode;
-				}
-				// Exit if we didn't find one
-				if(selectedNode === domNode) {
-					return false;
-				}
-				// Only set up variables if we have actions to invoke
-				if(actions) {
-					variables = $tw.utils.collectDOMVariables(selectedNode,self.domNode,event);
-				}
-			}
-			// Execute our actions with the variables
-			if(actions) {
-				// Add a variable for the modifier key
-				variables.modifier = $tw.keyboardManager.getEventModifierKeyDescriptor(event);
-				// Add a variable for the mouse button
-				if("button" in event) {
-					if(event.button === 0) {
-						variables["event-mousebutton"] = "left";
-					} else if(event.button === 1) {
-						variables["event-mousebutton"] = "middle";
-					} else if(event.button === 2) {
-						variables["event-mousebutton"] = "right";
-					}
-				}
-				variables["event-type"] = event.type.toString();
-				if(typeof event.detail === "object" && !!event.detail) {
-					$tw.utils.each(event.detail,function(detailValue,detail) {
-						variables["event-detail-" + detail] = detailValue.toString();
-					});
-				} else if(!!event.detail) {
-					variables["event-detail"] = event.detail.toString();
-				}
-				self.invokeActionString(actions,self,event,variables);
-			}
-			if((actions && stopPropagation === "onaction") || stopPropagation === "always") {
-				event.preventDefault();
-				event.stopPropagation();
-				return true;
-			}
-			return false;
-		},false);
-	});
+	this._lastPointerId = null;
+	// Add our event handlers
+	this.toggleListeners();
 	// Insert element
 	parent.insertBefore(domNode,nextSibling);
-	this.renderChildren(domNode,null);
 	this.domNodes.push(domNode);
+	this.renderChildren(domNode,null);
 };
 
 /*
@@ -122,11 +60,225 @@ EventWidget.prototype.execute = function() {
 			self.types.push(key.slice(1));
 		}
 	});
+	this.pointerCaptureMode = this.getAttribute("pointerCapture","no");
 	this.elementTag = this.getAttribute("tag");
 	// Make child widgets
 	this.makeChildWidgets();
 };
 
+/*
+Cache and pre-create all event listeners, called when first needed
+*/
+EventWidget.prototype.cacheEventListeners = function() {
+	if(this._eventListeners) {
+		return;
+	}
+	this._eventListeners = Object.create(null);
+	this._captureActiveListeners = Object.create(null);
+	this._dynamicOnlyEvents = ["pointerup","pointercancel","pointermove"];
+
+	// Dynamic pointer capture listeners
+	if(this.pointerCaptureMode === "dynamic") {
+		["pointerup","pointercancel"].forEach(type => {
+			this._eventListeners[type] = event => {
+				const selectedNode = this.checkEvent(event, type);
+				if(this._lastPointerId !== null && selectedNode) {
+					this.stopPointerCapture(event.pointerId);
+					this._captureTarget = null;
+				}
+				// Remove dynamic-only listeners
+				this.cleanupDynamicListeners();
+				return this.handleEvent(event, type, selectedNode);
+			};
+		});
+		if(!this.types.includes("pointerdown")) {
+			this.types.push("pointerdown");
+		}
+	}
+
+	// Create any listeners not already defined above
+	this.types.forEach(type => {
+		if(!this._eventListeners[type]) {
+			this._eventListeners[type] = event => {
+				const selectedNode = this.checkEvent(event, type);
+				if(!selectedNode) {
+					return false;
+				}
+				// Handle pointer capture for pointerdown
+				if(selectedNode && type === "pointerdown") {
+					if(this.pointerCaptureMode !== "no") {
+						this._captureTarget = event.target;
+						this.startPointerCapture(event.pointerId);
+					}
+
+					if(this.pointerCaptureMode === "dynamic") {
+						this._dynamicOnlyEvents.forEach(dt => {
+							const listener = this._eventListeners[dt];
+							if(listener) {
+								this._captureActiveListeners[dt] = listener;
+								this.domNode.addEventListener(dt, listener, false);
+							}
+						});
+					}
+				} else if(selectedNode && (type === "pointerup" || type === "pointercancel")) {
+					if(this._lastPointerId  !== null) {
+						this.stopPointerCapture(event.pointerId);
+						this._captureTarget = null;
+					}
+				}
+
+				return this.handleEvent(event, type, selectedNode);
+			};
+		}
+	});
+};
+
+/*
+Check if an event qualifies and return the matching selected node
+*/
+EventWidget.prototype.checkEvent = function(event, type) {
+	let selectedNode = event.target;
+
+	if(this._captureTarget && event.pointerId !== undefined) {
+		// Use captureTarget only if it’s still attached to the DOM
+		if(document.contains(this._captureTarget)) {
+			selectedNode = this._captureTarget;
+		} else {
+			// Fallback: clear stale reference
+			this._captureTarget = null;
+			this.stopPointerCapture(this._lastPointerId);
+			selectedNode = this.domNode;
+			return null;
+		}
+	}
+
+	if(selectedNode.nodeType === 3) {
+		selectedNode = selectedNode.parentNode;
+	}
+	const selector = this.getAttribute("selector"),
+		matchSelector = this.getAttribute("matchSelector");
+
+	if(matchSelector && !$tw.utils.domMatchesSelector(selectedNode, matchSelector)) {
+		return null;
+	}
+	if(selector) {
+		while(!$tw.utils.domMatchesSelector(selectedNode, selector) && selectedNode !== this.domNode) {
+			selectedNode = selectedNode.parentNode;
+		}
+		if(selectedNode === this.domNode) {
+			return null;
+		}
+	}
+	return selectedNode;
+};
+
+/*
+Handle the event and execute actions
+*/
+EventWidget.prototype.handleEvent = function(event, type, selectedNode) {
+	if(!selectedNode) {
+		return false;
+	}
+
+	let actions = this.getAttribute("$"+type),
+		stopPropagation = this.getAttribute("stopPropagation","onaction"),
+		variables = {};
+
+	if(actions) {
+		variables = $tw.utils.collectDOMVariables(selectedNode, this.domNode, event);
+		variables.modifier = $tw.keyboardManager.getEventModifierKeyDescriptor(event);
+		const mouseButtonMap = {0:"left",1:"middle",2:"right"};
+		variables["event-mousebutton"] = "button" in event ? mouseButtonMap[event.button] : undefined;
+		variables["event-type"] = event.type.toString();
+		if(typeof event.detail === "object" && !!event.detail) {
+			$tw.utils.each(event.detail,(detailValue,detail) => {
+				variables["event-detail-"+detail] = detailValue.toString();
+			});
+		} else if(!!event.detail) {
+			variables["event-detail"] = event.detail.toString();
+		}
+		if(event.animationName) {
+			variables["event-animationName"] = event.animationName;
+		}
+		this.invokeActionString(actions, this, event, variables);
+	}
+	if((actions && stopPropagation==="onaction") || stopPropagation==="always") {
+		event.preventDefault();
+		event.stopPropagation();
+		return true;
+	}
+	return false;
+};
+
+EventWidget.prototype.startPointerCapture = function(pointerId) {
+	if(this._lastPointerId === null && this.domNode && this.domNode.setPointerCapture) {
+		this.domNode.setPointerCapture(pointerId);
+		this._lastPointerId = pointerId;
+	}
+};
+
+EventWidget.prototype.stopPointerCapture = function(pointerId) {
+	if(this.domNode && this.domNode.hasPointerCapture && this.domNode.hasPointerCapture(pointerId)) {
+		this.domNode.releasePointerCapture(pointerId);
+	}
+	this._lastPointerId = null;
+};
+
+/*
+Attach all relevant listeners
+*/
+EventWidget.prototype.attachListeners = function() {
+	this.cacheEventListeners();
+	const domNode = this.domNode;
+	Object.keys(this._eventListeners).forEach(type => {
+		if(this.pointerCaptureMode === "dynamic" && this._dynamicOnlyEvents.includes(type)) {
+			return; //skip dynamic-only events
+		}
+		domNode.addEventListener(type, this._eventListeners[type], false);
+	});
+};
+
+/*
+Remove dynamic active listeners
+*/
+EventWidget.prototype.cleanupDynamicListeners = function() {
+	const domNode = this.domNode;
+	Object.keys(this._captureActiveListeners || {}).forEach(type => {
+		domNode.removeEventListener(type, this._captureActiveListeners[type], false);
+	});
+	this._captureActiveListeners = Object.create(null);
+};
+
+/*
+Remove all listeners
+*/
+EventWidget.prototype.removeAllListeners = function() {
+	if(this._lastPointerId !== null) {
+		this.stopPointerCapture(this._lastPointerId);
+	}
+	const domNode = this.domNode;
+	Object.keys(this._eventListeners || {}).forEach(type => {
+		domNode.removeEventListener(type, this._eventListeners[type], false);
+	});
+	this.cleanupDynamicListeners();
+	this._captureTarget = null;
+};
+
+/*
+Enable or disable listeners
+*/
+EventWidget.prototype.toggleListeners = function() {
+	let disabled = this.getAttribute("disabled","no") === "yes";
+	if(disabled) {
+		this.removeAllListeners();
+	} else {
+		this.attachListeners();
+	}
+};
+
+/*
+Assign DOM node classes
+*/
 EventWidget.prototype.assignDomNodeClasses = function() {
 	var classes = this.getAttribute("class","").split(" ");
 	classes.push("tc-eventcatcher");
@@ -134,18 +286,23 @@ EventWidget.prototype.assignDomNodeClasses = function() {
 };
 
 /*
-Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
+Refresh widget
 */
 EventWidget.prototype.refresh = function(changedTiddlers) {
-	var changedAttributes = this.computeAttributes(),
-		changedAttributesCount = $tw.utils.count(changedAttributes);
-	if(changedAttributesCount === 1 && changedAttributes["class"]) {
-		this.assignDomNodeClasses();
-	} else if(changedAttributesCount > 0) {
-		this.refreshSelf();
-		return true;
+	const changedAttributes = this.computeAttributes(),
+		changedKeys = Object.keys(changedAttributes),
+		canUpdateAttributes = changedKeys.every(key => key === "class" || key === "disabled");
+	if(canUpdateAttributes) {
+		if(changedAttributes["class"]) {
+			this.assignDomNodeClasses();
+		}
+		if(changedAttributes["disabled"]) {
+			this.toggleListeners();
+		}
+		return this.refreshChildren(changedTiddlers);
 	}
-	return this.refreshChildren(changedTiddlers);
+	this.refreshSelf();
+	return true;
 };
 
 exports.eventcatcher = EventWidget;
