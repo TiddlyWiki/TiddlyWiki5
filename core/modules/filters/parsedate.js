614e5a78f4039f735d18704162ff037197127374
linonetwo <linonetwo012@gmail.com>
2026-02-23 23:25:58 +0800

    fix(parsedate): fix AUTO mode spurious match via TW parseDate; update docs and tests

--- core/modules/filters/parsedate.js
+++ core/modules/filters/parsedate.js
@@ -6,25 +6,21 @@ module-type: filteroperator
 Filter operator converting different date formats into TiddlyWiki's date format
 
 \*/
+
 "use strict";
 
-var DEFAULT_UTC_OUTPUT_FORMAT = "[UTC]YYYY0MM0DD0hh0mm0ss0XXX";
-var DEFAULT_LOCAL_OUTPUT_FORMAT = "YYYY0MM0DD0hh0mm0ss0XXX";
-var INTEGER_INPUT_REGEX = /^-?\d+$/;
-var LOOSE_DATE_CLEANUP_REGEX = /(\d+)(st|nd|rd|th)|,/gi;
+const DEFAULT_LOCAL_OUTPUT_FORMAT = "YYYY0MM0DD0hh0mm0ss0XXX";
+const INTEGER_INPUT_REGEX = /^-?\d+$/;
+const LOOSE_DATE_CLEANUP_REGEX = /(\d+)(st|nd|rd|th)|,/gi;
+const INPUT_FORMATS = new Set(["JS","TW","UNIXTIME","NUMBER","AUTO"]);
+const NUMERIC_OUTPUT_FORMATS = new Set(["UNIXTIME","NUMBER"]);
 
-function isValidDate(date) {
-	return (date instanceof Date) && !isNaN(date.valueOf());
-}
+const isValidDate = (date) => date instanceof Date && !isNaN(date.valueOf());
 
-function sanitizeLooseDateInput(input) {
-	return input.replace(LOOSE_DATE_CLEANUP_REGEX,function(match,numeric) {
-		return numeric || "";
-	});
-}
+const sanitizeLooseDateInput = (input) => input.replace(LOOSE_DATE_CLEANUP_REGEX,(match,numeric) => numeric || "");
 
-function parseInputDate(title,inputFormat) {
-	var sanitizedTitle;
+const parseInputDate = (title, inputFormat) => {
+	let sanitizedTitle;
 	switch(inputFormat) {
 		case "JS":
 			return $tw.utils.parseECMAScriptDate(title);
@@ -32,24 +28,23 @@ function parseInputDate(title,inputFormat) {
 			return $tw.utils.parseDate(title);
 		case "UNIXTIME":
 		case "NUMBER":
+			if(INTEGER_INPUT_REGEX.test(title)) {
+				return new Date(Number(title));
+			}
 			sanitizedTitle = sanitizeLooseDateInput(title);
 			if(INTEGER_INPUT_REGEX.test(sanitizedTitle)) {
 				return new Date(Number(sanitizedTitle));
 			}
 			return new Date(sanitizedTitle);
 		case "AUTO": {
-			var parsedDate;
-			if(INTEGER_INPUT_REGEX.test(title)) {
-				return new Date(Number(title));
-			}
-			parsedDate = $tw.utils.parseECMAScriptDate(title);
+			// Try ECMAScript/ISO date format first
+			const parsedDate = $tw.utils.parseECMAScriptDate(title);
 			if(isValidDate(parsedDate)) {
 				return parsedDate;
 			}
-			parsedDate = $tw.utils.parseDate(title);
-			if(isValidDate(parsedDate)) {
-				return parsedDate;
-			}
+			// Sanitize loose date text (strips ordinal suffixes and commas),
+			// then treat pure-integer result as unix timestamp, otherwise
+			// fall back to native Date parsing
 			sanitizedTitle = sanitizeLooseDateInput(title);
 			if(INTEGER_INPUT_REGEX.test(sanitizedTitle)) {
 				return new Date(Number(sanitizedTitle));
@@ -59,36 +54,35 @@ function parseInputDate(title,inputFormat) {
 		default:
 			return null;
 	}
-}
+};
 
-function formatOutputDate(date,outputFormat) {
-	var normalizedOutputFormat = (outputFormat || "").toUpperCase();
+const formatOutputDate = (date, outputFormat) => {
+	const normalizedOutputFormat = (outputFormat || "").toUpperCase();
 	if(normalizedOutputFormat === "" || normalizedOutputFormat === "UTC") {
 		return $tw.utils.stringifyDate(date);
 	}
 	if(normalizedOutputFormat === "LOCAL") {
 		return $tw.utils.formatDateString(date,DEFAULT_LOCAL_OUTPUT_FORMAT);
 	}
-	if(normalizedOutputFormat === "UNIXTIME" || normalizedOutputFormat === "NUMBER") {
+	if(NUMERIC_OUTPUT_FORMATS.has(normalizedOutputFormat)) {
 		return date.getTime().toString();
 	}
 	return $tw.utils.formatDateString(date,outputFormat);
-}
+};
 
 /*
 Export our filter function
 */
-exports.parsedate = function(source,operator) {
-	var operands = operator.operands || [];
-	var inputFormat = (operands[0] || operator.operand || "JS").toUpperCase();
-	var outputFormat = operands[1] || operator.suffix || "";
-	if(["JS","TW","UNIXTIME","NUMBER","AUTO"].indexOf(inputFormat) === -1) {
+exports.parsedate = (source, operator) => {
+	const operands = operator.operands || [];
+	const inputFormat = (operands[0] || operator.operand || "JS").toUpperCase();
+	const outputFormat = operands[1] || operator.suffix || "";
+	if(!INPUT_FORMATS.has(inputFormat)) {
 		return [$tw.language.getString("Error/ParseDateFilterOperator")];
 	}
-
-	var results = [];
-	source(function(tiddler,title) {
-		var date = parseInputDate(title,inputFormat);
+	const results = [];
+	source((tiddler,title) => {
+		const date = parseInputDate(title,inputFormat);
 		if(isValidDate(date)) {
 			results.push(formatOutputDate(date,outputFormat));
 		}
