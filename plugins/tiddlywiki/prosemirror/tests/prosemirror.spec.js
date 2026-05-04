48b3c5c3f6204f7e0796cebcb6ae68d36eba2c98
lin onetwo <linonetwo012@gmail.com>
2026-05-04 20:52:34 +0800

    rename prosemirror spec files

--- plugins/tiddlywiki/prosemirror/tests/prosemirror.spec.js
+++ plugins/tiddlywiki/prosemirror/tests/prosemirror.spec.js
@@ -2,16 +2,16 @@
  * MIGRATION COMPLETE
  *
  * All tests have been moved to the specs/ subfolder:
- *   specs/01-basic-editing.spec.js   - Basic editing, headings, configuration, integration
- *   specs/02-lists.spec.js           - Bullet/numbered lists, indentation, visual parity
- *   specs/03-slash-menu.spec.js      - Slash menu (filter, IME, snippets, categories, heading)
- *   specs/04-images.spec.js          - Image nodes, picker, attribute round-trip
- *   specs/05-widget-blocks.spec.js   - Widget block nodeview (edit/save/delete)
- *   specs/06-hard-line-breaks.spec.js- Hard line breaks block, Shift-Enter split
- *   specs/07-drag-handle.spec.js     - Drag handle (show/hide, block menu)
- *   specs/08-typed-block.spec.js     - Typed block rendering, dropdown, edit button
- *   specs/09-source-panel.spec.js    - Source panel, preview types, markdown tiddlers
- *   specs/10-misc.spec.js            - Find & Replace, autocomplete, link tooltip,
+ *   specs/basic-editing.spec.js      - Basic editing, headings, configuration, integration
+ *   specs/lists.spec.js              - Bullet/numbered lists, indentation, visual parity
+ *   specs/slash-menu.spec.js         - Slash menu (filter, IME, snippets, categories, heading)
+ *   specs/images.spec.js             - Image nodes, picker, attribute round-trip
+ *   specs/widget-blocks.spec.js      - Widget block nodeview (edit/save/delete)
+ *   specs/hard-line-breaks.spec.js   - Hard line breaks block, Shift-Enter split
+ *   specs/drag-handle.spec.js        - Drag handle (show/hide, block menu)
+ *   specs/typed-block.spec.js        - Typed block rendering, dropdown, edit button
+ *   specs/source-panel.spec.js       - Source panel, preview types, markdown tiddlers
+ *   specs/misc.spec.js               - Find & Replace, autocomplete, link tooltip,
  *                                     definition list, blockquote, code block, table,
  *                                     horizontal rule, pragma blocks
  *
