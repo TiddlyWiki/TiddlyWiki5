de56bfa235ddfab378c7a6059ed1540e5faea931
lin onetwo <linonetwo012@gmail.com>
2026-06-11 17:41:38 +0800

    Read editor type filters from EditorTypes.tid in tests.
    Extract procedure bodies via getVariableInfo instead of duplicating filter strings in JavaScript, and add editor-types-select-options procedure for the select widget.

--- editions/test/tiddlers/tests/test-editor-types-settings.js
+++ editions/test/tiddlers/tests/test-editor-types-settings.js
@@ -12,6 +12,27 @@ Tests Editor Types settings: default mappings and dropdown option discovery.
 describe("Editor Types settings", function() {
 
 	var wiki = $tw.wiki;
+	var EDITOR_TYPES_TIDDLER = "$:/core/ui/ControlPanel/Settings/EditorTypes";
+
+	function makeEditorTypesWidget(variables) {
+		var parser = wiki.parseText("text/vnd.tiddlywiki", wiki.getTiddlerText(EDITOR_TYPES_TIDDLER));
+		var widgetNode = wiki.makeWidget(parser, {variables: variables || {}});
+		$tw.fakeDocument.setSequenceNumber(0);
+		widgetNode.render($tw.fakeDocument.createElement("div"), null);
+		return widgetNode;
+	}
+
+	function leafWidget(widgetNode) {
+		var node = widgetNode;
+		while (node.children.length > 0) {
+			node = node.children[0];
+		}
+		return node;
+	}
+
+	function getProcedureFilter(widgetNode, name) {
+		return leafWidget(widgetNode).getVariableInfo(name, {allowSelfAssigned: true}).text;
+	}
 
 	it("should provide default editor mappings for image and wikitext types", function() {
 		expect(wiki.getTiddlerText("$:/config/EditorTypeMappings/image/png")).toBe("bitmap");
@@ -19,18 +40,16 @@ describe("Editor Types settings", function() {
 	});
 
 	it("should discover installed edit-* widget names for dropdown options", function() {
-		var editorTypesFromModules = "[[widget]modules[]suffix[.js]regexp[.*/edit-.+]] :map[search-replace:g:regexp[.*/edit-(.+)\\.js],[$1]]";
-		var results = wiki.filterTiddlers(editorTypesFromModules);
+		var widgetNode = makeEditorTypesWidget();
+		var results = wiki.filterTiddlers(getProcedureFilter(widgetNode, "editor-types-filter"));
 		expect(results[0]).not.toMatch(/Filter Error/i);
 		expect(results).toContain("text");
 		expect(results).toContain("bitmap");
 	});
 
 	it("should combine current mapping with installed editor types for select options", function() {
-		var editorTypesFromModules = "[[widget]modules[]suffix[.js]regexp[.*/edit-.+]] :map[search-replace:g:regexp[.*/edit-(.+)\\.js],[$1]]";
-		var mappingValue = "[[$:/config/EditorTypeMappings/image/png]get[text]]";
-		var selectOptionsFilter = editorTypesFromModules + " " + mappingValue + " :sort:string[{!!title}]";
-		var results = wiki.filterTiddlers(selectOptionsFilter);
+		var widgetNode = makeEditorTypesWidget({mappingTiddler: "$:/config/EditorTypeMappings/image/png"});
+		var results = wiki.filterTiddlers(getProcedureFilter(widgetNode, "editor-types-select-options"), leafWidget(widgetNode));
 		expect(results[0]).not.toMatch(/Filter Error/i);
 		expect(results).toContain("bitmap");
 	});
