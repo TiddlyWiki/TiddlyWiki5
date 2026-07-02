91727e3a1d96e0278f84f6cbb0a3d20db3ff060e
Mario Pietsch <pmariojo@gmail.com>
2026-07-02 18:14:56 +0200

    Update plugins/tiddlywiki/wikitext-serialize/rules/codeinline.js
    Co-authored-by: Saq Imtiaz <saq.imtiaz@gmail.com>

--- plugins/tiddlywiki/wikitext-serialize/rules/codeinline.js
+++ plugins/tiddlywiki/wikitext-serialize/rules/codeinline.js
@@ -11,6 +11,6 @@ exports.name = "codeinline";
 exports.serialize = function(tree,serialize) {
 	var text = serialize(tree.children),
 		// Backticks inside and empty text need double delimiters, eg: ``a`b``
-		delimiter = (text === "" || text.indexOf("`") !== -1) ? "``" : "`";
+		delimiter = (text === "" || text.includes("`")) ? "``" : "`";
 	return delimiter + text + delimiter;
 };
